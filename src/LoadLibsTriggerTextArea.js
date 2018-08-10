8f5c5fbe568d7e1c7719b7fef6da8c995040e6e1
ColorfulCakeChen <colorfulcakechen@gmail.com>
2018-08-10 16:54:41 +0800

    Update LoadLibsTriggerTextArea.js

--- src/LoadLibsTriggerTextArea.js
+++ src/LoadLibsTriggerTextArea.js
@@ -7,12 +7,6 @@
 
   /*alert("hi1");*/
   let theTextArea = d.querySelector("textarea[title='"+TextAreaElementTooltip+"']");
-
-  if (urlArray.length <= 0) {
-    theTextArea.value = "No Library needs to load.";
-    theTextArea.dispatchEvent(new Event("input")); /* None to be loaded. Done. */
-  }
-
   let i = 0;
 
   function onLoadFailed(e) {
@@ -22,13 +16,11 @@
 
   function onLoadOk(e) {
     /*alert(`Library ({$i}/{$urlArray.length}) loaded: ({$urlArray[i]})`);*/
-    if (i > 0) {
-      if (i >= urlArray.length) {
-        theTextArea.value = `Library ({$i}/{$urlArray.length}) loaded. Done.`;
-        theTextArea.dispatchEvent(new Event("input")); /* The last one is loaded. Done. */
-      } else {
-        theTextArea.value = `Library ({$i}/{$urlArray.length}) loaded: ({$urlArray[i]})`;
-      }
+    if (i >= urlArray.length) {
+      theTextArea.value = `Library ({$i}/{$urlArray.length}) loaded. Done.`;
+      theTextArea.dispatchEvent(new Event("input")); /* The last one is loaded. Done. */
+    } else {
+      theTextArea.value = `Library ({$i}/{$urlArray.length}) loaded: ({$urlArray[i]})`;
     }
     /*theTextArea.value = `Library ({$i}/{$urlArray.length}) loading: ({$urlArray[i]})`;*/
     createPromiseLoadScript(urlArray[i]).then(onLoadOk).catch(onLoadFailed);
