0e5183660316c8e3e871ca8b9f2449a25db3e6ae
ColorfulCakeChen <colorfulcakechen@gmail.com>
2018-08-10 16:44:33 +0800

    Update LoadLibsTriggerTextArea.js

--- src/LoadLibsTriggerTextArea.js
+++ src/LoadLibsTriggerTextArea.js
@@ -15,21 +15,21 @@
 
   let i = 0;
 
+  function onLoadFailed(e) {
+    /*alert(`Library ({$i}/{$u.length}) loading FAILED! {$e} ({$u[i]})`);*/
+    theTextArea.value = `Library ({$i}/{$urlArray.length}) loading FAILED! {$e} ({$urlArray[i]})`;
+  }
+
   function onLoadOk(e) {
-    /*alert('Library ({$i}/{$urlArray.length}) loaded: ({$urlArray[i]})');*/
+    /*alert(`Library ({$i}/{$urlArray.length}) loaded: ({$urlArray[i]})`);*/
     if (i >= urlArray.length - 1) {
-      theTextArea.value = 'Library ({$i}/{$urlArray.length}) loaded. Done.';
+      theTextArea.value = `Library ({$i}/{$urlArray.length}) loaded. Done.`;
       theTextArea.dispatchEvent(new Event("input")); /* The last one is loaded. Done. */
     } else {
-      theTextArea.value = 'Library ({$i}/{$urlArray.length}) loaded: ({$urlArray[i]})';
+      theTextArea.value = `Library ({$i}/{$urlArray.length}) loaded: ({$urlArray[i]})`;
       ++i;
       createPromiseLoadScript(urlArray[i]).then(onLoadOk).catch(onLoadFailed);
     }
-  }
-
-  function onLoadFailed(e) {
-    /*alert('Library ({$i}/{$u.length}) loading FAILED! {$e} ({$u[i]})');*/
-    theTextArea.value = 'Library ({$i}/{$urlArray.length}) loading FAILED! {$e} ({$urlArray[i]})';
   }
 
   createPromiseLoadScript(urlArray[i]).then(onLoadOk).catch(onLoadFailed);
