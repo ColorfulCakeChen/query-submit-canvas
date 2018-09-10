b98abee95f8b38905dca9a7bcfc3afc386954861
ColorfulCakeChen <colorfulcakechen@gmail.com>
2018-09-10 11:48:59 +0800

    Update PartTime.mjs
    Place callback after generator.

--- CNN/PartTime.mjs
+++ CNN/PartTime.mjs
@@ -2,13 +2,13 @@
  * Periodically call generator.next() by setTimeout() until ( generator.next().done == true ). The generator
  * will generate in part-time. Just like a for..of loop but executes in part-time.
  *
- * @param {function}  callback          will be called as callback( generator.next().value ) when ( generator.next().done == false ).
  * @param {function}  generator         The generator.next() will be called periodically until done.
+ * @param {function}  callback          will be called as callback( generator.next().value ) when ( generator.next().done == false ).
  * @param {integer}   delayMilliseconds The delay time when schedule the next run. Default 0.
  *
  * @return A promise resolved with the ( generator.next().value ) when ( generator.next().done == true ).
  */
-export function forOf(callback, generator, delayMilliseconds = 0) {
+export function forOf(generator, callback, delayMilliseconds = 0) {
 
   function promiseTimeout() {
     return new Promise( (resolve, reject) => {
