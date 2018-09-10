f8c4bd12d00cc6490510b078da5b3c3d0788364d
ColorfulCakeChen <colorfulcakechen@gmail.com>
2018-09-10 10:36:02 +0800

    Update PartTime.mjs
    Call callback only if ( done == false ), just like the for..of loop.

--- CNN/PartTime.mjs
+++ CNN/PartTime.mjs
@@ -2,7 +2,7 @@
  * Periodically call generator.next() by setTimeout() until ( generator.next().done == true ). The generator
  * will generate in part-time. Just like a for..of loop but executes in part-time.
  *
- * @param {function}  callback          will be called as callback( generator.next() ) even if ( generator.next().done == true ).
+ * @param {function}  callback          will be called as callback( generator.next().value ) when ( generator.next().done == false ).
  * @param {function}  generator         The generator.next() will be called periodically until done.
  * @param {integer}   delayMilliseconds The delay time when setTimeout(). Default 0.
  *
@@ -14,10 +14,10 @@ export function forOf(callback, generator, delayMilliseconds = 0) {
     return new Promise( (resolve, reject) => {
       setTimeout(() => {
         let result = generator.next();
-        callback(result.value)
         if (result.done) {
           resolve(result.value);
         } else {
+          callback(result.value)
           resolve(promiseTimeout());
         }
       }, delayMilliseconds);
