486241de975e1e0fbdc6a3f33507c0b2cad4a82c
ColorfulCakeChen <colorfulcakechen@gmail.com>
2018-09-10 13:15:11 +0800

    Update SeparableConv2d.mjs
    (Temp) collect encoded character for decoding to integer weight.

--- CNN/SeparableConv2d.mjs
+++ CNN/SeparableConv2d.mjs
@@ -4,150 +4,125 @@ import * as ValueMaxDone from "./ValueMaxDone.mjs";
 export {Parser, Layer};
 
 /**
- * Parser for decoding string array to SeparableConv2d entities.
+ * Parsing and decoding string array to SeparableConv2d entities.
+ * 
+ * @param  {string[]} encodedStringArray     Every string is an encoded entity.
+ * @param  {number}   encodedWeightCharCount Every weight is encoded as string with this length. (e.g. 5 )
+ * @param  {number}   encodedWeightBase      Every weight is encoded by this base number. (e.g. 2 or 10 or 16 or 36) 
+ * @param  {number}   weightValueOffset      The value will be subtracted from the integer weight value.
+ * @param  {number}   weightValueDivisor     Divide the integer weight value by this value for converting to floating-point number.
+ *
+ * @param  {string}   htmlProgressTitle
+ *   The title of HTMLProgressElement for reporting progress and onclick will be called when done. If null, no reporting.
+ *
+ * @return {Promise} A promise resolved as an Object[] which is the decoded entity for separableConv2d().
+ *   The entity is an array of SeparableConv2d.Layer.
  */
-class Parser {
+function StringArrayToEntities(
+  encodedStringArray, encodedWeightCharCount, encodedWeightBase, weightValueOffset, weightValueDivisor, htmlProgressTitle) {
 
-  /**
-   *
-   * When ( generator.next().done == false ), the generator.next().value should be a number.
-   *
-   * For the first time ( generator.next().done == false ), the generator.next().value should be a positive number
-   * represents the maximum progress volume. The HTMLProgressElement.max will be set to this first value. The
-   * HTMLProgressElement.value will be set to 0.
-   *
-   * After that, everytime ( generator.next().done == false ), the generator.next().value should be a number
-   * represents the advanced progress volume. The HTMLProgressElement.value will be set to
-   * ( HTMLProgressElement.value + generator.next().value ).
-   *
-   * Finally, when ( generator.next().done == true ), the promise will resolve with the last time
-   * ( generator.next().value ) and the HTMLProgressElement.onclick() will be called for informing. 
-   *
-   * @param  {number}   encodedWeightCharCount Every weight is encoded as string with this length. (e.g. 5 )
-   * @param  {number}   encodedWeightBase      Every weight is encoded by this base number. (e.g. 2 or 10 or 16 or 36) 
-   * @param  {number}   weightValueOffset      The value will be subtracted from the integer weight value.
-   * @param  {number}   weightValueDivisor     Divide the integer weight value by this value for converting to floating-point number.
-   * @param  {string}   htmlProgressTitle      The title of HTMLProgressElement for reporting progress. If null, no reporting.
-   */
-  constructor(encodedWeightCharCount, encodedWeightBase, weightValueOffset, weightValueDivisor, htmlProgressTitle) {
-    this.encodedWeightCharCount = encodedWeightCharCount;
-    this.encodedWeightBase =      encodedWeightBase;
-    this.weightValueOffset =      weightValueOffset;
-    this.weightValueDivisor =     weightValueDivisor;
-    this.htmlProgressTitle =      htmlProgressTitle;
-  }
+  encodedWeightCharCount = max(1, encodedWeightCharCount); /* At least one character for a weight. */
 
   /**
    * @param {integer} integerWeight The integer which will be converted to floating-point number by subtract and divide.
    * @return The floating-point number.
    */
-  integerToFloat(integerWeight) {
-    return ( integerWeight - this.weightValueOffset ) / this.weightValueDivisor;
+  function integerToFloat(integerWeight) {
+    return ( integerWeight - weightValueOffset ) / weightValueDivisor;
   }
 
-  /**
-   * @param {string[]}            encodedStringArray     Every string is an encoded entity.
-   * 
-   */
-  * entitiesGenerator(encodedStringArray) {
-    this.entityCount =         encodedStringArray.length;
-    this.integerWeightsArray = new Array(encodedStringArray.length);
-    for (let i = 0; i < encodedStringArray.length; ++i) {
-      yield ( this.integerWeightsArray[ i ] = new Float32Array( encodedStringArray[ i ].length ) );
+  function* entitiesGenerator() {
+    const suspendWeightCount = 1000; /* When scan so many weight, yield for releasing CPU time. */
+    let progress = { value: 0, max: 0 };
+
+    /** Estimate maximum volume for progress reporting. */
+    for (let encodedString of encodedStringArray) {
+      progress.max += encodedString.length;
     }
-  }
 
+    let entityCount = encodedStringArray.length;
+    let entities =    new Array(entityCount);
 
-  /**
-   * @param {string[]}            encodedStringArray     Every string is an encoded entity.
-   */
-  * entityGenerator(encodedStringArray) {
-    let theMemoryGenerator = memoryGenerator(encodedStringArray);
+    yield progress; /* Report initial progress after first time memory allocation. */
 
-    promiseTimeout( (resolve, reject) => {
-      let memoryGeneratorResult = theMemoryGenerator().next();
-      if (memoryGeneratorResult.done)
-        
-      let integerWeights of ) {
-//!!! ...unfinished...
-        for (??? let entity of theEntityGenerator) {
-        }
-      }
-    });
+    let totalScanedCharCount = 0;
+    for (let i = 0; i < encodedStringArray.length; ++i) {
+      let encodedString = encodedStringArray[ i ];
+      let encodedWeightCount = Math.ceil(encodedString.length / encodedWeightCharCount);
+      let integerWeights = new Float32Array( encodedWeightCount );
 
-  }
+      let collectedCharCount = 0;
+      let encodedWeight;
+      let weightIndex = 0;
+      let weightIndexAfterYield = 0;
 
-  /**
-   * 
-   * @param  {string[]} encodedStringArray     Every string is an encoded entity.
-   *
-   * @return {Promise} A promise resolved as an Object[] which is the decoded entity for separableConv2d().
-   *   The entity is an array of SeparableConv2d.Layer.
-   */
-  StringArrayToSeparableConv2dEntities(encodedStringArray) {
-    let progressReceiver = ValueMaxDone.HTMLProgress.createByTitle_or_getDummy(this.htmlProgressTitle);
-    let theEntitiesGenerator = entitiesGenerator(encodedStringArray);
-    let p = PartTime.forOf((valueMax) => {
-      progressReceiver.setValueMax(valueMax); /* Report progress to UI. */
-    }, theEntitiesGenerator).then((doneValue) => {
-      progressReceiver.informDone(doneValue); /* Inform UI progress done. */
-    });
-    return p;
+      for (let encodedChar of encodedString) {
+        totalScanedCharCount++;
 
-    let theEntities = [];
-//!!! ...unfinished...
-    promiseTimeout( (resolve, reject) => {
-
-      promiseTimeout( (resolve, reject) => {
-        let memoryGeneratorResult = theMemoryGenerator().next();
-        if (memoryGeneratorResult.done)
-          reject();
+        if (0 == collectedCharCount)
+          encodedWeight = encodedChar;
         else
-          resolve(memoryGeneratorResult.value);
-      }).then((integerWeights) => {
+          encodedWeight += encodedChar;
+
+        collectedCharCount++;
+        if (collectedCharCount < encodedWeightCharCount)
+          continue; /* Collect characters for one weight. */
+
+        collectedCharCount = 0;
+ 
+        integerWeights[ weightIndex ] = parseInt(encodedWeight, encodedWeightBase); /* Decode as integer. */
+        weightIndex++;
+        weightIndexAfterYield++;
+
+        if (weightIndexAfterYield >= suspendWeightCount) { /* Every suspendWeightCount, release CPU time. */
+          progress.value = totalScanedCharCount;
+          yield progress;
+          weightIndexAfterYield = 0;
+        }
+ 
+//!!! ...unfinished...
+        progress.value += ;
+        yield progress;
+      }
+    }
 
 
 //!!! ...unfinished...
-      }).catch(() => {
-        resolve(theEntities);
-      });
-    });
 
 //!!! ...unfinished...
 
-    this.entityCount =            encodedStringArray.length;
-    this.integerWeightsArray =    new Array(encodedStringArray.length);
+    // RegExp for extracting an encoded weight from the encoded string. (e.g. /(.{5})/g )
+    let encodedWeightMatchRegExp = new RegExp("(.{" + encodedWeightCharCount + "})", "g");
+    let integerWeightsArray = Array.from(encodedStringArray, str => {
+      let encodedWeights = str.match(encodedWeightMatchRegExp);       // Split string.
+      let integerWeights = new Float32Array( encodedWeights.length );
+      encodedWeights.forEach( ( element, i ) => { integerWeights[ i ] = parseInt(element, encodedWeightBase); } ); // Decode as integer.
+      return integerWeights;
+    } );
 
-    let integerToFloat = this.integerToFloat.bind(this);
-
-    let p = new Promise( (resolve, reject) => {
-      setTimeout(() => {
-        // RegExp for extracting an encoded weight from the encoded string. (e.g. /(.{5})/g )
-        let encodedWeightMatchRegExp = new RegExp("(.{" + encodedWeightCharCount + "})", "g");
-        let integerWeightsArray = Array.from(encodedStringArray, str => {
-          let encodedWeights = str.match(encodedWeightMatchRegExp);       // Split string.
-          let integerWeights = new Float32Array( encodedWeights.length );
-          encodedWeights.forEach( ( element, i ) => { integerWeights[ i ] =  parseInt(element, encodedWeightBase); } ); // Decode as integer.
-          return integerWeights;
-        } );
-
-        let theEntities = integerWeightsArray.map( integerWeights => {
-          let theEntity = [], weightIndex = 0, inChannels = 4; /* Suppose the first layer's input channel count is always RGBA 4 channels. */
-          while ( weightIndex < integerWeights.length ) {
-            let layer = new SeparableConv2d.Layer(integerWeights, weightIndex, inChannels, integerToFloat);	
-            theEntity.push(layer);		
-            inChannels =  layer.params.outChannels;  /* The next layer's input channel count is the previous layer's output channel count. */
-            weightIndex = layer.weightIndexEnd;
-          }
-          return theEntity;
-        });
-
-        return theEntities;
-      });
+    let theEntities = integerWeightsArray.map( integerWeights => {
+      let theEntity = [], weightIndex = 0, inChannels = 4; /* Suppose the first layer's input channel count is always RGBA 4 channels. */
+      while ( weightIndex < integerWeights.length ) {
+        let layer = new SeparableConv2d.Layer(integerWeights, weightIndex, inChannels, integerToFloat);	
+        theEntity.push(layer);		
+        inChannels =  layer.params.outChannels;  /* The next layer's input channel count is the previous layer's output channel count. */
+        weightIndex = layer.weightIndexEnd;
+      }
+      return theEntity;
     });
-    return p;
+
   }
 
+
+  let progressReceiver = ValueMaxDone.HTMLProgress.createByTitle_or_getDummy(this.htmlProgressTitle);
+  let theEntitiesGenerator = entitiesGenerator(encodedStringArray);
+  let p = PartTime.forOf(theEntitiesGenerator, (valueMax) => {
+    progressReceiver.setValueMax(valueMax); /* Report progress to UI. */
+  }).then((doneValue) => {
+    progressReceiver.informDone(doneValue); /* Inform UI progress done. */
+    return this.entities;
+  });
+  return p;
 }
 
 /**
