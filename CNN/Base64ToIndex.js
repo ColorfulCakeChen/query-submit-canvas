15bbb32ace4943025524c9a31510e16f499e8377
ColorfulCakeChen <colorfulcakechen@gmail.com>
2019-06-05 14:20:39 +0800

    Update Base64ToIndex.js

--- CNN/Base64ToIndex.js
+++ CNN/Base64ToIndex.js
@@ -10,18 +10,7 @@ function base64ToIndex_withTable(base64ArrayBuffer, table_ByUint8) {
 }
 
 function byArray() {
-  //let table = new Array(256);
   let table = [];
-  for (let i = 0; i < base64String.length; ++i)
-    table[ base64String.codePointAt(i) ] = i;
-
-  return function (base64_ArrayBuffer) {
-    return base64ToIndex_withTable(base64_ArrayBuffer, table);
-  };
-}
-
-function byArrayBuffer() {
-  let table = new Uint8Array( new ArrayBuffer(256) );
   for (let i = 0; i < base64String.length; ++i)
     table[ base64String.codePointAt(i) ] = i;
 
@@ -32,6 +21,16 @@ function byArrayBuffer() {
 
 function byHash() {
   let table = {};
+  for (let i = 0; i < base64String.length; ++i)
+    table[ base64String.codePointAt(i) ] = i;
+
+  return function (base64_ArrayBuffer) {
+    return base64ToIndex_withTable(base64_ArrayBuffer, table);
+  };
+}
+
+function byArrayBuffer() {
+  let table = new Uint8Array( new ArrayBuffer(256) );
   for (let i = 0; i < base64String.length; ++i)
     table[ base64String.codePointAt(i) ] = i;
 
@@ -53,5 +52,5 @@ for (let i = 0; i < dataUint8Array.length; ++i) {
 }
 
 var base64ToIndex_ByArray = byArray();
-var base64ToIndex_ByArrayBuffer = byArrayBuffer();
 var base64ToIndex_ByHash = byHash();
+var base64ToIndex_ByArrayBuffer = byArrayBuffer();
