2f4b4790ea024a963fc587537c9cf922e51a8054
ColorfulCakeChen <colorfulcakechen@gmail.com>
2019-06-11 15:37:18 +0800

    Create Base64ArrayBufferToUint8Array_tester.js

---
+++ CNN/tester/Base64ArrayBufferToUint8Array_tester.js
@@ -0,0 +1,69 @@
+import * as ScriptLoader from "./ScriptLoader.js";
+import * as Base64ArrrayBufferToUint8Array from "../Base64ArrrayBufferToUint8Array.js";
+import * as PartTime from "../PartTime.js";
+import * as ValueMax_Percentage from "./Percentage.js";
+
+const base64EncodedString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+const base64DecodedString = atob(base64String);
+
+let tEncoder = new TextEncoder();
+const base64DecodedUint8Array = tEncoder.encode(base64DecodedString);
+
+let original = [
+  base64String,
+  
+  "\n"   + base64EncodedString,
+  "\r"   + base64EncodedString,
+  "\r\n" + base64EncodedString,
+  "\n\r" + base64EncodedString,
+
+  "qwerty\n"   + base64EncodedString,
+  "qwerty\r"   + base64EncodedString,
+  "qwerty\r\n" + base64EncodedString,
+  "qwerty\n\r" + base64EncodedString,
+];
+
+let result = [
+  base64DecodedUint8Array,
+
+  base64DecodedUint8Array,
+  base64DecodedUint8Array,
+  base64DecodedUint8Array,
+  base64DecodedUint8Array,
+
+  base64DecodedUint8Array,
+  base64DecodedUint8Array,
+  base64DecodedUint8Array,
+  base64DecodedUint8Array,
+];
+
+
+/** Aggregate all progress about downloading, JSON parsing, characters scanning, and weights scanning.  */
+class Progress extends ValueMax_Percentage.Aggregate {
+  constructor() {
+    let children = [
+      new ValueMax_Percentage.Concrete(), // Increased when downloading from network.
+      new ValueMax_Percentage.Concrete(), // Increased when parsing the downloaded data to Uint8Array.
+    ];
+
+    super(children);
+
+    [this.download, this.Uint8Array] = children;
+  }
+}
+
+let progress = new Progress();
+
+window.addEventListener("load", event => {
+  ScriptLoader.createPromise("https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.0.0/dist/tf.min.js").then(test); });
+
+function test() {
+  console.log("Hi! test()");
+
+  let r = await PartTime.forOf(Base64ArrrayBufferToUint8Array.decode_Generator(
+            original[ 0 ], progress, progress.Uint8Array, 0, 1);
+
+  tf.util.assert(
+    r == result[0],
+    `No skip line, base64 decode ${r} != ${result[0]}`);
+}
