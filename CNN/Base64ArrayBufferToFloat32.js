e1697aab9c5b72271bec258e71195b7262461683
ColorfulCakeChen <colorfulcakechen@gmail.com>
2019-06-11 13:02:41 +0800

    Update Base64ArrayBufferToFloat32.js

--- CNN/Base64ArrayBufferToFloat32.js
+++ CNN/Base64ArrayBufferToFloat32.js
@@ -6,41 +6,96 @@ const base64String = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012345
 
 /** Mapping table for base64 (as Uint8) to index. */
 let table_base64_Uint8_to_index = new Uint8Array( new ArrayBuffer(256) );
-for (let i = 0; i < base64String.length; ++i)
-  table_base64_Uint8_to_index[ base64String.codePointAt(i) ] = i;
+{
+  for (let i = 0; i < table_base64_Uint8_to_index.length; ++i)
+    table_base64_Uint8_to_index[ i ] = 255; // For identifying any non-base64 bytes.
+
+  for (let i = 0; i < base64String.length; ++i)
+    table_base64_Uint8_to_index[ base64String.codePointAt(i) ] = i;
+
+  // Support decoding URL-safe base64 strings, as Node.js does.
+  // See: https://en.wikipedia.org/wiki/Base64#URL_applications
+  table_base64_Uint8_to_index['-'.charCodeAt(0)] = 62
+  table_base64_Uint8_to_index['_'.charCodeAt(0)] = 63
+}
 
 /**
- * @param  {ArrayBuffer}                   base64ArrayBuffer The input base64 data as ArrayBuffer.
- * @param  {ValueMax.Percentage.Aggregate} progressToYield   Return this when every time yield.
- * @param  {ValueMax.Percentage.Concrete}  progressToAdvance Increase this when every time advanced.
- * @param  {Uint32}                        suspendByteCount  Everytime so many bytes decoded, yield for releasing CPU time.
- * @return {ArrayBuffer} Return decoded data when done.
+ *
+ * @param  {ArrayBuffer} sourceBase64ArrayBuffer
+ *   The input base64 data as ArrayBuffer. If the last bytes not enough 4 bytes, they will be discarded (will
+ * not be decoded). If an input byte is not a legal base64 code (i.e. not A..Z, a..z, 0..0, +, /), the byte
+ * will be skipped (as if it does not exist). So the input bytes can be separated by new line.
+ *
+ * @param  {ValueMax.Percentage.Aggregate} progressToYield
+ *   Return this when every time yield.
+ *
+ * @param  {ValueMax.Percentage.Concrete}  progressToAdvance
+ *   Increase this when every time advanced.
+ *
+ * @param  {Uint32} suspendByteCount
+ *   Everytime so many bytes decoded, yield for releasing CPU time. Default is 1024 bytes.
+ *
+ * @return {ValueMax.Percentage.Aggregate or Uint8Array}
+ *   Return progressYield when ( done = false ). Return decoded data as Uint8Array when ( done = true ).
  */
-function* Base64ArrayBuffer_To_ArrayBuffer_Generator(
-            base64ArrayBuffer,
-            progressYield,
-            progressAdvance,
-            suspendByteCount) {
+function* Base64ArrayBuffer_To_Uint8Array_Generator(
+  sourceBase64ArrayBuffer, progressToYield, progressToAdvance, suspendByteCount) {
 
   suspendByteCount = ( suspendByteCount <= 0 ) ? 1024 : suspendByteCount;
 
-  let sourceByteLength = base64ArrayBuffer.byteLength;
+  let sourceByteLength = sourceBase64ArrayBuffer.byteLength;
 
   if (progressToAdvance) {
     progressToAdvance.accumulation = 0;
     progressToAdvance.total = sourceByteLength;
   }
 
-  let resultByteLength = sourceByteLength * 0.75;
-  let resultArrayBuffer = new ArrayBuffer( resultByteLength );
+  // Decoding base64 will result a shorten data (75% in size). However, the source may not be pure base64 codes.
+  // So it is safer to prepare same size bufer as source.
+  let targetByteLength = sourceByteLength;
+  let targetArrayBuffer = new ArrayBuffer( targetByteLength );
 
-  let sourceBytes = new Uint8Array( base64ArrayBuffer );
-  let resultBytes = new Uint8Array( resultArrayBuffer );
-  for (let i = 0; i < sourceByteLength; ++i) {
+  let sourceBytes = new Uint8Array( sourceBase64ArrayBuffer );
+  let targetBytes = new Uint8Array( targetArrayBuffer );
+
+  const BYTES_PER_DECODE_UNIT = 4; // A decode unit consists of 4 base64 encoded source bytes.
+  let encodedArrayBuffer = new ArrayBuffer( BYTES_PER_DECODE_UNIT );
+  let encodedBytes = new Uint8Array( encodedArrayBuffer );
+
+  let resultBytes = 0; // Accumulate the real result byte count.
+  let byteCountAfterYield = 0;
+  let sourceIndex = 0;
+  while (sourceIndex < sourceByteLength) {
+
+!!! ...unfinished... Skip several lines
+
+    for (let j = 0; j < BYTES_PER_DECODE_UNIT; ++j, ++sourceIndex) {  // Extract 4 source bytes.
+      if (sourceIndex >= sourceByteLength)
+        break; // Decoding is done. (Ignore last non-4-bytes.)
+ 
+      let b = table_base64_Uint8_to_index[ sourceBytes[ sourceIndex ] ];
+      progressToAdvance.accumulation++;
+      byteCountAfterYield++;
+
+      if (byteCountAfterYield >= suspendByteCount) { // Every suspendByteCount, release CPU time.
+        yield progressToYield;
+        byteCountAfterYield = 0;
+      }
   
+      if (255 === b)
+        continue; // Skip any non-base64 bytes.
+
+      encodedBytes[ j ] = b;
+    }
+                                                  
+    if (sourceIndex >= sourceByteLength)
+      break; // Decoding is done. (Ignore last non-4-bytes.)
+
+    
 !!! ...unfinished...
 
-    resultBytes[ i ] = table_base64_Uint8_to_index[ sourceBytes[ i ] ];
+
+    targetBytes[ i ] = table_base64_Uint8_to_index[ sourceBytes[ i ] ];
   }
 
 
@@ -72,6 +127,8 @@ function* Base64ArrayBuffer_To_ArrayBuffer_Generator(
     };
 
 
+!!! ...unfinished... adjust resultBytes byteOffset , length]
+  let resultBytes = new Uint8Array( targetArrayBuffer, 0, resultBytes ); 
 
   return result;
 }
