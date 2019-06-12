a8ce33a93854f69494dd481f2720ceb476452c0a
ColorfulCakeChen <colorfulcakechen@gmail.com>
2019-06-12 10:04:00 +0800

    Update Base64ArrayBufferToUint8Array_tester.js

--- CNN/tester/Base64ArrayBufferToUint8Array_tester.js
+++ CNN/tester/Base64ArrayBufferToUint8Array_tester.js
@@ -6,39 +6,58 @@ import * as ValueMax from "../ValueMax.js";
 const base64EncodedString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 const base64DecodedString = atob(base64EncodedString);
 
-let tDecoder = new TextDecoder();
+const base64EncodedStrings_extra = [
+  base64EncodedString,
+
+  // Test extra has 0 to 3 bytes. (They should be ignored.)
+  base64EncodedString + "ab",
+  base64EncodedString + "ab",
+  base64EncodedString + "abc",
+
+  // Test extra non-base64 codes inside. (They should be ignored.)
+  base64EncodedString.slice(0, 3) + "\n"   + base64EncodedString.slice(3, -1),
+  base64EncodedString.slice(0, 3) + "\r"   + base64EncodedString.slice(3, -1),
+  base64EncodedString.slice(0, 3) + "\r\n" + base64EncodedString.slice(3, -1),
+  base64EncodedString.slice(0, 3) + "\n\r" + base64EncodedString.slice(3, -1),
+];
+
+//let tEncoder = new TextEncoder();
 //const base64EncodedUint8Array = tDecoder.decode(base64EncodedString);
 
-let tEncoder = new TextEncoder();
-const base64DecodedUint8Array = tEncoder.encode(base64DecodedString);
+let tDecoder = new TextDecoder();
+const base64DecodedUint8Array = tDecoder.decode(base64DecodedString);
 
-let original = [
-  tEncoder.encode(base64EncodedString),
+class TestCase {
+  constructor(source, skipLineCount, result, note) {
+    this.source = source;
+    this.skipLineCount = skipLineCount;
+    this.result = result;
+    this.note = note;
+  }
+}
 
-  tEncoder.encode("\n"   + base64EncodedString),
-  tEncoder.encode("\r"   + base64EncodedString),
-  tEncoder.encode("\r\n" + base64EncodedString),
-  tEncoder.encode("\n\r" + base64EncodedString),
+let testCases = [
+  TestCase( tEncoder.encode(base64EncodedStrings_shorten[0]), 0, base64DecodedUint8Array, "Extra 0 bytes" ),
+  TestCase( tEncoder.encode(base64EncodedStrings_shorten[1]), 0, base64DecodedUint8Array, "Extra 1 bytes" ),
+  TestCase( tEncoder.encode(base64EncodedStrings_shorten[2]), 0, base64DecodedUint8Array, "Extra 2 bytes" ),
+  TestCase( tEncoder.encode(base64EncodedStrings_shorten[3]), 0, base64DecodedUint8Array, "Extra 3 bytes" ),
+  TestCase( tEncoder.encode(base64EncodedStrings_shorten[4]), 0, base64DecodedUint8Array, "Extra LF inside" ),
+  TestCase( tEncoder.encode(base64EncodedStrings_shorten[5]), 0, base64DecodedUint8Array, "Extra CR inside" ),
+  TestCase( tEncoder.encode(base64EncodedStrings_shorten[6]), 0, base64DecodedUint8Array, "Extra CRLF inside" ),
+  TestCase( tEncoder.encode(base64EncodedStrings_shorten[7]), 0, base64DecodedUint8Array, "Extra LFCR inside" ),
 
-  tEncoder.encode("qwerty\n"   + base64EncodedString),
-  tEncoder.encode("qwerty\r"   + base64EncodedString),
-  tEncoder.encode("qwerty\r\n" + base64EncodedString),
-  tEncoder.encode("qwerty\n\r" + base64EncodedString),
+  TestCase( tEncoder.encode("\n"   + base64EncodedString), 1, base64DecodedUint8Array, "Begin LF" ),
+  TestCase( tEncoder.encode("\r"   + base64EncodedString), 1, base64DecodedUint8Array, "Begin CR" ),
+  TestCase( tEncoder.encode("\r\n" + base64EncodedString), 1, base64DecodedUint8Array, "Begin CRLF" ),
+  TestCase( tEncoder.encode("\n\r" + base64EncodedString), 2, base64DecodedUint8Array, "Begin LFCR" ),
+
+  TestCase( tEncoder.encode("qwerty\n"   + base64EncodedString), 1, base64DecodedUint8Array, "Text LF" ),
+  TestCase( tEncoder.encode("qwerty\r"   + base64EncodedString), 1, base64DecodedUint8Array, "Text CR" ),
+  TestCase( tEncoder.encode("qwerty\r\n" + base64EncodedString), 1, base64DecodedUint8Array, "Text CRLF" ),
+  TestCase( tEncoder.encode("qwerty\n\r" + base64EncodedString), 2, base64DecodedUint8Array, "Text LFCR" ),
 ];
 
-let result = [
-  base64DecodedUint8Array,
 
-  base64DecodedUint8Array,
-  base64DecodedUint8Array,
-  base64DecodedUint8Array,
-  base64DecodedUint8Array,
-
-  base64DecodedUint8Array,
-  base64DecodedUint8Array,
-  base64DecodedUint8Array,
-  base64DecodedUint8Array,
-];
 
 
 /** Aggregate all progress about downloading, JSON parsing, characters scanning, and weights scanning.  */
@@ -51,7 +70,7 @@ class Progress extends ValueMax.Percentage.Aggregate {
 
     super(children);
 
-    [this.download, this.Uint8Array] = children;
+    [this.download, this.uint8Array] = children;
   }
 }
 
@@ -62,15 +81,21 @@ window.addEventListener("load", event => {
   ScriptLoader.createPromise("https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.0.0/dist/tf.min.js").then(test); });
 
 function test() {
-  console.log("Hi! test()");
+  console.log("Base64 decode testing...");
 
-  PartTime.forOf(
-    Base64ArrayBufferToUint8Array.decode_Generator(
-      original[ 0 ], 0, progress, progress.Uint8Array, 5),
-    progrgess => {} 
-  ).then(r => {
-    tf.util.assert(
-      r == result[0],
-      `No skip line, base64 decode ${r} != ${result[0]}`);
-  });
+  for (let i = 0; i < testCases.length; ++i) {
+    let testCase = testCases[ i ];
+
+    PartTime.forOf(
+      Base64ArrayBufferToUint8Array.decode_Generator(
+        testCase.source, testCase.skipLineCount, progress, progress.uint8Array, 5),
+      progrgess => {} 
+    ).then(r => {
+      tf.util.assert(
+        r.toString() == testCase.result.toString(),
+        `[${i}] (${testCase.skipLineCount}) (${testCase.note}) ${r} != ${testCase.result}`);
+    });
+  }
+
+  console.log("Base64 decode testing... Done.");
 }
