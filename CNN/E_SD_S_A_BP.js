45ef19ba7444eeedd6aa5c6a791ce4acd0dc2677
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-04-13 14:49:05 +0800

    E_SD_S_A_BP.Shape consturtor

--- CNN/E_SD_S_A_BP.js
+++ CNN/E_SD_S_A_BP.js
@@ -34,7 +34,7 @@ export { NeuralNetwork };
  *
  * It does global average before pointwise convolution. The result should be same as global average after
  * pointwise convolution. The calculation performance, however, should be better because the amount of data
- * are reduced.
+ * is reduced.
  *
  *
  * - Biased Pointwise Convolution Layer (3rd layer)
@@ -51,6 +51,87 @@ class NeuralNetwork {
   }
 
 }
+
+
+/**
+ * Describe the shape of an E_SD_S_A_BP neural network.
+ *
+ */
+class Shape {
+
+  /**
+   * @param {number} input_Width
+   *   The horizontal size (pixel count) of input image.
+   *
+   * @param {number} input_Height
+   *   The vertical size (pixel count) of input image.
+   *
+   * @param {number} inputChannel_Count
+   *   The channel count of every pixel. This is the depth of a pixel.
+   *
+   * @param {number} inputChannel_One_EmbeddingChannel_Count
+   *   The embedding channel count for every input channel.
+   *
+   * @param {number} inputChannel_One_EmbeddingChannel_One_DepthwiseChannel_Count
+   *   The depthwise channel count for every embedding channel (of every input channel).
+   *
+   * @param {number} outputChannel_Count
+   *   The output channel count.
+   *
+   * @param {number} depthwiseFilter_Width
+   *   The horizontal size (weight count) of the depthwise convolution filter. Default is 3.
+   *
+   * @param {number} depthwiseFilter_Height
+   *   The vertical size (weight count) of the depthwise convolution filter. Default is 3.
+   */
+  constructor(
+    input_Width,
+    input_Height,
+    inputChannel_Count,
+    inputChannel_One_EmbeddingChannel_Count,
+    inputChannel_One_EmbeddingChannel_One_DepthwiseChannel_Count,
+    outputChannel_Count,
+    depthwiseFilter_Width = 3,
+    depthwiseFilter_Height = 3
+  ) {
+    this.input_Width = input_Width;
+    this.input_Height = input_Height;
+
+    this.inputChannel_Count = inputChannel_Count;
+
+    this.inputChannel_One_EmbeddingChannel_Count = inputChannel_One_EmbeddingChannel_Count;
+
+    this.inputChannel_One_EmbeddingChannel_One_DepthwiseChannel_Count
+      = inputChannel_One_EmbeddingChannel_One_DepthwiseChannel_Count;
+
+    this.outputChannel_Count = outputChannel_Count;
+
+    this.depthwiseFilter_Width = depthwiseFilter_Width;
+    this.depthwiseFilter_Height = depthwiseFilter_Height;
+  }
+
+  /** @return {number} The embedding channel count for all input channel. */
+  get inputChannel_All_EmbeddingChannel_Count() {
+  }
+
+  /** @return {number} The depthwise channel count for all embedding channel (of all input channel). */
+  get inputChannel_All_EmbeddingChannel_All_DepthwiseChannel_Count() {
+  }
+
+  /** @return {number} The input pixel count (= input_Width * input_Height ). */
+  get input_Pixel_Count() {
+    return ( this.input_Width * this.input_Height );
+  }
+
+  /** @return {number} The depthwise filter size (= depthwiseFilter_Width * depthwiseFilter_Height ). */
+  get depthwiseFilter_One_Weight_Count() {
+  }
+
+  /** @return {number} The pointwise filter size (= inputChannel_All_EmbeddingChannel_All_DepthwiseChannel_Count + 1 ). */
+  get pointwiseFilter_One_Weight_Count() {
+  }
+}
+
 
 /**
  * Create a NeuralNetwork from a byte array.
