c65db8b6dd76272d723d94842182e0fcf7744c72
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-04-19 16:46:04 +0800

    Update E_SD_S_A_BP.js
    Decoder initialize.

--- CNN/E_SD_S_A_BP.js
+++ CNN/E_SD_S_A_BP.js
@@ -48,7 +48,13 @@ export { NeuralNetwork };
  */
 class NeuralNetwork {
 
-  constructor() {
+  /**
+   * @param {Shape} shape
+   *   The shape of the neural network. It affects how the input image will be scaled when every time it
+   * is fed into this neural network.
+   */
+  constructor(shape) {
+    this.shape = shape;
   }
 
 }
@@ -113,15 +119,6 @@ class Shape {
     this.depthwiseFilterHeight = depthwiseFilterHeight;
   }
 
-//   /** @return {number} The total input pixel count (= inputWidth * inputHeight ). */
-//   get pixelCount_AllInput() {
-//     return ( this.inputWidth * this.inputHeight );
-//   }
-//
-//   /** @return {number} The total input weight count (= inputChannelCount * pixelCount_AllInput ). */
-//   get weightCount_AllInput() {
-//     return ( this.inputChannelCount * this.pixelCount_AllInput );
-//   }
 
   /** @return {number} The embedding vocabulary count for all input channel
    * (= embeddingVocabularyCount_PerInputChannel * inputChannelCount ).
@@ -239,14 +236,17 @@ class Shape {
  *   The input data as sourceUint8Array. It will be interpreted as Float32Array. If the last bytes not enough
  *  4 bytes (to compose a float32), they will be ignored (will not be used).
  *
+ * @param {Shape} shape
+ *   The shape of the neural network. It affects how the sourceUint8Array will be interpreted.
+ *
  * @param {ValueMax.Percentage.Aggregate} progressToYield
  *   Return this when every time yield. Usually, this is the container of the progressToAdvance.
  *
  * @param {ValueMax.Percentage.Concrete}  progressToAdvance
  *   Increase this when every time advanced. It will be initialized to zero when decoder starting.
  *
- * @param {Uint32} suspendByteCount
- *   Everytime so many bytes decoded, yield for releasing CPU time (and reporting progress).
+ * @param {Uint32} suspendWeightCount
+ *   Everytime so many weights decoded, yield for releasing CPU time (and reporting progress).
  *   Default is 1024 bytes.
  *
  * @yield {ValueMax.Percentage.Aggregate or Uint8Array}
@@ -254,28 +254,45 @@ class Shape {
  *   Yield ( value = decoded data as NeuralNetwork ) when ( done = true ).
  */
 NeuralNetwork.decoder = function* (
-  sourceUint8Array, progressToYield, progressToAdvance, suspendByteCount) {
+  sourceUint8Array, shape, progressToYield, progressToAdvance, suspendWeightCount) {
 
   // 0. Initialize.
 
   // If undefined or null or negative or zero or less than 1, set to default.
   // Note: Bitwising OR with zero is for converting to integer (if it is undefined or null).
-  if ((suspendByteCount | 0) <= 0)
-    suspendByteCount = 1024;
+  if ((suspendWeightCount | 0) <= 0)
+    suspendWeightCount = 1024;
 
-  let sourceFloat32Array;
-  {
-    let sourceByteOffset = sourceUint8Array.byteOffset;
-    let sourceByteLength = sourceUint8Array.byteLength;
-    sourceByteLength = Math.floor( sourceByteLength / Float32Array.BYTES_PER_ELEMENT ); // Ignore the last non-4-bytes.
-    sourceFloat32Array = new Float32Array( sourceUint8Array.buffer, sourceByteOffset, sourceByteLength );
-  }
+  // Interpret the source byte array as float32 array.
+  //
+  // floor() for ignoring the last non-4-bytes.
+  let sourceWeightCount = Math.floor( sourceUint8Array.byteLength / Float32Array.BYTES_PER_ELEMENT );
+  let sourceFloat32Array = new Float32Array( sourceUint8Array.buffer, sourceUint8Array.byteOffset, sourceWeightCount );
 
   // Initialize progress.
   progressToAdvance = progressToAdvance || {};  // If null, using a dummy object instead.
   progressToAdvance.accumulation = 0;
-  progressToAdvance.total = sourceByteLength;
+  progressToAdvance.total = sourceWeightCount;
+
+  // It is important that the nextYieldAccumulation is not greater than source length, so that
+  // it can be used as boundary checking to reduce checking times and increase performance.
+  let nextYieldAccumulation = Math.min( sourceWeightCount, progressToAdvance.accumulation + suspendWeightCount );
+
+  // 1. Decode.
+  while (progressToAdvance.accumulation < sourceWeightCount) {
 
 //!!! ...unfinished...
+
+    // Every suspendWeightCount, release CPU time (and report progress).
+    if (progressToAdvance.accumulation >= nextYieldAccumulation) {
+      nextYieldAccumulation = Math.min( sourceWeightCount, progressToAdvance.accumulation + suspendWeightCount );
+      yield progressToYield;
+    }
+  }
+
+  // 2. Result.
+  let resultNeuralNetwork = new NeuralNetwork( shape );
+  yield progressToYield; // Report the progress has been done (100%).
+  return resultNeuralNetwork;
 }
 
