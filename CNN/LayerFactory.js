561a8b2cfa8368e7d9df9209464b8042828b1ed2
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-04-27 16:36:05 +0800

    Create LayerFactory.js

---
+++ CNN/LayerFactory.js
@@ -0,0 +1,15 @@
+export { LayerFactory };
+
+import * as Layer from "./Layer.js"
+
+
+/**
+ * Create various kinds of neural network layers.
+ */
+class LayerFactory {
+
+  constructor() {
+  }
+
+  create
+}
