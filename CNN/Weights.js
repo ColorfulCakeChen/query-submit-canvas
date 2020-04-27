5c7f634ef85392dd4a4c082b8d23b381c86e9d9c
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-04-27 14:38:14 +0800

    Update and rename Layer.js to Weights.js
    ShapeFloat32Array

---
+++ CNN/Weights.js
@@ -0,0 +1,107 @@
+export { ShapeFloat32Array };
+
+/**
+ * A Float32Array with its shape. It can be used as CNN (depthwise, pointwise and bias) filter
+ * weights.
+ *
+ * @member {Float32Array} defaultInput
+ *   The default input Float32Array. Its byteOffset will be checked against defaultByteOffsetBegin.
+ * Its content will be interpret as weights if privilegeInput is null. Otherwise, its content
+ * will be ignored if privilegeInput is not null.
+ *
+ * @member {number} defaultByteOffsetBegin
+ *   The weights[] begins at defaultInput's defaultByteOffsetBegin (relative to defaultInput.buffer,
+ * not to defaultInput.byteOffset). If this value less than defaultInput.byteOffset, the
+ * initialization will fail (i.e. ( isValid() == false ) ).
+ *
+ * @member {number} defaultByteOffsetEnd
+ *   The weights[] ends at defaultInput's defaultByteOffsetEnd (relative to defaultInput.buffer,
+ * not to defaultInput.byteOffset) exclusively.
+ *
+ * @member {Float32Array} privilegeInput
+ *   The privilege input Float32Array. If not null, its content will be interpret as weights and
+ * the content of defaultInput will be ignored.
+ *
+ * @member {number} privilegeByteOffsetBegin
+ *   The weights[] begins at privilegeInput's privilegeByteOffsetBegin (relative to privilegeInput.buffer,
+ * not to privilegeInput.byteOffset). If this value less than privilegeInput.byteOffset, the
+ * initialization will fail (i.e. ( isValid() == false ) ).
+ *
+ * @member {number} privilegeByteOffsetEnd
+ *   The weights[] ends at privilegeInput's privilegeByteOffsetEnd (relative to privilegeInput.buffer,
+ * not to privilegeInput.byteOffset) exclusively.
+ *
+ * @member {number[]} shape
+ *   The filter shape (element count for every dimension). The shape.length is dimension. The initialization will
+ * fail (i.e. ( isValid() == false ) ) if shape is too large (or NaN) (exceeds the defaultInput (or, privilegeInput
+ * if not null) bounding).
+ *
+ * @member {Float32Array} weights
+ *  The values. It is a reference (sub-range) to the underlying defaultInput (or privilegeInput).
+ */
+class ShapeFloat32Array {
+
+//   constructor() {
+//   }
+
+  /**
+   * Create Float32Array weights[] over the defaultInput (or privilegeInput) according to the specific
+   * byteOffsetBegin, shape, and weightConverter.
+   *
+   * The defaultInput and privilegeInput can not both be null. If one of them is null, the non-null is used.
+   * If both are non-null, the privilegeInput will be used.
+   *
+   * @return {boolean} Return false, if initialization failed.
+   */ 
+  init( defaultInput, defaultByteOffsetBegin, privilegeInput, privilegeByteOffsetBegin, shape ) {
+
+    this.defaultInput =   defaultInput;
+    this.privilegeInput = privilegeInput;
+    this.shape =          shape;
+    this.weights =        null;   // So that ( isValid() == false ) if re-initialization failed.
+
+    let weightCount =     ( shape ) ? shape.reduce( ( accumulator, currentValue ) => accumulator * currentValue ) : 0;
+    let weightByteCount = weightCount * Float32Array.BYTES_PER_ELEMENT;
+
+    let input, byteOffsetBegin;
+    let byteOffsetEnd; // Exclusive. As the next filter's begin.
+
+    if ( privilegeInput ) {       // privilegeInput first.
+
+      if ( privilegeByteOffsetBegin < privilegeInput.byteOffset )
+        return false;  // Failed, the privilege beginning position is illegal (less than bounding).
+
+      input = privilegeInput;
+      byteOffsetBegin = this.privilegeByteOffsetBegin = privilegeByteOffsetBegin;
+      byteOffsetEnd =   this.privilegeByteOffsetEnd =   privilegeByteOffsetBegin + weightByteCount;
+      this.defaultByteOffsetBegin = this.defaultByteOffsetEnd = defaultByteOffsetBegin; // Stay at beginning for not used.
+
+    } else if ( defaultInput ) {  // defaultInput second.
+
+      if ( defaultByteOffsetBegin < defaultInput.byteOffset )
+        return false;  // Failed, the default beginning position is illegal (less than bounding).
+
+      input = defaultInput;
+      byteOffsetBegin = this.defaultByteOffsetBegin = defaultByteOffsetBegin;
+      byteOffsetEnd =   this.defaultByteOffsetEnd =   defaultByteOffsetBegin + weightByteCount;
+      this.privilegeByteOffsetBegin = this.privilegeByteOffsetEnd = privilegeByteOffsetBegin; // Stay at beginning for not used.
+
+    } else {
+      return false;  // Failed, both privilege and default input are null.
+    }
+
+    // Bounded by the input.byteLength.
+    let legalByteOffsetEnd = input.byteOffset + input.byteLength;
+    if ( byteOffsetEnd > legalByteOffsetEnd )
+      return false;  // Failed, if shape is too large (or NaN).
+
+    this.weights = new Float32Array( input.buffer, byteOffsetBegin, weightCount );  // Share the underlying array buffer.
+    return true;     // Success.
+  }
+
+  /** @return Return true, if initialization is success (i.e. ( this.weights != null )). */
+  isValid()                      { return ( this.weights ) ? true : false; }
+
+  get weightByteCount()          { return this.weights.byteLength; }
+  get weightCount()              { return this.weights.length; }
+}
