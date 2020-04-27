3470fb58a199a966767bc80194c6e2dc3260b063
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-04-27 16:31:39 +0800

    Update Weights.js
    Params with parameterCount, inChannels, outChannels.

--- CNN/Weights.js
+++ CNN/Weights.js
@@ -1,8 +1,8 @@
-export { ShapeFloat32Array };
+export { Base, Params };
 
 /**
- * A Float32Array with its shape. It can be used as CNN (depthwise, pointwise and bias) filter
- * weights.
+ * A base class for extracting and keeping weights. It composes of a Float32Array and a shape. It can
+ * be used as CNN (depthwise, pointwise and bias) filter weights.
  *
  * @member {Float32Array} defaultInput
  *   The default input Float32Array. Its byteOffset will be checked against defaultByteOffsetBegin.
@@ -32,17 +32,14 @@ export { ShapeFloat32Array };
  * not to privilegeInput.byteOffset) exclusively.
  *
  * @member {number[]} shape
- *   The filter shape (element count for every dimension). The shape.length is dimension. The initialization will
- * fail (i.e. ( isValid() == false ) ) if shape is too large (or NaN) (exceeds the defaultInput (or, privilegeInput
- * if not null) bounding).
+ *   The filter shape (element count for every dimension). The shape.length is dimension. The
+ * initialization will fail (i.e. ( isValid() == false ) ) if shape is too large (or NaN)
+ * (exceeds the defaultInput (or, privilegeInput if not null) bounding).
  *
  * @member {Float32Array} weights
  *  The values. It is a reference (sub-range) to the underlying defaultInput (or privilegeInput).
  */
-class ShapeFloat32Array {
-
-//   constructor() {
-//   }
+class Base {
 
   /**
    * Create Float32Array weights[] over the defaultInput (or privilegeInput) according to the specific
@@ -99,9 +96,88 @@ class ShapeFloat32Array {
     return true;     // Success.
   }
 
+  /** @return {number} Return the absolute value of the trucated value. */
+  static toPositiveInteger( v ) {
+    return Math.abs( Math.trunc( v ) );
+  }
+
   /** @return Return true, if initialization is success (i.e. ( this.weights != null )). */
   isValid()                      { return ( this.weights ) ? true : false; }
 
   get weightByteCount()          { return this.weights.byteLength; }
   get weightCount()              { return this.weights.length; }
+}
+
+
+/**
+ * The parameters for the weights of a neural network layer.
+ *
+ * @member {number} inChannels
+ *   The input channel count of this neural network layer for easily layer construction backtracking. This
+ * is inChannels of init()
+ *
+ * @member {number} outChannels
+ *   The output channel count of this neural network layer for easily layer construction backtracking. This
+ * is outChannels of init() if not null. Otherwise (i.e. init()'s outChannels is null), it is will be
+ * the last element of this.weightsModified[] which are extracted from inputFloat32Array. 
+ *
+ * @member {Float32Array} weightsModified
+ *  The copied extracted values. They are copied from inputFloat32Array or fixedWeights, and then converted
+ * to positive integer.
+ */
+class Params extends Base {
+
+  /**
+   * @param {Float32Array} inputFloat32Array
+   *   A Float32Array whose values will be interpret as weights.
+   *
+   * @param {number} byteOffsetBegin
+   *   The position to start to decode from the inputFloat32Array. This is relative to the inputFloat32Array.buffer
+   * (not to the inputFloat32Array.byteOffset).
+   *
+   * @param {number} parameterCount
+   *   Extract how many weights from inputFloat32Array or fixedWeights, and convert the values to positive integer.
+   *
+   * @param {Float32Array|Array} fixedWeights
+   *   If null, extract parameters from inputFloat32Array. If not null, extract parameters from it instead of
+   * inputFloat32Array. If not null, it should have parameterCount elements.
+   *
+   * @return {boolean} Return false, if initialization failed.
+   */
+  init( inputFloat32Array, byteOffsetBegin, parameterCount, inChannels, outChannels = null, fixedWeights = null ) {
+
+    this.inChannels = inChannels;
+    this.outChannels = outChannels;
+    this.weightsModified = null;     // So that distinguishable if re-initialization failed.
+
+    let privilegeInput;
+    if ( fixedWeights ) {
+      if ( fixedWeights instanceof Float32Array )
+        privilegeInput = fixedWeights;
+      else
+        privilegeInput = new Float32Array( fixedWeights );  // Convert to Float32Array.
+    }
+
+//     // Extract 6 weights from inputFloat32Array or fixedWeights, and convert the values to positive integer.
+//     let parameterCount = 6;
+    let bInitOk = super.init( inputFloat32Array, byteOffsetBegin, privilegeInput, 0, [ parameterCount ] );
+
+    // Copy and convert to integer.
+    //
+    // Do not modify the original array data. When backtracking (to try another neural network layer
+    // configuration), it is necessary to use the original data.
+    if ( bInitOk ) {
+      this.weightsModified = new Float32Array( this.weights );
+      this.weightsModified.forEach( ( element, i, array ) => array[ i ] = Base.toPositiveInteger( element ) );
+
+      // If not specified, use the last weight as output channel count. 
+      if ( !outChannels )
+        this.outChannels = this.weightsModified[ parameterCount - 1 ];
+    }
+
+    return bInitOk;
+  }
+
+
+//  get channelMultiplier() { return this.weightsModified[ 0 ]; }
 }
