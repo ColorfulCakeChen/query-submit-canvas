97eb7d3f9b468d97c7b3cf24ee2188635a26324c
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-05-02 15:25:14 +0800

    Update Weights.js
    Params has channelMultiplier (specifying or evolution), outChannels (specifying, depdend on channelMultiplier, or evolution).

--- CNN/Weights.js
+++ CNN/Weights.js
@@ -32,9 +32,9 @@ export { Base, Params };
  * not to privilegeInput.byteOffset) exclusively.
  *
  * @member {number[]} shape
- *   The filter shape (element count for every dimension). The shape.length is dimension. The
- * initialization will fail (i.e. ( isValid() == false ) ) if shape is too large (or NaN)
- * (exceeds the defaultInput (or, privilegeInput if not null) bounding).
+ *   The weights shape (element count for every dimension). The shape.length is dimension. If ( shape.length == 0 ),
+ * it is 0-dimension (i.e. scalar). Theinitialization will fail (i.e. ( isValid() == false ) ) if shape is too large
+ * (or NaN) (exceeds the defaultInput (or, privilegeInput if not null) bounding).
  *
  * @member {Float32Array} weights
  *  The values. It is a reference (sub-range) to the underlying defaultInput (or privilegeInput).
@@ -117,12 +117,16 @@ class Base {
  *   How many parameters are extracted from inputFloat32Array or fixedWeights at least.
  *
  * @member {number} parameterCountExtracted
- *   How many parameters are extracted from inputFloat32Array or fixedWeights. This will be the length
+ *   How many parameters are extracted from inputFloat32Array or fixedWeights in fact. This will be the length
  * of this.weights[] and this.weightsModified[].
  *   - = ( parameterCountExtractedAtLeast + 0 ), if both channelMultiplier and outChannels are not null.
  *   - = ( parameterCountExtractedAtLeast + 1 ), if only channelMultiplier is null.
  *   - = ( parameterCountExtractedAtLeast + 1 ), if only outChannels is null.
  *   - = ( parameterCountExtractedAtLeast + 2 ), if both channelMultiplier and outChannels are null.
+ *
+ * @member {number} parameterCount
+ *   Always ( parameterCountExtractedAtLeast + 2 )。 This is the total parameter count provided by this object
+ * if init() successfully.
  *
  * @member {number} inChannels
  *   The input channel count of this neural network layer.
@@ -135,16 +139,15 @@ class Base {
  *
  * @member {Float32Array} weightsModified
  *  The copied extracted values. They are copied from inputFloat32Array or fixedWeights, and then converted
- * to positive integer. Its length maybe ( parameterCountMax ) or ( parameterCountMax - 1 ) according to
- * whether init()'s outChannels is null or not.
+ * to positive integer. Its length will be the same as parameterCountExtracted.
  */
 class Params extends Base {
 
   /**
    * @param {Float32Array} inputFloat32Array
-   *   A Float32Array whose values will be interpret as weights. It should have ( parameterCountMax ) or
-   * ( parameterCountMax - 1 ) or ( parameterCountMax - 2 ) elements according to the combination of
-   * channelMultiplier and outChannels.
+   *   A Float32Array whose values will be interpret as weights. It should have ( parameterCountExtractedAtLeast ) or
+   * ( parameterCountExtractedAtLeast + 1 ) or ( parameterCountExtractedAtLeast + 2 ) elements according to the
+   * value of channelMultiplier and outChannels.
    *
    * @param {number} byteOffsetBegin
    *   The position to start to decode from the inputFloat32Array. This is relative to the inputFloat32Array.buffer
@@ -153,46 +156,49 @@ class Params extends Base {
    * @param {number} inChannels
    *   There will be how many input channels. (Input channel count)
    *
-???
    * @param {number} parameterCountExtractedAtLeast
    *   There will be at least how many parameters extracted from inputFloat32Array or fixedWeights.
    *
    * @param {number} channelMultiplier
    *   Every input channel will be expanded into how many channels.
-   *   - If null, extracted from inputFloat32Array or fixedWeights. (By evolution)
    *   - If not null, it will be used instead of extracting from inputFloat32Array or fixedWeights. (By specifying)
+   *   - If null, extracted from inputFloat32Array or fixedWeights. (By evolution)
    *
    * @param {number} outChannels
    *   There will be how many output channels. (Output channel count)
-   *   - If null, extracted from inputFloat32Array or fixedWeights. (By evolution)
-   *   - If not Number.isFinite(), it will be ( inChannels * channelMultiplier ). (By channelMultiplier)
    *   - If Number.isFinite(), it will be used instead of extracting from inputFloat32Array or fixedWeights. (By specifying)
+   *   - If Number.POSITIVE_INFINITY, it will be ( inChannels * channelMultiplier ). (By channelMultiplier)
+   *   - If null, extracted from inputFloat32Array or fixedWeights. (By evolution)
    *
    * @param {Float32Array|Array} fixedWeights
    *   If null, extract parameters from inputFloat32Array. If not null, extract parameters from it instead of
-   * inputFloat32Array. If not null, it should have ( parameterCountMax ) or ( parameterCountMax - 1 )
-   * or ( parameterCountMax - 2 ) elements according to the combination of channelMultiplier and outChannels.
+   * inputFloat32Array. If not null, it should have ( parameterCountExtractedAtLeast ) or
+   * ( parameterCountExtractedAtLeast + 1 ) or ( parameterCountExtractedAtLeast + 2 ) elements according to the
+   * value of channelMultiplier and outChannels.
    *
    * @return {boolean} Return false, if initialization failed.
    */
   init(
     inputFloat32Array, byteOffsetBegin, parameterCountExtractedAtLeast, inChannels,
+    channelMultiplier = null, outChannels = null, fixedWeights = null ) {
 
-    channelMultiplierByEvolution
-    channelMultiplierBySpecify
-     
-    outChannelsByChannelMultiplier  (Number.isNaN() == true)
-    outChannelsByEvolution
-    outChannelsBySpecify
-
-    outChannels = null, fixedWeights = null ) {
-
-    this.parameterCountExtractedAtLeast = this.parameterCountExtracted = parameterCountExtractedAtLeast;
+    // Prevent from negative. At least, should be zero.
+    let parameterCountExtracted = this.parameterCountExtractedAtLeast = Math.max( parameterCountExtractedAtLeast, 0 );
+    this.parameterCount = parameterCountExtracted + 2;  // +2 for channelMultiplier and outChannels.
     this.inChannels = inChannels;
 
-    this.outChannels = outChannels;
     this.weightsModified = null;     // So that distinguishable if re-initialization failed.
 
+    // Determine how many parameters should be extracted from array.
+    {
+      if ( !channelMultiplier ) // channelMultiplier by evolution.
+        ++parameterCountExtracted;
+
+      if ( !outChannels )       // outChannels by evolution.
+        ++parameterCountExtracted;
+    }
+
+    // If has fixedWeights, use it as priviledge input.
     let privilegeInput;
     if ( fixedWeights ) {
       if ( fixedWeights instanceof Float32Array )
@@ -201,18 +207,8 @@ class Params extends Base {
         privilegeInput = new Float32Array( fixedWeights );  // Convert to Float32Array.
     }
 
-    // Extract how many weights.
-    let parameterCount;
-    {
-      if ( outChannels )
-        parameterCount = parameterCountMax - 1;  // Since there is outChannels, extract fewer weights.
-      else
-        parameterCount = parameterCountMax;
-
-      parameterCount = Math.max( parameterCount, 0 ); // Prevent from negative. At least, should be zero.
-    }
-
-    let bInitOk = super.init( inputFloat32Array, byteOffsetBegin, privilegeInput, 0, [ parameterCount ] );
+    // Extract from array.
+    let bInitOk = super.init( inputFloat32Array, byteOffsetBegin, privilegeInput, 0, [ parameterCountExtracted ] );
 
     // Copy and convert to integer.
     //
@@ -222,12 +218,39 @@ class Params extends Base {
       this.weightsModified = new Float32Array( this.weights );
       this.weightsModified.forEach( ( element, i, array ) => array[ i ] = Base.toPositiveInteger( element ) );
 
-      // If not specified, use the last weight as output channel count. 
-      if ( !outChannels )
-        this.outChannels = this.weightsModified[ parameterCount - 1 ];
+      // Determine this.channelMultiplier and this.outChannels.
+      if ( channelMultiplier ) { // 1. channelMultiplier by specifying.
+        if ( outChannels ) {
+          if ( Number.isFinite( outChannels ) ) { // 1.1 outChannels by specifying.
+            this.channelMultiplier = channelMultiplier;
+            this.outChannels =       outChannels;
+          } else {                                // 1.2 outChannels by channelMultiplier.
+            this.channelMultiplier = channelMultiplier;
+            this.outChannels =       inChannels * channelMultiplier;
+          }
+        } else {                                  // 1.3 outChannels by evolution.
+          this.channelMultiplier =   channelMultiplier;
+          this.outChannels =         this.weightsModified[ parameterCountExtracted - 1 ];
+        }
+      } else {                   // 2. channelMultiplier by evolution.
+        if ( outChannels ) {
+          if ( Number.isFinite( outChannels ) ) { // 2.1 outChannels by specifying.
+            this.channelMultiplier = this.weightsModified[ parameterCountExtracted - 1 ];
+            this.outChannels =       outChannels;
+          } else {                                // 2.2 outChannels by channelMultiplier.
+            this.channelMultiplier = this.weightsModified[ parameterCountExtracted - 1 ];
+            this.outChannels =       inChannels * this.channelMultiplier;
+          }
+        } else {                                  // 2.3 outChannels by evolution.
+          this.channelMultiplier = this.weightsModified[ parameterCountExtracted - 2 ];
+          this.outChannels =       this.weightsModified[ parameterCountExtracted - 1 ];
+        }
+      }
+
     }
 
     return bInitOk;
   }
 
+  get parameterCountExtracted() { return this.shape.length; }
 }
