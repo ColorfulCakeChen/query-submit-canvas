7e48bc2831b772d2e962492c1ab631b69c5cec53
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-05-16 14:56:13 +0800

    Update Embedding2D.js
    buildTensors()

--- CNN/Layer/Embedding2D.js
+++ CNN/Layer/Embedding2D.js
@@ -42,6 +42,16 @@ class Params extends Weights.Params {
  * An embedding layer contains one params (this.params) and inChannels embedding vocabulary tables.
  * Every input channel has one embedding vocabulary table. Every embedding vocabulary table has
  * vocabularyCountPerInputChannel vocabularies. Every vocabulary has channelMultiplier embedding channels.
+ *
+ * @member {number} vocabularyCountPerInputChannel
+ *   Every input channel will have how many vocabularies. This is also vocabulary count per vocabulary table (because
+ * every input channel has a vocabulary table). For an image data (R-G-B-A four channels), there will be 256
+ * vocabularies per input channel because every channel is represented by one byte (8 bits) which has 2^8 = 256 kinds
+ * of possible values.
+ *
+ * @member {number} embeddingChannelCountPerInputChannel
+ *   Every vocabulary will have how many embedding channels. Every input channel will be expanded into so many
+ * embedding channels. This is same as the this.params.channelMultiplier.
  */
 class Layer {
 
@@ -55,12 +65,6 @@ class Layer {
    *
    * @param {number} inChannels
    *   The input channel count.
-   *
-   * @param {number} vocabularyCountPerInputChannel
-   *   Every input channel will have how many vocabularies. This is also vocabulary count per vocabulary table (because
-   * every input channel has a vocabulary table). For an image data (R-G-B-A four channels), there will be 256
-   * vocabularies per input channel because every channel is represented by one byte (8 bits) which has 2^8 = 256 kinds
-   * of possible values.
    *
    * @param {number} channelMultiplier
    *   Every vocabulary will have how many embedding channels. Every input channel will be expanded into so many
@@ -76,6 +80,7 @@ class Layer {
 // squeeze-and-excitation ?
 // Shuffled Grouped Pointwise Convolution ... ? (by tf.gather() ?)
 
+    disposeTensors();
     this.params = this.vocabularyTables = null; // So that distinguishable if re-initialization failed.
 
     this.vocabularyCountPerInputChannel = vocabularyCountPerInputChannel;
@@ -111,6 +116,44 @@ class Layer {
     return false;
   }
 
+  /** Build tf.tensor according to this.vocabularyTables[]. */
+  buildTensors() {
+    disposeTensor();
+
+    if ( !isValid() )
+      return;
+
+    this.vocabularyTablesTensors = tf.tidy( "Embedding2D.Layer.buildTensors", () => {
+      let inChannels =                           this.inChannels;
+      let vocabularyCountPerInputChannel =       this.vocabularyCountPerInputChannel;
+      let embeddingChannelCountPerInputChannel = this.embeddingChannelCountPerInputChannel;
+
+      let vocabularyTablesTensors = new Array( inChannels );
+
+      for ( let i = 0; i < inChannels; ++i ) {
+
+        let vocabularyTable = this.vocabularyTables[ i ];
+        if ( !vocabularyTable )
+          continue;
+
+        vocabularyTablesTensors[ i ] = tf.tensor2d(
+          vocabularyTable,
+          [ vocabularyCountPerInputChannel, embeddingChannelCountPerInputChannel ] );
+      }
+
+      return vocabularyTablesTensors;
+    });
+  }
+
+  /** Release tf.tensor. */
+  disposeTensors() {
+    tf.dispose( this.vocabularyTablesTensors );
+    this.vocabularyTablesTensors = null;
+  }
+
   get byteOffsetBegin() { return this.params.defaultByteOffsetBegin; }
   get byteOffsetEnd()   { return this.vocabularyTables[ this.params.inChannels - 1 ].defaultByteOffsetEnd; }
+
+  get inChannels()                           { return this.params.inChannels; }
+  get embeddingChannelCountPerInputChannel() { return this.params.channelMultiplier; }
 }
