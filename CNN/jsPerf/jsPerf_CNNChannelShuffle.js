57ce9d3370b9bd55570c413497828afcb0d79091
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-05-21 16:55:38 +0800

    Update jsPerf_CNNChannelShuffle.js

--- CNN/jsPerf/jsPerf_CNNChannelShuffle.js
+++ CNN/jsPerf/jsPerf_CNNChannelShuffle.js
@@ -166,6 +166,6 @@ globalThis.shuffledChannelIndicesTensor1dArray.map( ( shuffledChannelIndicesTens
 
 globalThis.dataTensor3dArray = dataTensor3dArray;
 
-globalThis.cnnShuffle_by_ConcatReshapeTransposeReshapeSplit = by_ConcatReshapeTransposeReshapeSplit();
-globalThis.cnnShuffle_by_ConcatGather = by_ConcatGather();
-globalThis.cnnShuffle_by_SplitSplitConcatConcat = by_SplitSplitConcatConcat();
+globalThis.cnnShuffle_by_ConcatReshapeTransposeReshapeSplit = by_ConcatReshapeTransposeReshapeSplit;
+globalThis.cnnShuffle_by_ConcatGather = by_ConcatGather;
+globalThis.cnnShuffle_by_SplitSplitConcatConcat = by_SplitSplitConcatConcat;
