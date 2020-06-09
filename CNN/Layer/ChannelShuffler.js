7708fe95f766a2d0e3b1d8623c5c02382075d984
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-06-09 12:49:04 +0800

    Update ChannelShuffler.js
    Move Layer.isTensorArrayEqual() to other file.

--- CNN/Layer/ChannelShuffler.js
+++ CNN/Layer/ChannelShuffler.js
@@ -623,34 +623,4 @@ class Layer {
     return outputTensor3DArray;
   }
 
-  /** @return Return true, if two array of tensor are equal by value. */
-  static isTensorArrayEqual( tensorArray1, tensorArray2 ) {
-
-    if ( tensorArray1 === tensorArray2 )
-      return true;
-
-    if ( tensorArray1 == null || tensorArray2 == null )
-      return false;
-
-    if ( tensorArray1.length !== tensorArray2.length )
-      return false;
-
-    for ( let i = 0; i < tensorArray1.length; ++i ) {
-//!!! wrong. They are tensor.
-//       if ( !tensorArray1[ i ].equal( tensorArray2[ i ] ) )
-//         return false;
-
-      let allElementEqual = tf.tidy( "ChannelShuffler.Layer.isTensorArrayEqual", () => {
-        let everyElementEqualTensor1d = tensorArray1[ i ].equal( tensorArray2[ i ] );
-        let allElementEqualTensor1d = everyElementEqualTensor1d.all();
-        return allElementEqualTensor1d.arraySync(); // 0: false. 1: true.
-      });
-
-      if ( !allElementEqual )
-        return false;
-    }
-
-    return true;
-  }
-
 }
