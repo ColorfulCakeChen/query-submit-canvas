76e2582881b621b4499e72734c0061a29457d216
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-06-09 15:36:06 +0800

    Update jsPerf_CNNHeightWidthResize.js

--- CNN/jsPerf/jsPerf_CNNHeightWidthResize.js
+++ CNN/jsPerf/jsPerf_CNNHeightWidthResize.js
@@ -131,8 +131,7 @@ class HeightWidthDepth {
       let quarterTensor1 = this.dataTensor3d.maxPool( this.filterHeightWidth, 1, "valid" );
       let quarterTensor2 = this.dataTensor3d.avgPool( this.filterHeightWidth, 1, "valid" );
       let quarterTensor3 = this.dataTensor3d.depthwiseConv2D( this.depthwiseConvFilters, 1, "valid" );
-//      let quarterTensor4 = this.dataTensor3d.depthwiseConv2D( this.depthwiseConv3x3Filters, 2, "same" );
-      let quarterTensor4 = this.dataTensor3d.depthwiseConv2D( this.depthwiseConv3x3Filters, 2, "valid" );
+      let quarterTensor4 = this.dataTensor3d.depthwiseConv2D( this.depthwiseConv3x3Filters, 2, "same" );
       let quarterTensor5 = this.dataTensor3d.resizeNearestNeighbor( this.targetSize, true );
       let quarterTensor6 = this.dataTensor3d.resizeBilinear( this.targetSize, true );
 
