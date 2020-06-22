9d51443369638d44d7c3e83d000a97e26843431e
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-06-22 11:36:16 +0800

    Update TestFilters2D.js

--- CNN/jsPerf/TestFilters2D.js
+++ CNN/jsPerf/TestFilters2D.js
@@ -1,14 +1,16 @@
 
+export { Base };
+
 /**
  * Testing Filters of multiple layers.
  */
-class TestFilters2D {
+class Base {
 
   /**
    * @param sourceHeight      The height (and width) of the source image which will be processed by apply().
    * @param sourceDepth       The channel count of the source image.
    * @param targetHeight      The taregt image height (and width).
-   * @param strAvgMaxConv    "Avg" or "Max" or "Conv". for average pooling, max pooling, depthwise convolution.
+   * @param strAvgMaxConv     "Avg" or "Max" or "Conv". for average pooling, max pooling, depthwise convolution.
    * @param filterHeight      The height (and width) of each depthwise convolution.
    * @param bPointwise        If true, there will be pointwise convolution after every layer of depthwise convolution.
    */
@@ -99,37 +101,72 @@ class TestFilters2D {
   apply( sourceImage, bReturn ) {
     return tf.tidy( () => {
 
-      let tNew;
+      let t, tNew;
+//      let depthwiseFiltersTensor4d;
+//      let pointwiseFiltersTensor4d;
 
       // Layer 0
       //
       // So that every layer could dispose previous result without worry about mis-dispose source.
-      let depthwiseFiltersTensor4d = this.depthwiseFiltersTensor4dArray[ 0 ];
-      let pointwiseFiltersTensor4d = this.pointwiseFiltersTensor4dArray[ 0 ];
 
-      let t = sourceImage.depthwiseConv2d( depthwiseFiltersTensor4d, 1, "valid" );  // Stride = 1
+      if ( this.bDepthwiseAvg ) {
+        t = sourceImage.pool( this.depthwiseFilterHeightWidth, "avg", "valid", 1, 1 );
+      } else if ( this.bDepthwiseMax ) {
+        t = sourceImage.pool( this.depthwiseFilterHeightWidth, "max", "valid", 1, 1 );
+      } else if ( this.bDepthwiseConv ) {
+        t = sourceImage.depthwiseConv2d( this.depthwiseFiltersTensor4dArray[ 0 ], 1, "valid" );  // Stride = 1
+      }
       // NOTE: Do not dispose the original data.
 
       if ( this.bPointwise ) {
-        tNew = t.conv2d( pointwiseFiltersTensor4d, 1, "valid" ); // 1x1, Stride = 1
-        tf.dispose( t );                                         // Dispose all intermediate (temporary) data.
+        tNew = t.conv2d( this.pointwiseFiltersTensor4dArray[ 0 ], 1, "valid" ); // 1x1, Stride = 1
+        t.dispose();                                         // Dispose all intermediate (temporary) data.
         t = tNew;
       }
 
       // Layer 1, ...
-      for ( let i = 1; i < this.stepCount; ++i ) {
-        depthwiseFiltersTensor4d = this.depthwiseFiltersTensor4dArray[ i ];
-        pointwiseFiltersTensor4d = this.pointwiseFiltersTensor4dArray[ i ];
+      if ( this.bDepthwiseAvg ) {
 
-        tNew = t.depthwiseConv2d( depthwiseFiltersTensor4d, 1, "valid" );  // Stride = 1
-        tf.dispose( t );                                           // Dispose all intermediate (temporary) data.
-        t = tNew;
-
-        if ( this.bPointwise ) {
-          tNew = t.conv2d( pointwiseFiltersTensor4d, 1, "valid" ); // 1x1, Stride = 1
-          tf.dispose( t );                                         // Dispose all intermediate (temporary) data.
+        for ( let i = 1; i < this.stepCount; ++i ) {
+          tNew = t.pool( this.depthwiseFilterHeightWidth, "avg", "valid", 1, 1 );
+          t.dispose();                                           // Dispose all intermediate (temporary) data.
           t = tNew;
+
+          if ( this.bPointwise ) {
+            tNew = t.conv2d( this.pointwiseFiltersTensor4dArray[ i ], 1, "valid" ); // 1x1, Stride = 1
+            t.dispose();                                         // Dispose all intermediate (temporary) data.
+            t = tNew;
+          }
         }
+
+      } else if ( this.bDepthwiseMax ) {
+
+        for ( let i = 1; i < this.stepCount; ++i ) {
+          tNew = t.pool( this.depthwiseFilterHeightWidth, "max", "valid", 1, 1 );
+          t.dispose();                                           // Dispose all intermediate (temporary) data.
+          t = tNew;
+
+          if ( this.bPointwise ) {
+            tNew = t.conv2d( this.pointwiseFiltersTensor4dArray[ i ], 1, "valid" ); // 1x1, Stride = 1
+            t.dispose();                                         // Dispose all intermediate (temporary) data.
+            t = tNew;
+          }
+        }
+
+      } else if ( this.bDepthwiseConv ) {
+
+        for ( let i = 1; i < this.stepCount; ++i ) {
+          tNew = t.depthwiseConv2d( this.depthwiseFiltersTensor4dArray[ i ], 1, "valid" );  // Stride = 1
+          t.dispose();                                           // Dispose all intermediate (temporary) data.
+          t = tNew;
+
+          if ( this.bPointwise ) {
+            tNew = t.conv2d( this.pointwiseFiltersTensor4dArray[ i ], 1, "valid" ); // 1x1, Stride = 1
+            t.dispose();                                         // Dispose all intermediate (temporary) data.
+            t = tNew;
+          }
+        }
+
       }
 
       if ( bReturn )
