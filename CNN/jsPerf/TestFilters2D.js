e3223776c18b65f3a9928afccb6924c8dfa27fca
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-06-25 15:59:12 +0800

    Update TestFilters2D.js

--- CNN/jsPerf/TestFilters2D.js
+++ CNN/jsPerf/TestFilters2D.js
@@ -13,7 +13,14 @@ class Base {
    * @param sourceDepth       The channel count of the source image.
    * @param targetHeight      The taregt image height (and width).
    * @param filterHeight      The height (and width) of each depthwise convolution.
-   * @param channelMultiplier Every input channel will become so many channels.
+   *
+   * @param {number} channelMultiplierBlock0
+   *   The first block (Block 0) will expand input channel by this factor.
+   *
+   * @param {number} stepCountPerBlock
+   *   If zero or negative (<= 0), every block will use only one tf.depthwiseConv2d( strides = 1, pad = "valid" ) for shrinking sourceHeight
+   * (minus ( filterHeight - 1 )). If positive (>= 1), every block will use one tf.depthwiseConv2d( strides = 2, pad = "same" ) to shrink
+   * (half downsample) and use ( stageCountPerBlock - 1 ) times tf.depthwiseConv2d( strides = 1, pad = "same" ) until the block end.
    *
    * @param strAvgMaxConv
    *   Depthwise operation. "Avg" or "Max" or "Conv" for average pooling, max pooling, depthwise convolution.
@@ -36,19 +43,20 @@ class Base {
    */
   init(
     sourceHeight, sourceDepth, targetHeight,
-    filterHeight, channelMultiplier,
+    filterHeight, channelMultiplierBlock0,
+    stepCountPerBlock,
     strAvgMaxConv, bDepthwiseBias, depthwiseActivationName,
     bPointwise, bPointwiseBias, pointwiseActivationName ) {
 
     this.disposeTensors();
 
-//    this.name = name;
+    this.stepCountPerBlock = stepCountPerBlock;
 
     let differenceHeight = sourceHeight - targetHeight;
     let filterWidth = filterHeight;
 
-    this.channelMultiplier = channelMultiplier;
-    this.channelCountPerStage = sourceDepth * channelMultiplier;
+    this.channelMultiplier = channelMultiplierBlock0;
+    this.channelCountBlock0 = sourceDepth * channelMultiplierBlock0;  // the channel count of the first block (Block 0).
 
     this.strAvgMaxConv = strAvgMaxConv;
     switch ( strAvgMaxConv ) {
@@ -85,27 +93,34 @@ class Base {
     // The height of processed image will be reduced a little for any depthwise filter larger than 1x1.
     let heightReducedPerStep = filterHeight - 1;
 
-    // The step count for reducing sourceHeight to targetHeight by depthwise convolution filter.
-    this.blockCount = Math.floor( differenceHeight / heightReducedPerStep );
+    if ( stepCountPerBlock >= 1 ) {
+      // The block count for reducing sourceHeight to targetHeight by tf.depthwiseConv2d( strides = 2, pad = "same" ).
+      this.blockCount = Math.floor( Math.log2( sourceHeight ) );
+    } else {
+      // The block count for reducing sourceHeight to targetHeight by tf.depthwiseConv2d( strides = 1, pad = "valid" ).
+      this.blockCount = Math.floor( differenceHeight / heightReducedPerStep );
+    }
 
-    // e.g. "D24_DConv_101x101_DBias_RELU__PConv_PBias_RELU__Block_1"
-    this.name = `D${sourceDepth}_${this.channelCountPerStage}__D${strAvgMaxConv}_${filterHeight}x${filterHeight}`
-      + `${ ( this.bDepthwiseBias ) ? ( "_DBias" ) : ""}`
-      + `${ ( this.depthwiseActivationFunction ) ? ( "_" + depthwiseActivationName ) : ""}`
-      + `${ ( this.bPointwise ) ? "__PConv" : ""}`
-      + `${ ( this.bPointwise && this.bPointwiseBias ) ? ( "_PBias" ) : ""}`
-      + `${ ( this.bPointwise && this.pointwiseActivationFunction ) ? ( "_" + pointwiseActivationName ) : ""}`
+    // e.g. "D24_24__DConv_101x101_DBias_RELU__PConv_PBias_RELU__Block_1__Step_1"
+    this.name = `D${sourceDepth}_${this.channelCountBlock0}`
+      + `__D${strAvgMaxConv}_${filterHeight}x${filterHeight}`
+      + `${ ( this.bDepthwiseBias ) ? ( "_DBias" ) : "" }`
+      + `${ ( this.depthwiseActivationFunction ) ? ( "_" + depthwiseActivationName ) : "" }`
+      + `${ ( this.bPointwise ) ? "__PConv" : "" }`
+      + `${ ( this.bPointwise && this.bPointwiseBias ) ? ( "_PBias" ) : "" }`
+      + `${ ( this.bPointwise && this.pointwiseActivationFunction ) ? ( "_" + pointwiseActivationName ) : "" }`
       + `__Block_${this.blockCount}`
+      + `__Step${stepCountPerBlock}`
     ;
 
     // Depthwise Filters and Biases
 
     this.depthwiseFilterHeightWidth = [ filterHeight, filterWidth ];
 
-    // First stage's depthwise filters will expand channel count from sourceDepth to ( sourceDepth * channelMultiplier ).
-    this.depthwiseFiltersShapeFirst = [ filterHeight, filterWidth, sourceDepth,               channelMultiplier ];
-    this.depthwiseFiltersShape =      [ filterHeight, filterWidth, this.channelCountPerStage,                 1 ];
-    this.depthwiseBiasesShape =       [            1,           1, this.channelCountPerStage ];
+    // First block's depthwise filters will expand channel count from sourceDepth to ( sourceDepth * channelMultiplierBlock0 ).
+    this.depthwiseFiltersShapeFirst = [ filterHeight, filterWidth,             sourceDepth, channelMultiplierBlock0 ];
+    this.depthwiseFiltersShape =      [ filterHeight, filterWidth, this.channelCountBlock0,                       1 ];
+    this.depthwiseBiasesShape =       [            1,           1, this.channelCountBlock0 ];
 
     // Every element (Tensor4d) is a depthwiseFilters for one block.
     this.depthwiseFiltersTensor4dArray
@@ -118,8 +133,8 @@ class Base {
 
     // Pointwise Filters and Biases
 
-    let pointwiseInputDepth =  this.channelCountPerStage;
-    let pointwiseOutputDepth = this.channelCountPerStage; // Assume output depth is the same as input.
+    let pointwiseInputDepth =  this.channelCountBlock0;
+    let pointwiseOutputDepth = this.channelCountBlock0; // Assume output depth is the same as input.
 
     this.pointwiseFilterHeightWidth = [ 1, 1 ];
     this.pointwiseFiltersShape = [ 1, 1, pointwiseInputDepth, pointwiseOutputDepth ];
