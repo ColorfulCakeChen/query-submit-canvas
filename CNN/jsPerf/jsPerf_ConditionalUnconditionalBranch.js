19f2f877443fb42a7c8ec7a52f31580a8156cf69
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-07-06 11:35:30 +0800

    Create jsPerf_ConditionalUnconditionalBranch.js

---
+++ CNN/jsPerf/jsPerf_ConditionalUnconditionalBranch.js
@@ -0,0 +1,32 @@
+//import * as TensorTools from "../util/TensorTools.js";
+
+/**
+ * Test performancce of conditional and unconditional branch.
+ *
+ * @see {@link https://jsperf.com/colorfulcakechen-conditional-unconditional-branch}
+ */
+
+/**
+ * A test set.
+ */
+class TestSet {
+
+  /**
+   */
+  constructor( height, width, depth, groupCount ) {
+
+
+  }
+
+  test_ConditionalBranch() {
+  }
+
+  // Testing whether the results of different implementation are the same.
+  testResultSame() {
+
+  }
+  
+}
+
+
+globalThis.testSet_110x110x24_g8 = new HeightWidthDepthGroup( 110, 110, 24, 8 ); // height, width, depth, groupCount
