0e85f44dddb785453055097032b51a4f2efef5c0
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-07-08 14:22:55 +0800

    Update HeightWidthDepth.js

--- CNN/jsPerf/HeightWidthDepth.js
+++ CNN/jsPerf/HeightWidthDepth.js
@@ -235,11 +235,14 @@ class Base {
     for ( let i = 0; i < this.testFiltersArray.length; ++i ) {
       let testFilters = this.testFiltersArray[ i ];
 
+//!!! Using canvas as source.
 //      let t = testFilters.apply( this.dataTensor3d, true );
       let t = testFilters.apply( this.testCnavas, true );
 
       tf.util.assert(
         tf.util.arraysEqual(
+//!!! Now, output channel is the same as input channel count.
+//          [ t.shape[ 0 ], t.shape[ 1 ], ( t.shape[ 2 ] / testFilters.depthwiseChannelMultiplierBlock0Step0 ) ], tensor_ResizeBilinear.shape ),
           [ t.shape[ 0 ], t.shape[ 1 ], ( t.shape[ 2 ] / testFilters.depthwiseChannelMultiplierBlock0Step0 ) ], tensor_ResizeBilinear.shape ),
         `Shape ${testFilters.name}() != ResizeBilinear()`);
 
