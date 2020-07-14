76f22ad883514da656c6d6426a9f3dc57b2ad999
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-07-14 13:42:55 +0800

    Update BlocksWorkersChain.js

--- CNN/Net/BlocksWorkersChain.js
+++ CNN/Net/BlocksWorkersChain.js
@@ -1,10 +1,10 @@
-//import * as TwinsWorker from "./TwinsWorker.js";
+//import * as BlocksWorker from "./BlocksWorker.js";
 
 export { Base };
 
 /**
- * Many workers cascade in chain. When apply() is called, the input (usually a large memory block) will be transffered to the 1st worker to start
- * computing, and then transffered to the 2nd worker to start computing, ... etc.
+ * Many workers cascade in chain. Every worker handles one neural network. When apply() is called, the input (usually a large memory block)
+ * will be transffered to the 1st worker to start computing, and then transffered to the 2nd worker to start computing, ... etc.
  *
  * When passing large data by Worker.postMessage(), it is preferred by transferring (not by copying). If the large data wants to be transferred
  * to many workers, the only possible way is to transferring them serially. This is why the workers are arranged in cascade chain.
@@ -20,21 +20,28 @@ class Base {
    *
    * @param {number} chainedWorkerCount
    *   There will be so many workers created in chain.
+   *
+   * @param {string} weightsURL
+   *   The URL of neural network weights. Every worker will load weights from the URL to initialize one neural network.
    */
-  init( chainedWorkerCount ) {
+  init( chainedWorkerCount, weightsURL ) {
+    disposeWorkers();
+
+    let message = { command: "init", remainedWorkerCount: chainedWorkerCount, weightsURL: weightsURL };
+
     this.worker0 = new Worker("./BlocksWorker.js");
-    this.worker1 = new Worker("./TwinsWorker.js");
-    this.workers = [ this.worker0, this.worker1 ];
+    this.worker0.postMessage( message );
   }
 
-  disposeTensors() {
-    if ( this.workers ) {
-      for ( let i = 0; i < this.workers.length; ++i ) {
-        let worker = this.workers[ i ];
-        worker.postMessage( ??? );
-      }
-      this.workers = null;
+  disposeWorkers() {
+    if ( this.worker0 ) {
+      let message = { command: "disposeWorker" };
+      this.worker0.postMessage( message );
+      this.worker0 = null;
     }
   }
 
+  apply() {
+  }
+    
 }
