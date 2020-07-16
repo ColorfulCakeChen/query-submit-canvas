d8353823193d794ed0415e2f90c4187f4111c18b
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-07-16 15:46:10 +0800

    Update NeuralNetwork.js
    Using Config object.

--- CNN/Net/NeuralNetwork.js
+++ CNN/Net/NeuralNetwork.js
@@ -1,9 +1,9 @@
 import * as ShuffleNetV2_MobileNetV2_Block from "../Layer/ShuffleNetV2_MobileNetV2_Block.js";
 
-export { Base };
+export { Config, Base };
 
 /**
- * A neural network composes of multiple blocks.
+ * A neural network's configuration.
  *
  * A special recommended configuration is 3x3 ShuffleNetV2 without (explicit) bias:
  *   - bChannelShuffler: true
@@ -17,11 +17,51 @@ export { Base };
  * The cosine activation function can achieve impilcit bias. This is because depthwise and pointwise convolution can easily achieve zero
  * and ( cos( 0 ) == 1 ). A constant value (e.g. 1) becomes an bias of the next convolution. So there is not necessary to use explicit bias
  * which has worse performance (than without it).
+ */
+class Config {
+  /**
+   * @param {number} depthwiseChannelMultiplierBlock0Step0
+   *   The depthwise convolution of the first step (Step 0) of the first block (Block 0) will expand input channel by this factor.
+   *
+   * @see ShuffleNetV2_MobileNetV2_Block.Base.init 
+   */
+  constructor(
+    sourceHeight, sourceWidth, sourceChannelCount,
+    stepCountPerBlock,
+    bChannelShuffler,
+    pointwise1ChannelCountRate,
+    strAvgMaxConv, depthwiseFilterHeight, depthwiseChannelMultiplierBlock0Step0, bBias, strActivationName )
+  {
+    this.sourceHeight = sourceHeight;
+    this.sourceWidth = sourceWidth;
+    this.sourceChannelCount = sourceChannelCount;
+    this.stepCountPerBlock = stepCountPerBlock;
+    this.bChannelShuffler = bChannelShuffler;
+    this.pointwise1ChannelCountRate = pointwise1ChannelCountRate;
+    this.strAvgMaxConv = strAvgMaxConv;
+    this.depthwiseFilterHeight = depthwiseFilterHeight;
+    this.depthwiseChannelMultiplierBlock0Step0 = depthwiseChannelMultiplierBlock0Step0;
+    this.bBias = bBias;
+    this.strActivationName = strActivationName;
+  }
+
+  /** The channel count of the first block (Block 0). */
+  get channelCountBlock0() {
+    return this.sourceChannelCount * this.depthwiseChannelMultiplierBlock0Step0;
+  }
+
+}
+
+/**
+ * A neural network composes of multiple blocks.
  *
  *
+ *
+ * @member {Config} config
+ *   This neural network's configuration.
  *
  * @member {string} structure
- *   This neural network's structure.
+ *   A string denote this neural network's structure (configuration).
  *
  * @member {number} totalChannelExpansionFactor
  *   The final output of this neural network will have ( totalChannelExpansionFactor * sourceChannelCount ) channel count.
@@ -32,55 +72,50 @@ export { Base };
 class Base {
 
   /**
-   * @param {number} depthwiseChannelMultiplierBlock0Step0
-   *   The depthwise convolution of the first step (Step 0) of the first block (Block 0) will expand input channel by this factor.
+   * @param {Config} config
+   *   The configuration of this neural network.
    *
    * @param {boolean} bKeepInputTensor
    *   If true, apply_and_destroy_or_keep() will not dispose inputTensor (i.e. keep).
    *
    * @see ShuffleNetV2_MobileNetV2_Block.init 
    */
-  init(
-    sourceHeight, sourceWidth, sourceChannelCount,
-    stepCountPerBlock,
-    bChannelShuffler,
-    pointwise1ChannelCountRate,
-    strAvgMaxConv, depthwiseFilterHeight, depthwiseChannelMultiplierBlock0Step0, bBias, strActivationName,
-    bKeepInputTensor
-  ) {
+  init( config, bKeepInputTensor ) {
 
     this.disposeTensors();
 
-    this.sourceChannelCount = sourceChannelCount;
-    this.stepCountPerBlock = stepCountPerBlock;
-    this.bChannelShuffler = bChannelShuffler;
-    this.pointwise1ChannelCountRate = pointwise1ChannelCountRate;
+    this.config = config;
+
+//     this.sourceChannelCount = sourceChannelCount;
+//     this.stepCountPerBlock = stepCountPerBlock;
+//     this.bChannelShuffler = bChannelShuffler;
+//     this.pointwise1ChannelCountRate = pointwise1ChannelCountRate;
     this.bKeepInputTensor = bKeepInputTensor;
 
     let targetHeight = 1; // The final output always has ( height x width ) = ( 1 x 1 ), i.e. only one pixel per channel.
 
-    let differenceHeight = sourceHeight - targetHeight;
-    let filterWidth = depthwiseFilterHeight;
+    let differenceHeight = config.sourceHeight - targetHeight;
+//    let filterWidth = depthwiseFilterHeight;
 
-    this.depthwiseChannelMultiplierBlock0Step0 = depthwiseChannelMultiplierBlock0Step0;
-    this.channelCountBlock0 = sourceChannelCount * depthwiseChannelMultiplierBlock0Step0;  // the channel count of the first block (Block 0).
+//  this.depthwiseChannelMultiplierBlock0Step0 = depthwiseChannelMultiplierBlock0Step0;
+//    this.channelCountBlock0 = config.sourceChannelCount * config.depthwiseChannelMultiplierBlock0Step0;  // the channel count of the first block (Block 0).
 
-    if ( stepCountPerBlock <= 0 ) { // Not ShuffleNetV2, Not MobileNetV2.
+    if ( config.stepCountPerBlock <= 0 ) { // Not ShuffleNetV2, Not MobileNetV2.
 
       // The height of processed image will be reduced a little for any depthwise filter larger than 1x1.
-      let heightReducedPerBlock = depthwiseFilterHeight - 1;
+      let heightReducedPerBlock = config.depthwiseFilterHeight - 1;
 
       // The block count for reducing sourceHeight to targetHeight by tf.depthwiseConv2d( strides = 1, pad = "valid" ).
       this.blockCount = Math.floor( differenceHeight / heightReducedPerBlock );
 
       // Channel count only be expanded by channel multiplier of depthwise convolution of step 0 of block 0.
-      this.totalChannelExpansionFactor = depthwiseChannelMultiplierBlock0Step0;
+      this.totalChannelExpansionFactor = config.depthwiseChannelMultiplierBlock0Step0;
 
     } else {  // ShuffleNetV2 or MobileNetV2. Halven per block.
 
       // The block count for reducing sourceHeight to targetHeight by tf.depthwiseConv2d( strides = 2, pad = "same" ).
 //      this.blockCount = Math.floor( Math.log2( sourceHeight ) );
-      this.blockCount = Math.ceil( Math.log2( sourceHeight ) );
+      this.blockCount = Math.ceil( Math.log2( config.sourceHeight ) );
 
 //!!! wrong
 //       // Channel count is expanded both by channel multiplier of depthwise convolution of step 0 of block 0
@@ -91,8 +126,8 @@ class Base {
       this.totalChannelExpansionFactor = Math.pow( 2, this.blockCount );
     }
 
-    let nextBlockInputChannelCount = sourceChannelCount;
-    let nextBlockDepthwiseChannelMultiplier = depthwiseChannelMultiplierBlock0Step0; // Only block 0 can have ( depthwise channel multiplier > 1 ).
+    let nextBlockInputChannelCount = config.sourceChannelCount;
+    let nextBlockDepthwiseChannelMultiplier = config.depthwiseChannelMultiplierBlock0Step0; // Only block 0 can have ( depthwise channel multiplier > 1 ).
 
     let nextKeepInputTensor = bKeepInputTensor; // Only step 0 may or may not keep the input tensor according to caller's necessary.
 
@@ -101,11 +136,11 @@ class Base {
     {
       let block = new ShuffleNetV2_MobileNetV2_Block.Base();
       block.init(
-        sourceHeight, sourceWidth, nextBlockInputChannelCount,
-        stepCountPerBlock,
-        bChannelShuffler,
-        pointwise1ChannelCountRate,
-        strAvgMaxConv, depthwiseFilterHeight, nextBlockDepthwiseChannelMultiplier, bBias, strActivationName,
+        config.sourceHeight, config.sourceWidth, nextBlockInputChannelCount,
+        config.stepCountPerBlock,
+        config.bChannelShuffler,
+        config.pointwise1ChannelCountRate,
+        config.strAvgMaxConv, config.depthwiseFilterHeight, nextBlockDepthwiseChannelMultiplier, config.bBias, config.strActivationName,
         nextKeepInputTensor
       );
 
@@ -118,23 +153,23 @@ class Base {
     let block0 = this.blocks[ 0 ];
 
     // e.g. "C24_24__DConv_101x101_DBias_RELU__PConv_PBias_RELU__Block_1__Step_1"
-    this.structure = `C${sourceChannelCount}_${this.channelCountBlock0}`
+    this.structure = `C${config.sourceChannelCount}_${config.channelCountBlock0}`
 
-      + `${ ( block0.step0.bPointwise1 ) ? "__PConv1(x" + pointwise1ChannelCountRate + ")" : "" }`
+      + `${ ( block0.step0.bPointwise1 ) ? "__PConv1(x" + config.pointwise1ChannelCountRate + ")" : "" }`
       + `${ ( block0.step0.bPointwise1 && block0.step0.bPointwise1Bias ) ? ( "_PBias1" ) : "" }`
       + `${ ( block0.step0.bPointwise1 && block0.step0.pointwise1ActivationFunction ) ? ( "_" + block0.step0.pointwise1ActivationName ) : "" }`
 
-      + `${ ( block0.step0.bDepthwise ) ? `__D${strAvgMaxConv}_${depthwiseFilterHeight}x${depthwiseFilterHeight}` : "" }`
+      + `${ ( block0.step0.bDepthwise ) ? `__D${config.strAvgMaxConv}_${config.depthwiseFilterHeight}x${config.depthwiseFilterHeight}` : "" }`
       + `${ ( block0.step0.bDepthwise && block0.step0.bDepthwiseBias ) ? ( "_DBias" ) : "" }`
-      + `${ ( block0.step0.bDepthwise && block0.step0.depthwiseActivationFunction ) ? ( "_" + strActivationName ) : "" }`
+      + `${ ( block0.step0.bDepthwise && block0.step0.depthwiseActivationFunction ) ? ( "_" + config.strActivationName ) : "" }`
 
       + `${ ( block0.step0.bPointwise2 ) ? "__PConv2" : "" }`
       + `${ ( block0.step0.bPointwise2 && block0.step0.bPointwise2Bias ) ? ( "_PBias2" ) : "" }`
       + `${ ( block0.step0.bPointwise2 && block0.step0.pointwise2ActivationFunction ) ? ( "_" + block0.step0.pointwise2ActivationName ) : "" }`
 
       + `__Block_${this.blockCount}`
-      + `__Step_${stepCountPerBlock}`
-      + `${ ( bChannelShuffler ) ? "__Shuffle" : ( ( stepCountPerBlock > 0 ) ? "__AddInput" : "" ) }`
+      + `__Step_${config.stepCountPerBlock}`
+      + `${ ( config.bChannelShuffler ) ? "__Shuffle" : ( ( config.stepCountPerBlock > 0 ) ? "__AddInput" : "" ) }`
       //+ `${ ( bKeepInputTensor ) ? "__KeepInput" : "" }`
     ;
 
