8503ffe2357910584f07b27602430befdf05681d
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-07-17 11:26:37 +0800

    Update WorkerProxy.js

--- CNN/Neural/WorkerProxy.js
+++ CNN/Neural/WorkerProxy.js
@@ -3,7 +3,7 @@
  *
  */
 
-//import * as NeuralNetwork from "./NeuralNetwork.js";
+//import * as Net from "./Net.js";
 
 export { Base };
 
@@ -24,10 +24,10 @@ class Base {
 
   /**
    *
-   * @param {number} thisWorkerId
+   * @param {number} workerId
    *   This worker's id. The id of the first worker should be 0.
    *
-   * @param {NeuralNetwork.Config} neuralNetworkConfig
+   * @param {Net.Config} neuralNetConfig
    *   The configuration of the neural network which will be created by this web worker.
    *
    * @param {number} totalWorkerCount
@@ -36,83 +36,33 @@ class Base {
    * @param {string} weightsURL
    *   The URL of neural network weights. Every worker will load weights from the URL to initialize one neural network.
    */
-  init__( thisWorkerId, neuralNetworkConfig, totalWorkerCount, weightsURL ) {
+  init( workerId, neuralNetConfig, totalWorkerCount, weightsURL ) {
 //??? in cascade ?
 
-    this.workerId = thisWorkerId;
-    this.neuralNetworkConfig = neuralNetworkConfig;
+    this.workerId = workerId;
+    this.neuralNetConfig = neuralNetConfig;
     this.weightsURL = weightsURL;
 
 //     // Assume the "BlocksWorker.js" is the file name of this module file.
 //     let workerURL = new URL( "BlocksWorker.js", import.meta.url );
 
-    this.workerURL = import.meta.url;        // Assume this file is both an importable module and a main javascript file of web worker.
+//    this.workerURL = import.meta.url;        // Assume this file is both an importable module and a main javascript file of web worker.
+
+    // Assume the main (i.e. body) javascript file of neural network web worker is a sibling file (i.e. inside the same folder) of this module file.
+    this.workerURL = new URL( import.meta.url, "WorkerBody.js" );
+
     this.workerOptions = { type: "module" }; // So that the worker script could use import statement.
     this.worker = new Worker( this.workerURL, this.workerOptions );
 
     // Initialize this worker.
     let message = {
       command: "init",
-      workerId: thisWorkerId,
-      neuralNetworkConfig: neuralNetworkConfig,
+      workerId: workerId,
+      neuralNetConfig: neuralNetConfig,
       totalWorkerCount: totalWorkerCount,
       weightsURL: weightsURL
     };
     this.worker.postMessage( message );
-  }
-
-//!!!???
-  /**
-   *
-   * @param {NeuralNetwork.Config} neuralNetworkConfig
-   *   The configuration of the neural network which will be created by this web worker.
-   *
-   * @param {number} totalWorkerCount
-   *   There are how many workers should be created in chain.
-   *
-   * @param {number} remainedWorkerCount
-   *   There are how many workers should be created in chain.
-   *
-   * @param {string} weightsURL
-   *   The URL of neural network weights. Every worker will load weights from the URL to initialize one neural network.
-   *
-   * @param {number} workerId
-   *   This worker's id. The id of the first worker should be 0. If ( workerId < ( totalWorkerCount - 1 ) ), the next new worker will be created.
-   */
-  init_and_create_next( neuralNetworkConfig, totalWorkerCount, remainedWorkerCount, weightsURL, workerId ) {
-//??? in cascade ?
-    this.disposeWorkers();
-
-    this.neuralNetworkConfig = neuralNetworkConfig;
-    this.totalWorkerCount = totalWorkerCount;
-    this.weightsURL = weightsURL;
-    this.workerId = workerId || 0;
-
-//    let remainedWorkerCount = totalWorkerCount - workerId;
-    if ( remainedWorkerCount <= 0 )
-      return;  // Done. All workers in the cascade chain are created.
-
-//     // Assume the "BlocksWorker.js" is the file name of this module file.
-//     let workerURL = new URL( "BlocksWorker.js", import.meta.url );
-
-    this.workerURL = import.meta.url;        // Assume this file is both an importable module and a main javascript file of web worker.
-    this.workerOptions = { type: "module" }; // So that the worker script could use import statement.
-    this.worker = new Worker( this.workerURL, this.workerOptions );
-
-    // Create this worker in the cascade chain.
-    let message = {
-      command: "createNextWorker",
-      neuralNetworkConfig: neuralNetworkConfig,
-      totalWorkerCount: totalWorkerCount,
-      weightsURL: weightsURL,
-      workerId: workerId //( workerId + 1 )
-    };
-    this.worker.postMessage( message );
-
-//!!!???
-    remainedWorkerCount -= 1; // Because a new worker is just created, the remained count reduces one.
-    if ( remainedWorkerCount <= 0 )
-      return;  // Done. All workers in the cascade chain are created.
   }
 
   /**
@@ -174,38 +124,4 @@ class Base {
   apply() {
   }
     
-}
-
-
-
-if ( globalThis.document ) {
-  // In main document context (Not in worker context). Do nothing.
-
-} else {
-  // In worker context. Create neural network. Register message handler.
-
-  globalThis.workerProxy = new Proxy();
-
-  globalThis.onmessage = function( e ) {
-    let message = e.data;
-
-    switch ( message.command ) {
-      case "init": //{ command: "init", workerId, neuralNetworkConfig, totalWorkerCount, weightsURL };
-        globalThis.workerProxy.init( message.workerId, message.neuralNetworkConfig, message.totalWorkerCount, message.weightsURL );
-        break;
-
-      case "createNextWorker": //{ command: "createNextWorker", neuralNetworkConfig, remainedWorkerCount, weightsURL, workerId };
-        globalThis.workerProxy.init_and_create_next( message.neuralNetworkConfig, message.remainedWorkerCount, message.weightsURL, message.workerId );
-        break;
-
-      case "disposeWorker": //{ command: "disposeWorker" };
-        globalThis.workerProxy.disposeWorker();
-        break;
-
-      case "apply": //{ command: "apply" };
-        globalThis.workerProxy.apply(  );
-        break;
-
-    }
-  }
 }
