2d37d33f8d2bf808dc907e775ccbb1f3c5f7079d
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-07-17 12:01:02 +0800

    Update WorkerBody.js

--- CNN/Neural/WorkerBody.js
+++ CNN/Neural/WorkerBody.js
@@ -7,7 +7,8 @@ import * as Net from "./Net.js";
 import * as WorkerProxy from "./WorkerProxy.js";
 
 /**
- * A wrapper of worker for
+ * The implementation of a neural network web worker.
+ *
  * Many workers cascade in chain. Every worker handles one neural network. When apply() is called, the input (usually a large memory block)
  * will be transffered to the 1st worker to start computing, and then transffered to the 2nd worker to start computing, ... etc.
  *
@@ -22,9 +23,11 @@ import * as WorkerProxy from "./WorkerProxy.js";
 class WorkerBody {
 
   /**
+   * If ( ( totalWorkerCount - workerId ) > 0 ), this method will create a neural network.
+   * If ( ( totalWorkerCount - workerId ) > 1 ), this method will create the next neural network web worker, too.
    *
    * @param {number} workerId
-   *   This worker's id. The id of the first worker should be 0.
+   *   A non-negative integer represents this worker's id. The id of the first worker should be 0.
    *
    * @param {Net.Config} neuralNetConfig
    *   The configuration of the neural network which will be created by this web worker.
@@ -36,16 +39,31 @@ class WorkerBody {
    *   The URL of neural network weights. Every worker will load weights from the URL to initialize one neural network.
    */
   init( workerId, neuralNetConfig, totalWorkerCount, weightsURL ) {
-//??? in cascade ?
+
+    workerId = workerId | 0;
+    if ( workerId < 0 )
+      workerId = 0;
 
     this.workerId = workerId;
     this.neuralNetConfig = neuralNetConfig;
     this.weightsURL = weightsURL;
 
-    let bKeepInputTensor = ???;
+    let remainedWorkerCount = totalWorkerCount - workerId; // How many web worker should be created.
+    if ( remainedWorkerCount <= 0 )
+      return; // Should not happen. If happened, just return without creating any neural network.
+
+    let bKeepInputTensor = false; // Because every web worker will copy the input, there is not necessary to keep input.
 
     this.neuralNet = new Net();
     this.neuralNet.init( neuralNetConfig, bKeepInputTensor );
+
+    remainedWorkerCount -= 1; // "-1" for a neural network is created.
+    if ( remainedWorkerCount <= 0 )
+      return; // All neural network web worker are created.
+
+    let nextWorkerId = workerId + 1;
+    let nextWorkerProxy = this.nextWorkerProxy = new WorkerProxy.Base();
+    nextWorkerProxy.init( nextWorkerId, neuralNetConfig, totalWorkerCount, weightsURL ); // Create the next web worker in cascade chain.
   }
 
   disposeWorker() {
