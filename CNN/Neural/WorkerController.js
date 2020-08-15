bcd8f36cf5cb604fa69b12ba3acb740c37d62c11
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-08-15 17:14:11 +0800

    Update WorkerController.js
    Using WorkerProxy.js

--- CNN/Neural/WorkerController.js
+++ CNN/Neural/WorkerController.js
@@ -1,81 +1,14 @@
 /**
- * @file This file is an importable module to handle neural (web) worker body.
+ * @file This file is an importable module to handle neural (web) worker body by worker proxy.
  *
  */
 
-import * as ValueMax from "../ValueMax.js";
+import * as WorkerProxy from "./WorkerProxy.js";
+//import * as ValueMax from "../ValueMax.js";
 //import * as Net from "./Net.js";
 
 export { Base };
 
-
-/**
- * Aggregate initialization progress of one web worker.
- * Including: download tensorflow and neural network library, download neural network weights, parse neural network weights.
- */
-class WorkerInitProgress extends ValueMax.Percentage.Aggregate {
-  constructor() {
-    let children = [
-      new ValueMax.Percentage.Concrete(), // Increased when downloading tensorflow and neural network library.
-      new ValueMax.Percentage.Concrete(), // Increased when downloading neural network weights.
-      new ValueMax.Percentage.Concrete(), // Increased when parsing neural network weights.
-    ];
-
-    super( children );
-    [ this.libraryDownload, this.weightsDownload, this.weightsParse ] = children;
-  }
-}
-
-/**
- * Aggregate initialization progress of two workers.
- */
-class WorkersInitProgress extends ValueMax.Percentage.Aggregate {
-  constructor() {
-    let children = [
-      new WorkerInitProgress(), // Increased when web worker 1 initializing.
-      new WorkerInitProgress(), // Increased when web worker 2 initializing.
-    ];
-
-    super( children );
-    [ this.worker1, this.worker2 ] = children;
-  }
-}
-
-/**
- * Hold a processing's id, promise, resolve (fulfilling function object), reject (rejecting function object).
- *
- * @member {number}   workerId     The array index of the worker owns this processing.
- * @member {number}   processingId The id of the processing.
- * @member {Promise}  promise      The pending promise for the processing.
- * @member {function} resolve      The fulfilling function object of the pending promise for the processing.
- * @member {function} reject       The rejecting function object of the pending promise for the processing.
- */
-class WorkerPendingPromiseInfo {
-
-  constructor( workerId, processingId ) {
-    this.workerId = workerId;
-    this.processingId = processingId;
-  }
-
-}
-
-/**
- * Hold the worker and its related promise map.
- *
- * @member {number} workerId              The array index of this worker proxy.
- * @member {Worker} worker                The worker.
- * @member {Map}    pendingPromiseInfoMap The map for promise of the unhandled processing.
- */
-class WorkerProxy {
-
-  constructor( workerId ) {
-    this.workerId = workerId;
-
-    // Every worker has a result promise map. The key of the map is processing id. The value of the map is a WorkerPendingPromiseInfo.
-    this.pendingPromiseInfoMap = new Map();
-  }
-
-}
 
 /**
  * The wrapper of a neural network web worker for handling easily.
@@ -150,7 +83,7 @@ class Base {
 
     this.workerProxyArray = new Array( totalWorkerCount );
     for ( let i = 0; i < totalWorkerCount; ++i ) {
-      let workerProxy = new WorkerProxy( i );
+      let workerProxy = new WorkerProxy.Base( i );
       this.workerProxyArray[ i ] = workerProxy;
 
       let worker = workerProxy.worker = new Worker( this.workerURL, this.workerOptions );
@@ -197,7 +130,7 @@ class Base {
 
       workerProxy.worker.postMessage( message, [ message.sourceImageData.data.buffer ] );
 
-      let pendingPromiseInfo = WorkerPendingPromiseInfo( i, processingId );
+      let pendingPromiseInfo = WorkerProxy.PendingPromiseInfo( i, processingId );
 
       pendingPromiseInfo.promise = this.resultPromiseArray[ i ] = new Promise( ( resolve, reject ) => {
         pendingPromiseInfo.resolve = resolve;
