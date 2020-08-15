a17903be707a35ac8ff9bbc238dc6a51b0631bfe
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-08-15 17:04:53 +0800

    Update WorkerController.js
    WorkerInitProgress and WorkersInitProgress

--- CNN/Neural/WorkerController.js
+++ CNN/Neural/WorkerController.js
@@ -3,10 +3,43 @@
  *
  */
 
+import * as ValueMax from "../ValueMax.js";
 //import * as Net from "./Net.js";
 
 export { Base };
 
+
+/**
+ * Aggregate initialization progress of one web worker.
+ * Including: download tensorflow and neural network library, download neural network weights, parse neural network weights.
+ */
+class WorkerInitProgress extends ValueMax.Percentage.Aggregate {
+  constructor() {
+    let children = [
+      new ValueMax.Percentage.Concrete(), // Increased when downloading tensorflow and neural network library.
+      new ValueMax.Percentage.Concrete(), // Increased when downloading neural network weights.
+      new ValueMax.Percentage.Concrete(), // Increased when parsing neural network weights.
+    ];
+
+    super( children );
+    [ this.libraryDownload, this.weightsDownload, this.weightsParse ] = children;
+  }
+}
+
+/**
+ * Aggregate initialization progress of two workers.
+ */
+class WorkersInitProgress extends ValueMax.Percentage.Aggregate {
+  constructor() {
+    let children = [
+      new WorkerInitProgress(), // Increased when web worker 1 initializing.
+      new WorkerInitProgress(), // Increased when web worker 2 initializing.
+    ];
+
+    super( children );
+    [ this.worker1, this.worker2 ] = children;
+  }
+}
 
 /**
  * Hold a processing's id, promise, resolve (fulfilling function object), reject (rejecting function object).
