af723e87f2edf5f2e91be7d8e8984ca1ee86c17f
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-08-17 16:32:51 +0800

    Update WorkerBody.js

--- CNN/Neural/WorkerBody.js
+++ CNN/Neural/WorkerBody.js
@@ -12,18 +12,6 @@
 /**
  * The implementation of a neural network web worker.
  *
-//!!! ...unfinished...
-
- * Many workers cascade in chain. Every worker handles one neural network. When apply() is called, the input (usually a large memory block)
- * will be transffered to the 1st worker to start computing, and then transffered to the 2nd worker to start computing, ... etc.
- *
- * When passing large data by Worker.postMessage(), it is preferred by transferring (not by copying). If the large data wants to be transferred
- * to many workers, the only possible way is to transferring them serially. This is why the workers are arranged in cascade chain.
- *
- * However, serially transferring hurts the performance. Workers are better to compute parallelly. So every worker should transfer data to next
- * worker as soon as possible. When they get the first part of calculation result (rather than wait for all calculation done), they should
- * transfer the input data to the next worker immediately.
- *
  */
 class WorkerBody {
 
