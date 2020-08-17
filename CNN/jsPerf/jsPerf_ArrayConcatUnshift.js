80aca5ede90f1aabdd443cb291d92a1e95e1de0f
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-08-17 10:32:27 +0800

    Create jsPerf_ArrayConcatUnshift.js

---
+++ CNN/jsPerf/jsPerf_ArrayConcatUnshift.js
@@ -0,0 +1,49 @@
+
+/**
+ * Test performancce of array concat and unshift.
+ *
+ * @see {@link https://www.measurethat.net/Benchmarks/Show/9224/0/colorfulcakechen-array-concat-unshift}
+ */
+
+/**
+ * A test set.
+ */
+class TestSet {
+
+  /**
+   */
+  constructor( arrayListLength ) {
+
+    this.arrayListLength = arrayListLength;
+    this.arrayList = new Array( arrayListLength );
+
+    for ( let i = 0; i < this.arrayListLength; ++i ) {
+      this.arrayList[ i ] = new Array( 1 );
+      this.arrayList[ i ][ 0 ] = i;
+    }
+  }
+
+  test_ArrayConcat() {
+    let result = [];
+    for ( let i = 0; i < this.arrayListLength; ++i ) {
+      result = result.concat( this.arrayList[ i ] );
+    }
+  }
+
+  test_ArrayUnshift() {
+    let result = [];
+    for ( let i = ( this.arrayListLength - 1 ); i >= 0; --i ) {
+      result = result.unsihft( this.arrayList[ i ] );
+    }
+  }
+
+  // Testing whether the results of different implementation are the same. Also, pre-compile the codes.
+  async testCaseLoader() {
+    this.test_ArrayConcat();
+    this.test_ArrayUnshift();
+  }
+ 
+}
+
+
+globalThis.testSet = new TestSet( 10 );
