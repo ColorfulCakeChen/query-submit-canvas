084e5b0c1624954662b37b260cce3cd068b3f0fa
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-08-21 13:43:14 +0800

    Update WorkerProxy.js

--- CNN/Neural/WorkerProxy.js
+++ CNN/Neural/WorkerProxy.js
@@ -178,7 +178,7 @@ class Base {
     this.processRelayPromisesMap.set( processingId, processRelayPromises );
     
     // Transfer (not copy) the source image data to this (worker proxy owned) web worker.
-    let message = { command: "processImageData", processingId: processingId, sourceImageData: sourceImageData };
+    let message = { command: "imageData_transferBack_processTensor", processingId: processingId, sourceImageData: sourceImageData };
     this.worker.postMessage( message, [ message.sourceImageData.data.buffer ] );
     // Now, sourceImageData.data.buffer has become invalid because it is transferred (not copied) to web worker.
 
@@ -210,7 +210,7 @@ class Base {
     this.processRelayPromisesMap.set( processingId, processRelayPromises );
     
     // Transfer (not copy) the source typed-array to this (worker proxy owned) web worker.
-    let message = { command: "processTypedArray", processingId: processingId, sourceTypedArray: sourceTypedArray };
+    let message = { command: "typedArray_TransferBack_processTensor", processingId: processingId, sourceTypedArray: sourceTypedArray };
     this.worker.postMessage( message, [ message.sourceTypedArray.buffer ] );
     // Now, sourceTypedArray.buffer has become invalid because it is transferred (not copied) to web worker.
 
