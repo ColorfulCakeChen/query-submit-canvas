b204efd22ef8bbcf3c231ac9325972b74907e594
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-09-01 15:51:00 +0800

    Create DifferentialEvolutionLoader.js

---
+++ CNN/Neural/DifferentialEvolutionLoader.js
@@ -0,0 +1,26 @@
+import * as NetProgress from "./NetProgress.js";
+
+export { Base };
+
+/**
+ * Download differential evolution summary and individual entity from network (a publish html of Google Sheets).
+ *
+ *
+ *
+ */
+class Base {
+
+//  constructor() {
+//  }
+
+  init() {
+  }
+
+  downloadSummary( progress ) {
+  }
+
+  downloadEntity( progress ) {
+    // Parent and offspring.
+  }
+
+}
