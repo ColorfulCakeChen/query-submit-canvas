a12bd2d23d75ebecef877c6aac1dd06cb6671b31
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-09-04 16:50:40 +0800

    Update gid_Versus.js

--- CNN/Neural/DifferentialEvolution/gid_Versus.js
+++ CNN/Neural/DifferentialEvolution/gid_Versus.js
@@ -22,34 +22,68 @@ class Base {
 //   }
 
   /**
-   * Create and return a gid_Versus by parsing the specified string.
+   * Set this object by string array.
    *
-   * @param {string} gid_Versus_string
-   *   The string contains gid, versus ids and win counts
-   * (e.g. gid|EntityNo_ParentGenerationNo_OffspringGenerationNo:WinCount|EntityNo_ParentGenerationNo_OffspringGenerationNo:WinCount|...).
+   * @param {string[]} string_array
+   *   The array string contains gid and versus.
+   * (e.g. [ "gid", "EntityNo_ParentGenerationNo_OffspringGenerationNo:WinCount", "EntityNo_ParentGenerationNo_OffspringGenerationNo:WinCount", ...] ).
+   *
+   * @param {string} gidString
+   *   If null, string_array[ 0 ] will be viewed as gid and string_array[ 1 ] (and after) will be viewed as versus. Otherwise, gidString is gid
+   * and all elements of string_array[] will be viewed as versus.
    */
-  setByParse( gid_Versus_string ) {
+  set_ByStringArray( string_array, gidString ) {
 
     this.dispose();
 
-    // Split the text of a td tag.
-    //
-    //   gid|EntityNo_ParentGenerationNo_OffspringGenerationNo:WinCount|EntityNo_ParentGenerationNo_OffspringGenerationNo:WinCount|...
-    //
-    // They are separated by vertical bar (|).
-    let string_array = gid_Versus_string.split( Base.gid_Versus_SplittingRegExp );
     if ( string_array.length < 1 )
       return; // Nothing in the string.
 
-    // 1. Got gid. (Element 0 is gid.)
-    this.gidString = string_array[ 0 ];
+    let firstVersusIndex; // Which array element is the first versus.
+
+    // 1. Got gid.
+    if ( gidString ) {
+      this.gidString = gidString;         // Specific gid. (i.e. Element 0 is not gid.)
+      firstVersusIndex = 0;               // Element 0 (and after) is versus.
+    } else {
+      this.gidString = string_array[ 0 ]; // Element 0 is gid.
+      firstVersusIndex = 1;               // Element 1 (and after) is versus.
+    }
+
     this.gid = Number.parseInt( this.gidString, 10 );
+    this.VersusId_WinCount_Array = new Array( string_array.length - firstVersusIndex );
 
     // 2. Parse all other elements as versus.
-    this.VersusId_WinCount_Array = new Array( string_array.length - 1 ); // All other elements are versus ids (with win counts).
-    for ( let i = 1; i < string_array.length; ++i ) {
-      this.VersusId_WinCount_Array[ i - 1 ] = VersusId_WinCount.Base.createByParse( string_array[ i ] );
+    for ( let i = firstVersusIndex; i < string_array.length; ++i ) {
+      this.VersusId_WinCount_Array[ i - firstVersusIndex ] = VersusId_WinCount.Base.createByParse( string_array[ i ] );
     }
+  }
+
+  /**
+   * Set this object by parsing the specified string.
+   *
+   * @param {string} gid_Versus_string
+   *   The string contains gid, versus ids and win counts.
+   * (e.g. gid|EntityNo_ParentGenerationNo_OffspringGenerationNo:WinCount|EntityNo_ParentGenerationNo_OffspringGenerationNo:WinCount|...).
+   * The string can only contain versus ids and win counts (without gid in prefix).
+   * (e.g. EntityNo_ParentGenerationNo_OffspringGenerationNo:WinCount|EntityNo_ParentGenerationNo_OffspringGenerationNo:WinCount|...).
+   *
+   * @param {string} gidString
+   *   If null, the first part of gid_Versus_string will be viewed as gid. Otherwise, all gid_Versus_string will be viewed as versus.
+   */
+  set_ByParse( gid_Versus_string, gidString ) {
+
+    // Split the text (if gidString is null):
+    //
+    //   gid|EntityNo_ParentGenerationNo_OffspringGenerationNo:WinCount|EntityNo_ParentGenerationNo_OffspringGenerationNo:WinCount|...
+    //
+    // Or (if gidString is not null),
+    //
+    //   EntityNo_ParentGenerationNo_OffspringGenerationNo:WinCount|EntityNo_ParentGenerationNo_OffspringGenerationNo:WinCount|...
+    //
+    // They are separated by vertical bar (|).
+    let string_array = gid_Versus_string.split( Base.gid_Versus_SplittingRegExp );
+    this.set_ByStringArray( string_array, gidString );
   }
 
   dispose() {
