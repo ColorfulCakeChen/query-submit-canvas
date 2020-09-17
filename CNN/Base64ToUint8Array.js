c39c586ebb2c669da85dc4480e6e7ecc2d37e074
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-09-17 12:10:37 +0800

    Update Base64ToUint8Array.js

--- CNN/Base64ToUint8Array.js
+++ CNN/Base64ToUint8Array.js
@@ -36,7 +36,7 @@ let table_base64_Uint8_to_index = new Array(256); // Faster than using Uint8Arra
  *   Skip how many lines in the source before decoding.
  *
  * @param {ValueMax.Percentage.Aggregate} progressToYield
- *   Return this when every time yield. Usually, this is the container of the progressToAdvance.
+ *   Return this when every time yield. Usually, this is the root container of the progressToAdvance.
  *
  * @param {ValueMax.Percentage.Concrete}  progressToAdvance
  *   Increase this when every time advanced. It will be initialized to zero when decoder starting.
