d837b3bf3af0f2f2a99e523aeb9b4fa43bc83164
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-12-07 11:24:34 +0800

    Update Downloader.js

--- CNN/Neural/DifferentialEvolution/Downloader.js
+++ CNN/Neural/DifferentialEvolution/Downloader.js
@@ -2,5 +2,7 @@
 /**
  * https://docs.google.com/spreadsheets/d/18YyEoy-OfSkODfw8wqBRApSrRnBTZpjRpRiwIKy8a0M/gviz/tq?range=AH59:AJ98&tqx=out:csv
  *
+ * https://docs.google.com/spreadsheets/d/18YyEoy-OfSkODfw8wqBRApSrRnBTZpjRpRiwIKy8a0M/gviz/tq?gid=816353147&range=AH59:AJ98&headers=0&tqx=out:csv
+ *
  */
 
