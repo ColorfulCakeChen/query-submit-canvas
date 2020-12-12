5823ce4fc39b0b42b00e6a1c792abe05dbf1a348
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-12-12 15:59:18 +0800

    Create GSheetAPIv4.js

---
+++ CNN/util/GSheet/GSheetAPIv4.js
@@ -0,0 +1,91 @@
+export { UrlComposer };
+
+/**
+ * Compose a URL for downloading cells data (as JSON) from a Google Sheets by using Google Sheets API v4.
+ * The target spreadsheet should be shared by either "Public on the web" or "Anyone with the link".
+ *
+ *
+ * The follwoings are some composed examples:
+ *
+ * @example
+ * https://sheets.googleapis.com/v4/spreadsheets/18YyEoy-OfSkODfw8wqBRApSrRnBTZpjRpRiwIKy8a0M/values/A:A?key=AIzaSyDQpdX3Z7297fkZ7M_jWdq7zjv_IIxpArU
+ * https://sheets.googleapis.com/v4/spreadsheets/18YyEoy-OfSkODfw8wqBRApSrRnBTZpjRpRiwIKy8a0M/values/A:A?majorDimension=COLUMNS&key=AIzaSyDQpdX3Z7297fkZ7M_jWdq7zjv_IIxpArU
+ * https://sheets.googleapis.com/v4/spreadsheets/18YyEoy-OfSkODfw8wqBRApSrRnBTZpjRpRiwIKy8a0M/values/Evolution.Param!A:B?key=AIzaSyDQpdX3Z7297fkZ7M_jWdq7zjv_IIxpArU
+ *
+ */
+class UrlComposer {
+
+  /**
+   * If no sheet name in the range's A1 notation, the first (most left) visible sheet inside the spreadsheet will be used.
+   *
+   * @param {string} spreadsheetId
+   *   The component after the "https://docs.google.com/spreadsheets/d/".
+   *
+   * @param {string} range
+   *   The cells' A1 notation of the cells. It describes the (name and) range of the sheet inside the spreadsheet.
+   *   - "A1" refers to one cell of the first (most left) visible sheet.
+   *   - "B2:C5" refers to cells of a rectangle of the first (most left) visible sheet.
+   *   - "Books!D8:D" refers to the column D of sheet named "Books" from rows 8 to the last rows.
+   *   - "'Name has space'!7:10" refers to the rows 7 to 10 of sheet named "Name has space".
+   *
+   * @param {string} APIKey
+   *   The API key string for accessing the spreadsheet.
+   *
+   * @see {@link https://developers.google.com/sheets/api/guides/concepts}
+   * @see {@link https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/get}
+   */
+  constructor( spreadsheetId, range, APIKey ) {
+    this.spreadsheetId = spreadsheetId;
+    this.range = range;
+    this.APIKey = APIKey;
+  }
+
+  /**
+   * @param {string} majorDimension
+   *   The major dimension of the arrangement for the downloaded json data. It could be "ROWS" or "COLUMNS". If null, the same as "ROWS".
+   *
+   * @return {string} The url for downloading the target as json format.
+   */
+  getUrl_forRead( majorDimension = "COLUMNS" ) {
+    let url = `${UrlComposer.spreadsheetUrlPrefix}/${encodeURIComponent(this.spreadsheetId)}/values/${
+      encodeURIComponent(this.range)}?${
+      ( majorDimension != null ) ? `majorDimension=${encodeURIComponent(majorDimension)}&` : "" }key=${encodeURIComponent(this.APIKey)}`
+      ;
+
+    return url;
+  }
+
+  /**
+   * If ( valueRange.majorDimension == "ROWS" ), collects valueRange.values[ n ][ m ] into a two dimension (column-major) array.
+   * The outer array has m inner sub-arrays (corresponding to m columns). Every inner sub-array has n elements (corresponding to n rows).
+   *
+   * @param {object} valueRange
+   *   The spreadsheets.values resource object of Google Sheets API v4.
+   *
+   * @return {array[]}
+   *   Return a two dimension (column-major) array. Return null if failed.
+   *
+   * @see {@link https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values}
+   */
+  static ValueRange_To_ColumnMajorArray( valueRange ) {
+    if ( !valueRange )
+      return null;
+
+    if ( "COLUMNS" == valueRange.majorDimension )
+      return valueRange.values;  // Already column major. Return the two dimension array directly.
+
+    let columnArray = new Array( valueRange.values.length );
+    for ( let columnNo = 0; columnNo < columnArray.length; ++columnNo ) {
+      let rowArray = columnArray[ columnNo ] = new Array( valueRange.values[ .rows.length );
+      for ( let rowNo = 0; rowNo < rowArray.length; ++rowNo ) {
+        rowArray[ rowNo ] = gvizDataTable.rows[ rowNo ].c[ columnNo ].v; // Always value (.v), ignore formatted value string (.f).
+      }
+    }
+
+    return columnArray;
+  }
+
+}
+
+/** The url prefix of Google Sheets API v4. */
+UrlComposer.spreadsheetUrlPrefix = "https://sheets.googleapis.com/v4/spreadsheets";
