99a7f3364a0f7fd65b3bdf0abbd5111815fdfc22
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-12-20 10:51:47 +0800

    Create jsPerf_MeasureThatAsync.js

---
+++ CNN/jsPerf/jsPerf_MeasureThatAsync.js
@@ -0,0 +1,11 @@
+
+export { init };
+
+//import * as ChannelShuffler from "../Conv/ChannelShuffler.js";
+
+/**
+ * How to test async procedure in MeasureThat?
+ *
+???
+ * @see {@link https://www.measurethat.net/Benchmarks/Show/10945/8/colorfulcakechen-measure-that-async}
+ */
