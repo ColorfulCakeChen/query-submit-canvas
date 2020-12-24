4da83754a7c6808f1c1a25c2974f8fa648cbec69
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-12-24 16:14:10 +0800

    Update jsPerf_CNNChannelShuffle.js

--- CNN/jsPerf/jsPerf_CNNChannelShuffle.js
+++ CNN/jsPerf/jsPerf_CNNChannelShuffle.js
@@ -6,10 +6,10 @@ import * as TensorTools from "../util/TensorTools.js";
 /**
  * Test different channel shuffle implementation for CNN ShuffleNet.
  *
- * @see {@link https://www.measurethat.net/Benchmarks/Show/10945/26/colorfulcakechen-cnn-channel-shuffler-5b48aa17030db00c8}
- * @see {@link https://www.measurethat.net/Benchmarks/Show/10988/15/colorfulcakechen-cnn-channel-shuffler-concatreshapetran}
- * @see {@link https://www.measurethat.net/Benchmarks/Show/10978/8/colorfulcakechen-cnn-channel-shuffler-concatgatherunsor}
- * @see {@link https://www.measurethat.net/Benchmarks/Show/10973/15/colorfulcakechen-cnn-channel-shuffler-concatpointwiseco}
+ * @see {@link https://www.measurethat.net/Benchmarks/Show/10945/27/colorfulcakechen-cnn-channel-shuffler-adf50b416a23b884a}
+ * @see {@link https://www.measurethat.net/Benchmarks/Show/10988/18/colorfulcakechen-cnn-channel-shuffler-concatreshapetran}
+ * @see {@link https://www.measurethat.net/Benchmarks/Show/10978/9/colorfulcakechen-cnn-channel-shuffler-concatgatherunsor}
+ * @see {@link https://www.measurethat.net/Benchmarks/Show/10973/16/colorfulcakechen-cnn-channel-shuffler-concatpointwiseco}
  */
 
 /**
