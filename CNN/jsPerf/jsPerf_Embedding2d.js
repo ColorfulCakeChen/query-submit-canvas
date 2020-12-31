14886a483c90b3fb61da902726f3efe9c7df79e4
ColorfulCakeChen <colorfulcakechen@gmail.com>
2020-12-31 13:14:16 +0800

    Update jsPerf_Embedding2d.js

--- CNN/jsPerf/jsPerf_Embedding2d.js
+++ CNN/jsPerf/jsPerf_Embedding2d.js
@@ -65,8 +65,8 @@ class HeightWidthDepth {
       }
 
       for ( let i = this.weightsElementOffsetBegin; i < wieghtsArrayLength; ++i ) { // Make-up the embedding weight values.
-        //this.weightsFloat32Array[ i ] = ( i - this.weightsByteOffsetBegin );
-        this.weightsFloat32Array[ i ] = Math.random() * 10;
+        this.weightsFloat32Array[ i ] = ( i - this.weightsByteOffsetBegin );  // For debugging more easily.
+//        this.weightsFloat32Array[ i ] = Math.random() * 10;
       }
     }
   }
