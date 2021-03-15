9b949113b5fe131af75705629db8a60ce4e2bfde
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-03-15 12:43:34 +0800

    Update Unpacker.js

--- CNN/Unpacker.js
+++ CNN/Unpacker.js
@@ -1,6 +1,6 @@
-export { ParamDesc, ValueDesc, ValueRange, Weights };
+export { ValueDesc, ValueRange, ParamDesc, Weights };
 
-import * as ParamDesc from "./Unpacker/ParamDesc.js";
-import * as ValueDesc from "./Unpacker/ValueDesc.js";
 import * as ValueRange from "./Unpacker/ValueRange.js";
+import * as ValueDesc from "./Unpacker/ValueDesc.js";
+import * as ParamDesc from "./Unpacker/ParamDesc.js";
 import * as Weights from "./Unpacker/Weights.js";
