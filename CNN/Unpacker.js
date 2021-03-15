4a08c01feeefa4b57a561c53c09d2fa8f8db0a01
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-03-15 15:27:19 +0800

    Delete Unpacker.js

--- CNN/Unpacker.js
+++
@@ -1,6 +0,0 @@
-export { ValueDesc, ValueRange, ParamDesc, Weights };
-
-import * as ValueRange from "./Unpacker/ValueRange.js";
-import * as ValueDesc from "./Unpacker/ValueDesc.js";
-import * as ParamDesc from "./Unpacker/ParamDesc.js";
-import * as Weights from "./Unpacker/Weights.js";
