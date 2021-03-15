2497a6ff5bf177e67750f86e25e458a927bee074
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-03-15 11:21:02 +0800

    Create Unpacker.js

---
+++ CNN/Unpacker.js
@@ -0,0 +1,5 @@
+export { ParamDesc, ParamRange, Weights };
+
+import * as ParamDesc from "./Unpacker/ParamDesc.js";
+import * as ParamRange from "./Unpacker/ParamRange.js";
+import * as Weights from "./Unpacker/Weights.js";
