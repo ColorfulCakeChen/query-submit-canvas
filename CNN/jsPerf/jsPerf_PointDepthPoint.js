fe60bb549e08336c9e784346517f7f150a16caa4
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-03-18 14:04:03 +0800

    Update jsPerf_PointDepthPoint.js

--- CNN/jsPerf/jsPerf_PointDepthPoint.js
+++ CNN/jsPerf/jsPerf_PointDepthPoint.js
@@ -201,11 +201,11 @@ class HeightWidthDepth {
         [
           2.1,  1.1, PointDepthPoint.Params.pointwise1ActivationId.valueDesc.Ids.RELU + 0.1,
           2.1,  3.1, 3.1,  3.2, PointDepthPoint.Params.depthwiseActivationId.valueDesc.Ids.RELU + 0.2,
-            8,  5.3, PointDepthPoint.Params.pointwise2ActivationId.valueDesc.Ids.RELU + 0.3,   6.4 ], // paramsInArray
+            8,  5.3, PointDepthPoint.Params.pointwise2ActivationId.valueDesc.Ids.RELU6 + 0.3,   6.4 ], // paramsInArray
 
         [   2, true, PointDepthPoint.Params.pointwise1ActivationId.valueDesc.Ids.RELU,
             2,    3,   0, true, PointDepthPoint.Params.depthwiseActivationId.valueDesc.Ids.RELU,
-            8, true, PointDepthPoint.Params.pointwise1ActivationId.valueDesc.Ids.RELU,       false ], // paramsOutArray
+            8, true, PointDepthPoint.Params.pointwise1ActivationId.valueDesc.Ids.RELU6,       false ], // paramsOutArray
 
         // pointwise1FiltersArray
         [
