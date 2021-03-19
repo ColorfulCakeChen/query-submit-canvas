3b5eb9a5fc4ef9b47888b96bd31a5d3f6795a312
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-03-19 13:10:40 +0800

    Update jsPerf_PointDepthPoint.js

--- CNN/jsPerf/jsPerf_PointDepthPoint.js
+++ CNN/jsPerf/jsPerf_PointDepthPoint.js
@@ -82,17 +82,17 @@ class HeightWidthDepth {
         // depthwiseFiltersArray
         // (If value too large (out of float32 range), the result will strange. So, use smaller and negative value.)
         [
-           1, -1, -1, -1,
-           2,  2,  2,  2,
-          -3,  3, -3, -3,
+           1, -9, -5,  7,
+           2,  8,  4,  1,
+          -3,  7, -6,  9,
 
-           4, -4, -4,  4,
-          -5, -5, -5, -5,
-           6,  6,  6, -6,
+           4, -6,  8, -2,
+           5,  5, -7, -3,
+           6,  4,  9,  5,
 
-           7,  7, -7,  7,
-          -8, -8,  8, -8,
-          -9,  9, -9,  9,
+           7,  3, -3,  4,
+          -8,  2,  1, -8,
+          -9,  1, -2,  6,
         ],
 
         // depthwiseBiasesArray
