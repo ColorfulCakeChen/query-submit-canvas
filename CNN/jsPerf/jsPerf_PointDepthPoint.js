101bbc8bef436c1f621e4c54b31eafab363da942
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-03-19 14:26:49 +0800

    Update jsPerf_PointDepthPoint.js

--- CNN/jsPerf/jsPerf_PointDepthPoint.js
+++ CNN/jsPerf/jsPerf_PointDepthPoint.js
@@ -61,7 +61,7 @@ class HeightWidthDepth {
       new PointDepthPoint_Reference.TestCase(
         [
           2.1,  1.1, PointDepthPoint.Params.pointwise1ActivationId.valueDesc.Ids.NONE + 0.1,
-          2.1,  3.1, 4.1,  3.2, PointDepthPoint.Params.depthwiseActivationId.valueDesc.Ids.NONE + 0.2,
+          0.1,  3.1, 4.1,  3.2, PointDepthPoint.Params.depthwiseActivationId.valueDesc.Ids.NONE + 0.2,
           0.2,  5.3, PointDepthPoint.Params.pointwise2ActivationId.valueDesc.Ids.NONE + 0.3,  6.4 ], // paramsInArray
 
         [   2, true, PointDepthPoint.Params.pointwise1ActivationId.valueDesc.Ids.NONE,
@@ -84,21 +84,21 @@ class HeightWidthDepth {
         // depthwiseFiltersArray
         // (If value too large (out of float32 range), the result will strange. So, use smaller and negative value.)
         [
-           1, -9, -5,  7,
-           2,  8,  4,  1,
-          -3,  7, -6,  9,
-
-           4, -6,  8, -2,
-           5,  5, -7, -3,
-           6,  4,  9,  5,
-
-           7,  3, -3,  4,
-          -8,  2,  1, -8,
-          -9,  1, -2,  6,
+//            1, -9, -5,  7,
+//            2,  8,  4,  1,
+//           -3,  7, -6,  9,
+//
+//            4, -6,  8, -2,
+//            5,  5, -7, -3,
+//            6,  4,  9,  5,
+//
+//            7,  3, -3,  4,
+//           -8,  2,  1, -8,
+//           -9,  1, -2,  6,
         ],
 
         // depthwiseBiasesArray
-        [ 101, 102, 103, 104 ],
+        [], //[ 101, 102, 103, 104 ],
 
         // pointwise2FiltersArray
         // (Some negative so that the result will not too large (out of float32 range). Otherwise, the result will strange.)
