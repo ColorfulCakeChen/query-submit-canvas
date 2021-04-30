d564593f6533f9f19af9bf92ef7c500a96925717
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-04-30 16:25:36 +0800

    Update ReturnOrClone.js

--- CNN/Conv/ReturnOrClone.js
+++ CNN/Conv/ReturnOrClone.js
@@ -20,7 +20,7 @@ class Base {
   }
 
   /**
-   * Return a copy of input (as output). Used for ( bKeepInputTensor == true  ).
+   * Return a copy of input (as output). Used for ( bKeepInputTensor == true ).
    *
    * @param {tf.tensor} inputTensor
    *   A tensor data. This inputTensor will be kept (i.e. not disposed).
@@ -30,6 +30,26 @@ class Base {
    */
   static keep_input_return_copy( inputTensor ) {
     return inputTensor.clone();
+  }
+
+  /**
+   * Return a copy of input (as output). Used for ( bKeepInputTensor == true ).
+   *
+   * @param {tf.tensor[]} inputTensorsArray
+   *   An array of tensor data. The tensors of this inputTensorsArray will be kept (i.e. not disposed).
+   *
+   * @return {tf.tensor[]}
+   *   Return a new array contains copy of all input tensors (could be null). Throw exception if failed (e.g. inputTensorsArray is null,
+   * or out of GPU memory).
+   */
+  static keep_input_return_copy_array( inputTensorsArray ) {
+    let outputTensorsArray = new Array( inputTensorsArray.length );
+    for ( let i = 0; i < inputTensorsArray.length; ++i ) {
+      let inputTensor = inputTensorsArray[ i ];
+      if ( inputTensor )
+        outputTensorsArray[ i ] = inputTensor.clone();
+    }
+    return outputTensorsArray;
   }
 
 }
