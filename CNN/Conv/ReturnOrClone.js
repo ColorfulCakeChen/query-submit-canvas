1bf4ba86d63f92f389cd80ac27dc596d4e2b53bf
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-05-01 21:38:03 +0800

    Update ReturnOrClone.js

--- CNN/Conv/ReturnOrClone.js
+++ CNN/Conv/ReturnOrClone.js
@@ -20,6 +20,24 @@ class Base {
   }
 
   /**
+   * Return the input (as output) directly. Used for ( bKeepInputTensor == false ).
+   *
+   * @param {tf.tensor[]} inputTensorsArray
+   *   An array of tensor data. They should be viewed as already disposed by this method. However, in fact, they are put into
+   * outputTensorsArray directly. It can not be null. But it can contain null element.
+   *
+   * @param {tf.tensor[]} outputTensorsArray
+   *   An array used to put all the input tensors. It can not be null. But it may contain null element.
+   */
+  static return_input_directly_array( inputTensorsArray, outputTensorsArray ) {
+    outputTensorsArray.length = inputTensorsArray.length;
+    for ( let i = 0; i < inputTensorsArray.length; ++i ) {
+      outputTensorsArray[ i ] = inputTensorsArray[ i ];
+    }
+  }
+
+
+  /**
    * Return a copy of input (as output). Used for ( bKeepInputTensor == true ).
    *
    * @param {tf.tensor} inputTensor
@@ -33,23 +51,25 @@ class Base {
   }
 
   /**
-   * Return a copy of input (as output). Used for ( bKeepInputTensor == true ).
+   * Return a copy of input (as output). Used for ( bKeepInputTensor == true ). Throw exception if failed (e.g. inputTensorsArray is null,
+   * or out of GPU memory).
    *
    * @param {tf.tensor[]} inputTensorsArray
-   *   An array of tensor data. The tensors of this inputTensorsArray will be kept (i.e. not disposed).
+   *   An array of tensor data. The tensors of this inputTensorsArray will be kept (i.e. not disposed). It can not be null. But it can
+   * contain null element.
    *
-   * @return {tf.tensor[]}
-   *   Return a new array contains copy of all input tensors (could be null). Throw exception if failed (e.g. inputTensorsArray is null,
-   * or out of GPU memory).
+   * @param {tf.tensor[]} outputTensorsArray
+   *   An array used to put the copy of all input tensors. It can not be null. But it may contain null element.
    */
-  static keep_input_return_copy_array( inputTensorsArray ) {
-    let outputTensorsArray = new Array( inputTensorsArray.length );
+  static keep_input_return_copy_array( inputTensorsArray, outputTensorsArray ) {
+    outputTensorsArray.length = inputTensorsArray.length;
     for ( let i = 0; i < inputTensorsArray.length; ++i ) {
       let inputTensor = inputTensorsArray[ i ];
       if ( inputTensor )
         outputTensorsArray[ i ] = inputTensor.clone();
+      else
+        outputTensorsArray[ i ] = null;
     }
-    return outputTensorsArray;
   }
 
 }
