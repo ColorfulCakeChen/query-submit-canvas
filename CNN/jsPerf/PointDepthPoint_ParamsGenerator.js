2d654cf7bad2e58f9cb92fa2b882051c04557970
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-05-21 16:24:15 +0800

    Update PointDepthPoint_ParamsGenerator.js

--- CNN/jsPerf/PointDepthPoint_ParamsGenerator.js
+++ CNN/jsPerf/PointDepthPoint_ParamsGenerator.js
@@ -1,7 +1,29 @@
-
+export { Base };
 
 //import * as ParamDesc from "../Unpacker/ParamDesc.js";
 import * as ValueDesc from "../Unpacker/ValueDesc.js";
+import * as ValueRange from "../Unpacker/ValueRange.js";
 import * as PointDepthPoint from "../Conv/PointDepthPoint.js";
 import * as PointDepthPoint_Reference from "./PointDepthPoint_Reference.js";
 
+class Base {
+
+  constructor() {
+  }
+
+//!!! ...unfinished... (2021/05/21 Remarked) should use ValueRange.Same.valueGenerator()
+  /**
+   * @param {number} valueRangeMin
+   *
+   * @param {number} valueRangeMax
+   *
+   * @yield {number[]}
+   *   Yield an array with two elements: [ valueInput, valueOutput ]. The valueOutput is a value from valueRangeMin to valueRangeMax.
+   * The valueInput is a value which 
+   */
+  static *ValuePairGenerator( valueRangeMin, valueRangeMax ) {
+     let generatedValuePair;
+//!!! ...unfinished... (2021/05/21)
+  }
+
+}
