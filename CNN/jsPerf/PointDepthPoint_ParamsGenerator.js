b026fcd5e541ba32b112c812bc2ca8459e7d83cb
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-05-21 14:23:08 +0800

    Create PointDepthPoint_ParamsGenerator.js

---
+++ CNN/jsPerf/PointDepthPoint_ParamsGenerator.js
@@ -0,0 +1,7 @@
+
+
+//import * as ParamDesc from "../Unpacker/ParamDesc.js";
+import * as ValueDesc from "../Unpacker/ValueDesc.js";
+import * as PointDepthPoint from "../Conv/PointDepthPoint.js";
+import * as PointDepthPoint_Reference from "./PointDepthPoint_Reference.js";
+
