15ec7a1771995469b127243d00688f75f60db4c5
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-05-25 12:59:47 +0800

    Update PointDepthPoint_ParamsGenerator.js

--- CNN/jsPerf/PointDepthPoint_ParamsGenerator.js
+++ CNN/jsPerf/PointDepthPoint_ParamsGenerator.js
@@ -28,6 +28,25 @@ class Base {
    */
   static *ParamsGenerator() {
 
+    // All the parameters to be tried.
+    let paramDescArray = [
+      PointDepthPoint.Params.pointwise1ChannelCount,
+      PointDepthPoint.Params.bPointwise1Bias,
+      PointDepthPoint.Params.pointwise1ActivationId,
+      PointDepthPoint.Params.depthwise_AvgMax_Or_ChannelMultiplier,
+      PointDepthPoint.Params.depthwiseFilterHeight,
+      PointDepthPoint.Params.depthwiseStridesPad,
+      PointDepthPoint.Params.bDepthwiseBias,
+      PointDepthPoint.Params.depthwiseActivationId,
+      PointDepthPoint.Params.pointwise21ChannelCount,
+      PointDepthPoint.Params.bPointwise21Bias,
+      PointDepthPoint.Params.pointwise21ActivationId,
+      PointDepthPoint.Params.pointwise22ChannelCount,
+      PointDepthPoint.Params.bPointwise22Bias,
+      PointDepthPoint.Params.pointwise22ActivationId,
+      PointDepthPoint.Params.inputTensorCount,
+    ];
+
 //!!! ...unfinished... (2021/05/24)
 //     paramsInArray, paramsOutArray,
 //     pointwise1FiltersArray, pointwise1BiasesArray,
@@ -39,34 +58,58 @@ class Base {
     let result = { in: {}, out: {} };
 
 //!!! ...unfinished... (2021/05/24)
-    for ( let pointwise1ChannelCount of PointDepthPoint.Params.pointwise1ChannelCount.range.valueInputOutputGenerator() ) {
+    for ( let i = 0; i < paramDescArray.length; ++i ) {
+      let paramDesc = paramDescArray[ i ];
 
-      result.out.pointwise1ChannelCount( pointwise1ChannelCount.valueOutput );
+      for ( let pair of paramDesc.valueDesc.range.valueInputOutputGenerator() ) {
 
-      paramsInArray.push( pointwise1ChannelCount.valueInput );
-      result.in.pointwise1ChannelCount = null;
+        result.out[ paramDesc.paramName ]( pair.valueOutput );
+
+        // Try parameter value assigned directly (i.e. by specifying).
+        result.in[ paramDesc.paramName ] = null;
+        paramsInArray.push( pair.valueInput );
+
+//!!! ...unfinished... (2021/05/25)
+
+        // Try parameter value assigned from inputFloat32Array (i.e. by evolution).
+        result.in[ paramDesc.paramName ] = pair.valueInput;
+        paramsInArray.pop();
+      }
+
+
+//!!! ...unfinished... (2021/05/24)
+
+  }
+
+  /**
+   *
+   * @param {ParamDesc.Base[]} paramDescArray
+   *   All the parameters to be tried.
+   *
+   * @param {number} currentIndex
+   *   The index into the paramDescArray[]. It represents the current parameter to be tried.
+   *
+   * @return {???}
+   *
+   */
+  static permuteParams( paramDescArray, currentIndex ) {
+
+    if ( currentIndex >= paramDescArray.length )
+      return; // All parameters are tried.
+
+//!!! ...unfinished... (2021/05/25)
+    let paramDesc = paramDescArray[ currentIndex ];
+    for ( let pair of paramDesc.valueDesc.range.valueInputOutputGenerator() ) {
+
+      result.out[ paramDesc.paramName ]( pair.valueOutput );
+
+      paramsInArray.push( pair.valueInput );
+      result.in[ paramDesc.paramName ] = null;
 
 
       paramsInArray.pop();
-      result.in.pointwise1ChannelCount = pointwise1ChannelCount.valueInput;
+      result.in[ paramDesc.paramName ] = pair.valueInput;
     }
-
-    PointDepthPoint.Params.bPointwise1Bias.range;
-    PointDepthPoint.Params.pointwise1ActivationId.range;
-    PointDepthPoint.Params.depthwise_AvgMax_Or_ChannelMultiplier.range;
-    PointDepthPoint.Params.depthwiseFilterHeight.range;
-    PointDepthPoint.Params.depthwiseStridesPad.range;
-    PointDepthPoint.Params.bDepthwiseBias.range;
-    PointDepthPoint.Params.depthwiseActivationId.range;
-    PointDepthPoint.Params.pointwise21ChannelCount.range;
-    PointDepthPoint.Params.bPointwise21Bias.range;
-    PointDepthPoint.Params.pointwise21ActivationId.range;
-    PointDepthPoint.Params.pointwise22ChannelCount.range;
-    PointDepthPoint.Params.bPointwise22Bias.range;
-    PointDepthPoint.Params.pointwise22ActivationId.range;
-    PointDepthPoint.Params.inputTensorCount.range;
-
-//!!! ...unfinished... (2021/05/24)
 
   }
 
