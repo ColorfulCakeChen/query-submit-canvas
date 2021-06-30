25a61f4aa87419928a2a5fdeb6734941617fbfbd
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-06-30 15:45:47 +0800

    Update ParamOpCounter.js

--- CNN/Conv/ParamOpCounter.js
+++ CNN/Conv/ParamOpCounter.js
@@ -1,14 +1,25 @@
-
 export { Base };
 
 /**
  * Count how many operations use a parameter behind this counter.
+ *
+ * @member {number} paramId
+ *   This parameter's identifier.
+ *
+ * @member {Base} previousParamOpCounter
+ *   The previous ParamOpCounter which is before this ParamOpCounter.
+ *
+ * @member {number} behindOperationCount
+ *   This parameter is used by how many operations which is behind this operation.
+ *
+ *
+ *
  */
 class Base {
 
-  constructor( paramId ) {
+  constructor( paramId, previousParamOpCounter ) {
     this.paramId = paramId;
-    this.previousParamOpCounter = ;
+    this.previousParamOpCounter = previousParamOpCounter;
     this.behindOperationCount = 0;
   }
 
