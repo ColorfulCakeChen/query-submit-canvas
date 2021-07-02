b191c6e0ad689df3a49c1fd15277a1be55d20840
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-07-02 10:24:07 +0800

    Update TensorOpCounter.js

--- CNN/Conv/TensorOpCounter.js
+++ CNN/Conv/TensorOpCounter.js
@@ -1,7 +1,7 @@
 export { Base };
 
 /**
- * Count how many operations use a tensor behind this counter.
+ * Count there are how many operations using this operation's output tensor.
  *
  * @member {any} tensorId
  *   This tensor's identifier.
@@ -10,16 +10,18 @@ export { Base };
  *   The object which implements the operation. It should have a pfnOperation() method.
  *
  * @member {Base} input0
- *   The first TensorOpCounter which is just before this TensorOpCounter.
+ *   The first TensorOpCounter which represents this operation's first input.
  *
  * @member {Base} input1
- *   The second TensorOpCounter which is just before this TensorOpCounter.
- *
- * @member {number} behindOperationCount
- *   This tensor is used by how many operations which is behind this operation. If zero, the tensor is not used by other operation.
+ *   The second TensorOpCounter which represents this operation's second input. It could be null, if this operation does not have
+ * second input tensor.
  *
  * @member {Base[]} nextOperationArray
  *   The operations behind this operation.
+ *
+ * @member {number} nextOperationCount
+ *   This operration's output tensor is used by how many operations which is behind this operation. This is the same as
+ * this.nextOperationArray.length. If zero, the tensor is not used by other operation.
  *
  *
  *
@@ -31,7 +33,6 @@ class Base {
     this.operationObject = operationObject;
     this.input0 = input0;
     this.input1 = input1;
-    this.behindOperationCount = 0;
     this.nextOperationArray = [];
 
     if ( input0 )
@@ -39,6 +40,10 @@ class Base {
 
     if ( input1 )
       input1.nextOperationArray.push( this );
+  }
+
+  get nextOperationCount() {
+    return this.nextOperationArray.length;
   }
 
 }
