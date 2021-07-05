cdb835d70f05bb6c3384e2741e2980b07ae5d0cf
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-07-05 15:29:34 +0800

    Update jsPerf_PointDepthPoint.js

--- CNN/jsPerf/jsPerf_PointDepthPoint.js
+++ CNN/jsPerf/jsPerf_PointDepthPoint.js
@@ -1071,7 +1071,7 @@ class HeightWidthDepth {
     let testParamsGenerator = testParamsBase.ParamsGenerator();
 
 //!!! ...unfinished... (2021/05/27)
-    let batchMessageInterval = 1000; // Every so many test case, display a message.
+    let batchMessageInterval = 10000; // Every so many test case, display a message.
     for ( let testParams of testParamsGenerator ) {
       if ( ( testParams.id % batchMessageInterval ) == 0 )
         console.log( `testParams.id between [${testParams.id} - ${testParams.id + batchMessageInterval - 1}] ...` );
