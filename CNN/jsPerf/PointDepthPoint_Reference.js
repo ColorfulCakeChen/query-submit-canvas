dc590eed26554d28470cbac4e1475283ab4451ba
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-07-05 14:25:23 +0800

    Update PointDepthPoint_Reference.js

--- CNN/jsPerf/PointDepthPoint_Reference.js
+++ CNN/jsPerf/PointDepthPoint_Reference.js
@@ -180,19 +180,26 @@ class Base {
 
           // Because floating-point accumulated error of float32 (GPU) and float64 (CPU) is different (especially activation function
           // is one of SIGMOID, TANH, SIN, COS), only some digits after decimal are compared. Otherwise, they may not pass this test.
-          {
-            let valueByTensorflow, valueByRef, j;
-            for ( j = 0; j < outputArray.length; ++i ) {
-              valueByTensorflow = Math.trunc( outputArray[ j ] * fractionDigitsMultiplier );
-              valueByRef = Math.trunc( outputArrayRef[ j ] * fractionDigitsMultiplier );
-              if ( valueByTensorflow != valueByRef )
-                break;
-            }
+          let elementIndex;
+          tf.util.assert( outputArray.every( ( value, index ) =>
+            Math.trunc( value * fractionDigitsMultiplier ) === Math.trunc( outputArrayRef[ elementIndex = index ] * fractionDigitsMultiplier ) ),
+            `PointDepthPoint output${i}[ ${elementIndex} ] ( ${outputArray[ elementIndex ]} ) should be ( ${outputArrayRef[ elementIndex ]} ) `
+              +`( ${outputArray} ) should be ( ${outputArrayRef} ). ${strNote}` );
 
-            tf.util.assert( valueByTensorflow === valueByRef,
-              `PointDepthPoint output${i}[ ${j} ] ( ${outputArray[ j ]} ) should be ( ${outputArrayRef[ j ]} ) `
-              +`( ${outputArray} ) should be ( ${outputArrayRef} ). ${strNote}`);
-          }
+//!!! (2021/07/05 Remarked) Old Codes. (slower)
+//           {
+//             let valueByTensorflow, valueByRef, j;
+//             for ( j = 0; j < outputArray.length; ++i ) {
+//               valueByTensorflow = Math.trunc( outputArray[ j ] * fractionDigitsMultiplier );
+//               valueByRef = Math.trunc( outputArrayRef[ j ] * fractionDigitsMultiplier );
+//               if ( valueByTensorflow != valueByRef )
+//                 break;
+//             }
+//
+//             tf.util.assert( valueByTensorflow === valueByRef,
+//               `PointDepthPoint output${i}[ ${j} ] ( ${outputArray[ j ]} ) should be ( ${outputArrayRef[ j ]} ) `
+//               +`( ${outputArray} ) should be ( ${outputArrayRef} ). ${strNote}`);
+//           }
         }
       }
 
