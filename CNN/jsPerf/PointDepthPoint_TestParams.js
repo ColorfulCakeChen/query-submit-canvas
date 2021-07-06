48d8895885f364edbfd0cabef44fc030861fc345
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-07-06 14:59:50 +0800

    Update PointDepthPoint_TestParams.js

--- CNN/jsPerf/PointDepthPoint_TestParams.js
+++ CNN/jsPerf/PointDepthPoint_TestParams.js
@@ -365,7 +365,7 @@ class Base {
     ];
 
     // Restrict some parameter's large kinds. Otherwise, too many combination will be generated.
-    this.maxKindsPerParameter = 10;
+    this.maxKindsPerParameter = 9;
   }
 
   /**
