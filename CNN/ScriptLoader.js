c1920325a658c92c2f113f4d480e6d6797bc3160
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-07-20 10:23:44 +0800

    Update ScriptLoader.js

--- CNN/ScriptLoader.js
+++ CNN/ScriptLoader.js
@@ -1,16 +1,37 @@
 export {createPromise};
 
 /**
- * @param {string}  url      The URL of the script to be loaded.
- * @param {boolean} isModule If true, the script will be loaded as javascript module.
- * @return A promise. It resolves when the script is loaded.
+ * @param {string}  url
+ *   The URL of the script to be loaded. A HTMLScriptElement will be created in document's head to load the script.
+ *
+ * @param {boolean} isModule
+ *   If true, the script will be loaded as javascript module.
+ *
+ * @param {string} HTMLElementId
+ *   If specified, it will become the id of the created HTMLScriptElement. And it will be used to determine whether the script
+ * has been loaded to prevent loading twice.
+ *
+ * @return {Promise}
+ *   It resolves when the script is loaded.
  */
-function createPromise(url, isModule) {
-  return new Promise((resolve, reject) => {
-    let attributes = { src:url, onload:e=>resolve(e), onerror:e=>reject(e) };
-    if (isModule)
+function createPromise( url, isModule, htmlElementId ) {
+  if ( htmlElementId ) {
+    let scriptElement = document.getElementById( htmlElementId );
+    if ( scriptElement )
+      return Promise.resolve();
+  }
+
+  //console.log( "Loading \"" + url + "\"" );
+  return new Promise( ( resolve, reject ) => {
+    let attributes = { src: url, onload: e => resolve(e), onerror: e => reject(e) };
+
+    if ( isModule )
       attributes.type = "module";
 
-    document.head.appendChild(Object.assign(document.createElement("script"), attributes));
+    if ( htmlElementId )
+      attributes.id = htmlElementId;
+
+    document.head.appendChild( Object.assign( document.createElement("script"), attributes ) );
   });
 }
+
