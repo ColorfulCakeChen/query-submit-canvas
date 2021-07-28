7eb5c5f9c358600b8e1582359bb4e54fe031b336
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-07-28 15:51:49 +0800

    Update Block.js

--- CNN/Conv/Block.js
+++ CNN/Conv/Block.js
@@ -1,7 +1,6 @@
 export { Params, Base, PointDepthPoint };
 
 import * as ValueMax from "../ValueMax.js";
-//import * as ValueRange from "../Unpacker/ValueRange.js";
 import * as ValueDesc from "../Unpacker/ValueDesc.js";
 import * as ParamDesc from "../Unpacker/ParamDesc.js";
 import * as Weights from "../Unpacker/Weights.js";
