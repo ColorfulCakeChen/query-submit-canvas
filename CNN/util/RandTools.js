49e71553aac1ed7db4b4f7f5e8de2cebacc1c09b
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-08-02 13:42:07 +0800

    Update RandTools.js

--- CNN/util/RandTools.js
+++ CNN/util/RandTools.js
@@ -1,4 +1,4 @@
-export { getRandomIntInclusive };
+export { getRandomIntInclusive, generate_numberArray };
 
 /**
  * Return a random integer between min and max. (This function comes from MDN's Math.random().)
@@ -13,4 +13,28 @@ function getRandomIntInclusive( min, max ) {
   let maxInt  = Math.floor( maxReal );
   let kindsInt = maxInt - minInt + 1;
   return Math.floor( ( Math.random() * kindsInt ) + minInt );
+}
+
+/**
+ *
+ * @param {number} elementCount
+ *   The returned number array will have so many elements.
+ *
+ * @param {number} randomOffsetMin
+ *   Every element of the generated number array will been shifted from the sequence id between
+ * [ randomOffsetMin, randomOffsetMax ] (inclusive) randomly.
+ *
+ * @param {number} randomOffsetMax
+ *   Every element of the generated number array will been shifted from the sequence id between
+ * [ randomOffsetMin, randomOffsetMax ] (inclusive) randomly.
+ *
+ * @return {number[]}
+ *   Return a number array.
+ */
+function generate_numberArray( elementCount, randomOffsetMin = 0, randomOffsetMax = 0 ) {
+  let numberArray = new Array( elementCount );
+  for ( let i = 0; i < elementCount; ++i ) {
+    numberArray[ i ] = i + getRandomIntInclusive( randomOffsetMin, randomOffsetMax );
+  }
+  return numberArray;
 }
