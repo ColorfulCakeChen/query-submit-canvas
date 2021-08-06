7a38812775544a4b736a7f7e5c76b0359b33a046
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-08-06 15:03:35 +0800

    Update TestParams.js

--- CNN/jsPerf/Ref/TestParams.js
+++ CNN/jsPerf/Ref/TestParams.js
@@ -1,17 +1,51 @@
-export { Base };
+export { ParamDescConfig, Base };
+
+//import * as RandTools from "../../util/RandTools.js";
+//import * as ParamDesc from "../../Unpacker/ParamDesc.js";
+//import * as ValueDesc from "../../Unpacker/ValueDesc.js";
+//import * as ValueRange from "../../Unpacker/ValueRange.js";
+//import * as TestParams from "./TestParams.js";
+
 
 /**
- * This is an object { id, in, out } which has one number and two sub-objects.
+ * Describe which parameter and how many combination for the parameter.
+ *
+ *
+ * @member {ParamDesc.Base} paramDesc
+ *   Which parameter to be used in the permutation.
+ *
+ * @member {number} maxKinds
+ *   An integer restricts the generator range to [ 0, maxKinds ] instead of [ 0, paramDesc.valueDesc.range.kinds ].
+ * If ( maxKinds == undefined ), the default is paramDesc.valueDesc.range.kinds. This parameter could lower the kinds
+ * to reduce test cases quantity. If zero or negative, only one value (between [ 0, paramDesc.valueDesc.range.kinds ]
+ * randomly) will be generated. In fact, this is the maxKinds parameter of ValueRange.XXX.valueInputOutputGenerator().
+ */
+class ParamDescConfig {
+  constructor( paramDesc, maxKinds ) {
+    this.paramDesc = this.paramDesc;
+    this.maxKinds = this.maxKinds;
+  }
+}
+
+
+/**
+ * This is an object { config, id, in, out } which has one number and two sub-objects.
+ *
+ * @member {object} config
+ *   The configuration for generating parameters combination.
+ *
+ * @param {ParamDescConfig[]} config.paramDescConfigArray
+ *   List all the parameters to be used in permutation combination.
  *
  * @member {number} id
- *   The numeric identifier of this testing parameter configuration.
+ *   The numeric identifier of this testing parameter combination.
  *
  * @member {object} in
  *   The "in" sub-object's data members represent every parameters of some (e.g. PointDepthPoint) Params's constructor. Besides,
  * it also has the following properties:
- *   - inputFloat32Array
- *   - byteOffsetBegin
- *   - weights
+ *   - paramsNumberArrayObject: All (non-concatenated) parameters (include filters and biases) which will be packed into inputFloat32Array.
+ *   - inputFloat32Array: A packed Float32Array from paramsNumberArrayObject with byteOffsetBegin.
+ *   - byteOffsetBegin: The offset in inputFloat32Array to the first parameter.
  *
  * @member {object} out
  *   The "out" sub-object's data members represent the "should-be" result of some (e.g. PointDepthPoint) Params.extract().
@@ -19,9 +53,106 @@ export { Base };
  *
  */
 class Base {
-  constructor() {
+
+  /**
+   *
+   */
+  constructor( paramDescConfigArray ) {
+    this.config = { paramDescConfigArray: paramDescConfigArray };
+
     this.id = -1;
-    this.in = {};
+    this.in = { paramsNumberArrayObject: {} };
     this.out = {};
   }
+
+  /**
+   * Called by permuteParamRecursively() when a combination of parameters is complete and before this object to be yielded.
+   *
+   * The the following data should already be ready:
+   *   - this.id
+   *   - this.in.paramsNumberArrayObject: Every should-be-packed parameter.
+   *   - this.in.Xxx: every non-packed parameter.
+   *   - this.out.Xxx: every parameter.
+   *
+   * This method should fill the following data:
+   *   - this.in.inputFloat32Array
+   *   - this.in.byteOffsetBegin
+   *
+   * Sub-class should override this method.
+   */
+  onBefore_Yield() {
+  }
+
+  /**
+   * Responsible for generating testing paramters combinations.
+   *
+   *
+   *
+   * @yield {Base}
+   *   Yield this object itself. The returned object (it is this object itself) should not be modified because it will be re-used.
+   */
+  * ParamsGenerator() {
+    this.in.paramsNumberArrayObject = {}; // All parameters which will be packed into weights array.
+    yield *Base.permuteParamRecursively.call( this, 0 );
+  }
+
+  /**
+   * This method will modify this.id and this.in.paramsNumberArrayObject. It also calls itself recursively to permute all parameters.
+   *
+   * @param {number} currentParamDescIndex
+   *   The index into the this.paramDescArray[]. It represents the current parameter to be tried.
+   *
+   * @yield {Base}
+   *   Every time one kind of parameters' combination is generated, the this.result will be yielded.
+   */
+  static * permuteParamRecursively( currentParamDescConfigIndex ) {
+
+    if ( currentParamDescConfigIndex >= this.paramDescConfigArray.length ) { // All parameters are used to be composed as one kind of combination.
+      ++this.id;  // Complete one kind of combination.
+
+      this.onBefore_Yield();
+
+//!!! (2021/08/06 Remarked) Old Codes.
+//       // For testing not start at the offset 0.
+//       let weightsElementOffsetBegin = RandTools.getRandomIntInclusive( 0, 3 ); // Skip a random un-used element count.
+//
+//       this.result.set_By_ParamsNumberArrayMap_ParamsOut(
+//         this.channelCount0_pointwise1Before, this.paramsNumberArrayObject, this.result.out, weightsElementOffsetBegin );
+
+      yield this;
+      return; // Stop this recusive. Back-track to another parameters combination.
+    }
+
+    let nextParamDescConfigIndex = currentParamDescConfigIndex + 1;
+
+    let paramDescConfig = this.paramDescConfigArray[ currentParamDescConfigIndex ];
+    let paramDesc = paramDescConfig.paramDesc;
+    for ( let pair of paramDesc.valueDesc.range.valueInputOutputGenerator( undefined, paramDescConfig.maxKinds ) ) {
+
+      //!!! (2021/07/06 Temp Debug) Check the algorithm might be wrong.
+      //if ( paramDesc.valueDesc.range.adjust( pair.valueInput ) != pair.valueOutput )
+      //  debugger;
+
+      this.out[ paramDesc.paramName ] = pair.valueOutput;
+
+      // Randomly place the parameter directly or in weights array.
+//!!! (2021/07/19 Temp Remarked)
+      let dice = Math.random();
+//      let dice = 0;
+      if ( dice < 0.5 ) {
+        // Try parameter value assigned directly (i.e. by specifying).      
+        this.in[ paramDesc.paramName ] = pair.valueInput;
+        yield *Base.permuteParamRecursively.call( this, nextParamDescConfigIndex );
+
+      } else {
+        // Try parameter value assigned from inputFloat32Array (i.e. by evolution).
+        this.in[ paramDesc.paramName ] = null;
+        this.in.paramsNumberArrayObject[ paramDesc.paramName ] = [ pair.valueInput ];
+        yield *Base.permuteParamRecursively.call( this, nextParamDescConfigIndex );
+
+        this.in.paramsNumberArrayObject[ paramDesc.paramName ] = undefined; // So that it could be re-tried as by-specifying when backtracking.
+      }
+    }
+  }
+
 }
