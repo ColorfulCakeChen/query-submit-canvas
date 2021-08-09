f50f3b6b48a0ec6dce33710bd8956ad55aed684a
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-08-09 14:06:37 +0800

    Create Block_TestParams.js

---
+++ CNN/jsPerf/Ref/Block_TestParams.js
@@ -0,0 +1,34 @@
+export { Base };
+
+import * as RandTools from "../../util/RandTools.js";
+//import * as ParamDesc from "../../Unpacker/ParamDesc.js";
+//import * as ValueDesc from "../../Unpacker/ValueDesc.js";
+//import * as ValueRange from "../../Unpacker/ValueRange.js";
+import * as TestParams from "./TestParams.js";
+import * as PointDepthPoint_TestParams from "./PointDepthPoint_TestParams.js";
+import * as PointDepthPoint from "../../Conv/Block.js";
+
+/**
+ *
+ * This is an object { id, in, out } which has one number and two sub-objects.
+ *
+ * @member {number} id
+ *   The numeric identifier of the parameter.
+ *
+ * @member {object} in
+ *   The "in" sub-object's data members represent every parameters of the Block.Params's constructor. That is,
+ * it has the following data members: sourceHeight, sourceWidth, sourceChannelCount, stepCountPerBlock, bChannelShuffler,
+ * pointwise1ChannelCountRate, depthwiseFilterHeight, bBias, nActivationId, nActivationIdAtBlockEnd, bKeepInputTensor.
+ * It also has the following properties:
+ *   - paramsNumberArrayObject
+ *   - inputFloat32Array
+ *   - byteOffsetBegin
+ *
+ * @member {object} out
+ *   The "out" sub-object's data members represent the "should-be" result of Block.Params's extract().
+ * That is, it has the above data members except paramsNumberArrayObject, inputFloat32Array, byteOffsetBegin.
+ *
+ */
+class Base extends TestParams.Base {
+  
+}
