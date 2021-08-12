92d42e551d204d4517ffdd2e9972ee19bdfdf7a9
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-08-12 12:18:42 +0800

    Update console_test_func_perf.js

--- CNN/jsPerf/console_test_func_perf.js
+++ CNN/jsPerf/console_test_func_perf.js
@@ -3,32 +3,39 @@ const x = tf.randomNormal([2000, 2000, 4]);
 //const b = tf.randomNormal([2000, 2000, 1]);
 const b = tf.randomNormal([1, 1, 4]);
 
-function foo( f ) {
-  let times = 10; //10 * 1000;
+function timesFunc( f ) {
+  let times = 100; //10 * 1000;
   let y;
   for ( let i = 0; i < times; ++i ) {
-  	y = f();
-    y.dispose();
+		y = f();
+		y.dispose();
   }
 }
 
 let testFuncArray = [
-  tf.add,
   tf.cos,
   tf.tanh,
   tf.sigmoid,
   tf.relu,
   tf.relu6,
   tf.log,
-  tf.log1p
+  tf.log1p,
+  tf.add,
 ];
 
-for ( let i = 0; i < testFuncArray.length; ++i ) {
-  let testFunc = testFuncArray[ i ];
-	let testFuncBind = testFunc.bind( null, x, b );
-	let bar = foo.bind( null, testFuncBind );
+async function testByBackend( backendName ) {
+	console.log( `backend = ${backendName}` );
+	await tf.setBackend( backendName );
 
-	const time = await tf.time( bar );
-	console.log(
-    `${testFunc.name}, kernelMs: ${time.kernelMs}, wallTimeMs: ${time.wallMs}`);
+	for ( let i = 0; i < testFuncArray.length; ++i ) {
+		let testFunc = testFuncArray[ i ];
+		let testFuncBind = testFunc.bind( null, x, b );
+		let testFuncTimes = timesFunc.bind( null, testFuncBind );
+
+		const time = await tf.time( testFuncTimes );
+		console.log( `${testFunc.name}, kernelMs: ${time.kernelMs}, wallTimeMs: ${time.wallMs}`);
+	}
 }
+
+await testByBackend( "cpu" );
+await testByBackend( "webgl" );
