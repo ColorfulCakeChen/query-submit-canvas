11abecc630804c6b981bf175a4cde8d55b4068d2
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-08-14 14:28:10 +0800

    Update console_test_func_perf.js

--- CNN/jsPerf/console_test_func_perf.js
+++ CNN/jsPerf/console_test_func_perf.js
@@ -1,7 +1,15 @@
+
+class NameFunc {
+  constructor( name, f ) {
+    this.name = name;
+    this.f = f;
+  }
+}
+
 const x = tf.randomNormal([2000, 2000, 4]);
-//const b = tf.randomNormal([2000, 2000, 4]);
-//const b = tf.randomNormal([2000, 2000, 1]);
-const b = tf.randomNormal([1, 1, 4]);
+const c_broadcast_none = tf.randomNormal([2000, 2000, 4]);
+const c_broadcast_channel = tf.randomNormal([2000, 2000, 1]);
+const c_broadcast_height_width_channel = tf.randomNormal([1, 1, 4]);
 
 function timesFunc( f ) {
   let times = 100; //10 * 1000;
@@ -13,22 +21,24 @@ function timesFunc( f ) {
 }
 
 let testFuncArray = [
-  tf.sin,
-//  tf.asin,
-  tf.cos,
-//  tf.acos,
-//  tf.tan,
-  tf.tanh,
-  tf.sigmoid,
-  tf.relu,
-  tf.relu6,
-  tf.log,
-  tf.log1p,
-  tf.exp,
-  tf.expm1,
-  tf.softplus,
-  tf.reciprocal,
-  tf.add,
+  new NameFunc( "sin", tf.sin.bind( null, x ) ),
+//  new NameFunc( "asin", tf.asin.bind( null, x ) ),
+  new NameFunc( "cos", tf.cos.bind( null, x ) ),
+//  new NameFunc( "acos", tf.acos.bind( null, x ) ),
+//  new NameFunc( "tan", tf.tan.bind( null, x ) ),
+  new NameFunc( "tanh", tf.tanh.bind( null, x ) ),
+  new NameFunc( "sigmoid", tf.sigmoid.bind( null, x ) ),
+  new NameFunc( "relu", tf.relu.bind( null, x ) ),
+  new NameFunc( "relu6", tf.relu6.bind( null, x ) ),
+  new NameFunc( "log", tf.log.bind( null, x ) ),
+  new NameFunc( "log1p", tf.log1p.bind( null, x ) ),
+  new NameFunc( "exp", tf.exp.bind( null, x ) ),
+  new NameFunc( "expm1", tf.expm1.bind( null, x ) ),
+  new NameFunc( "softplus", tf.softplus.bind( null, x ) ),
+  new NameFunc( "reciprocal", tf.reciprocal.bind( null, x ) ),
+  new NameFunc( "add_broadcast_none", tf.add.bind( null, x, c_broadcast_none ) ),
+  new NameFunc( "add_broadcast_channel", tf.add.bind( null, x, c_broadcast_channel ) ),
+  new NameFunc( "add_broadcast_height_width_channel", tf.add.bind( null, x, c_broadcast_height_width_channel ) ),
 ];
 
 async function testByBackend( backendName ) {
@@ -36,12 +46,11 @@ async function testByBackend( backendName ) {
   await tf.setBackend( backendName );
 
   for ( let i = 0; i < testFuncArray.length; ++i ) {
-    let testFunc = testFuncArray[ i ];
-    let testFuncBind = testFunc.bind( null, x, b );
-    let testFuncTimes = timesFunc.bind( null, testFuncBind );
+    let testNameFunc = testFuncArray[ i ];
+    let testFuncTimes = timesFunc.bind( null, testNameFunc.f );
 
     const time = await tf.time( testFuncTimes );
-    console.log( `${testFunc.name}, kernelMs: ${time.kernelMs}, wallTimeMs: ${time.wallMs}`);
+    console.log( `${testNameFunc.name}, kernelMs: ${time.kernelMs}, wallTimeMs: ${time.wallMs}`);
   }
 }
 
