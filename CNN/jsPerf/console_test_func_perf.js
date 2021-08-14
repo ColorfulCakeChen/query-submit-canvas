3ccb4bf65bd05e24fdb7fa5a080126f19e15af5b
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-08-14 14:36:51 +0800

    Update console_test_func_perf.js

--- CNN/jsPerf/console_test_func_perf.js
+++ CNN/jsPerf/console_test_func_perf.js
@@ -1,4 +1,7 @@
 
+/**
+ * A name and a binded function.
+ */
 class NameFunc {
   constructor( name, f ) {
     this.name = name;
@@ -6,10 +9,22 @@ class NameFunc {
   }
 }
 
+
+let f = tf.tensor( [ 1, 1, 1, 10E20 ], [ 2, 2, 1, 1 ] );
+//let f = tf.tensor( [ 0, 0, 0, 0 ], [ 2, 2, 1, 1 ] );
+let strides = 1;
+let pad = "same";
+let y = tf.depthwiseConv2d( x, f, strides, pad );
+y.print();
+
+
 const x = tf.randomNormal([2000, 2000, 4]);
 const c_broadcast_none = tf.randomNormal([2000, 2000, 4]);
 const c_broadcast_channel = tf.randomNormal([2000, 2000, 1]);
 const c_broadcast_height_width_channel = tf.randomNormal([1, 1, 4]);
+
+let depthwiseFilter = tf.randomNormal( [ 3, 3, 4, 4 ] );
+let pointwiseFilter = tf.randomNormal( [ 1, 1, 4, 4 ] );
 
 function timesFunc( f ) {
   let times = 100; //10 * 1000;
@@ -36,9 +51,13 @@ let testFuncArray = [
   new NameFunc( "expm1", tf.expm1.bind( null, x ) ),
   new NameFunc( "softplus", tf.softplus.bind( null, x ) ),
   new NameFunc( "reciprocal", tf.reciprocal.bind( null, x ) ),
+
   new NameFunc( "add_broadcast_none", tf.add.bind( null, x, c_broadcast_none ) ),
   new NameFunc( "add_broadcast_channel", tf.add.bind( null, x, c_broadcast_channel ) ),
   new NameFunc( "add_broadcast_height_width_channel", tf.add.bind( null, x, c_broadcast_height_width_channel ) ),
+
+  new NameFunc( "depthwise_3x3x4_strides1_padSame", tf.depthwiseConv2d.bind( null, x, depthwiseFilter, 1, "same" ) ),
+  new NameFunc( "pointwise_1x1x1_strides1_padValid", tf.conv2d.bind( null, x, pointwiseFilter, 1, "valid" ) ),
 ];
 
 async function testByBackend( backendName ) {
