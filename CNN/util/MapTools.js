9387fa9a3c871a8d23ae113cbdd97cc8129a53a5
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-09-03 11:21:21 +0800

    Update MapTools.js

--- CNN/util/MapTools.js
+++ CNN/util/MapTools.js
@@ -15,9 +15,25 @@ export { get_or_create };
  *     - If the key is found inside the containerMap, its value (also a Map object) will be returned.
  *     - If the key is not found, a new Map object will be created. It will be recorded into the containerMap, and then be returned.
  */
-get_or_create( map, key, newObjectClass = Map ) {
-  let value = map.get( key );
+function get_or_create( containerMap, key, newObjectClass = Map ) {
+  let value = containerMap.get( key );
   if ( !value )
-    map.set( key, value = new newObjectClass() );
+    containerMap.set( key, value = new newObjectClass() );
   return value;
+}
+
+/**
+ * @param {object} container
+ *   An object which may be a container or not. If container has method values(), it will be assumed to return an iterator. This
+ * function will visit every element by the iterator. If the element still has method values(), they will be visit recursively.
+ * If the element does not have method values(), the element will be yielded by this generator.
+ */
+function* values_recursively( container ) {
+  if ( container.values instanceof Function ) {
+    for ( let element of container.values() ) {
+      yield* values_recursively( element ); // Try it. Perhaps it also a container.
+    }
+  } else {
+    yield container; // Since not a container (i.e. just an element), yield it.
+  }
 }
