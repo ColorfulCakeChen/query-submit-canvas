61ab7f119778c492204fc41429e8aef1038a8402
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-09-03 10:49:20 +0800

    Update ChannelShufflerPool.js

--- CNN/Conv/ChannelShufflerPool.js
+++ CNN/Conv/ChannelShufflerPool.js
@@ -1,5 +1,6 @@
 export { Base };
 
+import * as MapTools from "../../util/MapTools.js";
 import * as ChannelShuffler from "./ChannelShuffler.js";
 
 /**
@@ -15,6 +16,11 @@ class Base {
    */
   constructor( channelShufflerClass = ChannelShuffler.ConcatPointwiseConv ) {
     this.channelShufflerClass = channelShufflerClass;
+
+    // channel shufflers indexed by [ concatenatedHeight, concatenatedWidth, concatenatedDepth, outputGroupCount ].
+    this.channelShufflersBy_height_width_depth_group = new Map();
+
+    this.concatenatedShape = [ null, null, null ]; // A re-used shared array for reducing memory allocation.
   }
 
   /**
@@ -35,7 +41,24 @@ class Base {
    *   A shared channel shuffler which could process the specific concatenatedShape and outputGroupCount.
    */
   getChannelShuffler_by( concatenatedHeight, concatenatedWidth, concatenatedDepth, outputGroupCount ) {
-//!!! ...unfinished... (2021/09/03)
+
+    let channelShufflersBy_width_depth_group = MapTools.get_or_create( this.channelShufflersBy_height_width_depth_group, concatenatedHeight );
+    let channelShufflersBy_depth_group = MapTools.get_or_create( channelShufflersBy_width_depth_group, concatenatedWidth );
+    let channelShufflersBy_group = MapTools.get_or_create( channelShufflersBy_depth_group, concatenatedDepth );
+
+    let channelShuffler = channelShufflersBy_group.get( outputGroupCount );
+
+    if ( channelShuffler )
+      return channelShuffler; // 1. The requested channel shufffler has already been created. Re-use it. Return it directly.
+
+    // 2. The requested channel shufffler has not yet existed. It should be created newly.
+    this.concatenatedShape[ 0 ] = concatenatedHeight;
+    this.concatenatedShape[ 1 ] = concatenatedWidth;
+    this.concatenatedShape[ 2 ] = concatenatedDepth;
+    channelShuffler = new (this.channelShufflerClass)( this.concatenatedShape, outputGroupCount );
+
+    channelShufflersBy_group.set( outputGroupCount, channelShuffler ); // Cache it.
+    return channelShuffler;
   }
 
   /** Release all channel shuffler and their tf.tensor. */
