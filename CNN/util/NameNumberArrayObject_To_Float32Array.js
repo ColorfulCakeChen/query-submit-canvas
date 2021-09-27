7b8a4e14c6a7ec1ff70af2477ec62fd6ca3204d8
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-09-27 15:50:32 +0800

    Update NameNumberArrayObject_To_Float32Array.js

--- CNN/util/NameNumberArrayObject_To_Float32Array.js
+++ CNN/util/NameNumberArrayObject_To_Float32Array.js
@@ -55,31 +55,6 @@ class Base {
       }
     }
 
-//!!! (2021/08/11 Remarked) Replace by codes without temporary array.
-//     // Prepare weights source and offset into a temporary array. So that they can be accessed by loop and total length could be calculated.
-//     let weightsTotalLength = weightsElementOffsetBegin;
-//     let weightsSourceArray = [];
-//     for ( let i = 0; i < paramsNameOrderArray.length; ++i ) {
-//       let paramName = paramsNameOrderArray[ i ];
-//       let numberArray = paramsNumberArrayObject[ paramName ];
-//       if ( numberArray ) {
-//         weightsSourceArray.push( { offset: weightsTotalLength, weights: numberArray } );
-//         weightsTotalLength += numberArray.length;
-//       }
-//     }
-//
-//     // Concatenate all number array into a Float32Array.
-//     this.weightsFloat32Array = new Float32Array( weightsTotalLength );
-//     {
-//       for ( let i = 0; i < this.weightsElementOffsetBegin; ++i ) { // Make-up the un-used weight values.
-//         this.weightsFloat32Array[ i ] = -i;
-//       }
-//
-//       for ( let i = 0; i < weightsSourceArray.length; ++i ) { // Concatenate all number array into a Float32Array.
-//         this.weightsFloat32Array.set( weightsSourceArray[ i ].weights, weightsSourceArray[ i ].offset );
-//       }
-//     }
-
   }
 
 }
