70bf8417750a046ffb95c7acebadb1df048cd9cd
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-10-07 13:50:46 +0800

    Create ObjectPropertyAsserter.js

---
+++ CNN/util/ObjectPropertyAsserter.js
@@ -0,0 +1,33 @@
+export { Base };
+
+/**
+ * Assert an object's properties.
+ */
+class Base {
+
+  /**
+   * @param {string} objectName
+   *   The object's name for debug easily. 
+   *
+   * @param {object} object
+   *   The step object for 's name for debug easily. 
+   *
+   * @param {string} contextDescription
+   *   Context message for debug easily. 
+   */
+  constructor( objectName, object, contextDescription ) {
+    this.objectName = objectName;
+    this.object = object;
+    this.contextDescription = contextDescription;
+  }
+
+  /**
+   * Assert ( this.object[ propertyName ] == value ).
+   */
+  assertPropertyValue( propertyName, value ) {
+    let propertyValue = this.object[ propertyName ];
+    tf.util.assert( ( propertyValue == value ),
+      `${this.objectName}.${propertyName} (${propertyValue}) should be (${value}). ${this.contextDescription}`);
+  }
+
+}
