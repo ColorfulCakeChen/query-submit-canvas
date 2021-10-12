299b536463bd4341b4d76cc49ebe276d79deca05
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-10-12 12:42:19 +0800

    Update Embedding2d.js

--- CNN/Conv/Embedding2d.js
+++ CNN/Conv/Embedding2d.js
@@ -99,6 +99,15 @@ Params.channelMultiplier = new ParamDesc.Int( "channelMultiplier", 1, 32 );
  * @member {number} outChannels
  *   Output channel count. It is always depending on channelMultiplier and equals to ( inChannels * channelMultiplier ).
  *
+!!!
+ * @member {number} tensorWeightCountTotal
+ *   The total wieght count used in tensors. Not including Params, because they are not used in tensors. Including inferenced
+ * weights, if they are used in tensors.
+ *
+ * @member {number} tensorWeightCountExtracted
+ *   The wieght count extracted from inputFloat32Array and used in tensors. Not including Params, because they are not used in
+ * tensors. Not including inferenced weights (even if they are used in tensors), because they are not extracted from inputFloat32Array.
+ *
  * @member {function} destroy_or_keep_input
  *   This is a function pointer to one of destroy_input(), keep_input(). If ( this.bKeepInputTensor == false ),
  * it pointer to destroy_input(). If ( this.bKeepInputTensor == true ), it pointer to keep_input().
@@ -499,6 +508,34 @@ class Base extends ReturnOrClone.Base {
    *   Return the total wieght count of this Embedding2d (including filter weights and bias weights, excluding Params).
    */
   get filterBiasWeightCount() {
+    let weightCount = 0;
+
+    if ( this.vocabularyTablesTensorArray ) {
+      for ( let i = 0; i < this.vocabularyTablesTensorArray.length; ++i ) {
+        let vocabularyTablesTensor = this.vocabularyTablesTensorArray[ i ];
+        if ( vocabularyTablesTensor ) {
+          weightCount += tf.util.sizeFromShape( vocabularyTablesTensor.shape );
+        }
+      }
+    }
+
+    if ( this.vocabularyTableTensor2d ) {
+      weightCount += tf.util.sizeFromShape( this.vocabularyTableTensor2d.shape );
+    }
+
+    if ( this.channelValueOffsetTensor3d ) {
+      weightCount += tf.util.sizeFromShape( this.channelValueOffsetTensor3d.shape );
+    }
+
+    return weightCount;
+  }
+
+!!!
+  /**
+   * @return {number}
+   *   Return the total wieght count of this Embedding2d (including filter weights and bias weights, excluding Params).
+   */
+  get learnableTensorSize() {
     let weightCount = 0;
 
     if ( this.vocabularyTablesTensorArray ) {
