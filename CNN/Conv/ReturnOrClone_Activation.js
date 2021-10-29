85d6a810e1e3b89602a63ab2dc878390699573aa
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-10-29 11:41:16 +0800

    Update ReturnOrClone_Activation.js

--- CNN/Conv/ReturnOrClone_Activation.js
+++ CNN/Conv/ReturnOrClone_Activation.js
@@ -26,4 +26,47 @@ class Base extends ReturnOrClone.Base {
     return pfn;
   }
 
+  /**
+   * Extract tf.tensor4d from inputFloat32Array (at this.byteOffsetEnd). The following data members will be modified:
+   *   - this.byteOffsetEnd
+   *   - this.tensorWeightCountExtracted
+   *   - this.tensorWeightCountTotal
+   *
+   * @param {Base} this                       The Base object to be modified.
+   * @param {Float32Array} inputFloat32Array  A Float32Array whose values will be interpreted as weights.
+   * @param {number[]} tensorShape            The shape of the tensor to be extracted.
+   *
+   * @return {tf.tensor}                      The extracted tensor. Return null, if failed.
+   */
+  static extractTensor( inputFloat32Array, tensorShape ) {
+    let tensorWeights = new Weights.Base( inputFloat32Array, this.byteOffsetEnd, tensorShape );
+    if ( !tensorWeights.extract() )
+      return null;  // e.g. input array does not have enough data.
+    this.byteOffsetEnd = tensorWeights.defaultByteOffsetEnd;
+
+    let t = tf.tensor4d( tensorWeights.weights, tensorShape );
+    let elementCount = tf.util.sizeFromShape( t.shape );
+    this.tensorWeightCountExtracted += elementCount;
+    this.tensorWeightCountTotal += elementCount;
+
+    return t;
+  }
+
+  /**
+   * Extract biases from inputFloat32Array (at this.byteOffsetEnd). The following data members will be modified:
+   *   - this.byteOffsetEnd
+   *   - this.tensorWeightCountExtracted
+   *   - this.tensorWeightCountTotal
+   *
+   * @param {Base} this                       The Base object to be modified.
+   * @param {Float32Array} inputFloat32Array  A Float32Array whose values will be interpreted as weights.
+   * @param {number} channelCount             The (input and output) channel count of the biases.
+   *
+   * @return {tf.tensor3d}                    The extracted biases. Return null, if failed.
+   */
+  static extractBiases( inputFloat32Array, channelCount ) {
+    let biasesShape = [ 1, 1, channelCount ];
+    return Base.extractTensor.call( inputFloat32Array, biasesShape );
+  }
+
 }
