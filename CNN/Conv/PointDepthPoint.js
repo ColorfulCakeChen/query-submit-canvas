95ceec76d160006db601c47b0147572aef169f76
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-11-26 18:53:25 +0800

    Update PointDepthPoint.js

--- CNN/Conv/PointDepthPoint.js
+++ CNN/Conv/PointDepthPoint.js
@@ -1037,21 +1037,21 @@ class Base extends ReturnOrClone.Base {
 
 
 //!!! ...unfinished... (2021/11/26) Problem: 
-        // Note: pointwise21ChannelCount is always not zero and contains the higher-pass-through part.
-        let outputChannelCount_lowerHalf_pointwise21 = Math.ceil( this.pointwise21ChannelCount / 2 );
-
-        // In this case, the input0's higher half is always the same as lower half of pointwise21's result. This guarantees
-        // they could be concat-shuffle-split.
-        let inputChannelCount_higherHalf_pointwise1 = outputChannelCount_lowerHalf_pointwise21;
-        inputChannelCount_lowerHalf_pointwise1 = this.channelCount0_pointwise1Before - inputChannelCount_higherHalf_pointwise1;
-        
-        if ( this.pointwise1ChannelCount > 0 ) {
-//???
-          outputChannelCount_lowerHalf_pointwise1 = this.pointwise1ChannelCount - inputChannelCount_lowerHalf_pointwise1;
-        } else {
-//???
-          outputChannelCount_lowerHalf_pointwise1 = inputChannelCount_lowerHalf_pointwise1; // So that both are positive.
-        }
+//         // Note: pointwise21ChannelCount is always not zero and contains the higher-pass-through part.
+//         let outputChannelCount_lowerHalf_pointwise21 = Math.ceil( this.pointwise21ChannelCount / 2 );
+//
+//         // In this case, the input0's higher half is always the same as lower half of pointwise21's result. This guarantees
+//         // they could be concat-shuffle-split.
+//         let inputChannelCount_higherHalf_pointwise1 = outputChannelCount_lowerHalf_pointwise21;
+//         inputChannelCount_lowerHalf_pointwise1 = this.channelCount0_pointwise1Before - inputChannelCount_higherHalf_pointwise1;
+//      
+//         if ( this.pointwise1ChannelCount > 0 ) {
+// //???
+//           outputChannelCount_lowerHalf_pointwise1 = this.pointwise1ChannelCount - inputChannelCount_lowerHalf_pointwise1;
+//         } else {
+// //???
+//           outputChannelCount_lowerHalf_pointwise1 = inputChannelCount_lowerHalf_pointwise1; // So that both are positive.
+//         }
 
       }
 
