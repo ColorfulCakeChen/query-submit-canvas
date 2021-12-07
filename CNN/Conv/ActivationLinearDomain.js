088c5fe9c3b4d53063d2240ee4eb6fe4c2bee600
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-12-07 15:50:10 +0800

    Update ActivationLinearDomain.js

--- CNN/Conv/ActivationLinearDomain.js
+++ CNN/Conv/ActivationLinearDomain.js
@@ -5,17 +5,6 @@ import * as ValueDesc from "../Unpacker/ValueDesc.js";
 /**
  * Describe the input value lower and upper bounds of an activation function for keeping the output almost linear.
  *
- * For example,
- *   - RELU6 is linear between[ 0, 6 ].
- *   - SIGMOID is alomost linear between[ -0.125, +0.125 ].
- *   - TANH is almost linear between[ -0.125, +0.125 ].
- *   - COS is almost linear between[ -( ( PI / 2 ) + 0.025 ), -( ( PI / 2 ) - 0.025 ) ].
- *   - SIN is almost linear between[ -0.025, +0.025 ].
- *   - RELU is linear between[ 0, 6 ].
- *
- *
- *
- *
  *
  *
  *
@@ -39,3 +28,26 @@ class Base {
   }
 
 }
+
+
+/**
+ * Predefined linear domain for every activation function.
+ *
+ *
+ * For example,
+ *   - RELU6 is linear between[ 0, 6 ].
+ *   - SIGMOID is alomost linear between[ -0.125, +0.125 ].
+ *   - TANH is almost linear between[ -0.125, +0.125 ].
+ *   - COS is almost linear between[ -( ( PI / 2 ) + 0.025 ), -( ( PI / 2 ) - 0.025 ) ].
+ *   - SIN is almost linear between[ -0.025, +0.025 ].
+ *   - RELU is linear between[ 0, 6 ].
+ *
+ */
+Base.PredefinedById = new Map( [
+  [ ValueDesc.ActivationFunction.Singleton.Ids.RELU6,   new Base(                            0,                            6 ) ],
+  [ ValueDesc.ActivationFunction.Singleton.Ids.SIGMOID, new Base(                       -0.125,                       +0.125 ) ],
+  [ ValueDesc.ActivationFunction.Singleton.Ids.TANH,    new Base(                       -0.125,                       +0.125 ) ],
+  [ ValueDesc.ActivationFunction.Singleton.Ids.COS,     new Base( -( ( Math.PI / 2 ) + 0.025 ), -( ( Math.PI / 2 ) - 0.025 ) ) ],
+  [ ValueDesc.ActivationFunction.Singleton.Ids.SIN,     new Base(                       -0.025,                       +0.025 ) ],
+  [ ValueDesc.ActivationFunction.Singleton.Ids.RELU,    new Base(                            0,                            6 ) ],
+] );
