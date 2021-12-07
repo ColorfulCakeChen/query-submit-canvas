7e9a10d4b1eff0944e683e0ffb5c447b13669089
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-12-07 15:42:25 +0800

    Update ActivationLinearDomain.js

--- CNN/Conv/ActivationLinearDomain.js
+++ CNN/Conv/ActivationLinearDomain.js
@@ -5,6 +5,18 @@ import * as ValueDesc from "../Unpacker/ValueDesc.js";
 /**
  * Describe the input value lower and upper bounds of an activation function for keeping the output almost linear.
  *
+ * For example,
+ *   - RELU6 is linear between[ 0, 6 ].
+ *   - SIGMOID is alomost linear between[ -0.125, +0.125 ].
+ *   - TANH is almost linear between[ -0.125, +0.125 ].
+ *   - COS is almost linear between[ -( ( PI / 2 ) + 0.025 ), -( ( PI / 2 ) - 0.025 ) ].
+ *   - SIN is almost linear between[ -0.025, +0.025 ].
+ *   - RELU is linear between[ 0, 6 ].
+ *
+ *
+ *
+ *
+ *
  *
  *
  * @member {number} nActivationId
@@ -22,8 +34,8 @@ class Base {
   
   constructor( nActivationId, lowerBound, upperBound ) {
     this.nActivationId = nActivationId;
-    this.lowerBound = lowerBound;
-    this.upperBound = upperBound;
+    this.lowerBound = Math.min( lowerBound, upperBound ); // Confirm ( lowerBound <= upperBound ).
+    this.upperBound = Math.max( lowerBound, upperBound );
   }
 
 }
