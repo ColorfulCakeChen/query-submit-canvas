3c928e6dfd44db66b604a2a23f7e69537bd8afeb
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-12-09 14:30:34 +0800

    Update ValueDesc.js

--- CNN/Unpacker/ValueDesc.js
+++ CNN/Unpacker/ValueDesc.js
@@ -6,6 +6,8 @@ export { ActivationFunction };
 export { WhetherShuffleChannel };
 
 import * as ValueRange from "./ValueRange.js";
+import * as ActivationFunction from "./ActivationFunction.js";
+import * as Weights from "./Weights.js";
 
 
 /**
@@ -231,6 +233,14 @@ AvgMax_Or_ChannelMultiplier.Singleton = new AvgMax_Or_ChannelMultiplier;
 
 /**
  * Describe activation function parameter's id, range, name.
+ *
+ * For inputDomainLinear,
+ *   - RELU6 is linear between[ 0, 6 ].
+ *   - SIGMOID is alomost linear between[ -0.125, +0.125 ].
+ *   - TANH is almost linear between[ -0.125, +0.125 ].
+ *   - COS is almost linear between[ -( ( PI / 2 ) + 0.025 ), -( ( PI / 2 ) - 0.025 ) ].
+ *   - SIN is almost linear between[ -0.025, +0.025 ].
+ *   - RELU is linear between[ 0, 6 ].
  */
 class ActivationFunction extends Int {
 
@@ -239,10 +249,45 @@ class ActivationFunction extends Int {
     //   - NONE: Beware. It easily results in infinity value because it does not have upper bound.
     //   - RELU: Beware. It easily results in infinity value because it does not have upper bound.
     //   - SOFTPLUS: Avoid. Backend WASM does not support it.
+
+//!!! (2021/12/09 Remarked) Use ActivationFunction.Info
+//     super( 0, 6,
+//       [ "NONE",  "RELU6",  "SIGMOID",  "TANH",  "COS",  "SIN",  "RELU" ], //  "SOFTPLUS" ],
+//       [   null, tf.relu6, tf.sigmoid, tf.tanh, tf.cos, tf.sin, tf.relu ]  // tf.softplus ]
+//     );
+
     super( 0, 6,
       [ "NONE",  "RELU6",  "SIGMOID",  "TANH",  "COS",  "SIN",  "RELU" ], //  "SOFTPLUS" ],
-      [   null, tf.relu6, tf.sigmoid, tf.tanh, tf.cos, tf.sin, tf.relu ]  // tf.softplus ]
+
+      [
+        new ActivationFunction.Info( 0, null, null, null ),
+
+        new ActivationFunction.Info( 1, tf.relu6,
+          new FloatValue.Bounds( 0, 6 ), new FloatValue.Bounds( 0, 6 ) ),
+
+        new ActivationFunction.Info( 2, tf.sigmoid,
+          new FloatValue.Bounds( -0.125, +0.125 ), new FloatValue.Bounds( 0, 1 ) ),
+
+        new ActivationFunction.Info( 3, tf.tanh,
+          new FloatValue.Bounds( -0.125, +0.125 ), new FloatValue.Bounds( -1, +1 ) ),
+
+        new ActivationFunction.Info( 4, tf.cos,
+          new FloatValue.Bounds( -( ( Math.PI / 2 ) + 0.025 ), -( ( Math.PI / 2 ) - 0.025 ) ), new FloatValue.Bounds( -1, +1 ) ),
+
+        new ActivationFunction.Info( 5, tf.sin,
+          new FloatValue.Bounds( -0.025, +0.025 ), new FloatValue.Bounds( -1, +1 ) ),
+
+        // Note: The input linear domain and output range of RELU are [ 0, +INF ].
+        //       However, for PointDepthPoint, using Weights.Base.ValueBounds.upper as upper bound should be sufficient.
+        new ActivationFunction.Info( 6, tf.relu,
+          new FloatValue.Bounds( 0, Weights.Base.ValueBounds.upper ), new FloatValue.Bounds( 0, Weights.Base.ValueBounds.upper ) ),
+
+        //new ActivationFunction.Info( 7, tf.softplus,
+        //  new Base( 0, 6 ), Weights.Base.ValueBounds ),
+
+      ]
     );
+
   }
 
 }
