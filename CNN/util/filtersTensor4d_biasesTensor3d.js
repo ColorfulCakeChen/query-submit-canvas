3b25507a8ad503bd32a17135d4583b33a4f235b9
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-12-10 11:24:12 +0800

    Create filtersTensor4d_biasesTensor3d.js

---
+++ CNN/util/filtersTensor4d_biasesTensor3d.js
@@ -0,0 +1,23 @@
+export { filtersTensor4d_biasesTensor3d  };
+
+/**
+ * An object contains filtersTensor4d and biasesTensor3d, and a method to dispose them.
+ */
+class filtersTensor4d_biasesTensor3d {
+
+  /**
+   * Release the tensors.
+   */
+  disposeTensors() {
+    if ( this.filtersTensor4d ) {
+      this.filtersTensor4d.dispose();
+      this.filtersTensor4d = null;
+    }
+
+    if ( this.biasesTensor3d ) {
+      this.biasesTensor3d.dispose();
+      this.biasesTensor3d = null;
+    }
+  }
+
+}
