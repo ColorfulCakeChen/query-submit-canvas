135607800c4b9321a5506ef9fba63e2470976d99
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-12-10 14:38:17 +0800

    Update Depthwise_Base.js

--- CNN/Conv/Depthwise/Depthwise_Base.js
+++ CNN/Conv/Depthwise/Depthwise_Base.js
@@ -1,275 +1,11 @@
 export { Base };
 
-import * as FloatValue from "../Unpacker/FloatValue.js";
-import * as ValueDesc from "../Unpacker/ValueDesc.js";
-import * as Weights from "../Unpacker/Weights.js";
-import * as ReturnOrClone_Activation from "./ReturnOrClone_Activation.js";
-import { PadInfoCalculator, PassThrough } from "./Depthwise_PassThrough.js";
+import * as FloatValue from "../../Unpacker/FloatValue.js";
+import * as ValueDesc from "../../Unpacker/ValueDesc.js";
+import * as Weights from "../../Unpacker/Weights.js";
+import * as ReturnOrClone_Activation from "../ReturnOrClone_Activation.js";
+import { PassThrough } from "./Depthwise_PassThrough.js";
 import { ValueBounds } from "./Depthwise_ValueBounds.js";
-
-/**
- * According to input image size and depthwise convolution parameters, calculate the padding information of the depthwise convolution.
- *
- * @member {number} imageInHeight         Input image height.
- * @member {number} imageInWidth          Input image width.
- * @member {number} imageInDepth          Input image channel count.
- * @member {number} AvgMax_Or_ChannelMultiplier   Depthwise operation. (ValueDesc.AvgMax_Or_ChannelMultiplier)
- * @member {number} filterHeight          The height of the depthwise convolution's filter.
- * @member {number} filterWidth           The width of the depthwise convolution's filter.
- * @member {number} stridesPad            The strides and padding of depthwise convolution. (PointDepthPoint.Params.depthwiseStridesPad)
- *
- * @member {number} channelMultiplier     The channel multiplier of the depthwise operation (according to AvgMax_Or_ChannelMultiplier).
- *
- * @member {number} dilationHeight        The depthwise filters's dilation across height dimension.
- * @member {number} dilationWidth         The depthwise filters's dilation across width dimension.
- *
- * @member {number} effectFilterHeight    The effect height of the depthwise convolution's filter including the dilationHeight.
- * @member {number} effectFilterWidth     The effect width of the depthwise convolution's filter including the dilationWidth.
- * @member {number} effectFilterSize      The effect size of the depthwise convolution's filter. (= effectFilterHeight * effectFilterWidth)
- *
- * @member {number} strides               The strides along the image's height and width dimension (according to stridesPad).
- * @member {number} stridesHeight         The strides along the image's height dimension (according to stridesPad).
- * @member {number} stridesWidth          The strides along the image's width dimension (according to stridesPad).
-
- * @member {number} pad                   The padding along the image's height and width dimension (according to stridesPad).
- * @member {number} padHeight             The padding along the input image's height dimension.
- * @member {number} padHeightTop          The padding along the input image's height dimension at the top.
- * @member {number} padHeightBottom       The padding along the input image's height dimension at the bottom.
- * @member {number} padWidth              The padding along the input image's width dimension.
- * @member {number} padWidthLeft          The padding along the input image's width dimension at the left.
- * @member {number} padWidthRight         The padding along the input image's width dimension at the right.
- *
- * @member {number} imageOutHeight        Output image height.
- * @member {number} imageOutWidth         Output image width.
- * @member {number} imageOutDepth         Output image channel count.
- * @member {number} imageOutLength        Output image elements count (= ( imageOutHeight * imageOutWidth * imageOutDepth ) ).
- */
-class PadInfoCalculator {
-  
-  constructor( imageInHeight, imageInWidth, imageInDepth, AvgMax_Or_ChannelMultiplier, filterHeight, filterWidth, stridesPad ) {
-    this.imageInHeight = imageInHeight;
-    this.imageInWidth = imageInWidth;
-    this.imageInDepth = imageInDepth;
-    this.AvgMax_Or_ChannelMultiplier = AvgMax_Or_ChannelMultiplier;
-    this.filterHeight = filterHeight;
-    this.filterWidth = filterWidth;
-    this.stridesPad = stridesPad;
-
-//!!! ...unfinished... (2021/03/17) What about ( filterHeight <= 0 ) or ( filterWidth <= 0 )?
-
-    this.channelMultiplier = AvgMax_Or_ChannelMultiplier;
-    if (   ( ValueDesc.AvgMax_Or_ChannelMultiplier.Singleton.Ids.AVG === AvgMax_Or_ChannelMultiplier )
-        || ( ValueDesc.AvgMax_Or_ChannelMultiplier.Singleton.Ids.MAX === AvgMax_Or_ChannelMultiplier ) ) {
-      this.channelMultiplier = 1;
-    }
-
-    // Strides and Padding.
-    switch ( stridesPad ) {
-      case 0:  this.strides = 1; this.pad = "valid"; break;
-      default:
-      case 1:  this.strides = 1; this.pad = "same";  break;
-      case 2:  this.strides = 2; this.pad = "same";  break;
-    }
-
-    // Assume strides width equals strides height.
-    this.stridesHeight = this.strides;
-    this.stridesWidth = this.strides;
-
-    // Currently, we can only handle dilation = 1.
-    this.dilationHeight = 1;
-    this.dilationWidth = 1;
-
-    // Effect filter size (includes dilation).
-    this.effectFilterHeight = this.dilationHeight * ( this.filterHeight - 1 ) + 1;
-    this.effectFilterWidth =  this.dilationWidth  * ( this.filterWidth  - 1 ) + 1;
-    this.effectFilterSize = this.effectFilterHeight * this.effectFilterWidth;
-
-    // (The following codes for output image height and width and padding calculation are copied from
-    // https://github.com/tensorflow/tfjs/blob/tfjs-v3.8.0/tfjs-core/src/ops/conv_util.ts)
-    {
-      // Determine output image height and width without padding.
-      if ( this.pad == "valid" ) {
-        this.imageOutHeight = Math.ceil( ( imageInHeight - this.effectFilterHeight + 1 ) / this.stridesHeight );
-        this.imageOutWidth =  Math.ceil( ( imageInWidth  - this.effectFilterWidth  + 1 ) / this.stridesWidth  );
-
-        this.padHeight = this.padHeightTop = this.padHeightBottom = this.padWidth = this.padWidthLeft = this.padWidthRight = 0;
-
-      // Determine output image height and width with padding around the input image height and width.
-      } else if ( this.pad == "same" ) {
-        this.imageOutHeight = Math.ceil( imageInHeight / this.stridesHeight );
-        this.imageOutWidth =  Math.ceil( imageInWidth  / this.stridesWidth  );
-
-        this.padHeight = Math.max( 0, ( this.imageOutHeight - 1 ) * this.stridesHeight + this.effectFilterHeight - imageInHeight );
-        this.padWidth =  Math.max( 0, ( this.imageOutWidth  - 1 ) * this.stridesWidth  + this.effectFilterWidth  - imageInWidth  );
-
-        this.padHeightTop =    Math.floor( this.padHeight / 2 );
-        this.padHeightBottom = this.padHeight - this.padHeightTop;
-        this.padWidthLeft =    Math.floor( this.padWidth /  2 );
-        this.padWidthRight =   this.padWidth  - this.padWidthLeft;
-      }
-    }
-
-    this.imageOutDepth = imageInDepth * this.channelMultiplier;
-    this.imageOutLength = ( this.imageOutHeight * this.imageOutWidth * this.imageOutDepth );
-  }
-
-  /**
-   * @param {number} filterValue
-   *   The (non-zero) value used in the pass-through depthwise convolution filter. Default is 1.
-   *
-   * @return {number[]} 
-   *   Return the depthwise convolution filters which could pass the input to output unchangely.
-   */
-  generate_PassThrough_FiltersArray( filterValue = 1 ) {
-
-    if (   ( ValueDesc.AvgMax_Or_ChannelMultiplier.Singleton.Ids.AVG === this.AvgMax_Or_ChannelMultiplier )
-        || ( ValueDesc.AvgMax_Or_ChannelMultiplier.Singleton.Ids.MAX === this.AvgMax_Or_ChannelMultiplier ) ) {
-      return null; // The depthwise filter of AVG pooling and MAX pooling can not be manipulated.
-    }
-
-    // Make up a depthwise convolution filter.
-    let depthwiseFiltersArray = new Array( this.filterHeight * this.filterWidth * this.imageInDepth * this.channelMultiplier );
-
-    // There is only one position (inside the effect depthwise filter) with value one. All other positions of the filter should be zero.
-    let oneEffectFilterY = this.padHeightTop;
-    let oneEffectFilterX = this.padWidthLeft;
-
-    // Note: Unfortunately, this does not work for ( dilation > 1 ). So, only ( dilation == 1 ) is supported.
-    for ( let inChannel = 0; inChannel < this.imageInDepth; ++inChannel ) {
-
-      for ( let outChannelSub = 0; outChannelSub < this.channelMultiplier; ++outChannelSub ) {
-
-        for ( let filterY = 0, effectFilterY = 0; filterY < this.filterHeight; ++filterY ) {
-          for ( let dilationFilterY = 0; dilationFilterY < this.dilationHeight; ++dilationFilterY, ++effectFilterY ) {
-            let filterIndexBaseX = ( filterY * this.filterWidth );
-
-            for ( let filterX = 0, effectFilterX = 0; filterX < this.filterWidth; ++filterX ) {
-              for ( let dilationFilterX = 0; dilationFilterX < this.dilationWidth; ++dilationFilterX, ++effectFilterX ) {
-
-                // The filter's dilation part can not be manipulated. (They are always zero.)
-                if ( ( 0 != dilationFilterY ) || ( 0 != dilationFilterX ) )
-                  continue;
-
-                let filterIndexBaseC = ( ( filterIndexBaseX + filterX ) * this.imageOutDepth );
-                let filterIndexBaseSubC = filterIndexBaseC + ( inChannel * this.channelMultiplier );
-
-                let filterIndex = filterIndexBaseSubC + outChannelSub;
-
-                if ( ( effectFilterY == oneEffectFilterY ) && ( effectFilterX == oneEffectFilterX ) ) {
-//!!! (2021/12/07 Remarked)
-//                  depthwiseFiltersArray[ filterIndex ] = 1; // The only one position with value one.
-                  depthwiseFiltersArray[ filterIndex ] = filterValue; // The only one position with value non-zero.
-                  filterValue
-                } else {
-                  depthwiseFiltersArray[ filterIndex ] = 0; // All other positions of the filter are value zero.
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-
-    return depthwiseFiltersArray;
-  }
-
-}
-
-
-//!!! ...unfinished... (2021/12/07)
-// Problem: Although depthwise (and pointwise) convolution could be past-through, the activation function will destroy the past-through result.
-
-
-//!!! ...unfinished... (2021/12/03) perhaps, provide a pool for DepthwisePassThrough of various size so that needs not regenerate again and again.
-
-/**
- * A depthwise convolution and bias which just pass the input to output.
- *
- * It is usually used in passing the higher half channels of the input to output (for achieving ShuffleNetV2_ByMopbileNetV1's body/tail).
- *
- *
- * @member {number} filterValue
- *   The value used as the pass-through depthwise convolution filter. Default is 1. If there will be no activation function after this
- * pass-through operation, value 1 is enough. However, if there wiil be an activation function, this past-through result might be
- * destroyed by the activation function. In order to alleviate this issue, a non-one filter value should be used. For example, if
- * every input value's range is [ 0,255 ] and RELU6 will be used as activation function, using 0.015625 (= 1 / 64 ) as filterValue is
- * appropriate because input values will be shrinked from [ 0, 255 ] into [ 0, 3.984375 ] which will still be kept linear by RELU6.
- *
- * @member {number} biasValue
- *   The value used as the pass-through bias (used only if ( bBias == true ) ). Default is 0. If there will be no activation function
- * after this pass-through operation, value 0 is enough. However, if there wiil be an activation function, this past-through result
- * might be destroyed by the activation function. In order to alleviate this issue, a non-zero bias value should be used. For example,
- * if every input value's range is [ -2, +2 ] and RELU6 will be used as activation function, using +2 as biasValue is appropriate
- * because input values will be shifted from [ -2, +2 ] into [ 0, 4 ] which will still be kept linear by RELU6.
- *
- * @member {number[]} depthwiseFiltersArray
- *   The depthwise convolution filter which could pass the input to output unchangely.
- *
- * @member {boolean} bInitOk
- *   If true, this object initialized (i.e. constructor()) successfully.
- */
-class PassThrough extends PadInfoCalculator {
-
-  /**
-   */
-  constructor(
-    imageInHeight, imageInWidth, imageInDepth, AvgMax_Or_ChannelMultiplier, filterHeight, filterWidth, stridesPad, bBias,
-    filterValue = 1, biasValue = 0 ) {
-
-    super( imageInHeight, imageInWidth, imageInDepth, AvgMax_Or_ChannelMultiplier, filterHeight, filterWidth, stridesPad );
-
-    this.bBias = bBias;
-    this.filterValue = filterValue;
-    this.biasValue = biasValue;
-
-    this.depthwiseFiltersArray = this.generate_PassThrough_FiltersArray( filterValue );
-
-    let filtersShape = [ this.filterHeight, this.filterWidth, this.imageInDepth, this.channelMultiplier ];
-
-    // Generate depthwise filters for just pass input to output.
-    this.filtersTensor4d = tf.tensor( this.depthwiseFiltersArray, filtersShape );
-
-    // Generate bias for just adding zero. (i.e. equals no bias).
-    if ( this.bBias ) {
-      let biasesShape =  [ 1, 1, this.imageOutDepth ];
-//!!! (2021/12/07 Remarked)
-//      this.biasesTensor3d = tf.zero( biasesShape );
-      this.biasesTensor3d = tf.fill( biasesShape, biasValue );
-    }
-
-    this.bInitOk = true;
-  }
-
-  disposeTensors() {
-    if ( this.filtersTensor4d ) {
-      this.filtersTensor4d.dispose();
-      this.filtersTensor4d = null;
-    }
-
-    if ( this.biasesTensor3d ) {
-      this.biasesTensor3d.dispose();
-      this.biasesTensor3d = null;
-    }
-  }
-}
-
-
-//!!! ...unfinished... (2021/12/08)
-/**
- *
- */
-class ValueBounds {
-  constructor( inputChannelCount, AvgMax_Or_ChannelMultiplier, filterHeight, filterWidth, bBias, nActivationId ) {
-
-//!!! ...unfinished... (2021/12/08)
-//     this.input = new FloatValue.Bounds( ??? );
-//     this.filterAfter = new FloatValue.Bounds( ??? );
-//     this.biasAfter = new FloatValue.Bounds( ??? );
-//     this.activationAfter = new FloatValue.Bounds( ??? );
-//     this.output = new FloatValue.Bounds( ??? );
-  }
-}
-
 
 /**
  * Handle depthwise convolution, bias and activation.
@@ -280,6 +16,9 @@ class ValueBounds {
  * @member {number} byteOffsetEnd
  *   The position which is ended to (non-inclusive) extract from inputFloat32Array.buffer by init(). Where to extract next weights.
  * Only meaningful when ( this.bInitOk == true ).
+ *
+ * @member {ValueBounds} valueBounds
+ *   The element value bounds of input, beforeActivation, and output for this depthwise convolution.
  *
  * @member {number} inputHeight
  *   The height of input image. Only used when ( bHigherHalfDifferent == true ). It and inputWidth should be both positive or both not.
@@ -363,11 +102,17 @@ class ValueBounds {
  */
 class Base extends ReturnOrClone_Activation.Base {
 
+  /**
+   * @param {FloatValue.Bounds} inputValueBounds
+   *   The bounds of the input element value. Or say, the domain of this pointwise convolution.
+   */
   constructor(
+    inputValueBounds,
     inputChannelCount, AvgMax_Or_ChannelMultiplier, filterHeight, filterWidth, stridesPad, bBias, nActivationId,
     bHigherHalfDifferent, inputHeight, inputWidth, inputChannelCount_lowerHalf ) {
 
     super();
+    this.valueBounds = new ValueBounds( inputValueBounds );
     this.inputChannelCount = inputChannelCount;
     this.AvgMax_Or_ChannelMultiplier = AvgMax_Or_ChannelMultiplier;
     this.filterHeight = filterHeight;
@@ -434,54 +179,45 @@ class Base extends ReturnOrClone_Activation.Base {
       case 2:  this.strides = 2; this.pad = "same";  break;
     }
 
+    // 1.
     Base.Setup_bDepthwise_pfn.call( this );
 
+    let bExtractOk;
     if ( !this.bDepthwise ) {
-      this.bInitOk = true;
-      return true; // no operation at all. No depthwise (e.g. zero or negative number) (so no channel multiplier, too).
-    }
+      bExtractOk = true; // 2. no operation at all. No depthwise (e.g. zero or negative number) (so no channel multiplier, too).
+
+    } else {
 
 //!!! ...unfinished... (2021/11/12) What if ( bHigherHalfDifferent == true ) when average/maximum pooling?
 // when ValueDesc.AvgMax_Or_ChannelMultiplier.Singleton.Ids.NONE?
 
-    if ( this.bDepthwiseAvg || this.bDepthwiseMax ) { // 1. Depthwise by AVG or MAX pooling (so no channel multiplier).
-      this.bInitOk = Base.extractAs_AvgMaxPooling.call( this, inputFloat32Array );
+      if ( this.bDepthwiseAvg || this.bDepthwiseMax ) { // 3. Depthwise by AVG or MAX pooling (so no channel multiplier).
+        bExtractOk = Base.extractAs_AvgMaxPooling.call( this, inputFloat32Array );
 
-    } else if ( this.bDepthwiseConv ) { // 2. Depthwise by convolution (with channel multiplier).
+      } else if ( this.bDepthwiseConv ) { // 4. Depthwise by convolution (with channel multiplier).
 
-      if ( this.bHigherHalfDifferent == true ) {
+        if ( this.bHigherHalfDifferent == true ) {
 
-        if ( ( this.inputHeight <= 0 ) || ( this.inputWidth <= 0 ) ) { // 2.1 i.e. bHigherHalfDepthwise2
-          this.bInitOk = Base.extractAs_HigherHalfDepthwise2.call( this, inputFloat32Array );
+          if ( ( this.inputHeight <= 0 ) || ( this.inputWidth <= 0 ) ) { // 4.1 i.e. bHigherHalfDepthwise2
+            bExtractOk = Base.extractAs_HigherHalfDepthwise2.call( this, inputFloat32Array );
 
-        } else { // 2.2 ( ( this.inputHeight > 0 ) && ( this.inputWidth > 0 ) ), i.e. bHigherHalfPassThrough
-          this.bInitOk = Base.extractAs_HigherHalfPassThrough.call( this, inputFloat32Array );
+          } else { // 4.2 ( ( this.inputHeight > 0 ) && ( this.inputWidth > 0 ) ), i.e. bHigherHalfPassThrough
+            bExtractOk = Base.extractAs_HigherHalfPassThrough.call( this, inputFloat32Array );
+          }
+
+        } else { // 4.3 Normal depthwise convolution.
+          bExtractOk = Base.extractAs_NormalDepthwise.call( this, inputFloat32Array );
         }
-
-      } else { // 2.3 Normal depthwise convolution.
-        this.bInitOk = Base.extractAs_NormalDepthwise.call( this, inputFloat32Array );
       }
     }
 
-//!!! (2021/12/03 Remarked) tensorWeightCountTotal become get property.
-//    // Verify the total weight count.
-//     {
-//       let tensorWeightCountTotal = 0;
-//       {
-//         tensorWeightCountTotal += tf.util.sizeFromShape( this.filtersTensor4d.shape );
-//
-//         if ( this.biasesTensor3d ) {
-//           tensorWeightCountTotal += tf.util.sizeFromShape( this.biasesTensor3d.shape );
-//         }
-//       }
-//
-//       tf.util.assert( ( this.tensorWeightCountTotal == tensorWeightCountTotal ),
-//         `Depthwise.Base.init(): `
-//           + `this.tensorWeightCountTotal ( ${this.tensorWeightCountTotal} ) should be `
-//           + `( ${tensorWeightCountTotal} ).`
-//       );
-//     }
+//!!! ...unfinished... (2021/12/10) How about avg/max pooling?
+    // 5. Determine output value bounds.
+    if ( bExtractOk ) {
+      this.valueBounds.set_by( this.filtersTensor4d, this.biasesTensor3d, this.nActivationId );
+    }
 
+    this.bInitOk = bExtractOk;
     return this.bInitOk;
   }
 
