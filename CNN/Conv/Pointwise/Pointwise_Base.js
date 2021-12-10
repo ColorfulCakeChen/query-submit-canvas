a4032041a87d368ce923392c2bc7536622da0575
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-12-10 14:29:35 +0800

    Update Pointwise_Base.js

--- CNN/Conv/Pointwise/Pointwise_Base.js
+++ CNN/Conv/Pointwise/Pointwise_Base.js
@@ -1,10 +1,10 @@
 export { Base };
 
-import * as FloatValue from "../Unpacker/FloatValue.js";
-import * as ValueDesc from "../Unpacker/ValueDesc.js";
-import * as Weights from "../Unpacker/Weights.js";
-import * as ReturnOrClone_Activation from "./ReturnOrClone_Activation.js";
-import * as ChannelShuffler from "./ChannelShuffler.js";
+import * as FloatValue from "../../Unpacker/FloatValue.js";
+import * as ValueDesc from "../../Unpacker/ValueDesc.js";
+import * as Weights from "../../Unpacker/Weights.js";
+import * as ReturnOrClone_Activation from "../ReturnOrClone_Activation.js";
+import * as ChannelShuffler from "../ChannelShuffler.js";
 import { PassThrough, AllZeros } from "./Pointwise_PassThrough.js";
 import { ValueBounds } from "./Pointwise_ValueBounds.js";
 
