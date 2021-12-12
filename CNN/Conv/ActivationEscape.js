921e969dd242938674dec7e03181d984fb542e68
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-12-12 14:53:44 +0800

    Update ActivationEscape.js

--- CNN/Conv/ActivationEscape.js
+++ CNN/Conv/ActivationEscape.js
@@ -20,7 +20,12 @@ class ScaleTranslateSet {
     this.undo = new FloatValue.ScaleTranslate();
   }
 
-//!!! ...unfinished... (2021/12/12)
+  /** Reset all scale-translate values. Default is ( scale = 1, translate = 0 ). */
+  reset( scale = 1, translate = 0 ) {
+    this.doWithoutPreviousUndo.set_scale_translate( scale , translate );
+    this.do.set_scale_translate( scale , translate );
+    this.undo.set_scale_translate( scale , translate );
+  }
 
 }
 
