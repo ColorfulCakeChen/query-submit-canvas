4a859d7a506fd34a647bc243e7a578c407f33017
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-12-12 14:43:11 +0800

    Create ActivationEscape.js

---
+++ CNN/Conv/ActivationEscape.js
@@ -0,0 +1,24 @@
+export { ScaleTranslateSet };
+
+import * as FloatValue from "../../Unpacker/FloatValue.js";
+import * as ValueDesc from "../../Unpacker/ValueDesc.js";
+
+
+/**
+ * Several scale-translate for escaping a value bounds from being activated (i.e. being non-linearized) by activation function.
+ *
+ *
+ * @member {FloatValue.ScaleTranslate} beforeActivation_to_activationLinearDomain_ScaleTranslate
+ *   The scale-translate for letting beforeActivation bounds moving into the linear domain of the activation function. That is,
+ * for letting beforeActivation escape from activation function's non-linear domain.
+ */
+class ScaleTranslateSet {
+
+//!!! ...unfinished... (2021/12/12)
+// class ActivationEscape.ScaleTranslateSet
+//   .doWithoutPreviousUndo
+//   .do
+//   .undo
+
+}
+
