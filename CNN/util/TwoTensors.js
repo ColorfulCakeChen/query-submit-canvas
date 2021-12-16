93360b4272b9f27d753e90c266a00dc71cf323ce
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-12-16 21:56:31 +0800

    Update TwoTensors.js

--- CNN/util/TwoTensors.js
+++ CNN/util/TwoTensors.js
@@ -1,9 +1,17 @@
 export { filtersTensor4d_biasesTensor3d };
 
 /**
- * An object contains filtersTensor4d and biasesTensor3d, and a method to dispose them.
+ * A function accepts a Base class and returns a sub-class of Base.
+ *
+ * @param {Class} Base
+ *   The base class to be inherited.
+ *
+ * @return {Class}
+ *   Return a sub-class of Base. The returned sub-class contains filtersTensor4d and biasesTensor3d, and a method to dispose them.
  */
-class filtersTensor4d_biasesTensor3d {
+let filtersTensor4d_biasesTensor3d = Base => class extends Base {
+//!!! (2021/12/16 Remarked)
+//class filtersTensor4d_biasesTensor3d {
 
   /**
    * Release the tensors.
