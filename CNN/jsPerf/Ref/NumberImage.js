a32faebfaa568233cf9752c8c6918d56e6dd6fc1
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-12-16 10:10:40 +0800

    Create NumberImage.js

---
+++ CNN/jsPerf/Ref/NumberImage.js
@@ -0,0 +1,28 @@
+export { Base };
+
+import * as ConvBiasActivation from "../../../Conv/ConvBiasActivation.js";
+
+/**
+ * Image composed from numbers. For testing.
+ *
+ *
+ * @member {number}   height    Image height
+ * @member {number}   width     Image width
+ * @member {number}   depth     Image channel count
+ * @member {number[]|Float32Array} dataArray Image data
+ *
+ * @member {ConvBiasActivation.ValueBoundsSet} valueBoundsSet
+ *   The element value bounds set of the pointwise or depthwise convolution.
+ */
+class Base {
+  
+  constructor( height, width, depth, dataArray, valueBoundsSet ) {
+    this.height = height;
+    this.width = width;
+    this.depth = depth;
+    this.dataArray = dataArray;
+    this.valueBoundsSet = valueBoundsSet;
+  }
+
+}
+ 
