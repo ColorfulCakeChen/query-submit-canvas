292981c4312f50be6569df937c9022c4b61eaef5
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-12-27 12:40:02 +0800

    Update Depthwise_PadInfoCalculator.js

--- CNN/Conv/Depthwise/Depthwise_PadInfoCalculator.js
+++ CNN/Conv/Depthwise/Depthwise_PadInfoCalculator.js
@@ -205,59 +205,5 @@ let PadInfoCalculator = ( Base = Object ) => class extends Base {
     return depthwiseFiltersArray;
   }
 
-//!!! ...unfinished... (2021/12/26) For testing to find out how to arrange the for-loop so that the filterIndex is in sequence order.
-// This will be used how to extract depthwise filters value faster.
-
-  test_findOut_filterIndex_inSequence() {
-
-//    let filterIndexArray = new Array( this.filterHeight * this.filterWidth * this.inputChannelCount * this.channelMultiplier );
-    let filterIndexArray = new Array();
-
-    for ( let filterY = 0, effectFilterY = 0; filterY < this.filterHeight; ++filterY ) {
-      for ( let dilationFilterY = 0; dilationFilterY < this.dilationHeight; ++dilationFilterY, ++effectFilterY ) {
-        let filterIndexBaseX = ( filterY * this.filterWidth );
-
-        for ( let filterX = 0, effectFilterX = 0; filterX < this.filterWidth; ++filterX ) {
-          for ( let dilationFilterX = 0; dilationFilterX < this.dilationWidth; ++dilationFilterX, ++effectFilterX ) {
-
-            // The filter's dilation part needs not be extracted from weights array. (They are always zero.)
-            if ( ( 0 != dilationFilterY ) || ( 0 != dilationFilterX ) )
-              continue;
-
-            let filterIndexBaseC = ( ( filterIndexBaseX + filterX ) * this.outputChannelCount );
-
-            for ( let inChannel = 0; inChannel < this.inputChannelCount; ++inChannel ) {
-              let filterIndexBaseSubC = filterIndexBaseC + ( inChannel * this.channelMultiplier );
-
-              for ( let outChannelSub = 0; outChannelSub < this.channelMultiplier; ++outChannelSub ) {
-                let filterIndex = filterIndexBaseSubC + outChannelSub;
-
-                filterIndexArray.push( filterIndex );
-                //console.log( `${filterIndex}, ` );
-
-              }
-            }
-          }
-        }
-      }
-    }
-
-    return filterIndexArray;
-    //debugger;
-    
-// let inputHeight = 10;
-// let inputWidth = 10;
-// let inputChannelCount = 2;
-// let AvgMax_Or_ChannelMultiplier = 2; //2;
-// let filterHeight = 2; //3;
-// let filterWidth = 2; //3;
-// let stridesPad = 0;
-//
-// let padInfo = new ( PadInfoCalculator() )( inputHeight, inputWidth, inputChannelCount, AvgMax_Or_ChannelMultiplier, filterHeight, filterWidth, stridesPad ) ;
-// let filterIndexArray = padInfo.test_findOut_filterIndex_inSequence();
-// console.log( filterIndexArray );
-
-  }
-
 }
 
