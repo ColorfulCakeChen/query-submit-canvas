cc0155a96fbd58cb9a4b5c587baa6a15448f0599
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-12-27 12:38:11 +0800

    Create Depthwise_FiltersArray_BiasesArray.js

---
+++ CNN/Conv/Depthwise/Depthwise_FiltersArray_BiasesArray.js
@@ -0,0 +1,33 @@
+export { FiltersArray_BiasesArray };
+
+import { PadInfoCalculator } from "./Depthwise_PadInfoCalculator.js";
+
+/**
+ * Extract depthwise convolution filters and biases.
+ *
+ * @member {number} byteOffsetBegin
+ *   The position which is started (inclusive) to extract from inputFloat32Array.buffer by init(). This is relative to the
+ * inputFloat32Array.buffer (not to the inputFloat32Array.byteOffset).
+ *
+ * @member {number} byteOffsetEnd
+ *   The position which is ended to (non-inclusive) extract from inputFloat32Array.buffer by init(). Where to extract next weights.
+ * Only meaningful when ( this.bInitOk == true ). This is relative to the inputFloat32Array.buffer (not to the inputFloat32Array.byteOffset).
+ *
+ *
+ * @member {number[]} filtersShape
+ *   The shape of the depthwise convolution filters array.
+ *
+ * @member {number[]} biasesShape
+ *   The shape of the depthwise convolution biases array.
+ *
+ * @member {number[]} filtersArray
+ *   The depthwise convolution filters array.
+ *
+ * @member {number[]} biasesArray
+ *   The depthwise convolution biases array.
+ *
+ * @see PadInfoCalculator
+ */
+let FiltersArray_BiasesArray = ( Base = Object ) => class extends PadInfoCalculator( Base ) {
+  
+}
