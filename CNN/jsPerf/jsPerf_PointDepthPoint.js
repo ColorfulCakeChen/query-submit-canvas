12474d6b50669b381cca29ec86013d4e958133d0
ColorfulCakeChen <colorfulcakechen@gmail.com>
2021-12-30 11:47:27 +0800

    Update jsPerf_PointDepthPoint.js

--- CNN/jsPerf/jsPerf_PointDepthPoint.js
+++ CNN/jsPerf/jsPerf_PointDepthPoint.js
@@ -14,6 +14,7 @@ import * as PointDepthPoint_Reference from "./Ref/PointDepthPoint_Reference.js";
 import * as PointDepthPoint_TestParams from "./Ref/PointDepthPoint_TestParams.js"; 
 import * as ImageSourceBag from "./Ref/ImageSourceBag.js"; 
 import * as NumberImage from "./Ref/NumberImage.js"; 
+import * as jsPerf_BoundsArray from "./jsPerf_BoundsArray.js"; 
 
 /**
  * Test CNN PointDepthPoint.
@@ -408,80 +409,7 @@ class HeightWidthDepth {
   }
 
   test_FloatValue_Bounds() {
-    class Case {
-      constructor( aLowerUpper, bLowerUpper, N, addedArray, multipledArray, aMultipledNArray ) {
-        this.aBounds = new FloatValue.Bounds( aLowerUpper[ 0 ], aLowerUpper[ 1 ] );
-        this.bBounds = new FloatValue.Bounds( bLowerUpper[ 0 ], bLowerUpper[ 1 ] );
-
-        this.addedBounds = this.aBounds.clone().add_Bounds( this.bBounds );
-        this.multipledBounds = this.aBounds.clone().multiply_Bounds( this.bBounds );
-        this.aMultipledNBounds = this.aBounds.clone().multiply_N( N );
-
-        this.assert_Bounds_byArray( "addedBounds", addedArray );
-        this.assert_Bounds_byArray( "multipledBounds", multipledArray );
-        this.assert_Bounds_byArray( "aMultipledNBounds", aMultipledNArray );
-
-//!!!
-        let BoundsArrayLength = 1;
-        this.aBoundsArray = ( new FloatValue.BoundsArray( BoundsArrayLength ) ).set_ aLowerUpper[ 0 ], aLowerUpper[ 1 ] ;
-        this.bBoundsArray = ( new FloatValue.BoundsArray( BoundsArrayLength ) ). bLowerUpper[ 0 ], bLowerUpper[ 1 ] );
-
-        this.addedBounds = this.aBounds.clone().add_Bounds( this.bBounds );
-        this.multipledBounds = this.aBounds.clone().multiply_Bounds( this.bBounds );
-        this.aMultipledNBounds = this.aBounds.clone().multiply_N( N );
-
-        this.assert_BoundsArray_byArray( "addedBoundsArray", addedArray );
-        this.assert_BoundsArray_byArray( "multipledBoundsArray", multipledArray );
-        this.assert_BoundsArray_byArray( "aMultipledNBoundsArray", aMultipledNArray );
-      }
-
-      assert_Bounds_byArray( strBoundsTestName, rhsArray ) {
-        this.assert_lower_or_upper( strBoundsTestName, "lower", rhsArray, 0 );
-        this.assert_lower_or_upper( strBoundsTestName, "upper", rhsArray, 1 );
-      }
-
-      assert_BoundsArray_byArray( strBoundsArrayTestName, rhsArray ) {
-        let lhsArrayIndex = 0;
-        this.assert_lowers_or_uppers( strBoundsArrayTestName, "lowers", lhsArrayIndex, rhsArray, 0 );
-        this.assert_lowers_or_uppers( strBoundsArrayTestName, "uppers", lhsArrayIndex, rhsArray, 1 );
-      }
-
-      assert_lower_or_upper( strBoundsTestName, lower_or_upper_name, rhsArray, rhsArrayIndex ) {
-        let thisValue = this[ strBoundsTestName ][ lower_or_upper_name ];
-        let rhsArrayValue = rhsArray[ rhsArrayIndex ];
-        tf.util.assert( thisValue == rhsArrayValue,
-          `test_FloatValue_Bounds(): Case.${strBoundsTestName}.${lower_or_upper_name} ( ${thisValue} ) should be ( ${rhsArrayValue} ).` );
-      }
-
-      assert_lowers_or_uppers( strBoundsArrayTestName, lowers_or_uppers_name, lhsArrayIndex, rhsArray, rhsArrayIndex ) {
-        let thisValue = this[ strBoundsArrayTestName ][ lowers_or_uppers_name ][ lhsArrayIndex ];
-        let rhsArrayValue = rhsArray[ rhsArrayIndex ];
-        tf.util.assert( thisValue == rhsArrayValue, `test_FloatValue_Bounds(): `
-          + `Case.${strBoundsArrayTestName}.${lowers_or_uppers_name}[ ${lhsArrayIndex} ] ( ${thisValue} ) should be ( ${rhsArrayValue} ).` );
-      }
-    }
-
-    let caseArray = [
-      new Case( [  1,  2 ], [  3,  4 ],  5, [  4,  6 ], [  3,  8 ], [   5,  10 ] ),
-      new Case( [ -1,  2 ], [  3,  4 ],  5, [  2,  6 ], [ -4,  8 ], [  -5,  10 ] ),
-      new Case( [  1, -2 ], [  3,  4 ],  5, [  1,  5 ], [ -8,  4 ], [ -10,   5 ] ),
-      new Case( [ -1, -2 ], [  3,  4 ],  5, [  1,  3 ], [ -8, -3 ], [ -10,  -5 ] ),
-
-      new Case( [  1,  2 ], [ -3,  4 ], -5, [ -2,  6 ], [ -6,  8 ], [ -10,  -5 ] ),
-      new Case( [ -1,  2 ], [ -3,  4 ], -5, [ -4,  6 ], [ -6,  8 ], [ -10,   5 ] ),
-      new Case( [  1, -2 ], [ -3,  4 ], -5, [ -5,  5 ], [ -8,  6 ], [  -5,  10 ] ),
-      new Case( [ -1, -2 ], [ -3,  4 ], -5, [ -5,  3 ], [ -8,  6 ], [   5,  10 ] ),
-
-      new Case( [  1,  2 ], [  3, -4 ],  5, [ -3,  5 ], [ -8,  6 ], [   5,  10 ] ),
-      new Case( [ -1,  2 ], [  3, -4 ],  5, [ -5,  5 ], [ -8,  6 ], [  -5,  10 ] ),
-      new Case( [  1, -2 ], [  3, -4 ],  5, [ -6,  4 ], [ -6,  8 ], [ -10,   5 ] ),
-      new Case( [ -1, -2 ], [  3, -4 ],  5, [ -6,  2 ], [ -6,  8 ], [ -10,  -5 ] ),
-
-      new Case( [  1,  2 ], [ -3, -4 ], -5, [ -3, -1 ], [ -8, -3 ], [ -10,  -5 ] ),
-      new Case( [ -1,  2 ], [ -3, -4 ], -5, [ -5, -1 ], [ -8,  4 ], [ -10,   5 ] ),
-      new Case( [  1, -2 ], [ -3, -4 ], -5, [ -6, -2 ], [ -4,  8 ], [  -5,  10 ] ),
-      new Case( [ -1, -2 ], [ -3, -4 ], -5, [ -6, -4 ], [  3,  8 ], [   5,  10 ] ),
-    ];
+    jsPerf_BoundsArray.test_FloatValue_Bounds();
   }
 
   test_Weights_Float32Array_RestrictedClone() {
