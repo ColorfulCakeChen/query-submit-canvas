b36f6bcd701c53babf4c8eb8de97485458cfa623
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-01-04 13:51:26 +0800

    Update Depthwise_FiltersArray_BiasesArray.js

--- CNN/Conv/Depthwise/Depthwise_FiltersArray_BiasesArray.js
+++ CNN/Conv/Depthwise/Depthwise_FiltersArray_BiasesArray.js
@@ -4,40 +4,40 @@ import * as FloatValue from "../../Unpacker/FloatValue.js";
 import * as ValueDesc from "../../Unpacker/ValueDesc.js";
 import { PadInfoCalculator } from "./Depthwise_PadInfoCalculator.js";
 
-// /**
-//  * Half channels information. Describe channel index range of lower half or higher half
-//  *
-//  * @member {boolean} bPassThrough
-//  *   If true, this is the half channels for pass-through input to output.
-//  */
-// class HalfPartInfo {
+/**
+ * Half channels information. Describe channel index range of lower half or higher half
+ *
+ * @member {boolean} bPassThrough
+ *   If true, this is the half channels for pass-through input to output.
+ */
+class HalfPartInfo {
 
-//   /**
-//    * @param {number} effectFilterY_passThrough, effectFilterX_passThrough
-//    *   For pass-through filters, there is only one position (inside the effect depthwise filter) with non-zero value. All other
-//    * positions of the filters should be zero.
-//    *   - Note: Unfortunately, the pass-through feature may not work for ( dilation > 1 ) because the non-zero-filter-value might
-//    *       be just at the dilation position which does not exist in a filter. So, only ( dilation == 1 ) is supported.
-//    *   - Negative value means this part is not for pass-through.
-//    */
-//   constructor( inChannelBegin, inChannelEnd, effectFilterY_passThrough = -1, effectFilterX_passThrough = -1 ) {
-//     this.inChannelBegin = inChannelBegin;
-//     this.inChannelEnd = inChannelEnd;
-//     this.effectFilterY_passThrough = effectFilterY_passThrough;
-//     this.effectFilterX_passThrough = effectFilterX_passThrough;
+  /**
+   * @param {number} effectFilterY_passThrough, effectFilterX_passThrough
+   *   For pass-through filters, there is only one position (inside the effect depthwise filter) with non-zero value. All other
+   * positions of the filters should be zero.
+   *   - Note: Unfortunately, the pass-through feature may not work for ( dilation > 1 ) because the non-zero-filter-value might
+   *       be just at the dilation position which does not exist in a filter. So, only ( dilation == 1 ) is supported.
+   *   - Negative value means this part is not for pass-through.
+   */
+  constructor( inChannelBegin, inChannelEnd, effectFilterY_passThrough = -1, effectFilterX_passThrough = -1 ) {
+    this.inChannelBegin = inChannelBegin;
+    this.inChannelEnd = inChannelEnd;
+    this.effectFilterY_passThrough = effectFilterY_passThrough;
+    this.effectFilterX_passThrough = effectFilterX_passThrough;
 
-//     this.bPassThrough = ( ( this.effectFilterY_passThrough >= 0 ) && ( this.effectFilterX_passThrough >= 0 ) );
-//   }
+    this.bPassThrough = ( ( this.effectFilterY_passThrough >= 0 ) && ( this.effectFilterX_passThrough >= 0 ) );
+  }
 
-//   /**
-//    * @return {boolean} Return true, if the specified position should be non-zero for pass-through input to output.
-//    */
-//   isPassThrough_FilterPosition_NonZero( effectFilterY, effectFilterX ) {
-//     if ( ( effectFilterY == this.effectFilterY_passThrough ) && ( effectFilterX == this.effectFilterX_passThrough ) ) {
-//       return true;
-//     return false;
-//   }      
-// }
+  /**
+   * @return {boolean} Return true, if the specified position should be non-zero for pass-through input to output.
+   */
+  isPassThrough_FilterPosition_NonZero( effectFilterY, effectFilterX ) {
+    if ( ( effectFilterY == this.effectFilterY_passThrough ) && ( effectFilterX == this.effectFilterX_passThrough ) )
+      return true;
+    return false;
+  }      
+}
 
 
 /**
