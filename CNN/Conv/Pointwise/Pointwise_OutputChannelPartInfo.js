9bdfa66c4311a090e32eb8751b167f9da9d77b6b
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-01-08 21:48:02 +0800

    Update Pointwise_OutputChannelPartInfo.js

--- CNN/Conv/Pointwise/Pointwise_OutputChannelPartInfo.js
+++ CNN/Conv/Pointwise/Pointwise_OutputChannelPartInfo.js
@@ -1,4 +1,6 @@
-export { OutputChannelPartInfo };
+export { ChannelPartInfo };
+
+import * as ConvBiasActivation from "../ConvBiasActivation.js";
 
 /**
  * Half channels information. Describe channel index range of lower half or higher half.
@@ -6,22 +8,19 @@ export { OutputChannelPartInfo };
  * @member {number} outputChannelCount
  *   The channel count of output.
  *
- * @member {number} inputChannelIndexStart
- *   If non-negative (i.e. zero or positive), this is a pass-through part and it is the input channel index (included) to start to be
- * past-through from input to output. If negative, this is a non-pass-through part (i.e. using filters and biases extracted from weights
- * array).
- *
  * @member {boolean} bPassThrough
- *   If ( this.inputChannelIndexStart >= 0 ), this will be true which means this is a pass-through part.
+ *   If true, this is a pass-through part and it will pass-through input channel index in [ inChannelBegin, inChannelEnd ) to output.
+ * Otherwise, this is a non-pass-through part and it will using filters and biases extracted from weights array to convolve input channel
+ * index in [ inChannelBegin, inChannelEnd ) to output.
  */
-class OutputChannelPartInfo {
+class ChannelPartInfo extends ConvBiasActivation.ChannelPartInfo {
 
   /**
    */
-  constructor( outputChannelCount, inputChannelIndexStart = -1 ) {
+  constructor( inChannelBegin, inChannelEnd, outputChannelCount, bPassThrough ) {
+    super( inChannelBegin, inChannelEnd );
     this.outputChannelCount = outputChannelCount;
-    this.inputChannelIndexStart = inputChannelIndexStart;
-    this.bPassThrough = ( this.inputChannelIndexStart >= 0 );
+    this.bPassThrough = bPassThrough;
   }
 
 }
