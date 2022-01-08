ec815ae8a3c4b9b32de385dcd8bc2476b9698bbd
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-01-08 20:24:53 +0800

    Update Pointwise.js

--- CNN/Conv/Pointwise.js
+++ CNN/Conv/Pointwise.js
@@ -1,5 +1,5 @@
-export { ChannelPartInfo } from "./Pointwise/Pointwise_ChannelPartInfo.js";
+export { OutputChannelPartInfo } from "./Pointwise/Pointwise_OutputChannelPartInfo.js";
 export { BoundsArraySet } from "./Pointwise/Pointwise_BoundsArraySet.js";
 export { FiltersArray_BiasesArray } from "./Pointwise/Pointwise_FiltersArray_BiasesArray.js";
-export { PassThrough_FiltersArray_BiasesArray, PassThrough, AllZeros } from "./Pointwise/Pointwise_PassThrough.js";
+//export { PassThrough_FiltersArray_BiasesArray, PassThrough, AllZeros } from "./Pointwise/Pointwise_PassThrough.js";
 export { Base } from "./Pointwise/Pointwise_Base.js";
