3a0936e76aba1716b44e12400ed4b32e290d9642
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-01-08 14:55:23 +0800

    Update Depthwise_FiltersArray_BiasesArray.js

--- CNN/Conv/Depthwise/Depthwise_FiltersArray_BiasesArray.js
+++ CNN/Conv/Depthwise/Depthwise_FiltersArray_BiasesArray.js
@@ -143,20 +143,9 @@ let FiltersArray_BiasesArray = ( Base = Object ) => class extends PadInfoCalcula
    * @param {ConvBiasActivation.BoundsArraySet} previous_ConvBiasActivation_BoundsArraySet
    *   The previous convolution-bias-activation value bounds set of this depthwise convolution.
    *
-
-//!!! (2022/01/04 Remarked) use previous_ConvBiasActivation_BoundsArraySet.activationEscaping_ScaleTranslateArraySet directly.
-//
-//    * @param {FloatValue.ScaleTranslateArray} extraScaleTranslateArray_byChannelIndex
-//    *   The extra scale-translate of every channel. They will be pre-applied to the filter value, bias value and convolution-bias-activation
-//    * value bounds set of this depthwise convolution. They are indexed by input channel index.
-
-   *
    * @return {boolean} Return true, if succeeded.
    */
-  set_filtersArray_biasesArray_by_extracting(
-    inputFloat32Array, byteOffsetBegin,
-    previous_ConvBiasActivation_BoundsArraySet
-  ) {
+  init( inputFloat32Array, byteOffsetBegin, previous_ConvBiasActivation_BoundsArraySet ) {
 
     // Q1: Why is the inputFloat32Array not a parameter of constructor?
     // A1: The reason is to avoid keeping it as this.inputFloat32Array so that it could be released by memory garbage collector.
