50d03c6c8d88de1c561223cdc1362c1d1088a579
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-01-11 20:51:33 +0800

    Update ValueDesc_ActivationFunction.js

--- CNN/Unpacker/ValueDesc/ValueDesc_ActivationFunction.js
+++ CNN/Unpacker/ValueDesc/ValueDesc_ActivationFunction.js
@@ -26,10 +26,11 @@ import * as FloatValue from "../FloatValue.js";
 
 //!!! ...unfinished... (2022/01/11)
 // What about:
-//   - tf.clipByValue( -3, +3 )?
+//   - tf.clipByValue( -1, +1 )?
 //   - tf.clipByValue( -2, +2 )?
+//   - tf.clipByValue( -3, +3 )?
 //   - tf.clipByValue( -64, +64 )?
-//   - tf.clipByValue( -128, +127 )?
+//   - tf.clipByValue( -128, +128 )?
 
  *   - TANH, SIN, ERF are good if pass-through by only scale (i.e. without translate) is needed. Because the output range of
  *       them includes both negative value and positive value near the origin point. (On the other hand, RELU, RELU6 are not
