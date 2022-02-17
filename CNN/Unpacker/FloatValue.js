50d63d456b49fcf6a36c0b3309f90f85193f5b45
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-02-17 15:05:07 +0800

    Update FloatValue.js

--- CNN/Unpacker/FloatValue.js
+++ CNN/Unpacker/FloatValue.js
@@ -3,3 +3,4 @@ export { ScaleTranslateArray } from "./FloatValue/ScaleTranslateArray.js";
 export { ScaleArray } from "./FloatValue/ScaleArray.js";
 export { Bounds } from "./FloatValue/Bounds.js";
 export { BoundsArray } from "./FloatValue/BoundsArray.js";
+export { ArrayInterleaver } from "./FloatValue/ArrayInterleaver.js";
