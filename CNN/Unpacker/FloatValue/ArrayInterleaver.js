24c39b124755520607cf8620f0e31c854ab723fb
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-02-17 15:04:27 +0800

    Create ArrayInterleaver.js

---
+++ CNN/Unpacker/FloatValue/ArrayInterleaver.js
@@ -0,0 +1,51 @@
+export { ArrayInterleaver };
+
+/**
+ *
+ */
+class ArrayInterleaver {
+
+  /**
+   * Rearrange array elements by interleaving.
+   *   - Only ( groupCount == 2 ) is supported.
+   *
+   * @param {Array} arrayIn
+   *   The array to be re-arranged.
+   *
+   * @param {number} indexBegin
+   *   Re-arrange the arrayIn from which element.
+   *
+   * @param {number} elementCount
+   *   How many elements (begin at indexBegin) will be interleaved. It must be even (i.e. divisible by 2).
+   *
+   * @param {Array} arrayTemp
+   *   A temporary array for placing the original elements (from arrayIn) temporarily. Provide this array could reduce memory re-allocation
+   * and improve performance.
+   *
+   * @return {Array}
+   *   Retrun the (modified) arrayIn itself.
+   */
+  static interleave_asGrouptTwo( arrayIn, indexBegin, elementCount, arrayTemp = [] ) {
+
+    tf.util.assert( ( ( elementCount % 2 ) == 0 ),
+      `ArrayInterleaver.interleave_byGrouptTwo(): `
+        + `elementCount ( ${elementCount} ) must be even (i.e. divisible by 2).`
+    );
+
+    // Copy the elements to be re-arrange.
+    arrayTemp.length = elementCount;
+    for ( let i = 0; i < elementCount; ++i ) {
+      arrayTemp[ i ] = arrayIn[ indexBegin + i ];
+    }
+
+    // Interleave the elements order.
+    let elementCountHalf = Math.floor( elementCount / 2 );
+    for ( let i = 0; i < elementCountHalf; i += 2 ) {
+      arrayIn[ indexBegin + i     ] = arrayTemp[ i ];
+      arrayIn[ indexBegin + i + 1 ] = arrayTemp[ i + elementCountHalf ];
+    }
+
+    return arrayIn;
+  }
+
+}
