806da9fb711c1effc59bd30d2b3083dda53912f1
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-02-23 12:38:49 +0800

    Update Depthwise_ChannelPartInfo.js

--- CNN/Conv/Depthwise/Depthwise_ChannelPartInfo.js
+++ CNN/Conv/Depthwise/Depthwise_ChannelPartInfo.js
@@ -3,7 +3,11 @@ export { ChannelPartInfo };
 import * as ConvBiasActivation from "../ConvBiasActivation.js";
 
 /**
- * Half channels information. Describe channel index range of lower half or higher half
+ * Half channels information. Describe channel index range of lower half or higher half.
+ *
+ *
+ * @member {number} inputChannelCount
+ *   This ChannelPart includes how many input channels.
  *
  * @member {number} effectFilterY_passThrough, effectFilterX_passThrough
  *   For pass-through filters, there is only one position (inside the effect depthwise filter) with non-zero value. All other
@@ -15,12 +19,17 @@ import * as ConvBiasActivation from "../ConvBiasActivation.js";
  * @member {boolean} bPassThrough
  *   If true, this is the half channels for pass-through input to output.
  */
-class ChannelPartInfo extends ConvBiasActivation.ChannelPartInfo {
+//!!! (2022/02/23 Remarked) Use inputChannelCount instead of inChannelBegin and inChannelEnd.
+//class ChannelPartInfo extends ConvBiasActivation.ChannelPartInfo {
+class ChannelPartInfo {
 
   /**
    */
-  constructor( inChannelBegin, inChannelEnd, effectFilterY_passThrough = -1, effectFilterX_passThrough = -1 ) {
-    super( inChannelBegin, inChannelEnd );
+//!!! (2022/02/23 Remarked) Use inputChannelCount instead of inChannelBegin and inChannelEnd.
+//   constructor( inChannelBegin, inChannelEnd, effectFilterY_passThrough = -1, effectFilterX_passThrough = -1 ) {
+//     super( inChannelBegin, inChannelEnd );
+  constructor( inputChannelCount, effectFilterY_passThrough = -1, effectFilterX_passThrough = -1 ) {
+    this.inputChannelCount = inputChannelCount;
     this.effectFilterY_passThrough = effectFilterY_passThrough;
     this.effectFilterX_passThrough = effectFilterX_passThrough;
 
