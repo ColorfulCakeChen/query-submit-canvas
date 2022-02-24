21ea3305ec95ba4169be11ffa1492481c47d2c5b
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-02-24 09:17:09 +0800

    Update Pointwise_ChannelPartInfo.js

--- CNN/Conv/Pointwise/Pointwise_ChannelPartInfo.js
+++ CNN/Conv/Pointwise/Pointwise_ChannelPartInfo.js
@@ -1,9 +1,13 @@
-export { ChannelPartInfo };
-
-import * as ConvBiasActivation from "../ConvBiasActivation.js";
+export { ChannelPartInfo, FiltersBiasesPartInfo };
 
 /**
  * Half channels information. Describe channel index range of lower half or higher half.
+ *
+ * @member {number} inChannelBegin
+ *   The beginning input channel index of this part.
+ *
+ * @member {number} inChannelEnd
+ *   The ending input channel index of this part.
  *
  * @member {number} outputChannelCount
  *   The channel count of output.
@@ -13,15 +17,38 @@ import * as ConvBiasActivation from "../ConvBiasActivation.js";
  * Otherwise, this is a non-pass-through part and it will using filters and biases extracted from weights array to convolve input channel
  * index in [ inChannelBegin, inChannelEnd ) to output.
  */
-class ChannelPartInfo extends ConvBiasActivation.ChannelPartInfo {
+class ChannelPartInfo {
 
   /**
    */
   constructor( inChannelBegin, inChannelEnd, outputChannelCount, bPassThrough ) {
-    super( inChannelBegin, inChannelEnd );
+    this.inChannelBegin = inChannelBegin;
+    this.inChannelEnd = inChannelEnd;    
     this.outputChannelCount = outputChannelCount;
     this.bPassThrough = bPassThrough;
   }
 
 }
 
+/**
+ * Describe a range for a (pointwise) filters and a biases.
+ *
+ *
+ * @member {number} inputChannelCount
+ *   The input channel count of this parts.
+ *
+ * @member {ChannelPartInfo[]} aChannelPartInfoArray
+ *   Every input-output relationship of this parts.
+ *
+ */
+class FiltersBiasesPartInfo {
+
+  /**
+   *
+   */
+  constructor( inputChannelCount, aChannelPartInfoArray ) {
+    this.inputChannelCount = inputChannelCount;
+    this.aChannelPartInfoArray = aChannelPartInfoArray;
+  }
+
+}
