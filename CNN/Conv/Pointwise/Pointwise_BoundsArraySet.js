4049dc74d2e9dd9e10005ec5cd706e12d94507aa
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-02-24 11:49:58 +0800

    Update Pointwise_BoundsArraySet.js

--- CNN/Conv/Pointwise/Pointwise_BoundsArraySet.js
+++ CNN/Conv/Pointwise/Pointwise_BoundsArraySet.js
@@ -26,6 +26,8 @@ class BoundsArraySet extends ConvBiasActivation.BoundsArraySet {
    */
   set_bPassThrough_all_byChannelPartInfoArray( aFiltersBiasesPartInfoArray ) {
     let inChannel = 0;
+    let outChannelBegin = 0;
+    let outChannelEnd = 0;   // Non-inclusive. (i.e. [ outChannelBegin, outChannelEnd ) is current output channel for extracting weights.
 
     FiltersBiasesPartIndexLoop:
     for ( let aFiltersBiasesPartIndex = 0; aFiltersBiasesPartIndex < aFiltersBiasesPartInfoArray.length; ++aFiltersBiasesPartIndex ) {
@@ -38,25 +40,37 @@ class BoundsArraySet extends ConvBiasActivation.BoundsArraySet {
 
 //!!! ...unfinished... (2022/02/24) aFiltersBiasesPartInfoArray
 
-      let outChannel = 0;
+        let outChannel = 0;
+        outChannelEnd = outChannelBegin;
 
-      InChannelPartIndexLoop:
-      for ( let inChannelPartIndex = 0; inChannelPartIndex < inChannelPartInfoArray.length; ++inChannelPartIndex ) {
-        let inChannelPartInfo = inChannelPartInfoArray[ inChannelPartIndex ];
+        InChannelPartIndexLoop:
+        for ( let inChannelPartIndex = 0; inChannelPartIndex < inChannelPartInfoArray.length; ++inChannelPartIndex ) {
+          let inChannelPartInfo = inChannelPartInfoArray[ inChannelPartIndex ];
 
-        for ( let outChannelSub = 0; outChannelSub < inChannelPartInfo.outputChannelCount; ++outChannelSub, ++outChannel ) {
-          if ( outChannel >= this.outputChannelCount )
-            break InChannelPartIndexLoop; // Never exceeds the total output channel count.
+          for ( let outChannelSub = 0; outChannelSub < inChannelPartInfo.outputChannelCount; ++outChannelSub, ++outChannel ) {
+            if ( outChannel >= this.outputChannelCount )
+              break InChannelPartIndexLoop; // Never exceeds the total output channel count.
 
-          if ( inChannelPartInfo.bPassThrough ) { // For pass-through half channels.
-            this.bPassThrough[ outChannel ] = true;
+            if ( outChannel >= outChannelBegin ) {
+              ++outChannelEnd;
 
-          } else { // Non-pass-through half channels. (i.e. input multiply filter weight.)
-            this.bPassThrough[ outChannel ] = false;
-          }
+              if ( inChannelPartInfo.bPassThrough ) { // For pass-through half channels.
+                this.bPassThrough[ outChannel ] = true;
 
-        } // outChannelSub, outChannel
-      } // inChannelPartIndex
+              } else { // Non-pass-through half channels. (i.e. input multiply filter weight.)
+                this.bPassThrough[ outChannel ] = false;
+              }
+
+            } else {
+              // Do nothing. All output channels which is not in range fills bPassThrough in another run.
+            }
+
+          } // outChannelSub, outChannel
+        } // inChannelPartIndex
+      } // inChannelSub, inChannel
+
+      outChannelBegin = outChannelEnd; // Advanced after every FiltersBiasesPart.
+
     } // aFiltersBiasesPartIndex
   }
 
