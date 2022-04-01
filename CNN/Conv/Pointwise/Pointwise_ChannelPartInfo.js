41685ec3212dd5cec4a2cd01d7f65bdc479d2f7e
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-04-01 13:40:00 +0800

    Update Pointwise_ChannelPartInfo.js

--- CNN/Conv/Pointwise/Pointwise_ChannelPartInfo.js
+++ CNN/Conv/Pointwise/Pointwise_ChannelPartInfo.js
@@ -1,4 +1,7 @@
-export { ChannelPartInfo, ChannelPart, FiltersBiasesPartInfo };
+export { ChannelPartInfo, FiltersBiasesPartInfo };
+
+//!!! (2022/04/01 Remarked) Hard to implement it efficiently.
+//export { ChannelPart };
 
 /**
  * Half channels information. Describe channel index range of lower half or higher half.
@@ -31,57 +34,58 @@ class ChannelPartInfo {
 }
 
 
-/**
- * The value of generator FiltersBiasesPartInfo.ChannelPartGenerator( this.outputChannelCount, inChannel ).next().
- */
-class ChannelPart {
-
-//   ChannelPart.info (i.e. ChannelPartInfo)
-//   ChannelPart.outChannelSub
-//   ChannelPart.outChannel
-//   ChannelPart.outChannelEnd
-//   ChannelPart.inChannelToPartBegin
-
-//!!! ...unfifnished... (2022/04/01)
-
-  /**
-   *
-   */
-  isOutChannelInRange() {
-  }
-
-//!!! ...unfifnished... (2022/04/01)
-
-  /**
-   *
-   */
-  isInChannelInRange() {
-  }
-
-//!!! ...unfifnished... (2022/04/01)
-
-  /**
-   *
-   */
-  isPassThrough() {
-  }
-
-//!!! ...unfifnished... (2022/04/01)
-
-  /**
-   *
-   */
-  isInChannelInRange() {
-  }
-
-  
-              if ( outChannel >= outChannelBegin ) {
-
-                if ( ( inChannelToPartBegin >= 0 ) && ( inChannel < inChannelPartInfo.inChannelEnd ) ) {
-                  if ( inChannelPartInfo.bPassThrough ) { // For pass-through half channels.
-                    if ( inChannelToPartBegin == outChannelSub ) { // The only one filter position (in the pass-through part) has non-zero value.
-
-}
+//!!! (2022/04/01 Remarked) Hard to implement it efficiently.
+//
+// /**
+//  * The value of generator FiltersBiasesPartInfo.ChannelPartGenerator( this.outputChannelCount, inChannel ).next().
+//  */
+// class ChannelPart {
+//
+// //   ChannelPart.info (i.e. ChannelPartInfo)
+// //   ChannelPart.outChannelSub
+// //   ChannelPart.outChannel
+// //   ChannelPart.outChannelEnd
+// //   ChannelPart.inChannelToPartBegin
+//
+// //!!! ...unfifnished... (2022/04/01)
+//
+//   /**
+//    *
+//    */
+//   isOutChannelInRange() {
+//   }
+//
+// //!!! ...unfifnished... (2022/04/01)
+//
+//   /**
+//    *
+//    */
+//   isInChannelInRange() {
+//   }
+//
+// //!!! ...unfifnished... (2022/04/01)
+//
+//   /**
+//    *
+//    */
+//   isPassThrough() {
+//   }
+//
+// //!!! ...unfifnished... (2022/04/01)
+//
+//   /**
+//    *
+//    */
+//   isInChannelSameAsOutChannel() {
+//   }
+//
+//
+// //   if ( outChannel >= outChannelBegin ) {
+// //     if ( ( inChannelToPartBegin >= 0 ) && ( inChannel < inChannelPartInfo.inChannelEnd ) ) {
+// //       if ( inChannelPartInfo.bPassThrough ) { // For pass-through half channels.
+// //         if ( inChannelToPartBegin == outChannelSub ) { // The only one filter position (in the pass-through part) has non-zero value.
+//
+// }
 
 
 /**
@@ -105,33 +109,35 @@ class FiltersBiasesPartInfo {
     this.aChannelPartInfoArray = aChannelPartInfoArray;
   }
 
-  /**
-   *
-   * @param {number} outChannelBegin
-   *   The output channel index of the ChannelPart's beginning.
-   *
-   * @param {number} outputChannelCount
-   *   The total output channel count which is used as the upper bounds of yielded ChannelPart.outChannel.
-   *
-   * @param {number} inChannel
-   *   The current input channel index which is used to calculate ChannelPart.inChannelToPartBegin.
-   *
-   * @return {Generator}
-   *   Return a generator whose .next().value will be a ChannelPart object.
-   */
-  * ChannelPartGenerator( outChannelBegin, outputChannelCount, inChannel ) {
-
-//!!! ...unfifnished... (2022/04/01)
-
-
-//!!! ...unfifnished... (2022/04/01)
-//            outChannelEnd = Math.min( outChannelEnd + inChannelPartInfo.outputChannelCount, this.outputChannelCount );
-
-  }
-
-//!!! ...unfifnished... (2022/04/01)
-  * FiltersBiasesPartGenerator() {
-  }
+//!!! (2022/04/01 Remarked) Hard to implement it efficiently.
+//
+//   /**
+//    *
+//    * @param {number} outChannelBegin
+//    *   The output channel index of the ChannelPart's beginning.
+//    *
+//    * @param {number} outputChannelCount
+//    *   The total output channel count which is used as the upper bounds of yielded ChannelPart.outChannel.
+//    *
+//    * @param {number} inChannel
+//    *   The current input channel index which is used to calculate ChannelPart.inChannelToPartBegin.
+//    *
+//    * @return {Generator}
+//    *   Return a generator whose .next().value will be a ChannelPart object.
+//    */
+//   * ChannelPartGenerator( outChannelBegin, outputChannelCount, inChannel ) {
+//
+// //!!! ...unfifnished... (2022/04/01)
+//
+//
+// //!!! ...unfifnished... (2022/04/01)
+// //            outChannelEnd = Math.min( outChannelEnd + inChannelPartInfo.outputChannelCount, this.outputChannelCount );
+//
+//   }
+//
+// //!!! ...unfifnished... (2022/04/01)
+//   * FiltersBiasesPartGenerator() {
+//   }
 
 //!!! ...unfifnished... (2022/02/24)
 //   /**
