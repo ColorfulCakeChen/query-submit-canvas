80faa81efcdf7cee5e3b6e8404fbf462805bf617
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-04-01 14:08:51 +0800

    Update Pointwise_BoundsArraySet.js

--- CNN/Conv/Pointwise/Pointwise_BoundsArraySet.js
+++ CNN/Conv/Pointwise/Pointwise_BoundsArraySet.js
@@ -49,12 +49,11 @@ class BoundsArraySet extends ConvBiasActivation.BoundsArraySet {
         for ( let inChannelPartIndex = 0; inChannelPartIndex < inChannelPartInfoArray.length; ++inChannelPartIndex ) {
           let inChannelPartInfo = inChannelPartInfoArray[ inChannelPartIndex ];
 
-          for ( let outChannelSub = 0; outChannelSub < inChannelPartInfo.outputChannelCount; ++outChannelSub, ++outChannel ) {
+          for ( let outChannelSub = 0; outChannelSub < inChannelPartInfo.outputChannelCount; ++outChannelSub, ++outChannel, ++outChannelEnd ) {
             if ( outChannel >= this.outputChannelCount )
               break InChannelPartIndexLoop; // Never exceeds the total output channel count.
 
             if ( outChannel >= outChannelBegin ) {
-              ++outChannelEnd;
 
               if ( inChannelPartInfo.bPassThrough ) { // For pass-through half channels.
                 this.bPassThrough[ outChannel ] = true;
