86f1f73630637908e789aa7d10f4c40bcc3f996b
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-04-03 10:31:08 +0800

    Update Pointwise_BoundsArraySet.js

--- CNN/Conv/Pointwise/Pointwise_BoundsArraySet.js
+++ CNN/Conv/Pointwise/Pointwise_BoundsArraySet.js
@@ -25,68 +25,31 @@ class BoundsArraySet extends ConvBiasActivation.BoundsArraySet {
    *   The input channel range array which describe lower/higher half channels index range.
    */
   set_bPassThrough_all_byChannelPartInfoArray( aFiltersBiasesPartInfoArray ) {
-    //!!! (2022/02/24 Remarked) No need process input channel.
-    //let inChannel = 0;
-
-//!!! (2022/04/03 Remarked) Deprecate outChannelBegin and outChannelEnd. bPassThrough should be enough.
-//     let outChannelBegin = 0;
-//     let outChannelEnd = 0;   // Non-inclusive. (i.e. [ outChannelBegin, outChannelEnd ) is current output channel for extracting weights.
 
     FiltersBiasesPartIndexLoop:
     for ( let aFiltersBiasesPartIndex = 0; aFiltersBiasesPartIndex < aFiltersBiasesPartInfoArray.length; ++aFiltersBiasesPartIndex ) {
       let aFiltersBiasesPartInfo = aFiltersBiasesPartInfoArray[ aFiltersBiasesPartIndex ];
       let inChannelPartInfoArray = aFiltersBiasesPartInfo.aChannelPartInfoArray;
 
-      //!!! (2022/02/24 Remarked) No need process input channel.
-      //for ( let inChannelSub = 0; inChannelSub < aFiltersBiasesPartInfo.inputChannelCount; ++inChannelSub, ++inChannel ) {
-      //  if ( inChannel >= this.inputChannelCount )
-      //    break FiltersBiasesPartIndexLoop; // Never exceeds the total input channel count.
-
       {
         let outChannel = 0;
-        outChannelEnd = outChannelBegin;
 
         InChannelPartIndexLoop:
         for ( let inChannelPartIndex = 0; inChannelPartIndex < inChannelPartInfoArray.length; ++inChannelPartIndex ) {
           let inChannelPartInfo = inChannelPartInfoArray[ inChannelPartIndex ];
 
-          for ( let outChannelSub = 0; outChannelSub < inChannelPartInfo.outputChannelCount; ++outChannelSub, ++outChannel, ++outChannelEnd ) {
+          for ( let outChannelSub = 0; outChannelSub < inChannelPartInfo.outputChannelCount; ++outChannelSub, ++outChannel ) {
             if ( outChannel >= this.outputChannelCount )
               break InChannelPartIndexLoop; // Never exceeds the total output channel count.
 
-            if ( outChannel >= outChannelBegin ) {
-
-              if ( inChannelPartInfo.bPassThrough ) { // For pass-through half channels.
-                this.bPassThrough[ outChannel ] = true;
-
-              } else { // Non-pass-through half channels.
-                this.bPassThrough[ outChannel ] = false;
-              }
-
-            } else {
-              // Do nothing. All output channels which is not in range fills bPassThrough in another run.
-            }
+              this.bPassThrough[ outChannel ] = inChannelPartInfo.bPassThrough;
 
           } // outChannelSub, outChannel
         } // inChannelPartIndex
       }
 
-      //!!! (2022/02/24 Remarked) No need process input channel.
-      //} // inChannelSub, inChannel
-
-      outChannelBegin = outChannelEnd; // Advanced after every FiltersBiasesPart.
-
     } // aFiltersBiasesPartIndex
 
-    //!!! (2022/02/24 Remarked) No need process input channel.
-    //tf.util.assert( ( inChannel == this.input.length ),
-    //  `Pointwise.BoundsArraySet.set_bPassThrough_all_byChannelPartInfoArray(): `
-    //    + `aFiltersBiasesPartInfoArray[] total input channel count ( ${inChannel} ) should be ( ${this.input.length} ).` );
-
-    tf.util.assert( ( outChannelEnd == this.output.length ),
-      `Pointwise.BoundsArraySet.set_bPassThrough_all_byChannelPartInfoArray(): `
-        + `aFiltersBiasesPartInfoArray[ inChannelPartInfoArray[] ] total output channel count ( ${outChannelEnd} ) `
-        + `should be ( ${this.output.length} ).` );
   }
 
 }
