b766004086cf01ecdd13e994aa5e4c1c1c924b96
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-04-05 12:41:29 +0800

    Update Pointwise_FiltersArray_BiasesArray.js

--- CNN/Conv/Pointwise/Pointwise_FiltersArray_BiasesArray.js
+++ CNN/Conv/Pointwise/Pointwise_FiltersArray_BiasesArray.js
@@ -538,6 +538,7 @@ let FiltersArray_BiasesArray = ( Base = Object ) => class extends Base {
 
 //!!! ...unfinished... (2022/04/05)
 // filterIndex needs setup a beginning value for 1st inChannel (especially for HIGHER_HALF_ANOTHER_POINTWISE (3)).
+// seems be the outChannel of all the previous FiltersBiasesPartInfo
 
       { // this.filtersArray
 
