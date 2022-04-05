235b76066642b6d704b7f5d0b03065a3598a9f35
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-04-05 18:55:12 +0800

    Update Depthwise_BoundsArraySet.js

--- CNN/Conv/Depthwise/Depthwise_BoundsArraySet.js
+++ CNN/Conv/Depthwise/Depthwise_BoundsArraySet.js
@@ -4,7 +4,7 @@ import * as ConvBiasActivation from "../ConvBiasActivation.js";
 import * as FloatValue from "../../Unpacker/FloatValue.js";
 import * as ValueDesc from "../../Unpacker/ValueDesc.js";
 import * as Weights from "../../Unpacker/Weights.js";
-import { ChannelPartInfo } from  "./Depthwise_ChannelPartInfo.js";
+import { ChannelPartInfo, FiltersBiasesPartInfo } from  "./Depthwise_ChannelPartInfo.js";
 
 /**
  * The element value bounds set for depthwise convolution-bias-activation.
@@ -21,10 +21,22 @@ class BoundsArraySet extends ConvBiasActivation.BoundsArraySet {
   /**
    * Set this.bPassThrough[] according to inChannelPartInfoArray.
    *
-   * @param {Depthwise.ChannelPartInfo[]} inChannelPartInfoArray
+   * @param {Depthwise.FiltersBiasesPartInfo[]} aFiltersBiasesPartInfoArray
    *   The input channel range array which describe lower/higher half channels index range.
    */
-  set_bPassThrough_all_byChannelPartInfoArray( inChannelPartInfoArray ) {
+  set_bPassThrough_all_byChannelPartInfoArray( aFiltersBiasesPartInfoArray ) {
+    
+//!!! ...unfinished... (2022/04/05)
+//     let outChannelBegin = 0, outChannelEnd = 0; // [ outChannelBegin, outChannelEnd ) are output channels of the current FiltersBiasesPart.
+//
+//     FiltersBiasesPartIndexLoop:
+//     for ( let aFiltersBiasesPartIndex = 0; aFiltersBiasesPartIndex < aFiltersBiasesPartInfoArray.length; ++aFiltersBiasesPartIndex ) {
+//       let aFiltersBiasesPartInfo = aFiltersBiasesPartInfoArray[ aFiltersBiasesPartIndex ];
+//       let inChannelPartInfoArray = aFiltersBiasesPartInfo.aChannelPartInfoArray;
+//
+//       outChannelBegin = outChannelEnd; // Begin from the ending of the previous FiltersBiasesPart.
+
+
     let inChannel = 0;
     let outChannel = 0;
 
