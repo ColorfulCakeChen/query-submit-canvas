e07f4ad7af9b6b3ee981e1f6d7f32156f3182615
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-04-11 21:48:20 +0800

    Update InputsOutputs.js

--- CNN/Conv/BoundsArraySet/InputsOutputs.js
+++ CNN/Conv/BoundsArraySet/InputsOutputs.js
@@ -72,9 +72,9 @@ class InputsOutputs {
   }
 
   /**
-   * @return {BoundsArraySet.InputsOutputs}
-   *   Return a newly created BoundsArraySet.InputsOutputs which is a copy of this BoundsArraySet.InputsOutputs. The this.inputs
-   * will just past to new InputsOutputs (i.e. NOT copied). The this.outputs will be copied.
+   * @return {InputsOutputs}
+   *   Return a newly created InputsOutputs which is a copy of this InputsOutputs. The this.inputs will just past
+   * to new InputsOutputs (i.e. NOT copied). But the this.outputs will be copied.
    */
   clone() {
     let result = new InputsOutputs( this.inputs, this.outputChannelCount0, this.outputChannelCount1 );
@@ -84,83 +84,79 @@ class InputsOutputs {
     return result;
   }
 
+  /**
+   * @param {FloatValue.Bounds} aBounds
+   *   Set all this.outputs[] to the same as the specified aBounds.
+   *
+   * @return {InputsOutputs}
+   *   Return this (modified) object.
+   */
+  set_outputs_all_byBounds( aBounds ) {
+    for ( let outTensorIndex = 0; outTensorIndex < this.outputs.length; ++outTensorIndex ) {
+      this.outputs[ outTensorIndex ].set_all_byBounds( aBounds );
+    }
+    return this;
+  }
+
+  /**
+   * @param {BoundsArray} outputBoundsArray
+   *   The BoundsArray to be copied to all .outputs[].
+   *
+   * @return {InputsOutputs}
+   *   Return this (modified) object.
+   */
+  set_outputs_all_byBoundsArray( outputBoundsArray ) {
+    for ( let outTensorIndex = 0; outTensorIndex < this.outputs.length; ++outTensorIndex ) {
+      this.outputs[ outTensorIndex ].set_all_byBoundsArray( outputBoundsArray );
+    }
+    return this;
+  }
+
+  /**
+   * Set .outputs[] by .input[ 0 ].
+   *
+   * @return {InputsOutputs}
+   *   Return this (modified) object.
+   */
+  set_outputs_all_byBoundsArray_input0() {
+    for ( let outTensorIndex = 0; outTensorIndex < this.outputs.length; ++outTensorIndex ) {
+      this.outputs[ outTensorIndex ].set_all_byBoundsArray( this.input[ 0 ] );
+    }
+    return this;
+  }
+
+  /**
+   * Set .outputs[] by concatenate .input[ 0 ] and .input[ 1 ].
+   *
+   * @return {InputsOutputs}
+   *   Return this (modified) object.
+   */
+  set_outputs_all_byBoundsArray_concat_input0_input1() {
+    let rLength = this.inputs[ 0 ].length + this.inputs[ 1 ].length;
+
+    for ( let outTensorIndex = 0; outTensorIndex < this.outputs.length; ++outTensorIndex ) {
+      this.outputs[ outTensorIndex ].length = rLength;
+      this.outputs[ outTensorIndex ].set_all_byBoundsArray_concat_input0_input1( this.inputs[ 0 ], this.inputs[ 1 ] );
+    }
+    return this;
+  }
+
 //!!! ...unfinished... (2022/04/11)
 
   /**
-   * @param {FloatValue.Bounds} aBounds
-   *   Set all BoundsArray to the same as the specified aBounds. Set the this.activationEscaping_ScaleArraySet to default ( 1 );
-   *
-   * @return {BoundsArraySet}
-   *   Return this (modified) object.
-   */
-  set_all_byBounds( aBounds ) {
-    this.input.set_all_byBounds( aBounds );
-    this.afterUndoPreviousActivationEscaping.set_all_byBounds( aBounds );
-    this.afterFilter.set_all_byBounds( aBounds );
-    this.afterBias.set_all_byBounds( aBounds );
-    this.afterActivationEscaping.set_all_byBounds( aBounds );
-    this.afterActivation.set_all_byBounds( aBounds );
-    this.set_all_activationEscaping_bPassThrough_none();
-    return this;
-  }
-
-  /**
-   * @param {BoundsArray} inputBoundsArray
-   *   The BoundsArray to be copied to .input and .afterUndoPreviousActivationEscaping.
-   *
-   * @param {BoundsArray} outputBoundsArray
-   *   The BoundsArray to be copied to .afterFilter, .afterBias, .afterActivationEscaping, .afterActivation.
-   *
-   * @return {BoundsArraySet}
-   *   Return this (modified) object.
-   */
-  set_all_byBoundsArray_input_output( inputBoundsArray, outputBoundsArray ) {
-    this.input                              .set_all_byBoundsArray( inputBoundsArray );
-    this.output                    .set_all_byBoundsArray( outputBoundsArray );
-    return this;
-  }
-
-  /**
-   * @param {BoundsArray} aBoundsArray
-   *   The BoundsArray to be copied. It will be copied to all BoundsArray in this object.
-   *
-   * @return {BoundsArraySet}
-   *   Return this (modified) object.
-   */
-  set_all_byBoundsArray( aBoundsArray ) {
-    return this.set_all_byBoundsArray_input_output( aBoundsArray, aBoundsArray );
-  }
-
-  /**
-   * @param {BoundsArray} inputBoundsArray0
-   *   The BoundsArray of the 1st input.
-   *
-   * @param {BoundsArray} inputBoundsArray1
-   *   The BoundsArray of the 2nd input.
-   *
-   * @return {BoundsArraySet}
-   *   Return a newly created object.
-   */
-  static create_byBoundsArray_concat_input0_input1( inputBoundsArray0, inputBoundsArray1 ) {
-    let rLength = inputBoundsArray0.length + inputBoundsArray1.length;
-    let rBoundsArraySet = new InputsOutputs( rLength, rLength );
-
-    // Concat value bounds array.
-    rBoundsArraySet.input.set_all_byBoundsArray_concat_input0_input1( inputBoundsArray0, inputBoundsArray1 );
-
-    // Spread to all value bounds array.
-    return rBoundsArraySet.set_all_byBoundsArray_input_output( rBoundsArraySet.input, rBoundsArraySet.input );
-  }
-
-  /**
    * @param {BoundsArraySet.InputsOutputs} aBoundsArraySet
-   *   The BoundsArraySet to be copied.
+   *   The BoundsArraySet to be copied. Precondition: ( this.outputs.length == aBoundsArraySet.outputs.length ).
    *
-   * @return {BoundsArraySet}
+   * @return {InputsOutputs}
    *   Return this (modified) object which is copied from aBoundsArraySet.
    */
-  set_all_byBoundsArraySet( aBoundsArraySet ) {
-    this.input                              .set_all_byBoundsArray( aBoundsArraySet.input );
+  set_outputs_all_byBoundsArraySet( aBoundsArraySet ) {
+    for ( let outTensorIndex = 0; outTensorIndex < this.outputs.length; ++outTensorIndex ) {
+      this.outputs[ outTensorIndex ].set_outputs_all_byBoundsArray( this.outputs[ outTensorIndex ] );
+    }
+
+    this.set_outputs_all_byBoundsArray( aBoundsArraySet.outputs );
+
     this.afterUndoPreviousActivationEscaping.set_all_byBoundsArray( aBoundsArraySet.afterUndoPreviousActivationEscaping );
     this.afterFilter                        .set_all_byBoundsArray( aBoundsArraySet.afterFilter );
     this.afterBias                          .set_all_byBoundsArray( aBoundsArraySet.afterBias );
@@ -170,103 +166,6 @@ class InputsOutputs {
 
     for ( let i = 0; i < this.bPassThrough.length; ++i ) {
       this.bPassThrough[ i ] = aBoundsArraySet.bPassThrough[ i ];
-    }
-
-    return this;
-  }
-
-  /**
-   * Determine .activationEscaping_ScaleArraySet and .afterActivationEscaping and .afterActivation, by .afterBias and .bPassThrough and nActivationId.
-   *
-   * The following properties will be used:
-   *   - this.afterBias
-   *   - this.bPassThrough
-   *
-   * The following properties will be modified:
-   *   - this.activationEscaping_ScaleArraySet
-   *   - this.afterActivationEscaping
-   *   - this.afterActivation
-   *
-   * @param {number} nActivationId
-   *   The activation function id (ValueDesc.ActivationFunction.Singleton.Ids.Xxx) of this depthwise convolution.
-   *
-   * @return {BoundsArraySet}
-   *   Return this (modified) object.
-   */
-  set_activationEscaping_afterActivationEscaping_afterActivation_by_afterBias_bPassThrough_nActivationId( nActivationId ) {
-    let theActivationFunctionInfo = ValueDesc.ActivationFunction.Singleton.getInfoById( nActivationId );
-
-    for ( let outChannel = 0; outChannel < this.afterBias.length; ++outChannel ) {
-
-      // 1. Determine .activationEscaping_ScaleArraySet
-      {
-        // 1.1 Determine .do
-
-        if ( nActivationId == ValueDesc.ActivationFunction.Singleton.Ids.NONE ) {
-
-          // Since no activation function, no need to escape. (i.e. scale = 1 for no scale)
-          this.activationEscaping_ScaleArraySet.do.set_one_byN( outChannel, 1 );
-
-        } else {
-
-          if ( this.bPassThrough[ outChannel ] ) { // For pass-through half channels.
-
-            // Calculate the scale for escaping bias result from activation function's non-linear domain into linear domain.
-            //
-            // Note: This does not work for avg/max pooling.
-            this.activationEscaping_ScaleArraySet.do.set_one_by_fromLowerUpper_toLowerUpper( outChannel,
-              this.afterBias.lowers[ outChannel ], this.afterBias.uppers[ outChannel ],
-              theActivationFunctionInfo.inputDomainLinear.lower, theActivationFunctionInfo.inputDomainLinear.upper
-            );
-
-            let doEscapingScale = this.activationEscaping_ScaleArraySet.do.scales[ outChannel ];
-            tf.util.assert( ( Number.isNaN( doEscapingScale ) == false ),
-              `ConvBiasActivation.BoundsArraySet.`
-                + `set_activationEscaping_afterActivationEscaping_afterActivation_by_afterBias_bPassThrough_nActivationId(): `
-                + `this.activationEscaping_ScaleArraySet.do.scales[ ${outChannel} ] ( ${doEscapingScale} ) `
-                + `should not be NaN. `
-                + `Please use activation function (e.g. tanh()) which has both negative and positive parts near origin point.`
-            );
-
-          } else { // Non pass-through half channels.
-            // Since non-pass-through, no need to escape. (i.e. scale = 1 for no scale)
-            this.activationEscaping_ScaleArraySet.do.set_one_byN( outChannel, 1 );
-          }
-        }
-
-        // 1.2 Determine .undo (Prepared for the next convolution-bias-activation. Not for this.)
-        this.activationEscaping_ScaleArraySet.undo.set_one_byUndo_ScaleArray(
-          outChannel, this.activationEscaping_ScaleArraySet.do, outChannel );
-      }
-
-      // 2. Determine .afterActivationEscaping
-      this.afterActivationEscaping
-        .set_one_byBoundsArray( outChannel, this.afterBias, outChannel )
-        .multiply_one_byNs( outChannel, this.activationEscaping_ScaleArraySet.do.scales, outChannel );
-
-      // 3. Determine .afterActivation
-      {
-        // If no activation function, the output range is determined by .afterActivationEscaping.
-        if ( nActivationId == ValueDesc.ActivationFunction.Singleton.Ids.NONE ) {
-          this.afterActivation.set_one_byBoundsArray( outChannel, this.afterActivationEscaping, outChannel )
-
-        // Otherwise, the activation function dominates the output range.
-        //
-        // Note: Consider the implementation of ScaleArray.set_one_by_fromLowerUpper_toLowerUpper(), they are all not so good no
-        //       matter using set_one_byXxx() or clamp_one_byXxx() of this.afterActivation. However, when using clamp_one_byXxx(),
-        //       even if the activation function output range has Infinity (e.g. RELU is [ 0, +Infinity ]), the result bounds
-        //       is more feasible (at least, will not become another bounds with Infinity).
-        } else {
-          if ( this.bPassThrough[ outChannel ] ) { // For pass-through half channels, it is clamped by the output range for linearDomainLinear.
-            //this.afterActivation.set_one_byBounds( outChannel, theActivationFunctionInfo.outputRangeLinear );
-            this.afterActivation.clamp_one_byBounds( outChannel, theActivationFunctionInfo.outputRangeLinear );
-
-          } else { // Non pass-through half channels, it is clamped by the output range for the whole input domain.
-            //this.afterActivation.set_one_byBounds( outChannel, theActivationFunctionInfo.outputRange );
-            this.afterActivation.clamp_one_byBounds( outChannel, theActivationFunctionInfo.outputRange );
-          }
-        }
-      }
     }
 
     return this;
