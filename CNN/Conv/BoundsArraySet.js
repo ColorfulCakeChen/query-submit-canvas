601ce9d4cb4f0605e86609bb08ca64ff1607b9bb
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-04-13 20:02:55 +0800

    Update BoundsArraySet.js

--- CNN/Conv/BoundsArraySet.js
+++ CNN/Conv/BoundsArraySet.js
@@ -1,4 +1,3 @@
-export { BoundsArray_ActivationEscaping } from "./BoundsArraySet/BoundsArray_ActivationEscaping.js";
 export { InputsOutputs } from "./BoundsArraySet/BoundsArraySet_InputsOutputs.js";
 export { ConvBiasActivation } from "./BoundsArraySet/BoundsArraySet_ConvBiasActivation.js";
 export { Pointwise } from "./BoundsArraySet/BoundsArraySet_Pointwise.js";
