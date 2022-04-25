9d81902ec5e50b7234c21f074bc5ca5dbab10579
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-04-25 11:39:43 +0800

    Update Pointwise_FiltersArray_BiasesArray.js

--- CNN/Conv/Pointwise/Pointwise_FiltersArray_BiasesArray.js
+++ CNN/Conv/Pointwise/Pointwise_FiltersArray_BiasesArray.js
@@ -406,7 +406,7 @@ let FiltersArray_BiasesArray = ( Base = Object ) => class extends Base {
 
         // Determine .activationEscaping_ScaleArraySet, .afterActivationEscaping, .afterActivation
         this.boundsArraySet.set_bPassThrough_all_byChannelPartInfoArray( aFiltersBiasesPartInfoArray );
-        this.boundsArraySet.set_afterActivationEscaping_afterActivation_by_afterBias_bPassThrough_nActivationId( this.nActivationId );
+        this.boundsArraySet.set_afterActivationEscaping_output0_by_afterBias_bPassThrough_nActivationId( this.nActivationId );
       }
 
       // Round 2
