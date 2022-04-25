e2ac796c89051357be4e5a6d176bdb5036d3991b
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-04-25 10:57:37 +0800

    Update NumberImage.js

--- CNN/jsPerf/Ref/NumberImage.js
+++ CNN/jsPerf/Ref/NumberImage.js
@@ -427,6 +427,9 @@ class Base {
       new BoundsArraySet.InputsOutputs( this.boundsArraySet.output0, undefined, this.depth )
     );
 
+
+//!!! ...unfinished... (2022/04/25) What about the ScaleArraySet?
+
     // Calculate value bounds of every output channels (i.e. .afterActivation; .output).
     imageOutNew.boundsArraySet.output0
       .set_all_byBoundsArray( imageOutNew.boundsArraySet.input0 )
