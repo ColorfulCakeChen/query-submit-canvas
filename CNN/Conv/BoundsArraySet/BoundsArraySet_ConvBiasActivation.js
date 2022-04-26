d4d9ebdf61246d5a1b59be5f216e4c6f0b9f73bc
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-04-26 22:06:11 +0800

    Update BoundsArraySet_ConvBiasActivation.js
    Deprecate .afterActivationEscaping.

--- CNN/Conv/BoundsArraySet/BoundsArraySet_ConvBiasActivation.js
+++ CNN/Conv/BoundsArraySet/BoundsArraySet_ConvBiasActivation.js
@@ -21,11 +21,18 @@ import { InputsOutputs } from "./BoundsArraySet_InputsOutputs.js";
  * @member {FloatValue.BoundsArray} afterBias
  *   The element value bounds (per channel) after applying the convolution biases to this.afterFilter. (i.e. beforeActivationEscaping)
  *
- * @member {FloatValue.BoundsArray} afterActivationEscaping
- *   The element value bounds (per channel) after applying this.activationEscaping_ScaleArraySet.do to this.afterBias. (i.e. beforeActivation)
+
+//!!! (2022/04/26 Remarked) Deprecated. .afterFilter and .afterBias will be adjusted directly.
+//  * @member {FloatValue.BoundsArray} afterActivationEscaping
+//  *   The element value bounds (per channel) after applying this.activationEscaping_ScaleArraySet.do to this.afterBias. (i.e. beforeActivation)
+//  *
+//  * @member {FloatValue.BoundsArray} afterActivation
+//  *   The element value bounds (per channel) after applying activation function to this.afterActivationEscaping. It is just
+//  * the this.output0.boundsArray (without this.output0.scaleArraySet).
+
  *
  * @member {FloatValue.BoundsArray} afterActivation
- *   The element value bounds (per channel) after applying activation function to this.afterActivationEscaping. It is just
+ *   The element value bounds (per channel) after applying activation function to this.afterBias. It is just
  * the this.output0.boundsArray (without this.output0.scaleArraySet).
  *
  * @member {boolean[]} bPassThrough
@@ -51,7 +58,9 @@ class ConvBiasActivation extends InputsOutputs {
 
     this.afterFilter = new FloatValue.BoundsArray( outputChannelCount0 );
     this.afterBias = new FloatValue.BoundsArray( outputChannelCount0 );
-    this.afterActivationEscaping = new FloatValue.BoundsArray( outputChannelCount0 );
+
+//!!! (2022/04/26 Remarked) Deprecated. .afterFilter and .afterBias will be adjusted directly.
+//    this.afterActivationEscaping = new FloatValue.BoundsArray( outputChannelCount0 );
 
     this.bPassThrough = new Array( outputChannelCount0 );
 
@@ -121,9 +130,9 @@ class ConvBiasActivation extends InputsOutputs {
 
   /**
    * @param {FloatValue.Bounds} aBounds
-   *   Set all outputs related BoundsArray (.afterFilter, .afterBias, .afterActivationEscaping, .output0.boundsArray
-   * (i.e. .afterActivation), .bPassThrough) to the same as the specified aBounds. Set the this.output0.scaleArraySet
-   * to default ( 1 ). The .input0 are not modified.
+   *   Set all outputs related BoundsArray (.afterFilter, .afterBias, .output0.boundsArray (i.e. .afterActivation),
+   * .bPassThrough) to the same as the specified aBounds. Set the this.output0.scaleArraySet to default ( 1 ).
+   * The .input0 are not modified.
    *
    * @return {ConvBiasActivation}
    *   Return this (modified) object.
@@ -131,7 +140,10 @@ class ConvBiasActivation extends InputsOutputs {
   set_outputs_all_byBounds( aBounds ) {
     this.afterFilter.set_all_byBounds( aBounds );
     this.afterBias.set_all_byBounds( aBounds );
-    this.afterActivationEscaping.set_all_byBounds( aBounds );
+
+//!!! (2022/04/26 Remarked) Deprecated. .afterFilter and .afterBias will be adjusted directly.
+//    this.afterActivationEscaping.set_all_byBounds( aBounds );
+
     super.set_outputs_all_byBounds( aBounds ); // i.e. .output0.boundsArray (i.e. .afterActivation), .output0.scaleArraySet
     this.set_bPassThrough_all_none();
     return this;
@@ -159,7 +171,9 @@ class ConvBiasActivation extends InputsOutputs {
     this.afterUndoPreviousActivationEscaping.set_all_byBoundsArray( aBoundsArraySet.afterUndoPreviousActivationEscaping );
     this.afterFilter                        .set_all_byBoundsArray( aBoundsArraySet.afterFilter );
     this.afterBias                          .set_all_byBoundsArray( aBoundsArraySet.afterBias );
-    this.afterActivationEscaping            .set_all_byBoundsArray( aBoundsArraySet.afterActivationEscaping );
+
+//!!! (2022/04/26 Remarked) Deprecated. .afterFilter and .afterBias will be adjusted directly.
+//    this.afterActivationEscaping            .set_all_byBoundsArray( aBoundsArraySet.afterActivationEscaping );
 
     // .output0.boundsArray (i.e. .afterActivation), .output0.scaleArraySet, .output1.boundsArray, .output1.scaleArraySet
     super.set_outputs_all_byBoundsArraySet( aBoundsArraySet );
@@ -172,15 +186,15 @@ class ConvBiasActivation extends InputsOutputs {
   }
 
   /**
-   * Determine .afterActivationEscaping, .output0.boundsArray (i.e. .afterActivation) and .output0.scaleArraySet,
-   * by .afterBias and .bPassThrough and nActivationId.
+   * Determine .output0.boundsArray (i.e. .afterActivation) and .output0.scaleArraySet by .afterBias and .bPassThrough and nActivationId.
    *
    * The following properties will be used:
    *   - this.afterBias
    *   - this.bPassThrough
    *
    * The following properties will be modified:
-   *   - this.afterActivationEscaping
+   *   - this.afterFilter
+   *   - this.afterBias
    *   - this.output0.boundsArray (i.e. this.afterActivation)
    *   - this.output0.scaleArraySet
    *
@@ -190,9 +204,10 @@ class ConvBiasActivation extends InputsOutputs {
    * @return {ConvBiasActivation}
    *   Return this (modified) object.
    */
-  set_afterActivationEscaping_output0_by_afterBias_bPassThrough_nActivationId( nActivationId ) {
+  set_output0_by_afterBias_bPassThrough_nActivationId( nActivationId ) {
     const theActivationFunctionInfo = ValueDesc.ActivationFunction.Singleton.getInfoById( nActivationId );
 
+    let doEscapingScale;
     for ( let outChannel = 0; outChannel < this.afterBias.length; ++outChannel ) {
 
       // 1. Determine (activationEscaping) .scaleArraySet (of .output0)
@@ -203,6 +218,7 @@ class ConvBiasActivation extends InputsOutputs {
 
           // Since no activation function, no need to escape. (i.e. scale = 1 for no scale)
           this.output0.scaleArraySet.do.set_one_byN( outChannel, 1 );
+          doEscapingScale = 1;
 
         } else {
 
@@ -216,10 +232,10 @@ class ConvBiasActivation extends InputsOutputs {
               theActivationFunctionInfo.inputDomainLinear.lower, theActivationFunctionInfo.inputDomainLinear.upper
             );
 
-            let doEscapingScale = this.output0.scaleArraySet.do.scales[ outChannel ];
+            doEscapingScale = this.output0.scaleArraySet.do.scales[ outChannel ];
             tf.util.assert( ( Number.isNaN( doEscapingScale ) == false ),
               `BoundsArraySet.ConvBiasActivation.`
-                + `set_afterActivationEscaping_output0_by_afterBias_bPassThrough_nActivationId( `
+                + `set_output0_by_afterBias_bPassThrough_nActivationId( `
                   + ` ${ValueDesc.ActivationFunction.Singleton.getStringOf( nActivationId )}(${nActivationId}) ): `
                 + `this.output0.scaleArraySet.do.scales[ ${outChannel} ] ( ${doEscapingScale} ) `
                 + `should not be NaN. `
@@ -229,23 +245,29 @@ class ConvBiasActivation extends InputsOutputs {
           } else { // Non pass-through half channels.
             // Since non-pass-through, no need to escape. (i.e. scale = 1 for no scale)
             this.output0.scaleArraySet.do.set_one_byN( outChannel, 1 );
+            doEscapingScale = 1;
           }
         }
 
         // 1.2 Determine .undo (Prepared for the next convolution-bias-activation. Not for this.)
-        this.output0.scaleArraySet.undo.set_one_byUndo_ScaleArray( outChannel, this.output0.scaleArraySet.do, outChannel );
+        this.output0.scaleArraySet.undo.set_one_byUndo_N( outChannel, doEscapingScale );
       }
 
-      // 2. Determine .afterActivationEscaping
-      this.afterActivationEscaping
-        .set_one_byBoundsArray( outChannel, this.afterBias, outChannel )
-        .multiply_one_byNs( outChannel, this.output0.scaleArraySet.do.scales, outChannel );
+//!!! (2022/04/26 Remarked) Deprecated. .afterFilter and .afterBias will be adjusted directly.
+//       // 2. Determine .afterActivationEscaping
+//       this.afterActivationEscaping
+//         .set_one_byBoundsArray( outChannel, this.afterBias, outChannel )
+//         .multiply_one_byNs( outChannel, this.output0.scaleArraySet.do.scales, outChannel );
+
+      // 2. Adjust .afterFilter and .afterBias
+      this.afterFilter.multiply_one_byN( outChannel, doEscapingScale );
+      this.afterBias.multiply_one_byN( outChannel, doEscapingScale );
 
       // 3. Determine .afterActivation (i.e. .output0.boundsArray)
       {
-        // If no activation function, the output range is determined by .afterActivationEscaping.
+        // If no activation function, the output range is determined by adjusted .afterBias.
         if ( nActivationId == ValueDesc.ActivationFunction.Singleton.Ids.NONE ) {
-          this.output0.boundsArray.set_one_byBoundsArray( outChannel, this.afterActivationEscaping, outChannel );
+          this.output0.boundsArray.set_one_byBoundsArray( outChannel, this.afterBias, outChannel );
 
         // Otherwise, the activation function dominates the output range.
         //
@@ -267,70 +289,15 @@ class ConvBiasActivation extends InputsOutputs {
         }
       }
 
-//!!! ...unfinished... (2022/04/25 Remarked)
-
-//!!! ...unfinished... (2022/04/25)
-// Since .afterActivationEscaping has already multiplied the .output0.scaleArraySet.do.scales,
-// it has already been the clamped value (i.e. already inside the theActivationFunctionInfo.outputRangeLinear.
-//
-// That is, .afterActivation (i.e. .output0.boundsArray) should be the same as .afterActivationEscaping.
-// So, the .afterActivationEscaping should be deprecated.
-//
-
-//!!! ...unfinished... (2022/04/25)
-// The above description is WRONG!
-//
-// The .afterActivation (i.e. .output0.boundsArray) should be generated by applying theActivationFunctionInfo.pfn()
-// to .afterActivationEscaping no matter whether is .bPassThrough[].
-//
-//
-
-//!!! (2022/04/25 Remarked)
-//
-// The .afterActivation (i.e. .output0.boundsArray) should be generated by applying theActivationFunctionInfo.pfn()
-// to .afterActivationEscaping no matter whether is .bPassThrough[].
-//
-//       // 3. Determine .afterActivation (i.e. .output0.boundsArray)
-//       {
-//         this.output0.boundsArray.set_one_byBoundsArray( outChannel, this.afterActivationEscaping, outChannel );
-//
-//         // If there is activation function, the activation function dominates the output range.
-//         //
-//         // Note: Consider the implementation of ScaleArray.set_one_by_fromLowerUpper_toLowerUpper(), they are all not so good no
-//         //       matter using set_one_byXxx() or clamp_one_byXxx() of this.afterActivation. However, when using clamp_one_byXxx(),
-//         //       even if the activation function output range has Infinity (e.g. RELU is [ 0, +Infinity ]), the result's bounds
-//         //       is more feasible (at least, will not become another bounds with Infinity).
-//         if ( nActivationId != ValueDesc.ActivationFunction.Singleton.Ids.NONE ) {
-//
-//           if ( this.bPassThrough[ outChannel ] ) { // For pass-through half channels, it is clamped by the output range for linearDomainLinear.
-//             //this.output0.boundsArray.set_one_byBounds( outChannel, theActivationFunctionInfo.outputRangeLinear );
-//             this.output0.boundsArray.clamp_one_byBounds( outChannel, theActivationFunctionInfo.outputRangeLinear );
-//
-//           } else { // Non pass-through half channels, it is clamped by the output range for the whole input domain.
-//             //this.output0.boundsArray.set_one_byBounds( outChannel, theActivationFunctionInfo.outputRange );
-//             this.output0.boundsArray.clamp_one_byBounds( outChannel, theActivationFunctionInfo.outputRange );
-//           }
-//
-//         // Otherwise, no activation function, the output range is determined by .afterActivationEscaping.
-//         }
-//       }
-
     }
-
-//!!! (2022/04/25 Add and Remarked)
-//
-// //!!! ...unfinished... (2022/04/25) It may be slow to call activation function.
-//
-//     // 3. Determine .afterActivation (i.e. .output0.boundsArray)
-//     this.output0.boundsArray.set_all_byBoundsArray_activation( this.afterActivationEscaping, nActivationId );
 
     return this;
   }
 
   /**
-   * Rearrange output related channel information (.afterFilter, .afterBias, .afterActivationEscaping, .afterActivation
-   * (i.e. .output0.boundsArray), output0.scaleArraySet (i.e. activationEscaping), .bPassThrough) by interleaving as
-   * ( groupCount == 2 ). The channel count must be even (i.e. divisible by 2).
+   * Rearrange output related channel information (.afterFilter, .afterBias, .output0.boundsArray (i.e. .afterActivation),
+   * output0.scaleArraySet (i.e. activationEscaping), .bPassThrough) by interleaving as ( groupCount == 2 ). The channel
+   * count must be even (i.e. divisible by 2).
    *
    * @param {Array} arrayTemp
    *   A temporary array for placing the original elements temporarily. Provide this array could reduce memory re-allocation
@@ -342,7 +309,10 @@ class ConvBiasActivation extends InputsOutputs {
   set_outputs_all_byInterleave_asGrouptTwo( arrayTemp ) {
     this.afterFilter.set_all_byInterleave_asGrouptTwo( arrayTemp );
     this.afterBias.set_all_byInterleave_asGrouptTwo( arrayTemp );
-    this.afterActivationEscaping.set_all_byInterleave_asGrouptTwo( arrayTemp );
+
+//!!! (2022/04/26 Remarked) Deprecated. .afterFilter and .afterBias will be adjusted directly.
+//    this.afterActivationEscaping.set_all_byInterleave_asGrouptTwo( arrayTemp );
+
     super.set_outputs_all_byInterleave_asGrouptTwo( arrayTemp ); // i.e. this.afterActivation
     FloatValue.ArrayInterleaver.interleave_asGrouptTwo( this.bPassThrough, 0, this.bPassThrough.length, arrayTemp );
     return this;
