3c32ca2e0a4e95707a441fb0bd40a7790f2328bc
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-04-26 22:06:36 +0800

    Update Pointwise_FiltersArray_BiasesArray.js

--- CNN/Conv/Pointwise/Pointwise_FiltersArray_BiasesArray.js
+++ CNN/Conv/Pointwise/Pointwise_FiltersArray_BiasesArray.js
@@ -600,8 +600,9 @@ let FiltersArray_BiasesArray = ( Base = Object ) => class extends Base {
           let doEscapingScale = this.boundsArraySet.output0.scaleArraySet.do.scales[ outChannel ];
           this.filtersArray[ filterIndex ] *= doEscapingScale; // filter wieghts scaled.
 
-//!!! ...unfinished... (2022/04/26) Is this correct?
-          this.boundsArraySet.afterFilter.multiply_one_byN( outChannel, doEscapingScale ); // value bounds after filter also scaled.
+//!!! (2022/04/26 Remarked) Moved to set_output0_by_afterBias_bPassThrough_nActivationId()
+// //!!! ...unfinished... (2022/04/26) Is this correct?
+//           this.boundsArraySet.afterFilter.multiply_one_byN( outChannel, doEscapingScale ); // value bounds after filter also scaled.
 
           ++filterIndex;
 
@@ -617,8 +618,9 @@ let FiltersArray_BiasesArray = ( Base = Object ) => class extends Base {
         let doEscapingScale = this.boundsArraySet.output0.scaleArraySet.do.scales[ outChannel ];
         this.biasesArray[ biasIndex ] *= doEscapingScale; // bias wieghts scaled.
 
-//!!! ...unfinished... (2022/04/26) Is this correct?
-        this.boundsArraySet.afterBias.multiply_one_byN( outChannel, doEscapingScale ); // value bounds after bias also scaled.
+//!!! (2022/04/26 Remarked) Moved to set_output0_by_afterBias_bPassThrough_nActivationId()
+// //!!! ...unfinished... (2022/04/26) Is this correct?
+//         this.boundsArraySet.afterBias.multiply_one_byN( outChannel, doEscapingScale ); // value bounds after bias also scaled.
 
         ++biasIndex;
 
