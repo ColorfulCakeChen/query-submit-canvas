c19b63891bba919872d742c92b8879180b6504ca
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-04-29 10:33:21 +0800

    Update jsPerf_PointDepthPoint.js

--- CNN/jsPerf/jsPerf_PointDepthPoint.js
+++ CNN/jsPerf/jsPerf_PointDepthPoint.js
@@ -306,49 +306,57 @@ class HeightWidthDepth {
         = PointDepthPoint_Reference.Base.pointDepthPoint_create(
             testCase_pointwise1_4to8_bias_COS_depthwise_8to8_strides_1_pad_same_bias_COS_pointwise2_8to4_bias_COS_AddInputToOutput,
             inputScaleBoundsArray0, inputScaleBoundsArray1,
-            channelShuffler_ConcatPointwiseConv, arrayTemp_forInterleave_asGrouptTwo ),
+            null, //channelShuffler_ConcatPointwiseConv,
+            arrayTemp_forInterleave_asGrouptTwo ),
 
       this.pointDepthPoint_Avg_bias_COS_AddInputToOutput
         = PointDepthPoint_Reference.Base.pointDepthPoint_create(
             testCase_pointwise1_4to8_bias_COS_depthwise_avg_strides_1_pad_same_bias_COS_pointwise2_8to4_bias_COS_AddInputToOutput,
             inputScaleBoundsArray0, inputScaleBoundsArray1,
-            channelShuffler_ConcatPointwiseConv, arrayTemp_forInterleave_asGrouptTwo ),
+            null, //channelShuffler_ConcatPointwiseConv,
+            arrayTemp_forInterleave_asGrouptTwo ),
 
       this.pointDepthPoint_Max_bias_COS_AddInputToOutput
         = PointDepthPoint_Reference.Base.pointDepthPoint_create(
             testCase_pointwise1_4to8_bias_COS_depthwise_max_strides_1_pad_same_bias_COS_pointwise2_8to4_bias_COS_AddInputToOutput,
             inputScaleBoundsArray0, inputScaleBoundsArray1,
-            channelShuffler_ConcatPointwiseConv, arrayTemp_forInterleave_asGrouptTwo ),
+            null, //channelShuffler_ConcatPointwiseConv,
+            arrayTemp_forInterleave_asGrouptTwo ),
 
       this.pointDepthPoint_DConv_2_bias_COS_AddInputToOutput
         = PointDepthPoint_Reference.Base.pointDepthPoint_create(
             testCase_pointwise1_4to8_bias_COS_depthwise_8to16_strides_1_pad_same_bias_COS_pointwise2_16to4_bias_COS_AddInputToOutput,
             inputScaleBoundsArray0, inputScaleBoundsArray1,
-            channelShuffler_ConcatPointwiseConv, arrayTemp_forInterleave_asGrouptTwo ),
+            null, //channelShuffler_ConcatPointwiseConv,
+            arrayTemp_forInterleave_asGrouptTwo ),
 
       this.pointDepthPoint_DConv_2_COS_AddInputToOutput
         = PointDepthPoint_Reference.Base.pointDepthPoint_create(
             testCase_pointwise1_4to8_noBias_COS_depthwise_8to16_strides_1_pad_same_noBias_COS_pointwise2_16to4_noBias_COS_AddInputToOutput,
             inputScaleBoundsArray0, inputScaleBoundsArray1,
-            channelShuffler_ConcatPointwiseConv, arrayTemp_forInterleave_asGrouptTwo ),
+            null, //channelShuffler_ConcatPointwiseConv,
+            arrayTemp_forInterleave_asGrouptTwo ),
 
       this.pointDepthPoint_DConv_2_COS
         = PointDepthPoint_Reference.Base.pointDepthPoint_create(
             testCase_pointwise1_4to8_noBias_COS_depthwise_8to16_strides_1_pad_same_noBias_COS_pointwise2_16to4_noBias_COS,
             inputScaleBoundsArray0, inputScaleBoundsArray1,
-            channelShuffler_ConcatPointwiseConv, arrayTemp_forInterleave_asGrouptTwo ),
+            null, //channelShuffler_ConcatPointwiseConv,
+            arrayTemp_forInterleave_asGrouptTwo ),
 
       this.pointDepthPoint_DConv_32_bias_COS_P128_bias
         = PointDepthPoint_Reference.Base.pointDepthPoint_create(
             testCase_pointwise1_none_depthwise_4to128_strides_1_pad_same_bias_COS_pointwise2_128to128_bias,
             inputScaleBoundsArray0, inputScaleBoundsArray1,
-            channelShuffler_ConcatPointwiseConv, arrayTemp_forInterleave_asGrouptTwo ),
+            null, //channelShuffler_ConcatPointwiseConv,
+            arrayTemp_forInterleave_asGrouptTwo ),
 
       this.pointDepthPoint_P128_bias_COS_P128_bias
         = PointDepthPoint_Reference.Base.pointDepthPoint_create(
             testCase_pointwise1_4to128_bias_COS_depthwise_none_COS_pointwise2_128to128_bias,
             inputScaleBoundsArray0, inputScaleBoundsArray1,
-            channelShuffler_ConcatPointwiseConv, arrayTemp_forInterleave_asGrouptTwo ),
+            null, //channelShuffler_ConcatPointwiseConv,
+            arrayTemp_forInterleave_asGrouptTwo ),
 
     ];
 
