1ed3e8b59811c2d49859f9e7916c91579e10b36c
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-02 11:34:45 +0800

    Update ValueDesc_Other.js

--- CNN/Unpacker/ValueDesc/ValueDesc_Other.js
+++ CNN/Unpacker/ValueDesc/ValueDesc_Other.js
@@ -2,7 +2,6 @@ export { channelCount1_pointwise1Before };
 export { Pointwise_HigherHalfDifferent };
 export { Depthwise_HigherHalfDifferent };
 export { AvgMax_Or_ChannelMultiplier };
-export { StridesPad };
 export { WhetherShuffleChannel };
 
 import { Int } from "./ValueDesc_Base.js";
@@ -114,74 +113,6 @@ class AvgMax_Or_ChannelMultiplier extends Int {
 
 /** The only one ValueDesc.AvgMax_Or_ChannelMultiplier instance. */
 AvgMax_Or_ChannelMultiplier.Singleton = new AvgMax_Or_ChannelMultiplier;
-
-
-/** Describe id, range, name of (convolution's) strides and pad.
- *
- * Convert number value into integer between [ 0, 3 ] representing strides and pad:
- *   -  0: STRIDES_1_PAD_VALID (strides = 1, pad = "valid")
- *   -  1: STRIDES_1_PAD_SAME  (strides = 1, pad = "same")
- *   -  2: STRIDES_2_PAD_SAME  (strides = 2, pad = "same")
- *   -  3: STRIDES_2_PAD_VALID (strides = 2, pad = "valid")
- */
-class StridesPad extends Int {
-
-  constructor() {
-    super( 0, 3, [
-      "STRIDES_1_PAD_VALID", // (0) (strides = 1, pad = "valid")
-      "STRIDES_1_PAD_SAME",  // (1) (strides = 1, pad = "same")
-      "STRIDES_2_PAD_SAME",  // (2) (strides = 2, pad = "same")
-      "STRIDES_2_PAD_VALID", // (3) (strides = 2, pad = "valid")
-    ] );
-  }
-
-  /**
-   * @param {number} nStridesPadIds  The numeric identifier of StridesPad. (StridesPad.Singleton.Ids.Xxx)
-   * @return {number} Return the strides (1 or 2) of the StridesPad.Singleton.Ids.Xxx.
-   */
-  static strides_get( nStridesPadIds ) {
-    switch ( nStridesPadIds ) {
-      case StridesPad.Singleton.Ids.STRIDES_1_PAD_VALID: return 1; // (0)
-      case StridesPad.Singleton.Ids.STRIDES_1_PAD_SAME:  return 1; // (1)
-      case StridesPad.Singleton.Ids.STRIDES_2_PAD_SAME:  return 2; // (2)
-      case StridesPad.Singleton.Ids.STRIDES_2_PAD_VALID: return 2; // (3)
-      default:
-        return NaN;
-    }
-  }
-
-  /**
-   * @param {number} nStridesPadIds  The numeric identifier of StridesPad. (StridesPad.Singleton.Ids.Xxx)
-   * @return {boolean} Return true, if the pad of the StridesPad.Singleton.Ids.Xxx is "valid".
-   */
-  static pad_isValid( nStridesPadIds ) {
-    switch ( nStridesPadIds ) {
-      case StridesPad.Singleton.Ids.STRIDES_1_PAD_VALID: // (0)
-      case StridesPad.Singleton.Ids.STRIDES_2_PAD_VALID: // (3)
-        return true;
-      default:
-        return false;
-    }
-  }
-
-  /**
-   * @param {number} nStridesPadIds  The numeric identifier of StridesPad. (StridesPad.Singleton.Ids.Xxx)
-   * @return {boolean} Return true, if the pad of the StridesPad.Singleton.Ids.Xxx is "same".
-   */
-  static pad_isSame( nStridesPadIds ) {
-    switch ( nStridesPadIds ) {
-      case StridesPad.Singleton.Ids.STRIDES_1_PAD_SAME:  // (1)
-      case StridesPad.Singleton.Ids.STRIDES_2_PAD_SAME:  // (2)
-        return true;
-      default:
-        return false;
-    }
-  }
-
-}
-
-/** The only one ValueDesc.StridesPad instance. */
-StridesPad.Singleton = new StridesPad;
 
 
 /** Describe id, range, name of WhetherShuffleChannel.
