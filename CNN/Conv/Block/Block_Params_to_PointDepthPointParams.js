344e9e352d3373ae911cc5c1d67a9ef1170a335a
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-04 12:06:05 +0800

    Update and rename Block_Params_to_PointDepthPointParams.js to PointDepthPointParamsCreator.js

--- CNN/Conv/Block/Block_Params_to_PointDepthPointParams.js
+++
@@ -1,8 +0,0 @@
-export { Base } from "./Block_Params_to_PointDepthPointParams/Base.js";
-export { MobileNetV1 } from "./Block_Params_to_PointDepthPointParams/MobileNetV1.js";
-export { MobileNetV2 } from "./Block_Params_to_PointDepthPointParams/MobileNetV2.js";
-export { MobileNetV2_Thin } from "./Block_Params_to_PointDepthPointParams/MobileNetV2_Thin.js";
-export { ShuffleNetV2 } from "./Block_Params_to_PointDepthPointParams/ShuffleNetV2.js";
-export { ShuffleNetV2_ByPointwise22 } from "./Block_Params_to_PointDepthPointParams/ShuffleNetV2_ByPointwise22.js";
-export { ShuffleNetV2_ByMobileNetV1 } from "./Block_Params_to_PointDepthPointParams/ShuffleNetV2_ByMobileNetV1.js";
-export { ShuffleNetV2_ByMobileNetV1_padValid } from "./Block_Params_to_PointDepthPointParams/ShuffleNetV2_ByMobileNetV1_padValid.js";
