9f8c4d7a75bdd3af88b9c93b6972aff54da178ea
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-04 10:59:30 +0800

    Create Params_to_PointDepthPointParams.js

---
+++ CNN/Conv/Block/Params_to_PointDepthPointParams.js
@@ -0,0 +1,8 @@
+export { Base } from "./Params_to_PointDepthPointParams/Base.js";
+export { MobileNetV1 } from "./Params_to_PointDepthPointParams/MobileNetV1.js";
+export { MobileNetV2 } from "./Params_to_PointDepthPointParams/MobileNetV2.js";
+export { MobileNetV2_Thin } from "./Params_to_PointDepthPointParams/MobileNetV2_Thin.js";
+export { ShuffleNetV2 } from "./Params_to_PointDepthPointParams/ShuffleNetV2.js";
+export { ShuffleNetV2_ByPointwise22 } from "./Params_to_PointDepthPointParams/ShuffleNetV2_ByPointwise22.js";
+export { ShuffleNetV2_ByMobileNetV1 } from "./Params_to_PointDepthPointParams/ShuffleNetV2_ByMobileNetV1.js";
+export { ShuffleNetV2_ByMobileNetV1_padValid } from "./Params_to_PointDepthPointParams/ShuffleNetV2_ByMobileNetV1_padValid.js";
