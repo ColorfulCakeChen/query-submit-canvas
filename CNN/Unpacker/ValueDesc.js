2b98cd665c0ee811a058b8fd48d1bd6f7396f1a1
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-04 21:56:46 +0800

    Update ValueDesc.js

--- CNN/Unpacker/ValueDesc.js
+++ CNN/Unpacker/ValueDesc.js
@@ -4,5 +4,6 @@ export { channelCount1_pointwise1Before } from "./ValueDesc/ValueDesc_Other.js";
 export { Pointwise_HigherHalfDifferent } from "./ValueDesc/ValueDesc_Other.js";
 export { Depthwise_HigherHalfDifferent } from "./ValueDesc/ValueDesc_Other.js";
 export { AvgMax_Or_ChannelMultiplier } from "./ValueDesc/ValueDesc_Other.js";
+export { ConvBlockType } from "./ValueDesc/ValueDesc_ConvBlockType.js";
 export { StridesPad } from "./ValueDesc/ValueDesc_StridesPad.js";
 export { WhetherShuffleChannel } from "./ValueDesc/ValueDesc_Other.js";
