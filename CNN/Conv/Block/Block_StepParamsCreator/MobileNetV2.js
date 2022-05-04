6646aed94506994c10e08bcc1ff4befcb40b6fe7
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-04 22:35:24 +0800

    Update MobileNetV2.js

--- CNN/Conv/Block/Block_StepParamsCreator/MobileNetV2.js
+++ CNN/Conv/Block/Block_StepParamsCreator/MobileNetV2.js
@@ -2,14 +2,14 @@ export { MobileNetV2 };
 
 import * as ValueDesc from "../../../Unpacker/ValueDesc.js";
 import { Params } from "../Block_Param.js";
-import { MobileNetV1 } from "./MobileNetV1.js";
+import { MobileNetV2_Thin } from "./MobileNetV2_Thin.js";
 
 /**
  * Provide parameters for MobileNetV2 (i.e. add-inut-to-output, pointwise1 is tiwce size of pointwise21).
  *
  *
  */
-class MobileNetV2 extends MobileNetV1 {
+class MobileNetV2 extends MobileNetV2_Thin {
 
   /** @override */
   configTo_beforeStep0() {
@@ -30,11 +30,6 @@ class MobileNetV2 extends MobileNetV1 {
   /** @override */
   configTo_afterStep0() {
     super.configTo_afterStep0(); // step1, 2, 3, ...'s inputHeight0, inputWidth0.
-
-    // In MobileNetV2:
-    //   - All steps (except step0) do add-input-to-output (without concatenation).
-    //   - All steps (include step0) do not use input1.
-    this.channelCount1_pointwise1Before = ValueDesc.channelCount1_pointwise1Before.Singleton.Ids.ONE_INPUT_ADD_TO_OUTPUT;
   }
 
   /** @override */
