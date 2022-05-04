e50b0a9d8b96ae3c57f50312d9f8117edccb742f
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-04 12:08:15 +0800

    Update and rename PointDepthPointParamsCreator.js to Block_StepParamsCreator.js

---
+++ CNN/Conv/Block/Block_StepParamsCreator.js
@@ -0,0 +1,8 @@
+export { Base } from "./Block_StepParamsCreator/Base.js";
+export { MobileNetV1 } from "./Block_StepParamsCreator/MobileNetV1.js";
+export { MobileNetV2 } from "./Block_StepParamsCreator/MobileNetV2.js";
+export { MobileNetV2_Thin } from "./Block_StepParamsCreator/MobileNetV2_Thin.js";
+export { ShuffleNetV2 } from "./Block_StepParamsCreator/ShuffleNetV2.js";
+export { ShuffleNetV2_ByPointwise22 } from "./Block_StepParamsCreator/ShuffleNetV2_ByPointwise22.js";
+export { ShuffleNetV2_ByMobileNetV1 } from "./Block_StepParamsCreator/ShuffleNetV2_ByMobileNetV1.js";
+export { ShuffleNetV2_ByMobileNetV1_padValid } from "./Block_StepParamsCreator/ShuffleNetV2_ByMobileNetV1_padValid.js";
--- CNN/Conv/Block/PointDepthPointParamsCreator.js
+++
@@ -1,8 +0,0 @@
-export { Base } from "./PointDepthPointParamsCreator/Base.js";
-export { MobileNetV1 } from "./PointDepthPointParamsCreator/MobileNetV1.js";
-export { MobileNetV2 } from "./PointDepthPointParamsCreator/MobileNetV2.js";
-export { MobileNetV2_Thin } from "./PointDepthPointParamsCreator/MobileNetV2_Thin.js";
-export { ShuffleNetV2 } from "./PointDepthPointParamsCreator/ShuffleNetV2.js";
-export { ShuffleNetV2_ByPointwise22 } from "./PointDepthPointParamsCreator/ShuffleNetV2_ByPointwise22.js";
-export { ShuffleNetV2_ByMobileNetV1 } from "./PointDepthPointParamsCreator/ShuffleNetV2_ByMobileNetV1.js";
-export { ShuffleNetV2_ByMobileNetV1_padValid } from "./PointDepthPointParamsCreator/ShuffleNetV2_ByMobileNetV1_padValid.js";
