9f9a49f5f4a8a28b57d1b65533b098c7782a0271
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-05 11:56:11 +0800

    Update ValueDesc_ConvBlockType.js

--- CNN/Unpacker/ValueDesc/ValueDesc_ConvBlockType.js
+++ CNN/Unpacker/ValueDesc/ValueDesc_ConvBlockType.js
@@ -6,24 +6,26 @@ import { Int } from "./ValueDesc_Base.js";
  *
  * Convert number value into integer between [ 0, 6 ] representing operation:
  *   - 0: MOBILE_NET_V1                             (i.e. no-add-inut-to-output, pointwise1 is same size of pointwise21)
- *   - 1: MOBILE_NET_V2                             (i.e. add-inut-to-output, pointwise1 is tiwce size of pointwise21)
- *   - 2: MOBILE_NET_V2_THIN                        (i.e. add-inut-to-output, pointwise1 is same size of pointwise21)
- *   - 3: SHUFFLE_NET_V2                            (i.e. by channel shuffler)
- *   - 4: SHUFFLE_NET_V2_BY_POINTWISE22             (i.e. by pointwise22)
- *   - 5: SHUFFLE_NET_V2_BY_MOBILE_NET_V1           (i.e. by integrated pointwise1, depthwise1, pointwise21)
- *   - 6: SHUFFLE_NET_V2_BY_MOBILE_NET_V1_PAD_VALID (i.e. by depthwise1 with ( pad = "valid" ) )
+ *   - 1: MOBILE_NET_V1_PAD_VALID                   (i.e. no-add-inut-to-output, pointwise1 is same size of pointwise21, depthwise1 with ( pad = "valid" ))
+ *   - 2: MOBILE_NET_V2                             (i.e. add-inut-to-output, pointwise1 is tiwce size of pointwise21)
+ *   - 3: MOBILE_NET_V2_THIN                        (i.e. add-inut-to-output, pointwise1 is same size of pointwise21)
+ *   - 4: SHUFFLE_NET_V2                            (i.e. by channel shuffler)
+ *   - 5: SHUFFLE_NET_V2_BY_POINTWISE22             (i.e. by pointwise22)
+ *   - 6: SHUFFLE_NET_V2_BY_MOBILE_NET_V1           (i.e. by integrated pointwise1, depthwise1, pointwise21)
+ *   - 7: SHUFFLE_NET_V2_BY_MOBILE_NET_V1_PAD_VALID (i.e. by depthwise1 with ( pad = "valid" ) )
  */
 class ConvBlockType extends Int {
 
   constructor() {
-    super( 0, 6, [
+    super( 0, 7, [
       "MOBILE_NET_V1",                             // (0)
-      "MOBILE_NET_V2",                             // (1)
-      "MOBILE_NET_V2_THIN",                        // (2)
-      "SHUFFLE_NET_V2",                            // (3)
-      "SHUFFLE_NET_V2_BY_POINTWISE22",             // (4)
-      "SHUFFLE_NET_V2_BY_MOBILE_NET_V1",           // (5)
-      "SHUFFLE_NET_V2_BY_MOBILE_NET_V1_PAD_VALID", // (6)
+      "MOBILE_NET_V1_PAD_VALID",                   // (1)
+      "MOBILE_NET_V2",                             // (2)
+      "MOBILE_NET_V2_THIN",                        // (3)
+      "SHUFFLE_NET_V2",                            // (4)
+      "SHUFFLE_NET_V2_BY_POINTWISE22",             // (5)
+      "SHUFFLE_NET_V2_BY_MOBILE_NET_V1",           // (6)
+      "SHUFFLE_NET_V2_BY_MOBILE_NET_V1_PAD_VALID", // (7)
     ] );
   }
 
@@ -34,8 +36,9 @@ class ConvBlockType extends Int {
   static isMobileNet( nConvBlockType ) {
     switch ( nConvBlockType ) {
       case ValueDesc.ConvBlockType.Ids.MOBILE_NET_V1: // (0)
-      case ValueDesc.ConvBlockType.Ids.MOBILE_NET_V2: // (1)
-      case ValueDesc.ConvBlockType.Ids.MOBILE_NET_V2_THIN: // (2)
+      case ValueDesc.ConvBlockType.Ids.MOBILE_NET_V1_PAD_VALID: // (1)
+      case ValueDesc.ConvBlockType.Ids.MOBILE_NET_V2: // (2)
+      case ValueDesc.ConvBlockType.Ids.MOBILE_NET_V2_THIN: // (3)
         return true;
       default:
         return false;
@@ -48,10 +51,10 @@ class ConvBlockType extends Int {
    */
   static isShuffleNet( nConvBlockType ) {
     switch ( nConvBlockType ) {
-      case ValueDesc.ConvBlockType.Ids.SHUFFLE_NET_V2: // (3)
-      case ValueDesc.ConvBlockType.Ids.SHUFFLE_NET_V2_BY_POINTWISE22: // (4)
-      case ValueDesc.ConvBlockType.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1: // (5)
-      case ValueDesc.ConvBlockType.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_PAD_VALID: // (6)
+      case ValueDesc.ConvBlockType.Ids.SHUFFLE_NET_V2: // (4)
+      case ValueDesc.ConvBlockType.Ids.SHUFFLE_NET_V2_BY_POINTWISE22: // (5)
+      case ValueDesc.ConvBlockType.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1: // (6)
+      case ValueDesc.ConvBlockType.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_PAD_VALID: // (7)
         return true;
       default:
         return false;
