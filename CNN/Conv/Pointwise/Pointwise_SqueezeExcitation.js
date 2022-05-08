36dcb542e771608bd242f3258a36213002614a68
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-08 10:41:06 +0800

    Update Pointwise_SqueezeExcitation.js

--- CNN/Conv/Pointwise/Pointwise_SqueezeExcitation.js
+++ CNN/Conv/Pointwise/Pointwise_SqueezeExcitation.js
@@ -6,11 +6,53 @@ export { Base };
 import * as BoundsArraySet from "../BoundsArraySet.js";
 import { Base } from "./Pointwise_Base.js";
 
-//!!! ...unfinished... (2022/05/08)
+//!!! ...unfinished... (2022/05/08) Add squeeze and excitation before pointiwse.
+// globale avg pooling - pointwise - pointwise - multiplyToInput
+// And the, the original pointwise
 
 /**
  *
  */
 class Pointwise_SqueezeExcitation extends Pointwise_Base {
+
+//!!! ...unfinished... (2022/05/08)
+
+
+  /**
+   */
+  constructor(
+    inputChannelCount, outputChannelCount, bBias, nActivationId,
+    nHigherHalfDifferent, inputChannelCount_lowerHalf, outputChannelCount_lowerHalf, channelShuffler_outputGroupCount,
+
+//!!! ...unfinished... (2022/05/08) squeeze channel count? ratio?
+
+
+    ) {
+
+    super(
+      inputChannelCount, outputChannelCount, bBias, nActivationId,
+      nHigherHalfDifferent, inputChannelCount_lowerHalf, outputChannelCount_lowerHalf, channelShuffler_outputGroupCount );
+  }
+
+  /**
+   * @param {Float32Array} inputFloat32Array
+   *   A Float32Array whose values will be interpreted as weights.
+   *
+   * @param {ActivationEscaping.ScaleBoundsArray} inputScaleBoundsArray
+   *   The element value bounds (per channel) of input. Usually, it is The .output of the previous convolution-bias-activation value bounds
+   * set of this pointwise convolution. It will be kept (not cloned) directly. So caller should not modify them.
+   *
+   * @param {Array} arrayTemp_forInterleave_asGrouptTwo
+   *   A temporary array for placing the original elements temporarily. Provide this array could reduce memory re-allocation
+   * and improve performance when doing Interleave_asGrouptTwo.
+   *
+   * @return {boolean} Return true, if succeeded.
+   */
+  init( inputFloat32Array, byteOffsetBegin, inputScaleBoundsArray, arrayTemp_forInterleave_asGrouptTwo ) {
+
+//!!! ...unfinished... (2022/05/08)
+
+
+  }
 
 }
