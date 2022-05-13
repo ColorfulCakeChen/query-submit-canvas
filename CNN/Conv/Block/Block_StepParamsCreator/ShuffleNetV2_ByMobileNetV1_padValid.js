89098f079105247ad5a9f8cd02b25048175c348c
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-13 15:27:17 +0800

    Update ShuffleNetV2_ByMobileNetV1_padValid.js

--- CNN/Conv/Block/Block_StepParamsCreator/ShuffleNetV2_ByMobileNetV1_padValid.js
+++ CNN/Conv/Block/Block_StepParamsCreator/ShuffleNetV2_ByMobileNetV1_padValid.js
@@ -1,7 +1,7 @@
 export { ShuffleNetV2_ByMobileNetV1_padValid };
 
 import * as ValueDesc from "../../../Unpacker/ValueDesc.js";
-import { Params } from "../Block_Param.js";
+import { Params } from "../Block_Params.js";
 import { ShuffleNetV2_ByMobileNetV1 } from "./ShuffleNetV2_ByMobileNetV1.js";
 
 /**
