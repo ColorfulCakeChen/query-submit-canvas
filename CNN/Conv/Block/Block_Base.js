71913e3a0045ec8fe6edebe86c17eed4750145fd
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-14 14:16:41 +0800

    Update Block_Base.js

--- CNN/Conv/Block/Block_Base.js
+++ CNN/Conv/Block/Block_Base.js
@@ -450,7 +450,7 @@ class Base {
   }
 
   /**
-   * Release all steps' BoundsArraySet. This could reduce memory footprint.
+   * Release all steps' BoundsArraySet. (But this block's BoundsArraySet is kept.) This could reduce memory footprint.
    */
   dispose_all_sub_BoundsArraySet() {
     if ( !this.stepsArray )
