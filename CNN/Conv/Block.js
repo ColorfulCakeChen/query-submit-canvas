397fabcf14c12886016e8e97b6a7465dd75747ac
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-16 15:59:28 +0800

    Update and rename Block.js to Stage.js

--- CNN/Conv/Block.js
+++
@@ -1,3 +0,0 @@
-export { Params } from "./Block/Block_Params.js";
-export * as StepParamsCreator from "./Block/Block_StepParamsCreator.js";
-export { Base } from "./Block/Block_Base.js";
---
+++ CNN/Conv/Stage.js
@@ -0,0 +1,3 @@
+export { Params } from "./Stage/Stage_Params.js";
+export * as BlockParamsCreator from "./Stage/Stage_BlockParamsCreator.js";
+export { Base } from "./Stage/Stage_Base.js";
