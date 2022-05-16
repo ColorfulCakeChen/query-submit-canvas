d9c69a7bd0282a8e21fe21bea955a409511f9599
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-16 15:54:41 +0800

    Update ValueDesc.js

--- CNN/Unpacker/ValueDesc.js
+++ CNN/Unpacker/ValueDesc.js
@@ -6,4 +6,4 @@ export { AvgMax_Or_ChannelMultiplier } from "./ValueDesc/ValueDesc_Other.js";
 export { StridesPad } from "./ValueDesc/ValueDesc_StridesPad.js";
 export { ActivationFunction } from "./ValueDesc/ValueDesc_ActivationFunction.js";
 export { PassThroughStyle } from "./ValueDesc/ValueDesc_PassThroughStyle.js";
-export { ConvBlockType } from "./ValueDesc/ValueDesc_ConvBlockType.js";
+export { ConvStageType } from "./ValueDesc/ValueDesc_ConvStageType.js";
