1f4a4e9c3855446c148900100904906dda1d2b91
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-16 16:06:15 +0800

    Update and rename CNN/Conv/Block/Block_StepParamsCreator.js to CNN/Conv/Stage/Stage_BlockParamsCreator.js

---
+++ CNN/Conv/Stage/Stage_BlockParamsCreator.js
@@ -0,0 +1,9 @@
+export { Base } from "./Stage_BlockParamsCreator/Base.js";
+export { MobileNetV1 } from "./Stage_BlockParamsCreator/MobileNetV1.js";
+export { MobileNetV1_padValid } from "./Stage_BlockParamsCreator/MobileNetV1_padValid.js";
+export { MobileNetV2_Thin } from "./Stage_BlockParamsCreator/MobileNetV2_Thin.js";
+export { MobileNetV2 } from "./Stage_BlockParamsCreator/MobileNetV2.js";
+export { ShuffleNetV2 } from "./Stage_BlockParamsCreator/ShuffleNetV2.js";
+export { ShuffleNetV2_ByPointwise22 } from "./Stage_BlockParamsCreator/ShuffleNetV2_ByPointwise22.js";
+export { ShuffleNetV2_ByMobileNetV1 } from "./Stage_BlockParamsCreator/ShuffleNetV2_ByMobileNetV1.js";
+export { ShuffleNetV2_ByMobileNetV1_padValid } from "./Stage_BlockParamsCreator/ShuffleNetV2_ByMobileNetV1_padValid.js";
