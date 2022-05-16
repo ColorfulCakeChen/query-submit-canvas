b70b2ac921f496b72e7de18b48fd5ea664c812de
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-16 16:23:05 +0800

    Update and rename PointDepthPoint.js to Block.js

---
+++ CNN/Conv/Block.js
@@ -0,0 +1,2 @@
+export { Params } from "./Block/Block_Params.js";
+export { Base } from "./Block/Block_Base.js";
--- CNN/Conv/PointDepthPoint.js
+++
@@ -1,2 +0,0 @@
-export { Params } from "./PointDepthPoint/PointDepthPoint_Params.js";
-export { Base } from "./PointDepthPoint/PointDepthPoint_Base.js";
