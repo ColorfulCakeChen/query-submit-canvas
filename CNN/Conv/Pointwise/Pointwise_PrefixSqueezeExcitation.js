dc6a197bb26d868a3e79013de788a192cb6f3e5f
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-17 18:54:54 +0800

    Create Pointwise_PrefixSqueezeExcitation.js

---
+++ CNN/Conv/Pointwise/Pointwise_PrefixSqueezeExcitation.js
@@ -0,0 +1,17 @@
+export { PrefixSqueezeExcitation };
+
+import * as BoundsArraySet from "../BoundsArraySet.js";
+import * as SqueezeExcitation from "../SqueezeExcitation.js";
+import { SameWhenPassThrough } from "./Pointwise_SameWhenPassThrough.js";
+
+/**
+ * A Pointwise_SameWhenPassThrough with a SqueezeExcitation in front of it.
+ *
+ */
+class Pointwise_PrefixSqueezeExcitation extends SameWhenPassThrough {
+
+//!!! ...unfinished... (2022/05/17)
+
+//!!! new SqueezeExcitation.Base()
+
+}
