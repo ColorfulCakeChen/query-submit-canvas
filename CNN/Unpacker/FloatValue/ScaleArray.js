0ddeaf746c111557ea4fd6731e7ffe76a48726cb
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-18 09:00:07 +0800

    Update ScaleArray.js

--- CNN/Unpacker/FloatValue/ScaleArray.js
+++ CNN/Unpacker/FloatValue/ScaleArray.js
@@ -212,7 +212,7 @@ class ScaleArray {
    * Rearrange scales by interleaving as ( groupCount == 2 ). This element count must be even (i.e. divisible by 2).
    *
    * @param {Array} arrayTemp
-   *   A temporary array for placing the original elements temporarily. Provide this array could reduce memory re-allocation
+   *   A temporary array for placing the original elements temporarily. Providing this array could reduce memory re-allocation
    * and improve performance.
    *
    * @return {ScaleArray} Return this (modified) object.
