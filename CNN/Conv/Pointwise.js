6bbd5cf25825853c06ebc311906616d684430f31
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-19 13:50:00 +0800

    Update Pointwise.js

--- CNN/Conv/Pointwise.js
+++ CNN/Conv/Pointwise.js
@@ -3,4 +3,5 @@ export { FiltersArray_BiasesArray } from "./Pointwise/Pointwise_FiltersArray_Bia
 export { PassThrough_FiltersArray_BiasesArray, PassThrough, AllZeros } from "./Pointwise/Pointwise_PassThrough.js";
 export { Base } from "./Pointwise/Pointwise_Base.js";
 export { SameWhenPassThrough } from "./Pointwise/Pointwise_SameWhenPassThrough.js";
+export { SameWhenPassThrough_PrefixSqueezeExcitation } from "./Pointwise/Pointwise_SameWhenPassThrough_PrefixSqueezeExcitation.js";
 export { ConstantWhenPassThrough } from "./Pointwise/Pointwise_ConstantWhenPassThrough.js";
