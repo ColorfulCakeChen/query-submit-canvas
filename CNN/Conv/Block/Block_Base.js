e0963ae42b8ae303436a636fd258577a0052f187
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-20 14:43:16 +0800

    Update Block_Base.js

--- CNN/Conv/Block/Block_Base.js
+++ CNN/Conv/Block/Block_Base.js
@@ -32,40 +32,40 @@ import { Params } from "./Block_Params.js";
  *     - ( channelCount1_pointwise1Before == -5 ): ONE_INPUT_HALF_THROUGH: (ShuffleNetV2_ByMobileNetV1's body/tail)
  *     - ( channelCount1_pointwise1Before == -4 ): ONE_INPUT_HALF_THROUGH_EXCEPT_DEPTHWISE1: (ShuffleNetV2_ByMobileNetV1's head)
  * <pre>
- * input0 - pointwise1 - depthwise1 ---------------- pointwise21 (include pointwise211 and pointwise212)
+ * input0 - pointwise1 - depthwise1 - (squeezeExcitation) - pointwise21 (include pointwise211 and pointwise212)
  * </pre>
  *
  *
  *   - When ( channelCount1_pointwise1Before == -3 ) and ( bOutput1Requested == true ): TWO_INPUTS_CONCAT_POINTWISE21_INPUT1: TWO_OUTPUT:
  * (ShuffleNetV2's body)
  * <pre>
- * input0 - pointwise1 - depthwise1 - pointwise21 - concat2ShuffleSplit - output0
- * input1 ----------------------------------------/                     \ output1
+ * input0 - pointwise1 - depthwise1 - (squeezeExcitation) - pointwise21 - concat2ShuffleSplit - output0
+ * input1 --------------------------------------------------------------/                     \ output1
  * </pre>
  *
  *
  *   - When ( channelCount1_pointwise1Before == -3 ) and ( bOutput1Requested == false ): TWO_INPUTS_CONCAT_POINTWISE21_INPUT1: ONE_OUTPUT:
  * (ShuffleNetV2's tail)
  * <pre>
- * input0 - pointwise1 - depthwise1 - pointwise21 - concat2(ShuffleSplit) - output0
- * input1 ----------------------------------------/
+ * input0 - pointwise1 - depthwise1 - (squeezeExcitation) - pointwise21 - concat2(ShuffleSplit) - output0
+ * input1 --------------------------------------------------------------/
  * </pre>
  *
  *
  *   - When ( channelCount1_pointwise1Before == -2 ): ONE_INPUT_TWO_DEPTHWISE:
  * (simplified ShuffleNetV2's head with ( pointwise1ChannelCount >= 1 ) )
  * <pre>
- * input0 - pointwise1 - depthwise1 - concat1 - pointwise21
- *        \------------- depthwise2 /         \ pointwise22
+ * input0 - pointwise1 - depthwise1 - concat1 - (squeezeExcitation) - pointwise21
+ *        \------------- depthwise2 /         \ (squeezeExcitation) - pointwise22
  * </pre>
  *
  *
  *   - When ( channelCount1_pointwise1Before == -1 ): ONE_INPUT_ADD_TO_OUTPUT: (MobileNetV2's body and tail)
  * <pre>
- *        /------------------------------------------------------\
- * input0 - pointwise1 - depthwise1 ---------------- pointwise21 - addInput0ToPointwise21
- *        \                                        \ pointwise22 - addInput0ToPointwise22
- *         \-----------------------------------------------------/
+ *        /----------------------------------------------------------------------------\
+ * input0 - pointwise1 - depthwise1 ---------------- (squeezeExcitation) - pointwise21 - addInput0ToPointwise21
+ *        \                                        \ (squeezeExcitation) - pointwise22 - addInput0ToPointwise22
+ *         \---------------------------------------------------------------------------/
  * </pre>
  *
  *
@@ -73,15 +73,15 @@ import { Params } from "./Block_Params.js";
  *     - ( channelCount1_pointwise1Before == 0 ): ONE_INPUT:
  *       (MobileNetV1 or MobileNetV2's head or simplified ShuffleNetV2(_ByPointwise22)'s head with ( blockParams.bPointwise1 == false ) )
  * <pre>
- * input0 - pointwise1 - depthwise1 ---------------- pointwise21
- *                                                 \ pointwise22
+ * input0 - pointwise1 - depthwise1 ---------------- (squeezeExcitation) - pointwise21
+ *                                                 \ (squeezeExcitation) - pointwise22
  * </pre>
  *
  *
  *   - When ( channelCount1_pointwise1Before > 0 ): TWO_INPUTS: (ShuffleNetV2_ByPointwise22's body and tail)
  * <pre>
- * input0 - pointwise1 - depthwise1 - concat1 - pointwise21
- * input1 --------------------------/         \ pointwise22
+ * input0 - pointwise1 - depthwise1 - concat1 - (squeezeExcitation) - pointwise21
+ * input1 --------------------------/         \ (squeezeExcitation) - pointwise22
  * </pre>
  *
  *
