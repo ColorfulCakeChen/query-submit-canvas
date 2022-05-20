e2747c5b187ec748717c56834f5729791584a693
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-20 14:32:22 +0800

    Update Pointwise_SameWhenPassThrough_PrefixSqueezeExcitation.js

--- CNN/Conv/Pointwise/Pointwise_SameWhenPassThrough_PrefixSqueezeExcitation.js
+++ CNN/Conv/Pointwise/Pointwise_SameWhenPassThrough_PrefixSqueezeExcitation.js
@@ -5,34 +5,6 @@ import * as ReturnOrClone from "../ReturnOrClone.js";
 import * as SqueezeExcitation from "../SqueezeExcitation.js";
 import { SameWhenPassThrough } from "./Pointwise_SameWhenPassThrough.js";
 
-
-//!!! ...unfinished... (2022/05/14)
-// Q: Perhaps, let pointwise1 become squeeze and excitation before depthwise.
-// A: It may not be possible because input and output channel count may be different.
-
-//!!! ...unfinished... (2022/05/08) Add squeeze and excitation before pointwise.
-// globale avg pooling - pointwise - pointwise - multiplyToInput
-// And the, the original pointwise
-
-
-//!!! ...unfinished... (2022/05/09) What if:
-// pointwise1 ( bias, activation )
-// depthwise ( channelMultipler > 1, bias / no bias, activation / no activation )
-// pointwiseSE ( bias, activation )
-// pointwise2 ( bias, activation )
-//
-// pointwise1 - depthwise - pointwiseSE - multiply - pointwise2
-//                        \-------------/
-//
-// No global average pooloing.
-//
-//
-//
-//
-
-
-
-
 /**
  * A Pointwise_SameWhenPassThrough with a SqueezeExcitation in front of it.
  *
