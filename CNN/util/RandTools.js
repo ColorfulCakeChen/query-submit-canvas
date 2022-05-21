f63066514a0242c8bcb6152b1bdf18b3372be682
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-21 13:00:23 +0800

    Update RandTools.js

--- CNN/util/RandTools.js
+++ CNN/util/RandTools.js
@@ -19,6 +19,38 @@ function getRandomIntInclusive( min, max ) {
 
 /**
  *
+ * @param {number[]} io_numberArray
+ *   The number array to be filled.
+ *
+ * @param {number} randomOffsetMin
+ *   Every element of the generated number array will been shifted from the sequence id between
+ * [ randomOffsetMin, randomOffsetMax ] (inclusive) randomly.
+ *
+ * @param {number} randomOffsetMax
+ *   Every element of the generated number array will been shifted from the sequence id between
+ * [ randomOffsetMin, randomOffsetMax ] (inclusive) randomly.
+ *
+ * @param {FloatValue.Bounds} oBounds
+ *   If not null, it will be filled as the value lower and upper bounds of the returned number array.
+ *
+ * @return {number[]}
+ *   Return the io_numberArray.
+ */
+function fill_numberArray( io_numberArray, randomOffsetMin = 0, randomOffsetMax = 0, oBounds = null ) {
+  for ( let i = 0; i < io_numberArray.length; ++i ) {
+    io_numberArray[ i ] = i + getRandomIntInclusive( randomOffsetMin, randomOffsetMax );
+  }
+
+  if ( oBounds ) {
+    oBounds.set_byLowerUpper( 0, io_numberArray.length - 1 ); // Basically, value is between [ 0, ( io_numberArray.length - 1 ) ].
+    oBounds.add_byLowerUpper( randomOffsetMin, randomOffsetMax ); // Plus the random range.
+  }
+
+  return io_numberArray.length;
+}
+
+/**
+ *
  * @param {number} elementCount
  *   The returned number array will have so many elements.
  *
@@ -38,14 +70,5 @@ function getRandomIntInclusive( min, max ) {
  */
 function generate_numberArray( elementCount, randomOffsetMin = 0, randomOffsetMax = 0, oBounds = null ) {
   let numberArray = new Array( elementCount );
-  for ( let i = 0; i < elementCount; ++i ) {
-    numberArray[ i ] = i + getRandomIntInclusive( randomOffsetMin, randomOffsetMax );
-  }
-  
-  if ( oBounds ) {
-    oBounds.set_byLowerUpper( 0, elementCount - 1 ); // Basically, value is between [ 0, ( elementCount - 1 ) ].
-    oBounds.add_byLowerUpper( randomOffsetMin, randomOffsetMax ); // Plus the random range.
-  }
-
-  return numberArray;
+  return fill_numberArray( numberArray, randomOffsetMin, randomOffsetMax, oBounds );
 }
