082d2b8270532f5f54ad77ed5c22b0caf761ab96
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-21 19:41:33 +0800

    Update Pointwise_SameWhenPassThrough.js

--- CNN/Conv/Pointwise/Pointwise_SameWhenPassThrough.js
+++ CNN/Conv/Pointwise/Pointwise_SameWhenPassThrough.js
@@ -7,7 +7,7 @@ import { Base } from "./Pointwise_Base.js";
  * Pointwise convolution whose output will be the same as input when pass-through.
  *
  * It has the following properties:
- *   - ( nPassThroughStyleId == ValueDesc.PassThroughStyle.Singleton.Ids.PASS_THROUGH_STYLE_FILTER_1_BIAS_0_ACTIVATION_ESCAPING ) (0).
+ *   - ( nPassThroughStyleId == ValueDesc.PassThroughStyle.Singleton.Ids.PASS_THROUGH_STYLE_FILTER_1_BIAS_0 ) (0).
  *
  * Usually, this is used for non-squeeze-and-excitaion pointwise.
  *
@@ -22,7 +22,7 @@ class SameWhenPassThrough extends Base {
 
     super(
       inputChannelCount, outputChannelCount, bBias, nActivationId,
-      ValueDesc.PassThroughStyle.Singleton.Ids.PASS_THROUGH_STYLE_FILTER_1_BIAS_0_ACTIVATION_ESCAPING,
+      ValueDesc.PassThroughStyle.Singleton.Ids.PASS_THROUGH_STYLE_FILTER_1_BIAS_0,
       nHigherHalfDifferent, inputChannelCount_lowerHalf, outputChannelCount_lowerHalf, channelShuffler_outputGroupCount );
   }
 
