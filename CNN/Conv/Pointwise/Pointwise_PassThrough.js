a8a4c60188992317e2195359a0b2bb6ad716f67f
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-23 13:30:41 +0800

    Update Pointwise_PassThrough.js

--- CNN/Conv/Pointwise/Pointwise_PassThrough.js
+++ CNN/Conv/Pointwise/Pointwise_PassThrough.js
@@ -105,6 +105,53 @@ let PassThrough_FiltersArray_BiasesArray = ( Base = Object ) => class extends Ba
 }
 
 
+
+/**
+ * A pool for PassThrough_FiltersArray_BiasesArray with various parameters. It could reduce re-create them of same parameters again
+ * and again to improve performance.
+ *
+ */
+class PassThrough_FiltersArray_BiasesArray_Bag {
+
+  constructor() {
+    this.map = new MultiLayerMap();
+  }
+
+  get_by_nPassThroughStyleId( inputChannelCount, outputChannelCount, inputChannelIndexStart, bBias, nPassThroughStyleId ) {
+
+//!!! ...unfinished... (2022/05/23)
+//     let nPassThroughStyleId = ValueDesc.PassThroughStyle.Singleton.Ids.PASS_THROUGH_STYLE_FILTER_1_BIAS_0;
+//     const thePassThroughStyleInfo = ValueDesc.PassThroughStyle.Singleton.getInfoById( nPassThroughStyleId );
+
+  }
+
+  get_by_filterValue_biasValue( inputChannelCount, outputChannelCount, inputChannelIndexStart, bBias, filterValue = 1, biasValue = 0 ) {
+
+//!!! ...unfinished... (2022/05/23)
+    let by_outputChannelCount_inputChannelIndexStart_bBias_filterValue_biasValue
+      = MapTools.get_or_create(
+          this.by_inputChannelCount_outputChannelCount_inputChannelIndexStart_bBias_filterValue_biasValue, inputChannelCount );
+
+  }
+
+//!!! ...unfinished... (2022/05/23)
+  /** Release all tensors. */
+  disposeTensors() {
+    if ( this.tensorsBy_originalHeight_originalWidth_channelCount_filterHeight_filterWidth_stridesPad ) {
+
+      for ( let tensor
+        of MapTools.values_recursively( this.tensorsBy_originalHeight_originalWidth_channelCount_filterHeight_filterWidth_stridesPad ) ) {
+
+        tensor.dispose();
+      }
+
+      this.tensorsBy_originalHeight_originalWidth_channelCount_filterHeight_filterWidth_stridesPad.clear();
+    }
+  }
+
+}
+
+
 /**
  * A pointwise convolution and bias which just pass the input to output.
  *
