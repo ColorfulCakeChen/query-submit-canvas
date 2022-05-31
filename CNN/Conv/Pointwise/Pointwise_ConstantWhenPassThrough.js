81479b9b2777676945654e622ea1fc097dd350a9
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-31 19:37:13 +0800

    Update Pointwise_ConstantWhenPassThrough.js

--- CNN/Conv/Pointwise/Pointwise_ConstantWhenPassThrough.js
+++ CNN/Conv/Pointwise/Pointwise_ConstantWhenPassThrough.js
@@ -17,10 +17,12 @@ class ConstantWhenPassThrough extends Base {
   /**
    */
   constructor(
+    inputTensorPlaceholder0,
     inputChannelCount, outputChannelCount, bBias, nActivationId,
     nHigherHalfDifferent, inputChannelCount_lowerHalf, outputChannelCount_lowerHalf, channelShuffler_outputGroupCount ) {
 
     super(
+      inputTensorPlaceholder0,
       inputChannelCount, outputChannelCount, bBias, nActivationId,
       ValueDesc.PassThroughStyle.Singleton.Ids.PASS_THROUGH_STYLE_FILTER_0_BIAS_1,
       nHigherHalfDifferent, inputChannelCount_lowerHalf, outputChannelCount_lowerHalf, channelShuffler_outputGroupCount );
