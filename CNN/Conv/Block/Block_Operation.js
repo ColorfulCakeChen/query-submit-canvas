d5b5be672d53d127b9b2c38a9563a47bb8296e1c
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-31 12:56:57 +0800

    Update Block_Operation.js

--- CNN/Conv/Block/Block_Operation.js
+++ CNN/Conv/Block/Block_Operation.js
@@ -1,9 +1,21 @@
 export { Operation };
 
+import { TensorPlaceholderSet } from "./Block_TensorPlaceholderSet.js";
+
 /**
  *
  */
 let Operation = ( Base = Object ) => class extends Base {
+
+//!!! ...unfinished... (2022/05/31)
+  /**
+   *
+   * @param {number} outputTensorCount
+   *   If 1, only the this.output0 will be created. If 2, the this.output0 and this.output1 will be created.
+   */
+  TensorPlaceholderSet_set( input0, input1, outputTensorCount = 1 ) {
+    this.tensorPlaceholderSet = new TensorPlaceholderSet( input0, input1, outputTensorCount );
+  }
 
   /**
    * Adjust this.apply so that this.apply() will or will not dispose its inputTensors.
@@ -17,6 +29,13 @@ let Operation = ( Base = Object ) => class extends Base {
    * input tensor (i.e. does not have 2nd input tensor).
    */
   setKeepInputTensor( bKeepInputTensor0, bKeepInputTensor1 ) {
+  }
+
+//!!! ...unfinished... (2022/05/31)
+  /**
+   *
+   */
+  apply() {
   }
 
 }
