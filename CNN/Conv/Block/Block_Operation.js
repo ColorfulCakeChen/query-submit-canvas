2e5f4194e819af7ab19f4e913122748bceebf175
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-05-31 15:31:52 +0800

    Create Block_Operation.js

---
+++ CNN/Conv/Block/Block_Operation.js
@@ -0,0 +1,6 @@
+export { Base } from "./Block_Operation/Block_Operation_Base.js";
+export { Depthwise } from "./Block_Operation/Block_Operation_Depthwise.js";
+export { Pointwise } from "./Block_Operation/Block_Operation_Pointwise.js";
+export { AddTwoTensors } from "./Block_Operation/Block_Operation_AddTwoTensors.js";
+export { ConcatShuffleSplit } from "./Block_Operation/Block_Operation_ConcatShuffleSplit.js";
+export { ConcatAlongAxisId2 } from "./Block_Operation/Block_Operation_ConcatAlongAxisId2.js";
