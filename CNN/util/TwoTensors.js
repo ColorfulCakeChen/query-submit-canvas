464da20ac7d5e88a22e2f04a820bd58a3a3b8ad5
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-01 18:56:56 +0800

    Update TwoTensors.js

--- CNN/util/TwoTensors.js
+++ CNN/util/TwoTensors.js
@@ -3,13 +3,13 @@ export { filtersTensor4d_biasesTensor3d };
 /**
  * A function accepts a Base class and returns its sub-class which contains filtersTensor4d and biasesTensor3d, and a method to dispose them.
  *
- * @param {Class} Base
- *   The base class to be inherited. Default is Object.
+ * @param {Class} ParentClass
+ *   The parent class to be inherited. Default is Object.
  *
  * @return {Class}
  *   Return a sub-class of Base.
  */
-let filtersTensor4d_biasesTensor3d = ( Base = Object )  => class extends Base {
+let filtersTensor4d_biasesTensor3d = ( ParentClass = Object )  => class extends ParentClass {
 
   /**
    * Release the tensors.
@@ -23,6 +23,11 @@ let filtersTensor4d_biasesTensor3d = ( Base = Object )  => class extends Base {
     if ( this.biasesTensor3d ) {
       this.biasesTensor3d.dispose();
       this.biasesTensor3d = null;
+    }
+
+    // If parent class has the same method, call it.
+    if ( super.disposeTensors instanceof Function ) {
+      super.disposeTensors();
     }
   }
 
