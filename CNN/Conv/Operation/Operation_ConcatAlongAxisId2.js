40e0c8da594f89560bda6de70c9f913c6f18d041
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-01 11:59:07 +0800

    Update and rename CNN/Conv/ConcatAlongAxisId2.js to CNN/Conv/Operation/Operation_ConcatAlongAxisId2.js

--- CNN/Conv/Operation/Operation_ConcatAlongAxisId2.js
+++ CNN/Conv/Operation/Operation_ConcatAlongAxisId2.js
@@ -1,7 +1,8 @@
-export { Base };
+export { ConcatAlongAxisId2 };
 
-import * as Operation from "./Operation.js";
+import * as TensorPlaceholder from "../TensorPlaceholder.js";
 import * as BoundsArraySet from "./BoundsArraySet.js";
+import { Base } from "./Operation_Base.js";
 
 /**
  * Concatenate two tensor3d ( height x width x channel ) always along the last axis (i.e. axisId = 2, along the channel axis). It could
@@ -25,28 +26,33 @@ import * as BoundsArraySet from "./BoundsArraySet.js";
  *   The element value bounds (per channel) of this concatenation operation.
  *
  * @member {function} apply
- *   This is a method. It has one parameter inputTensorsArray and return a outputTensor. The inputTensorsArray (tf.tensor3d[])
- * represents all the images ( height x width x channel ) which will be concatenated. They should have the same ( height x width )
- * but could have different channel count. The outputTensor (tf.tensor3d) represents the result of concatenating the inputs along
- * the last axis (i.e. the channel axis ( axisId = 2 ) ). The inputTensor may or may not be disposed. In fact, this method calls
- * one of Concat_and_keep0_keep1(), Concat_and_keep0_destroy1(), Concat_and_destroy0_keep1(), Concat_and_destroy0_destroy1()
+ *   This is a method. It processes this.input0.realTensor and this.input1.realTensor as inputTensors. It put to this.output0.realTensor
+ * as outputTensor. Both inputTensors are tf.tensor3d and represents an images ( height x width x channel ) which will be concatenated.
+ * They should have the same ( height x width ) but could have different channel count. The outputTensor (tf.tensor3d) represents
+ * the result of concatenating the inputs along the last axis (i.e. the channel axis ( axisId = 2 ) ). The inputTensor may or may not
+ * be disposed. In fact, this method calls one of Concat_and_keep0_keep1(), Concat_and_keep0_destroy1(), Concat_and_destroy0_keep1(),
+ * Concat_and_destroy0_destroy1()
  * according to the parameters.
  *
  */
-class Base extends Operation.Base() {
+class ConcatAlongAxisId2 extends Base() {
 
   constructor(
-//!!! ...unfinished... (2022/05/21)
     inputTensorPlaceholder0, inputTensorPlaceholder1,
-    bKeepInputTensor0, bKeepInputTensor1, inputScaleBoundsArray0, inputScaleBoundsArray1 ) {
+    inputScaleBoundsArray0, inputScaleBoundsArray1,
+    bKeepInputTensor0, bKeepInputTensor1
+  ) {
 
     super( inputTensorPlaceholder0, inputTensorPlaceholder1, 1 );
 
     this.bKeepInputTensor0 = bKeepInputTensor0;
     this.bKeepInputTensor1 = bKeepInputTensor1;
 
+    this.inputTensors = new Array( 2 ); // For reducing memory re-allocation.
+
     Base.adjust_pfn.call( this );
     Base.setup_BoundsArraySet.call( this, inputScaleBoundsArray0, inputScaleBoundsArray1 );
+    Base.setup_output0_TensorPlaceholder.call( this );
   }
 
   /**
@@ -101,30 +107,40 @@ class Base extends Operation.Base() {
     this.boundsArraySet.set_outputs_all_by_concat_input0_input1(); // The outputChannelCount0 will be adjusted.
   }
 
-  /** Concatenate along axis id 2. (Both the inputTensorsArray[ 0 ] and inputTensorsArray[ 1 ] will not be disposed. */
-  static Concat_and_keep0_keep1( inputTensorsArray ) {
-    return tf.concat( inputTensorsArray, 2 ); // AxisId = 2
+//!!!
+
+  /** Concatenate along axis id 2. (Both the input0 and input1 will not be disposed. */
+  static Concat_and_keep0_keep1() {
+    this.inputTensors[ 0 ] = this.input0.realTensor;
+    this.inputTensors[ 1 ] = this.input1.realTensor;
+    this.output0.realTensor = tf.concat( this.inputTensors, 2 ); // AxisId = 2
   }
 
-  /** Concatenate along axis id 2. (The inputTensorsArray[ 0 ] will not be disposed. The inputTensorsArray[ 1 ] will be disposed. */
-  static Concat_and_keep0_destroy1( inputTensorsArray ) {
-    let t = tf.concat( inputTensorsArray, 2 ); // AxisId = 2
-    inputTensorsArray[ 1 ].dispose();
+  /** Concatenate along axis id 2. (The input0 will not be disposed. The input1 will be disposed. */
+  static Concat_and_keep0_destroy1() {
+    this.inputTensors[ 0 ] = this.input0.realTensor;
+    this.inputTensors[ 1 ] = this.input1.realTensor;
+    this.output0.realTensor = tf.concat( this.inputTensors, 2 ); // AxisId = 2
+    this.input1.realTensor.dispose();
     return t;
   }
 
-  /** Concatenate along axis id 2. (The inputTensorsArray[ 0 ] will be disposed. The inputTensorsArray[ 1 ] will not be disposed. */
-  static Concat_and_destroy0_keep1( inputTensorsArray ) {
-    let t = tf.concat( inputTensorsArray, 2 ); // AxisId = 2
-    inputTensorsArray[ 0 ].dispose();
+  /** Concatenate along axis id 2. (The input0 will be disposed. The input1 will not be disposed. */
+  static Concat_and_destroy0_keep1() {
+    this.inputTensors[ 0 ] = this.input0.realTensor;
+    this.inputTensors[ 1 ] = this.input1.realTensor;
+    this.output0.realTensor = tf.concat( this.inputTensors, 2 ); // AxisId = 2
+    this.input0.realTensor.dispose();
     return t;
   }
 
-  /** Concatenate along axis id 2. (Both the inputTensorsArray[ 0 ] and inputTensorsArray[ 1 ] will be disposed. */
-  static Concat_and_destroy0_destroy1( inputTensorsArray ) {
-    let t = tf.concat( inputTensorsArray, 2 ); // AxisId = 2
-    inputTensorsArray[ 0 ].dispose();
-    inputTensorsArray[ 1 ].dispose();
+  /** Concatenate along axis id 2. (Both the input0 and input1 will be disposed. */
+  static Concat_and_destroy0_destroy1() {
+    this.inputTensors[ 0 ] = this.input0.realTensor;
+    this.inputTensors[ 1 ] = this.input1.realTensor;
+    this.output0.realTensor = tf.concat( this.inputTensors, 2 ); // AxisId = 2
+    this.input0.realTensor.dispose();
+    this.input1.realTensor.dispose();
     return t;
   }
 
