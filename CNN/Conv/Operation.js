365b35d4b4e53b1000640eddc76e37610c7443e4
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-01 10:48:58 +0800

    Update and rename CNN/Conv/Block/old/old_Block_Operation.js to CNN/Conv/Operation.js

---
+++ CNN/Conv/Operation.js
@@ -0,0 +1,6 @@
+export { Base } from "./Operation/Operation_Base.js";
+export { Depthwise } from "./Operation/Operation_Depthwise.js";
+export { Pointwise } from "./Operation/Operation_Pointwise.js";
+export { AddTwoTensors } from "./Operation/Operation_AddTwoTensors.js";
+export { ConcatShuffleSplit } from "./Operation/Operation_ConcatShuffleSplit.js";
+export { ConcatAlongAxisId2 } from "./Operation/Operation_ConcatAlongAxisId2.js";
