4801e41c09a3f9983e75acda2a75197105d2bf1a
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-02 09:16:29 +0800

    Update Operation_Depthwise.js

--- CNN/Conv/Operation/Operation_Depthwise.js
+++ CNN/Conv/Operation/Operation_Depthwise.js
@@ -97,7 +97,16 @@ class Depthwise extends FiltersArray_BiasesArray( TwoTensors.filtersTensor4d_bia
       this.boundsArraySet = new BoundsArraySet.Depthwise( inputScaleBoundsArray, inputScaleBoundsArray.channelCount );
       this.boundsArraySet.output0.set_all_byScaleBoundsArray( inputScaleBoundsArray ); // Bypass previous to next.
 
-      this.output0 = this.input0; // Bypass previous to next.
+//!!! (2022/06/02 Remarked)
+// Note: The .outputX and .inputX should always be different object (but can have the same content).
+//       Otherwise, the apply() will destroy the content of .inputX (especially when keep-input-tensor).
+//      this.output0 = this.input0; // Bypass previous to next.
+
+      // Bypass previous to next.
+      //
+      // Note: The .outputX and .inputX should always be different object (but can have the same content).
+      //       Otherwise, the apply() will destroy the content of .inputX (especially when keep-input-tensor).
+      this.output0.set_height_width_channelCount_scaleBoundsArray_byTensorPlaceholder( this.input0 );
 
     } else { // 3.
 
@@ -118,12 +127,8 @@ class Depthwise extends FiltersArray_BiasesArray( TwoTensors.filtersTensor4d_bia
             this.output0.height = this.outputHeight;
             this.output0.width = this.outputWidth;
             this.output0.channelCount = this.outputChannelCount;
-
-            if ( this.outputChannelCount_lowerHalf != undefined )
-              this.output0.channelCount_lowerHalf = this.outputChannelCount_lowerHalf;
-
-            if ( this.outputChannelCount_higherHalf != undefined )
-              this.output0.channelCount_higherHalf = this.outputChannelCount_higherHalf;
+            this.output0.channelCount_lowerHalf = this.outputChannelCount_lowerHalf;
+            this.output0.channelCount_higherHalf = this.outputChannelCount_higherHalf;
           }
 
         } catch ( e ) {  // If failed (e.g. memory not enough), return false.      
