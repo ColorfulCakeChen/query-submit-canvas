cf02c771e1eade70291d603ae4888222dca0e7fa
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-09 14:52:43 +0800

    Update BoundsArraySet_Asserter.js

--- CNN/jsPerf/Ref/BoundsArraySet_Asserter.js
+++ CNN/jsPerf/Ref/BoundsArraySet_Asserter.js
@@ -70,17 +70,11 @@ function assert_ScaleBoundsArray( asserter_Equal, aScaleBoundsArray, refScaleBou
 function assert_ScaleBoundsArray_output0_output1( asserter_Equal,
   output0_scaleBoundsArray, output1_scaleBoundsArray, imageOutReferenceArray, prefixMsg, postfixMsg ) {
 
-  assert_ScaleBoundsArray( output0_scaleBoundsArray, imageOutReferenceArray[ 0 ].boundsArraySet.output0,
+  assert_ScaleBoundsArray( asserter_Equal, output0_scaleBoundsArray, imageOutReferenceArray[ 0 ].boundsArraySet.output0,
     "output0", "output0_Ref", prefixMsg, postfixMsg );
 
   if ( output1_scaleBoundsArray ) {
-    //!!! (2022/04/27 Remarked) input tensor count may be different.
-    //assert_byIndex( "input", 0, aBoundsArraySet.input0, imageOutReferenceArray[ 1 ].boundsArraySet.input0 );
-    //assert_byIndex( "input", 1, aBoundsArraySet.input1, imageOutReferenceArray[ 1 ].boundsArraySet.input1 );
-
-//!!! (2022/06/08 Remarked)
-//    assert_byIndex( "output", 1, aBoundsArraySet.output1, imageOutReferenceArray[ 1 ].boundsArraySet.output0 );
-    assert_ScaleBoundsArray( output1_scaleBoundsArray, imageOutReferenceArray[ 1 ].boundsArraySet.output0,
+    assert_ScaleBoundsArray( asserter_Equal, output1_scaleBoundsArray, imageOutReferenceArray[ 1 ].boundsArraySet.output0,
       "output1", "output1_ref", prefixMsg, postfixMsg );
   }
 }
