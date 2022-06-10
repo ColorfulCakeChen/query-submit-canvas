cce13be9cd4aefd030df8c385693afd210ef35d7
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-10 10:38:06 +0800

    Update Depthwise_PadInfoCalculator.js

--- CNN/Conv/Depthwise/Depthwise_PadInfoCalculator.js
+++ CNN/Conv/Depthwise/Depthwise_PadInfoCalculator.js
@@ -8,10 +8,10 @@ import * as ValueDesc from "../../Unpacker/ValueDesc.js";
  * @member {number} inputHeight           Input image height.
  * @member {number} inputWidth            Input image width.
  * @member {number} inputChannelCount     Input image channel count.
- * @member {number} AvgMax_Or_ChannelMultiplier   Depthwise operation. (ValueDesc.AvgMax_Or_ChannelMultiplier)
+ * @member {number} AvgMax_Or_ChannelMultiplier   Depthwise operation. (ValueDesc.AvgMax_Or_ChannelMultiplier.Singleton.Ids.Xxx)
  * @member {number} filterHeight          The height of the depthwise convolution's filter.
  * @member {number} filterWidth           The width of the depthwise convolution's filter.
- * @member {number} stridesPad            The strides and padding of (depthwise) convolution. (ValueDesc.StridesPad)
+ * @member {number} stridesPad            The strides and padding of (depthwise) convolution. (ValueDesc.StridesPad.Singleton.Ids.Xxx)
  *
  * @member {number} channelMultiplier     The channel multiplier of the depthwise operation (according to AvgMax_Or_ChannelMultiplier).
  * @member {number} filterSize            The size of the depthwise convolution's filter. (= filterHeight * filterWidth)
@@ -23,10 +23,12 @@ import * as ValueDesc from "../../Unpacker/ValueDesc.js";
  * @member {number} effectFilterWidth     The effect width of the depthwise convolution's filter including the dilationWidth.
  * @member {number} effectFilterSize      The effect size of the depthwise convolution's filter. (= effectFilterHeight * effectFilterWidth)
  *
+ * @member {ValueDesc.StridesPad.Info} stridesPadInfo  The information getted according to stridesPad.
+ *
  * @member {number} strides               The strides along the image's height and width dimension (according to stridesPad).
  * @member {number} stridesHeight         The strides along the image's height dimension (according to stridesPad).
  * @member {number} stridesWidth          The strides along the image's width dimension (according to stridesPad).
-
+ *
  * @member {number} pad                   The padding along the image's height and width dimension (according to stridesPad).
  * @member {number} padHeight             The padding along the input image's height dimension.
  * @member {number} padHeightTop          The padding along the input image's height dimension at the top.
@@ -164,24 +166,27 @@ let PadInfoCalculator = ( ParentClass = Object ) => class extends ParentClass {
 
 //!!! ...unfinished... (2022/06/10)
   /**
+   * @param {number} depthwise_AvgMax_Or_ChannelMultiplier  Depthwise operation. (ValueDesc.AvgMax_Or_ChannelMultiplier.Singleton.Ids.Xxx)
    *
+   * @return {boolean} If the channel count of the depthwise operation's output is the same as its input, return true.
    */
-  static output_is_same_channelCount_as_input( depthwise_AvgMax_Or_ChannelMultiplier ) {
+  static output_channelCount_is_same_as_input( depthwise_AvgMax_Or_ChannelMultiplier ) {
     return ( depthwise_AvgMax_Or_ChannelMultiplier <= 1 ); // e.g. avg pooling, or max pooling, or none, or ( channelMultipler == 1 ).
   }
 
-//!!! ...unfinished... (2022/06/10)
   /**
-   * @return {boolean}
-   *   If the ( height, width ) of the depthwise operation's output is the same as its input, return true.
+   * @param {number} depthwise_AvgMax_Or_ChannelMultiplier       Depthwise operation. (ValueDesc.AvgMax_Or_ChannelMultiplier.Singleton.Ids.Xxx)
+   * @param {ValueDesc.StridesPad.Info} depthwiseStridesPadInfo  The information of stridesPad.
+   *
+   * @return {boolean} If the ( height, width ) of the depthwise operation's output is the same as its input, return true.
    */
-  static output_is_same_height_width_as_input(
+  static output_height_width_is_same_as_input(
     inputHeight, inputWidth,
     depthwise_AvgMax_Or_ChannelMultiplier, depthwiseFilterHeight, depthwiseFilterWidth, depthwiseStridesPadInfo
   ) {
     return
          ( depthwise_AvgMax_Or_ChannelMultiplier == ValueDesc.AvgMax_Or_ChannelMultiplier.Singleton.Ids.NONE ) // (0), no-op
-      || ( ( 1 == inputHeight ) && ( 1 == inputWidth ) )  // input height and width can not be shrinked any more.
+      || ( ( 1 == inputHeight ) && ( 1 == inputWidth ) )  // input's height and width can not be shrinked any more.
       || (   ( 1 == depthwiseStridesPadInfo.strides )     // Not shrinked by strides.
           && (   ( depthwiseStridesPadInfo.pad_isSame() ) // Not shrinked by filters for ( pad is "same" ).
               || ( depthwiseStridesPadInfo.pad_isValid() && ( 1 == depthwiseFilterHeight ) && ( 1 == depthwiseFilterWidth ) ) // Not shrinked by filters for ( pad is "valid" ). (i.e. ( filter size is 1x1 ) )
@@ -189,10 +194,20 @@ let PadInfoCalculator = ( ParentClass = Object ) => class extends ParentClass {
          );
   }
 
-
-    let bNoNeighborAnalysis =
-         ( ( inputHeight == 1 ) && ( inputWidth == 1 ) )
-      || ( ( 1 == depthwiseFilterHeight ) && ( 1 == depthwiseFilterWidth ) );
+  /**
+   * @param {number} depthwise_AvgMax_Or_ChannelMultiplier  Depthwise operation. (ValueDesc.AvgMax_Or_ChannelMultiplier.Singleton.Ids.Xxx)
+   *
+   * @return {boolean} If the depthwise operation does not analyze the neighbor in the direction of height and width, return true.
+   */
+  static output_height_width_is_no_neighbor_analysis(
+    inputHeight, inputWidth,
+    depthwise_AvgMax_Or_ChannelMultiplier, depthwiseFilterHeight, depthwiseFilterWidth
+  ) {
+    return
+         ( depthwise_AvgMax_Or_ChannelMultiplier == ValueDesc.AvgMax_Or_ChannelMultiplier.Singleton.Ids.NONE ) // (0), no-op
+      || ( ( 1 == inputHeight ) && ( 1 == inputWidth ) )  // input's height and width do not have neighbor to be analyzed.
+      || ( ( 1 == depthwiseFilterHeight ) && ( 1 == depthwiseFilterWidth ) ); // The filters do not analyze any neighbor.
+  }
 
 
   /**
