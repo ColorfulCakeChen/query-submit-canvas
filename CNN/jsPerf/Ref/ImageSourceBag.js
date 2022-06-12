f97f61e6072229262f4ef9ba59f05b710158e8b5
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-12 20:58:17 +0800

    Update ImageSourceBag.js

--- CNN/jsPerf/Ref/ImageSourceBag.js
+++ CNN/jsPerf/Ref/ImageSourceBag.js
@@ -102,56 +102,6 @@ class Base {
   static internal_getImage_by(
     originalHeight, originalWidth, channelCount, depthwiseFilterHeight = 1, depthwiseFilterWidth = 1, depthwiseStridesPad = 0 ) {
 
-//!!! (2022/05/23 Remarked) Replaced by MultiLayerMap.
-//     let imagesBy_originalWidth_channelCount_filterHeight_filterWidth_stridesPad
-//       = MapTools.get_or_create( this.imagesBy_originalHeight_originalWidth_channelCount_filterHeight_filterWidth_stridesPad, originalHeight );
-//
-//     let imagesBy_channelCount_filterHeight_filterWidth_stridesPad
-//       = MapTools.get_or_create( imagesBy_originalWidth_channelCount_filterHeight_filterWidth_stridesPad, originalWidth );
-//
-//     let imagesBy_filterHeight_filterWidth_stridesPad
-//       = MapTools.get_or_create( imagesBy_channelCount_filterHeight_filterWidth_stridesPad, channelCount );
-//
-//     let imagesBy_filterWidth_stridesPad = MapTools.get_or_create( imagesBy_filterHeight_filterWidth_stridesPad, depthwiseFilterHeight );
-//     let imagesBy_stridesPad = MapTools.get_or_create( imagesBy_filterWidth_stridesPad, depthwiseFilterWidth );
-//
-//     let image = imagesBy_stridesPad.get( depthwiseStridesPad );
-//     if ( image )
-//       return image; // 1. The requested image has already been created. Re-use it. Return it directly.
-//
-//     // 2. The requested has not yet existed. It should be created newly.
-//
-//     // 2.1 The original image is requested.
-//     if ( ( depthwiseFilterHeight == 1 ) && ( depthwiseFilterWidth == 1 ) && ( depthwiseStridesPad == 0 ) ) {
-//       let randomOffsetMin = -200; // Just choosed randomly.
-//       let randomOffsetMax = +200;
-//
-//       //!!! (2022/04/21 Remaked) Using Weights.Base.ValueBounds is more like real use case.
-//       //let bAutoBounds = true;  // Image pixel channel value bounds are inside the real generated value bounds.
-//       let bAutoBounds = false; // Image pixel channel value bounds are inside the default value bounds (i.e. Weights.Base.ValueBounds).
-//       image = NumberImage.Base.create_bySequenceRandom( originalHeight, originalWidth, channelCount, randomOffsetMin, randomOffsetMax );
-//
-//     // 2.2 The shrinked image requested.
-//     } else {
-//
-//       // Use original image to create shrinked image.
-//       let originalImage = Base.internal_getImage_by.call( this, originalHeight, originalWidth, channelCount );
-//
-//       // Borrow the calcDepthwise() function to create an input image which is shrinked by specified filter size and strides and pad.
-//       image = originalImage.cloneBy_depthwise(
-//         ValueDesc.AvgMax_Or_ChannelMultiplier.Singleton.Ids.MAX, // Max Pooling is faster and without filter weights.
-//         depthwiseFilterHeight, depthwiseFilterWidth,
-//         depthwiseStridesPad,
-//         null, false,  // depthwiseFiltersArray, bDepthwiseBias, 
-//         null, ValueDesc.ActivationFunction.Singleton.Ids.NONE, // depthwiseBiasesArray, depthwiseActivationId,
-//         "Base.internal_getImage_by()", ""
-//       );
-//     }
-//
-//     imagesBy_stridesPad.set( depthwiseStridesPad, image ); // Cache it.
-//     return image;
-
-
     return this.images.get_or_create_by_arguments1_etc(
 
       // Called when the requested image has not yet existed.
@@ -198,34 +148,6 @@ class Base {
   static internal_getTensor3d_by(
     originalHeight, originalWidth, channelCount, depthwiseFilterHeight = 1, depthwiseFilterWidth = 1, depthwiseStridesPad = 0 ) {
 
-//!!! (2022/05/23 Remarked) Replaced by MultiLayerMap.
-//     let tensorsBy_originalWidth_channelCount_filterHeight_filterWidth_stridesPad
-//       = MapTools.get_or_create( this.tensorsBy_originalHeight_originalWidth_channelCount_filterHeight_filterWidth_stridesPad, originalHeight );
-//
-//     let tensorsBy_channelCount_filterHeight_filterWidth_stridesPad
-//       = MapTools.get_or_create( tensorsBy_originalWidth_channelCount_filterHeight_filterWidth_stridesPad, originalWidth );
-//
-//     let tensorsBy_filterHeight_filterWidth_stridesPad
-//       = MapTools.get_or_create( tensorsBy_channelCount_filterHeight_filterWidth_stridesPad, channelCount );
-//
-//     let tensorsBy_filterWidth_stridesPad = MapTools.get_or_create( tensorsBy_filterHeight_filterWidth_stridesPad, depthwiseFilterHeight );
-//     let tensorsBy_stridesPad = MapTools.get_or_create( tensorsBy_filterWidth_stridesPad, depthwiseFilterWidth );
-//
-//     let tensor = tensorsBy_stridesPad.get( depthwiseStridesPad );
-//     if ( tensor )
-//       return tensor; // 1. The requested tensor has already been created. Re-use it. Return it directly.
-//
-//     // 2. Create new tensor according to its corresponding image.
-//     let image = Base.internal_getImage_by.call( this,
-//       originalHeight, originalWidth, channelCount, depthwiseFilterHeight, depthwiseFilterWidth, depthwiseStridesPad );
-//
-//     let shape = [ image.height, image.width, image.depth ];
-//     tensor = tf.tensor3d( image.dataArray, shape ); // Create new tensor of specified specification.
-//
-//     tensorsBy_stridesPad.set( depthwiseStridesPad, tensor ); // Cache it.
-//     return tensor;
-
-
     return this.tensors.get_or_create_by_arguments1_etc(
 
       // Called when the requested tensor has not yet existed.
@@ -245,29 +167,8 @@ class Base {
   /** Release all tensors. */
   disposeTensors() {
 
-//!!! (2022/05/23 Remarked) Replaced by MultiLayerMap.
-//     if ( this.tensorsBy_originalHeight_originalWidth_channelCount_filterHeight_filterWidth_stridesPad ) {
-//
-//       for ( let tensor
-//         of MapTools.values_recursively( this.tensorsBy_originalHeight_originalWidth_channelCount_filterHeight_filterWidth_stridesPad ) ) {
-//
-//         tensor.dispose();
-//       }
-//
-//       this.tensorsBy_originalHeight_originalWidth_channelCount_filterHeight_filterWidth_stridesPad.clear();
-//     }
-
-
-//!!! (2022/05/23 Remarked) Replaced by values().
-//     if ( this.tensors ) {
-//       this.tensors.visit_all_and_call( tensor => {
-//         tensor.dispose();
-//       } );
-//       this.tensors.clear();
-//     }
-
     if ( this.tensors ) {
-      for ( let tensor of this.values() ) {
+      for ( let tensor of this.tensors.values() ) {
         tensor.dispose();
       }
       this.tensors.clear();
