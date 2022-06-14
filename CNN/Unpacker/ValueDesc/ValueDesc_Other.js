0e39ce321b1f86aed10c05ae87753e122ecc644d
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-14 10:11:53 +0800

    Update ValueDesc_Other.js

--- CNN/Unpacker/ValueDesc/ValueDesc_Other.js
+++ CNN/Unpacker/ValueDesc/ValueDesc_Other.js
@@ -10,18 +10,26 @@ import { Int } from "./ValueDesc_Base.js";
 
 /** Describe id, range, name of channelCount1_pointwise1Before.
  *
- * Convert number value into integer between [ -5, ( 10 * 1024 ) ] representing operation:
+ * Convert number value into integer between [ -6, ( 10 * 1024 ) ] representing operation:
+ *   - -6: SHUFFLE_NET_V2_HEAD                      (ShuffleNetV2's head)
  *   - -5: ONE_INPUT_HALF_THROUGH                   (ShuffleNetV2_ByMobileNetV1's body/tail)
  *   - -4: ONE_INPUT_HALF_THROUGH_EXCEPT_DEPTHWISE1 (ShuffleNetV2_ByMobileNetV1's head)
  *   - -3: TWO_INPUTS_CONCAT_POINTWISE20_INPUT1     (ShuffleNetV2's body/tail)
- *   - -2: ONE_INPUT_TWO_DEPTHWISE                  (ShuffleNetV2's head simplified with ( pointwise1ChannelCount >= 1 ) or
- *                                                   ShuffleNetV2_ByPointwise21's head with ( pointwise1ChannelCount >= 1 ))
+ *   - -2: SHUFFLE_NET_V2_BY_POINTWISE21_HEAD       (ShuffleNetV2_ByPointwise21's head with ( pointwise1ChannelCount >= 1 ))
+ 
+//!!! (2022/06/14 Remarked)
+// *   - -2: ONE_INPUT_TWO_DEPTHWISE                  (ShuffleNetV2's head simplified with ( pointwise1ChannelCount >= 1 ) or
+// *                                                   ShuffleNetV2_ByPointwise21's head with ( pointwise1ChannelCount >= 1 ))
+
  *
  *   - -1: ONE_INPUT_ADD_TO_OUTPUT                  (MobileNetV2)
  *   -  0: ONE_INPUT                                (General Pointwise1-Depthwise1-Pointwise2)
  *                                                  (MobileNetV1 or
  *                                                   MobileNetV2's head or
- *                                                   ShuffleNetV2's head simplified with ( pointwise1ChannelCount == 0 ) or
+
+//!!! (2022/06/14 Remarked)
+// *                                                   ShuffleNetV2's head simplified with ( pointwise1ChannelCount == 0 ) or
+
  *                                                   ShuffleNetV2_ByPointwise21's head with ( pointwise1ChannelCount == 0 ))
  *
  *   - [ 1, ( 10 * 1024 ) ]: TWO_INPUTS with the second input channel count between 1 and 10240 (inclusive). (without names defined.)
@@ -30,11 +38,12 @@ import { Int } from "./ValueDesc_Base.js";
 class channelCount1_pointwise1Before extends Int {
 
   constructor() {
-    super( -5, ( 10 * 1024 ), [
+    super( -6, ( 10 * 1024 ), [
+      "SHUFFLE_NET_V2_HEAD",                      // (-6)
       "ONE_INPUT_HALF_THROUGH",                   // (-5)
       "ONE_INPUT_HALF_THROUGH_EXCEPT_DEPTHWISE1", // (-4)
       "TWO_INPUTS_CONCAT_POINTWISE20_INPUT1",     // (-3)
-      "ONE_INPUT_TWO_DEPTHWISE",                  // (-2)
+      "SHUFFLE_NET_V2_BY_POINTWISE21_HEAD",       // (-2)
       "ONE_INPUT_ADD_TO_OUTPUT",                  // (-1)
       "ONE_INPUT",                                // ( 0)
 
