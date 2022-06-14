bed564ca87d48154582a791593d043525ba1dd3f
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-14 11:05:46 +0800

    Update ValueDesc_ConvBlockType.js

--- CNN/Unpacker/ValueDesc/ValueDesc_ConvBlockType.js
+++ CNN/Unpacker/ValueDesc/ValueDesc_ConvBlockType.js
@@ -23,21 +23,97 @@ import { Int } from "./ValueDesc_Base.js";
 class ConvBlockType extends Int {
 
   constructor() {
-    super( 0, 9, [
-      "MOBILE_NET_V1_HEAD_BODY_TAIL",               // ( 0)
-      "MOBILE_NET_V2_BODY_TAIL",                    // ( 1)
-      "SHUFFLE_NET_V2_HEAD",                        // ( 2)
-      "SHUFFLE_NET_V2_BODY",                        // ( 3)
-      "SHUFFLE_NET_V2_TAIL",                        // ( 4)
-      "SHUFFLE_NET_V2_BY_POINTWISE21_HEAD",         // ( 5)
-      "SHUFFLE_NET_V2_BY_POINTWISE21_BODY",         // ( 6)
-      "SHUFFLE_NET_V2_BY_POINTWISE21_TAIL",         // ( 7)
-      "SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD",       // ( 8)
-      "SHUFFLE_NET_V2_BY_MOBILE_NET_V1_BODY_TAIL",  // ( 9)
-    ] );
+    super( 0, 9,
+      [
+        "MOBILE_NET_V1_HEAD_BODY_TAIL",               // ( 0)
+        "MOBILE_NET_V2_BODY_TAIL",                    // ( 1)
+        "SHUFFLE_NET_V2_HEAD",                        // ( 2)
+        "SHUFFLE_NET_V2_BODY",                        // ( 3)
+        "SHUFFLE_NET_V2_TAIL",                        // ( 4)
+        "SHUFFLE_NET_V2_BY_POINTWISE21_HEAD",         // ( 5)
+        "SHUFFLE_NET_V2_BY_POINTWISE21_BODY",         // ( 6)
+        "SHUFFLE_NET_V2_BY_POINTWISE21_TAIL",         // ( 7)
+        "SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD",       // ( 8)
+        "SHUFFLE_NET_V2_BY_MOBILE_NET_V1_BODY_TAIL",  // ( 9)
+      ],
+
+      [
+         new ConvBlockType.Info( 0, 1, 1 ),
+         new ConvBlockType.Info( 1, 1, 1 ),
+         new ConvBlockType.Info( 2, 1, 2 ),
+         new ConvBlockType.Info( 3, 2, 2 ),
+         new ConvBlockType.Info( 4, 2, 1 ),
+         new ConvBlockType.Info( 5, 1, 2 ),
+         new ConvBlockType.Info( 6, 2, 2 ),
+         new ConvBlockType.Info( 7, 2, 1 ),
+         new ConvBlockType.Info( 8, 1, 1 ),
+         new ConvBlockType.Info( 9, 1, 1 ),
+      ]
+    );
+  }
+
+
+  /**
+   * @param {number} nConvBlockTypeId  The numeric identifier of ConvBlockType. (ConvBlockType.Singleton.Ids.Xxx)
+   * @return {number} Return the input tensor count (1 or 2) of the ConvBlockType.Singleton.Ids.Xxx.
+   */
+  static inputTensorCount_get( nConvBlockTypeId ) {
+    let info = ConvBlockType.Singleton.getInfoById( nConvBlockTypeId );
+    if ( info )
+      return info.inputTensorCount;
+    return NaN;
+  }
+
+  /**
+   * @param {number} nConvBlockTypeId  The numeric identifier of ConvBlockType. (ConvBlockType.Singleton.Ids.Xxx)
+   * @return {number} Return the output tensor count (1 or 2) of the ConvBlockType.Singleton.Ids.Xxx.
+   */
+  static outputTensorCount_get( nConvBlockTypeId ) {
+    let info = ConvBlockType.Singleton.getInfoById( nConvBlockTypeId );
+    if ( info )
+      return info.outputTensorCount;
+    return NaN;
+  }
+
+  /**
+   * Convert convolution block type id to information object.
+   *
+   * @param {umber} nConvBlockTypeId
+   *   It should be one of ValueDesc.ConvBlockType.Singleton.Ids.Xxx.
+   *
+   * @return {ConvBlockType.Info}
+   *   It should be one of ValueDesc.ConvBlockType.Singleton.integerToObjectMap according to the nStridesPadId.
+   */
+  getInfoById( nConvBlockTypeId ) {
+    let info = this.integerToObjectMap.get( nConvBlockTypeId );
+    return info;
   }
 
 }
+
+
+/**
+ *
+ * @member {number} nConvBlockTypeId
+ *   The convolution block type id (ValueDesc.ConvBlockType.Singleton.Ids.Xxx).
+ *
+ * @member {number} inputTensorCount
+ *   The input tensor count for The convolution block type. Either 1 or 2.
+ *
+ * @member {number} outputTensorCount
+ *   The output tensor count for The convolution block type. Either 1 or 2.
+ *
+ */
+ConvBlockType.Info = class {
+
+  constructor( nConvBlockTypeId, inputTensorCount, outputTensorCount ) {
+    this.nConvBlockTypeId = nConvBlockTypeId;
+    this.inputTensorCount = inputTensorCount;
+    this.outputTensorCount = outputTensorCount;
+  }
+
+}
+
 
 /** The only one ValueDesc.ConvBlockType instance. */
 ConvBlockType.Singleton = new ConvBlockType;
