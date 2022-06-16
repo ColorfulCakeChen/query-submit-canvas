639a82690c50b7e60f6b2d228ab693d6e9596358
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-16 11:37:57 +0800

    Update ValueDesc_ConvBlockType.js

--- CNN/Unpacker/ValueDesc/ValueDesc_ConvBlockType.js
+++ CNN/Unpacker/ValueDesc/ValueDesc_ConvBlockType.js
@@ -5,9 +5,9 @@ import { Int } from "./ValueDesc_Base.js";
 /** Describe id, range, name of ConvBlockType.
  *
  * Convert number value into integer between [ 0, 11 ] representing operation:
- *   -  0: MOBILE_NET_V1_HEAD_BODY_TAIL                     (General Pointwise1-Depthwise1-Pointwise2)
- *                                                          (MobileNetV1 or
- *                                                           MobileNetV2's head)
+ *   -  0: MOBILE_NET_V1_HEAD_BODY_TAIL                      (General Pointwise1-Depthwise1-Pointwise2)
+ *                                                           (MobileNetV1 or
+ *                                                            MobileNetV2's head)
  *
  *   -  1: MOBILE_NET_V2_BODY_TAIL
  *   -  2: SHUFFLE_NET_V2_HEAD
@@ -16,8 +16,8 @@ import { Int } from "./ValueDesc_Base.js";
  *   -  5: SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD
  *   -  6: SHUFFLE_NET_V2_BY_MOBILE_NET_V1_BODY
  *   -  7: SHUFFLE_NET_V2_BY_MOBILE_NET_V1_TAIL
- *   -  8: SHUFFLE_NET_V2_BY_POINTWISE21_HEAD_NO_POINTWISE  (ShuffleNetV2_ByPointwise21's head with ( pointwise1ChannelCount == 0 ))
- *   -  9: SHUFFLE_NET_V2_BY_POINTWISE21_HEAD               (ShuffleNetV2_ByPointwise21's head with ( pointwise1ChannelCount >= 1 ))
+ *   -  8: SHUFFLE_NET_V2_BY_POINTWISE21_HEAD_NO_POINTWISE1  (ShuffleNetV2_ByPointwise21's head with ( pointwise1ChannelCount == 0 ))
+ *   -  9: SHUFFLE_NET_V2_BY_POINTWISE21_HEAD                (ShuffleNetV2_ByPointwise21's head with ( pointwise1ChannelCount >= 1 ))
  *   - 10: SHUFFLE_NET_V2_BY_POINTWISE21_BODY
  *   - 11: SHUFFLE_NET_V2_BY_POINTWISE21_TAIL
  */
@@ -26,18 +26,18 @@ class ConvBlockType extends Int {
   constructor() {
     super( 0, 11,
       [
-        "MOBILE_NET_V1_HEAD_BODY_TAIL",                     // ( 0)
-        "MOBILE_NET_V2_BODY_TAIL",                          // ( 1)
-        "SHUFFLE_NET_V2_HEAD",                              // ( 2)
-        "SHUFFLE_NET_V2_BODY",                              // ( 3)
-        "SHUFFLE_NET_V2_TAIL",                              // ( 4)
-        "SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD",             // ( 5)
-        "SHUFFLE_NET_V2_BY_MOBILE_NET_V1_BODY",             // ( 6)
-        "SHUFFLE_NET_V2_BY_MOBILE_NET_V1_TAIL",             // ( 7)
-        "SHUFFLE_NET_V2_BY_POINTWISE21_HEAD_NO_POINTWISE",  // ( 8)
-        "SHUFFLE_NET_V2_BY_POINTWISE21_HEAD",               // ( 9)
-        "SHUFFLE_NET_V2_BY_POINTWISE21_BODY",               // (10)
-        "SHUFFLE_NET_V2_BY_POINTWISE21_TAIL",               // (11)
+        "MOBILE_NET_V1_HEAD_BODY_TAIL",                      // ( 0)
+        "MOBILE_NET_V2_BODY_TAIL",                           // ( 1)
+        "SHUFFLE_NET_V2_HEAD",                               // ( 2)
+        "SHUFFLE_NET_V2_BODY",                               // ( 3)
+        "SHUFFLE_NET_V2_TAIL",                               // ( 4)
+        "SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD",              // ( 5)
+        "SHUFFLE_NET_V2_BY_MOBILE_NET_V1_BODY",              // ( 6)
+        "SHUFFLE_NET_V2_BY_MOBILE_NET_V1_TAIL",              // ( 7)
+        "SHUFFLE_NET_V2_BY_POINTWISE21_HEAD_NO_POINTWISE1",  // ( 8)
+        "SHUFFLE_NET_V2_BY_POINTWISE21_HEAD",                // ( 9)
+        "SHUFFLE_NET_V2_BY_POINTWISE21_BODY",                // (10)
+        "SHUFFLE_NET_V2_BY_POINTWISE21_TAIL",                // (11)
       ],
 
       [
