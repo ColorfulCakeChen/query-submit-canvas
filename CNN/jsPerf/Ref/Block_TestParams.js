b1203147f0e36ea87e16039d3056a38282528f31
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-17 14:55:58 +0800

    Update Block_TestParams.js

--- CNN/jsPerf/Ref/Block_TestParams.js
+++ CNN/jsPerf/Ref/Block_TestParams.js
@@ -1113,10 +1113,15 @@ class Base extends TestParams.Base {
            ) {
           depthwise2_inputChannelCount = paramsAll.input0_channelCount; // Use input0.
 
-        // Use pointwise1.outputChannelCount as input1_channelCount so that it has the same structure of depthwise1 and pointwise20.
-        //
+//!!! (2022/06/17 Remarked) should be the same as input0
+//        // Use pointwise1.outputChannelCount as input1_channelCount so that it has the same structure of depthwise1 and pointwise20.
+//        //
         } else if ( this.nConvBlockTypeId__is__SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD() ) { // (5)
-          depthwise2_inputChannelCount = pointwise1_resultOutputChannelCount;
+
+//!!! (2022/06/17 Remarked) should be the same as input0
+//          depthwise2_inputChannelCount = pointwise1_resultOutputChannelCount;
+
+          depthwise2_inputChannelCount = input0_channelCount_original;
         }
 
         // Only if depthwise operation is requested and necessary, create them.
