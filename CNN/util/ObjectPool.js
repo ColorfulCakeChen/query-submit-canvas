6d3ca4743367c6ba37b682c36adfddb1277f123c
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-21 18:46:23 +0800

    Create ObjectPool.js

---
+++ CNN/util/ObjectPool.js
@@ -0,0 +1,29 @@
+export { Base };
+
+/**
+ *
+ */
+class Base {
+  
+  /**
+   *
+   */
+  constructor(
+    ObjectClass,
+    pfnSetAsConstructor,
+  ) {
+  }
+
+  /**
+   *
+   */
+  get_or_create( ...RestArgs ) {
+  }
+
+  /**
+   * @param {Object} 
+   */
+  recycle( object ) {
+  }
+
+}
