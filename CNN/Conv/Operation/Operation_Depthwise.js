39f9a1ba76e31e8a71d2452beae6a91272198f6a
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-22 12:25:08 +0800

    Update Operation_Depthwise.js

--- CNN/Conv/Operation/Operation_Depthwise.js
+++ CNN/Conv/Operation/Operation_Depthwise.js
@@ -119,7 +119,12 @@ class Depthwise extends Base( FiltersArray_BiasesArray( TwoTensors.filtersTensor
             this.boundsArraySet.output0
           );
 
-          this.boundsArraySet = null; // Release for reducing memory usage. (Since it has been inside the output tensor placeholder.)
+          // Release for reducing memory usage. (Since it has been inside the output tensor placeholder.)
+          {
+            this.boundsArraySet.output0 = null; // Because it has already been transferred to TensorPlaceholder this.output0
+            BoundsArraySet.DepthwisePool.Singleton.recycle( this.boundsArraySet );
+            this.boundsArraySet = null;
+          }
 
         } catch ( e ) {  // If failed (e.g. memory not enough), return false.      
           bExtractOk = false;
