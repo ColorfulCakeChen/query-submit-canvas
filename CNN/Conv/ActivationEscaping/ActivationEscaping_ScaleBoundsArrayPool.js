151e48195e59bba9d3ee26e9c461c950c0149dc1
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-22 10:51:21 +0800

    Create ActivationEscaping_ScaleBoundsArrayPool.js

---
+++ CNN/Conv/ActivationEscaping/ActivationEscaping_ScaleBoundsArrayPool.js
@@ -0,0 +1,31 @@
+export { ScaleBoundsArrayPool };
+
+import * as Pool from "../../util/Pool.js";
+
+/**
+ * Providing ScaleBoundsArray by specifying length.
+ *
+ */
+class ScaleBoundsArrayPool extends Pool.Root {
+  
+  constructor() {
+    super( ScaleBoundsArray, ScaleBoundsArrayPool.setAsConstructor_by_length );
+  }
+
+  /**
+   * @param {ScaleBoundsArray} this
+   *   The ScaleBoundsArray object to be set length.
+   *
+   * @param {number} newLength
+   *   The this.length to be set to newLength.
+   *
+   * @return {ScaleBoundsArray}
+   *   Return the this object.
+   */
+  static setAsConstructor_by_length( newLength ) {
+    this.length = newLength;
+    return this;
+  }
+
+}
+
