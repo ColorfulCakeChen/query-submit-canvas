207b46112abf17bfcbe7f67ee2996ab824b599a9
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-23 15:53:52 +0800

    Update Block.js

--- CNN/Conv/Block.js
+++ CNN/Conv/Block.js
@@ -1,2 +1,4 @@
 export { Params } from "./Block/Block_Params.js";
+export { ParamsPool } from "./Block/Block_Params.js";
 export { Base } from "./Block/Block_Base.js";
+export { BasePool } from "./Block/Block_Base.js";
