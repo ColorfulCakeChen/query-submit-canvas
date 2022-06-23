54a907efb42b100d48a6d7854c60b61d359aa7fa
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-23 19:45:48 +0800

    Create ChannelShuffler_PerformanceTest.js

---
+++ CNN/Conv/ChannelShuffler/ChannelShuffler_PerformanceTest.js
@@ -0,0 +1,4 @@
+export { ShuffleInfo } from "./PerformanceTest/ChannelShuffler_PerformanceTest_ShuffleInfo.js";
+export { ConcatGather } from "./PerformanceTest/ChannelShuffler_PerformanceTest_ConcatGather.js";
+export { SplitConcat } from "./PerformanceTest/ChannelShuffler_PerformanceTest_SplitConcat.js";
+export { ConcatPointwiseConv } from "./PerformanceTest/ChannelShuffler_PerformanceTest_ConcatPointwiseConv.js";
