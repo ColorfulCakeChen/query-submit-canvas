3e506f100e3e66cf65a6011633ab60578ff4a7a8
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-23 11:37:28 +0800

    Update ActivationEscaping.js

--- CNN/Conv/ActivationEscaping.js
+++ CNN/Conv/ActivationEscaping.js
@@ -1,3 +1,3 @@
 export { ScaleArraySet } from "./ActivationEscaping/ActivationEscaping_ScaleArraySet.js";
 export { ScaleBoundsArray } from "./ActivationEscaping/ActivationEscaping_ScaleBoundsArray.js";
-export { ScaleBoundsArrayPool } from "./ActivationEscaping/ActivationEscaping_ScaleBoundsArrayPool.js";
+export { ScaleBoundsArrayPool } from "./ActivationEscaping/ActivationEscaping_ScaleBoundsArray.js";
