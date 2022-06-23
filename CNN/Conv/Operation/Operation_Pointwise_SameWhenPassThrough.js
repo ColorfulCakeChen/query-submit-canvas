fcf4f685e655126e960c89c9e60e06fcc3b0e569
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-23 15:48:30 +0800

    Update Operation_Pointwise_SameWhenPassThrough.js

--- CNN/Conv/Operation/Operation_Pointwise_SameWhenPassThrough.js
+++ CNN/Conv/Operation/Operation_Pointwise_SameWhenPassThrough.js
@@ -1,6 +1,8 @@
 export { Pointwise_SameWhenPassThrough };
+export { Pointwise_SameWhenPassThroughPool };
 
 import * as ValueDesc from "../../Unpacker/ValueDesc.js";
+import * as Pool from "../../util/Pool.js";
 import { Pointwise } from "./Operation_Pointwise.js";
 
 /**
@@ -26,6 +28,56 @@ class Pointwise_SameWhenPassThrough extends Pointwise {
       outputChannelCount, bBias, nActivationId,
       ValueDesc.PassThroughStyle.Singleton.Ids.PASS_THROUGH_STYLE_FILTER_1_BIAS_0,
       nHigherHalfDifferent, outputChannelCount_lowerHalf, channelShuffler_outputGroupCount );
+
+    this.setAsConstructor(
+      inputTensorPlaceholder0,
+      outputChannelCount, bBias, nActivationId,
+      nHigherHalfDifferent, outputChannelCount_lowerHalf, channelShuffler_outputGroupCount );
+  }
+
+  /**
+   * @return {Pointwise_SameWhenPassThrough}
+   *   Return the this object.
+   */
+  setAsConstructor(
+    inputTensorPlaceholder0,
+    outputChannelCount, bBias, nActivationId,
+    nHigherHalfDifferent, outputChannelCount_lowerHalf, channelShuffler_outputGroupCount ) {
+
+    super.setAsConstructor(
+      inputTensorPlaceholder0,
+      outputChannelCount, bBias, nActivationId,
+      ValueDesc.PassThroughStyle.Singleton.Ids.PASS_THROUGH_STYLE_FILTER_1_BIAS_0,
+      nHigherHalfDifferent, outputChannelCount_lowerHalf, channelShuffler_outputGroupCount );
+
+    return this;
+  }
+
+  /**
+   * After calling this method, this object should be viewed as disposed and should not be operated again.
+   */
+  disposeResources_and_recycleToPool() {
+    this.disposeResources();
+    Pointwise_SameWhenPassThroughPool.Singleton.recycle( this );
   }
 
 }
+
+
+/**
+ * Providing Operation.Pointwise_SameWhenPassThrough
+ *
+ */
+class Pointwise_SameWhenPassThroughPool extends Pool.Root {
+
+  constructor() {
+    super( Pointwise_SameWhenPassThrough, Pointwise_SameWhenPassThrough.setAsConstructor );
+  }
+
+}
+
+/**
+ * Used as default Operation.Pointwise_SameWhenPassThrough provider.
+ */
+Pointwise_SameWhenPassThroughPool.Singleton = new Pointwise_SameWhenPassThroughPool();
+
