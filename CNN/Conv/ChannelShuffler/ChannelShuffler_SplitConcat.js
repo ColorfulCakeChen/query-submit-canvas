9f4aa29c5a22d0fb6484b3d8e6a8c598e385d4e1
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-24 11:07:25 +0800

    Update ChannelShuffler_SplitConcat.js

--- CNN/Conv/ChannelShuffler/ChannelShuffler_SplitConcat.js
+++ CNN/Conv/ChannelShuffler/ChannelShuffler_SplitConcat.js
@@ -1,7 +1,9 @@
 export { SplitConcat };
+export { SplitConcatPool };
 
-import { ShuffleInfo } from "./ChannelShuffler_ShuffleInfo.js";
-import { ConcatGather } from "./ChannelShuffler_ConcatGather.js";
+import * as Pool from "../../util/Pool.js";
+import { ShuffleInfo, ShuffleInfoPool } from "./ChannelShuffler_ShuffleInfo.js";
+import { ConcatGather, ConcatGatherPool } from "./ChannelShuffler_ConcatGather.js";
 
 /**
  * Implement the channel shuffler by tf.split() and tf.concat().
@@ -24,13 +26,13 @@ import { ConcatGather } from "./ChannelShuffler_ConcatGather.js";
  * @member {number[][]} shuffledChannelIndicesArray
  *   The look up table for tf.gather()'s channel index. This table is composed of array of integers.
  *
- * @member {number} tensorWeightCountTotal
- *   The total wieght count used in tensors. Not including Params, because they are not used in tensors. Including inferenced
- * weights, if they are used in tensors.
- *
  * @member {number} tensorWeightCountExtracted
  *   The wieght count extracted from inputFloat32Array and used in tensors. Not including Params, because they are not used in
  * tensors. Not including inferenced weights (even if they are used in tensors), because they are not extracted from inputFloat32Array.
+ *
+ * @member {number} tensorWeightCountTotal
+ *   The total wieght count used in tensors. Not including Params, because they are not used in tensors. Including inferenced
+ * weights, if they are used in tensors.
  *
  * @member {function} splitConcat
  *   Concatenate, permute and split the input tensor by split-concat-gather. It is a function pointer to one of
@@ -46,42 +48,65 @@ class SplitConcat {
    * @param {number} outputGroupCount
    *   Used to calculate shuffleInfo.
    *
-   * @return {boolean}
-   *   If failed (e.g. out of GPU memory), return false. Otherwise, return true.
+   * @exception {Object} If failed (e.g. out of GPU memory).
    *
    * @see ConcatGather
    */
   constructor( concatenatedShape, outputGroupCount ) {
+    this.setAsConstructor( concatenatedShape, outputGroupCount );
+  }
 
-    this.disposeResources(); // So that distinguishable if re-initialization failed.
+  /**
+   *
+   * @param {number[]} concatenatedShape
+   *   Used to calculate shuffleInfo.
+   *
+   * @param {number} outputGroupCount
+   *   Used to calculate shuffleInfo.
+   *
+   * @exception {Object} If failed (e.g. out of GPU memory).
+   *
+   * @return {SplitConcat}
+   *   Return the this object.
+   *
+   * @see ConcatGather
+   */
+  setAsConstructor( concatenatedShape, outputGroupCount ) {
 
-    let concatGather = new ConcatGather( concatenatedShape, outputGroupCount );
+    this.tensorWeightCountExtracted = 0;
+    this.tensorWeightCountTotal = 0;
+
+    let concatGather = ConcatGatherPool.Singleton.get_or_create_by( concatenatedShape, outputGroupCount );
 
     try {
       // Shuffled channel indices (one dimension integers) for SplitConcat()
-      this.shuffledChannelIndicesArray = new Array( concatGather.shuffledChannelIndicesTensor1dArray.length );
-      concatGather.shuffledChannelIndicesTensor1dArray.forEach( ( shuffledChannelIndicesTensor1d, i ) => {
+      this.shuffledChannelIndicesArray = Pool.Array.Singleton.get_or_create_by( concatGather.shuffledChannelIndicesTensor1dArray.length );
+      for ( let i = 0; i < concatGather.shuffledChannelIndicesTensor1dArray.length; ++i ) {
+        let shuffledChannelIndicesTensor1d = concatGather.shuffledChannelIndicesTensor1dArray[ i ];
         let shuffledChannelIndices = shuffledChannelIndicesTensor1d.arraySync(); // Download from GPU memory.
 
         // Sorting from small to large for improving memory locality (and memory access performance).
         this.shuffledChannelIndicesArray[ i ] = shuffledChannelIndices.sort( ( n1, n2 ) => ( n1 - n2 ) );
-      });
+      }
 
       this.shuffleInfo = concatGather.shuffleInfo; // Need the shuffle info.
+      concatGather.shuffleInfo = null; // (Because transferred.)
 
       // Shared pre-allocate memory could speed up the process of splitting.
-      this.singleChannelTensorArray = new Array( this.shuffleInfo.totalChannelCount );
-      this.tensorArrayForOneGroup = new Array( this.shuffleInfo.channelCountPerGroup );
+      this.singleChannelTensorArray = Pool.Array.Singleton.get_or_create_by( this.shuffleInfo.totalChannelCount );
+      this.tensorArrayForOneGroup = Pool.Array.Singleton.get_or_create_by( this.shuffleInfo.channelCountPerGroup );
 
     // Exception if failed (e.g. out of (GPU) memory).
     } catch ( e ) {
       throw e;
 
     } finally {
-      concatGather.disposeResources(); // Always release the look up table (by tensor1d).
+      concatGather.disposeResources_and_recycleToPool(); // Always release the look up table (by tensor1d).
     }
 
     this.splitConcat = this.splitConcat_loop;
+
+    return this;
   }
 
   /**
@@ -90,13 +115,44 @@ class SplitConcat {
    * Sub-class should override this method (and call super.disposeResources() before return).
    */
   disposeResources() {
+
+    this.splitConcat = null;
+
+    this.tensorWeightCountTotal = 0;
+    this.tensorWeightCountExtracted = 0;
+
+    Pool.Array.Singleton.recycle( this.tensorArrayForOneGroup );
+    this.tensorArrayForOneGroup = null;
+
+    Pool.Array.Singleton.recycle( this.singleChannelTensorArray );
+    this.singleChannelTensorArray = null;
+
     if ( this.shuffleInfo ) {
-      this.shuffleInfo.disposeResources();
+      this.shuffleInfo.disposeResources_and_recycleToPool();
       this.shuffleInfo = null;
     }
 
-    this.tensorWeightCountTotal = this.tensorWeightCountExtracted = 0;
+    if ( this.shuffledChannelIndicesArray ) {
+
+      for ( let i = 0; i < this.shuffledChannelIndicesArray.length; ++i ) {
+        let shuffledChannelIndices = this.shuffledChannelIndicesArray[ i ];
+        Pool.Array.Singleton.recycle( shuffledChannelIndices );
+        this.shuffledChannelIndicesArray[ i ] = null;
+      }
+
+      Pool.Array.Singleton.recycle( this.shuffledChannelIndicesArray );
+      this.shuffledChannelIndicesArray = null;
+    }
+
     //super.disposeResources();
+  }
+
+  /**
+   * After calling this method, this object should be viewed as disposed and should not be operated again.
+   */
+  disposeResources_and_recycleToPool() {
+    this.disposeResources();
+    SplitConcatPool.Singleton.recycle( this );
   }
 
   get concatenatedShape() {
@@ -161,4 +217,22 @@ class SplitConcat {
   }
 
 }
+
+
+/**
+ * Providing ChannelShuffler.SplitConcat
+ *
+ */
+class SplitConcatPool extends Pool.Root {
+
+  constructor() {
+    super( "ChannelShuffler.SplitConcatPool", SplitConcat, SplitConcat.setAsConstructor );
+  }
+
+}
+
+/**
+ * Used as default ChannelShuffler.SplitConcat provider.
+ */
+SplitConcatPool.Singleton = new SplitConcatPool();
 
