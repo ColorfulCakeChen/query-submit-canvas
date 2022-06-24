ae32e0c1dd0afc9ed00f289ca6dd4cd82340523e
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-24 14:51:58 +0800

    Update ChannelShuffler_Bag.js

--- CNN/Conv/ChannelShuffler/ChannelShuffler_Bag.js
+++ CNN/Conv/ChannelShuffler/ChannelShuffler_Bag.js
@@ -89,6 +89,8 @@ class Bag extends MultiLayerMap.Base {
 
   /**
    * After calling this method, this object should be viewed as disposed and should not be operated again.
+   *
+   * Sub-class should override this method for recycling to its pool (and NEVER call super.disposeResources_and_recycleToPool()).
    */
   disposeResources_and_recycleToPool() {
     this.disposeResources();
