34ba7190d72c3841e6e07f2fd8ae846589d95b91
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-24 10:05:27 +0800

    Update ChannelShuffler_Bag.js

--- CNN/Conv/ChannelShuffler/ChannelShuffler_Bag.js
+++ CNN/Conv/ChannelShuffler/ChannelShuffler_Bag.js
@@ -1,25 +1,45 @@
 export { Bag };
+export { BagPool };
 
 import * as MultiLayerMap from "../../util/MultiLayerMap.js";
-import { ConcatPointwiseConv } from "./ChannelShuffler_ConcatPointwiseConv.js";
+import * as Pool from "../../util/Pool.js";
+import { ConcatPointwiseConvPool } from "./ChannelShuffler_ConcatPointwiseConv.js";
 
 /**
  * A container which provides shared channel shufflers. This could simplify memory management.
  *
+ * @member {Pool.Base} channelShufflerPool
+ *   The pool object for creating new channel shuffler. It is one of the following:
+ *   - ChannelShuffler.ShuffleInfoPool.Singleton
+ *   - ChannelShuffler.ConcatGatherPool.Singleton
+ *   - ChannelShuffler.SplitConcatPool.Singleton
+ *   - ChannelShuffler.ConcatPointwiseConvPool.Singleton
  */
 class Bag extends MultiLayerMap.Base {
 
   /**
-   * @param {object} channelShufflerClass
-   *   The class object for creating new channel shuffler. It is one of ChannelShuffler.ShuffleInfo, ChannelShuffler.ConcatGather,
-   * ChannelShuffler.SplitConcat, ChannelShuffler.ConcatPointwiseConv.
    */
-  constructor( channelShufflerClass = ConcatPointwiseConv ) {
-    super();
+  constructor( channelShufflerPool, ...restArgs ) {
+    super( ...restArgs );
+    this.setAsConstructor( channelShufflerPool, ...restArgs );
+  }
 
-    this.channelShufflerClass = channelShufflerClass;
+  /**
+   *
+   * @return {Bag}
+   *   Return the this object.
+   */
+  setAsConstructor( channelShufflerPool = ConcatPointwiseConvPool.Singleton, ...restArgs ) {
 
-    this.concatenatedShape = [ null, null, null ]; // A re-used shared array for reducing memory allocation.
+    if ( super.setAsConstructor instanceof Function )
+      super.setAsConstructor( ...restArgs );
+
+    this.channelShufflerPool = channelShufflerPool;
+
+    // A re-used shared array for reducing memory allocation.
+    this.concatenatedShape = Pool.Array.Singleton.get_or_create_by( 3 );
+
+    return this;
   }
 
   /**
@@ -44,7 +64,7 @@ class Bag extends MultiLayerMap.Base {
         this.concatenatedShape[ 0 ] = concatenatedHeight;
         this.concatenatedShape[ 1 ] = concatenatedWidth;
         this.concatenatedShape[ 2 ] = concatenatedDepth;
-        let channelShuffler = new (this.channelShufflerClass)( this.concatenatedShape, outputGroupCount );
+        let channelShuffler = channelShufflerPool.get_or_create_by( this.concatenatedShape, outputGroupCount );
         return channelShuffler;
       },
       concatenatedHeight, concatenatedWidth, concatenatedDepth, outputGroupCount );
@@ -52,10 +72,43 @@ class Bag extends MultiLayerMap.Base {
 
   /** Release all channel shufflers and their tf.tensor. */
   disposeResources() {
+    
+    Pool.Array.Singleton.recycle( this.concatenatedShape );
+    this.concatenatedShape = null;
+    
+    this.channelShufflerPool = null;
+
     for ( let channelShuffler of this.values() ) {
-      channelShuffler.disposeResources();
+      channelShuffler.disposeResources_and_recycleToPool();
     }
     this.clear();
   }
 
+  /**
+   * After calling this method, this object should be viewed as disposed and should not be operated again.
+   */
+  disposeResources_and_recycleToPool() {
+    this.disposeResources();
+    BagPool.Singleton.recycle( this );
+  }
+
 }
+
+
+/**
+ * Providing ChannelShuffler.Bag
+ *
+ */
+class BagPool extends Pool.Root {
+
+  constructor() {
+    super( "ChannelShuffler.BagPool", Bag, Bag.setAsConstructor );
+  }
+
+}
+
+/**
+ * Used as default ChannelShuffler.Bag provider.
+ */
+BagPool.Singleton = new BagPool();
+
