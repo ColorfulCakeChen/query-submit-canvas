b459b3c6fd1705524652f597886878fcd18556a9
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-24 09:59:48 +0800

    Update ChannelShuffler.js

--- CNN/Conv/ChannelShuffler.js
+++ CNN/Conv/ChannelShuffler.js
@@ -1,6 +1,16 @@
-export { ShuffleInfo } from "./ChannelShuffler/ChannelShuffler_ShuffleInfo.js";
-export { ConcatGather } from "./ChannelShuffler/ChannelShuffler_ConcatGather.js";
-export { SplitConcat } from "./ChannelShuffler/ChannelShuffler_SplitConcat.js";
-export { ConcatPointwiseConv } from "./ChannelShuffler/ChannelShuffler_ConcatPointwiseConv.js";
 export { Bag } from "./ChannelShuffler/ChannelShuffler_Bag.js";
+export { BagPool } from "./ChannelShuffler/ChannelShuffler_Bag.js";
+
+export { ShuffleInfo } from "./ChannelShuffler/ChannelShuffler_ShuffleInfo.js";
+export { ShuffleInfoPool } from "./ChannelShuffler/ChannelShuffler_ShuffleInfo.js";
+
+export { ConcatGather } from "./ChannelShuffler/ChannelShuffler_ConcatGather.js";
+export { ConcatGatherPool } from "./ChannelShuffler/ChannelShuffler_ConcatGather.js";
+
+export { SplitConcat } from "./ChannelShuffler/ChannelShuffler_SplitConcat.js";
+export { SplitConcatPool } from "./ChannelShuffler/ChannelShuffler_SplitConcat.js";
+
+export { ConcatPointwiseConv } from "./ChannelShuffler/ChannelShuffler_ConcatPointwiseConv.js";
+export { ConcatPointwiseConvPool } from "./ChannelShuffler/ChannelShuffler_ConcatPointwiseConv.js";
+
 export * as PerformanceTest from "./ChannelShuffler/ChannelShuffler_PerformanceTest.js";
