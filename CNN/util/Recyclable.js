d02d6cbc7ab4a03a62cd3ad57cf3ee4111d6ba92
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-25 10:53:46 +0800

    Update Recyclable.js

--- CNN/util/Recyclable.js
+++ CNN/util/Recyclable.js
@@ -3,29 +3,40 @@ export { Base, Root };
 //import * as Pool from "./Pool.js";
 
 /**
+ * The base class representing a object could be recycled (i.e. disposed without release its main object memory for re-using in the
+ * future).
+ *
+ * Every sub-class of this Recyclable.Base MUST define a static propery named RecyclePool which is usually an instance of Pool.Base:
+ * <pre>
+ * class SomeClass extends Recyclable.Base {
+ *
+ *   static RecyclePool = new XxxPool();
+ *
+ * }
+ * </pre>
+ *
+ *
  *
  */
 let Base = ( ParentClass = Object ) => class Base extends ParentClass {
 
-//   static Pool = new BasePool();
-
-
   /**
    * This method will do the following in sequence:
-   *   - call this.disposeResources() (if exists), and then
-   *   - call this.constructor.Pool.recycle().
-   *
-   * After calling this method, this object should be viewed as disposed and should not be operated again.
+   *   - call this.disposeResources() (if exists)
+   *   - call this.constructor.RecyclePool.recycle()
    *
    * Sub-class should NEVER override this method (so NEVER call super.disposeResources_and_recycleToPool()).
+   *
+   *
+   * After calling this method, this object should be viewed as disposed and should not be operated again.
    */
   disposeResources_and_recycleToPool() {
 
-    if ( this.disposeResources instanceof Function ) { // If this object needs dispose, do it before being recyled.
+    if ( this.disposeResources instanceof Function ) { // If this object needs disposing, do it before being recyled.
       this.disposeResources();
     }
 
-    this.constructor.Pool.recycle( this );
+    this.constructor.RecyclePool.recycle( this );
   }
 
 }
@@ -33,7 +44,7 @@ let Base = ( ParentClass = Object ) => class Base extends ParentClass {
 
 /**
  * Almost the same as Recyclable.Base class except its parent class is fixed to Object. In other words, caller can not specify the
- * parent class of Operation.Root (so it is named "Root" which can not have parent class).
+ * parent class of Recyclable.Root (so it is named "Root" which can not have parent class).
  */
 class Root extends Base {
 }
