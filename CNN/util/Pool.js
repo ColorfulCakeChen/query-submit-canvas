fe083c11e5db1d75dcd570c0c35ee93da92a6bfd
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-25 22:04:21 +0800

    Update Pool.js

--- CNN/util/Pool.js
+++ CNN/util/Pool.js
@@ -1,4 +1,4 @@
 export { Base, Root } from "./Pool/Pool_Base.js";
-export { IssuedObjetcs } from "./Pool/Pool_IssuedObjetcs.js";
-export { All } from "./Pool/Pool_IssuedObjetcs.js";
+export { IssuedObjects } from "./Pool/Pool_IssuedObjects.js";
+export { All } from "./Pool/Pool_IssuedObjects.js";
 export { Float32Array } from "./Pool/Pool_Float32Array.js";
