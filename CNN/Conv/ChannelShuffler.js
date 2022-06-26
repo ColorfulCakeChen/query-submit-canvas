367988af9cd5c864827560d2748c1e7a960010c7
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-26 12:13:11 +0800

    Update ChannelShuffler.js

--- CNN/Conv/ChannelShuffler.js
+++ CNN/Conv/ChannelShuffler.js
@@ -1,6 +1,6 @@
-export { Bag, BagPool } from "./ChannelShuffler/ChannelShuffler_Bag.js";
-export { ShuffleInfo, ShuffleInfoPool } from "./ChannelShuffler/ChannelShuffler_ShuffleInfo.js";
-export { ConcatGather, ConcatGatherPool } from "./ChannelShuffler/ChannelShuffler_ConcatGather.js";
-export { SplitConcat, SplitConcatPool } from "./ChannelShuffler/ChannelShuffler_SplitConcat.js";
-export { ConcatPointwiseConv, ConcatPointwiseConvPool } from "./ChannelShuffler/ChannelShuffler_ConcatPointwiseConv.js";
+export { Bag } from "./ChannelShuffler/ChannelShuffler_Bag.js";
+export { ShuffleInfo } from "./ChannelShuffler/ChannelShuffler_ShuffleInfo.js";
+export { ConcatGather } from "./ChannelShuffler/ChannelShuffler_ConcatGather.js";
+export { SplitConcat } from "./ChannelShuffler/ChannelShuffler_SplitConcat.js";
+export { ConcatPointwiseConv } from "./ChannelShuffler/ChannelShuffler_ConcatPointwiseConv.js";
 export * as PerformanceTest from "./ChannelShuffler/ChannelShuffler_PerformanceTest.js";
