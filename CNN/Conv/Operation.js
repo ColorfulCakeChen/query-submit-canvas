47ccb00708a55aa0449ac9c4fa81a6af72a7cda3
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-26 12:14:04 +0800

    Update Operation.js

--- CNN/Conv/Operation.js
+++ CNN/Conv/Operation.js
@@ -1,33 +1,13 @@
 export { Base } from "./Operation/Operation_Base.js";
 export { Root } from "./Operation/Operation_Base.js";
-export { RootPool } from "./Operation/Operation_Base.js";
-
 export { TwinArray } from "./Operation/Operation_TwinArray.js";
-export { TwinArrayPool } from "./Operation/Operation_TwinArray.js";
-
 export { AddTwoTensors } from "./Operation/Operation_AddTwoTensors.js";
-export { AddTwoTensorsPool } from "./Operation/Operation_AddTwoTensors.js";
-
 export { MultiplyTwoTensors } from "./Operation/Operation_MultiplyTwoTensors.js";
-export { MultiplyTwoTensorsPool } from "./Operation/Operation_MultiplyTwoTensors.js";
-
 export { ConcatShuffleSplit } from "./Operation/Operation_ConcatShuffleSplit.js";
-export { ConcatShuffleSplitPool } from "./Operation/Operation_ConcatShuffleSplit.js";
-
 export { ConcatAlongAxisId2 } from "./Operation/Operation_ConcatAlongAxisId2.js";
-export { ConcatAlongAxisId2Pool } from "./Operation/Operation_ConcatAlongAxisId2.js";
-
 export { Depthwise } from "./Operation/Operation_Depthwise.js";
-export { DepthwisePool } from "./Operation/Operation_Depthwise.js";
 export { Depthwise_SameWhenPassThrough } from "./Operation/Operation_Depthwise_SameWhenPassThrough.js";
-export { Depthwise_SameWhenPassThroughPool } from "./Operation/Operation_Depthwise_SameWhenPassThrough.js";
 export { Depthwise_ConstantWhenPassThrough } from "./Operation/Operation_Depthwise_ConstantWhenPassThrough.js";
-export { Depthwise_ConstantWhenPassThroughPool } from "./Operation/Operation_Depthwise_ConstantWhenPassThrough.js";
-
 export { Pointwise } from "./Operation/Operation_Pointwise.js";
-export { PointwisePool } from "./Operation/Operation_Pointwise.js";
 export { Pointwise_SameWhenPassThrough } from "./Operation/Operation_Pointwise_SameWhenPassThrough.js";
-export { Pointwise_SameWhenPassThroughPool } from "./Operation/Operation_Pointwise_SameWhenPassThrough.js";
 export { Pointwise_ConstantWhenPassThrough } from "./Operation/Operation_Pointwise_ConstantWhenPassThrough.js";
-export { Pointwise_ConstantWhenPassThroughPool } from "./Operation/Operation_Pointwise_ConstantWhenPassThrough.js";
-
