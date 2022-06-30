411dfb23ff7eff79690d3a9a8cd5ff07c5170911
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-30 10:55:17 +0800

    Update ParamDesc_SequenceArray.js

--- CNN/Unpacker/ParamDesc/ParamDesc_SequenceArray.js
+++ CNN/Unpacker/ParamDesc/ParamDesc_SequenceArray.js
@@ -11,6 +11,14 @@ class SequenceArray {
    *
    * @param {ParamDesc.Base[]} aParamDescArray
    *   An array of ParamDesc.Xxx objects. It will be kept (i.e. owned; not cloned) by this SequenceArray object.
+   *
+   *   - every element should be a ParamDesc.Xxx object (one of ParamDesc.Base, ParamDesc.Same, ParamDesc.Bool)
+   *       describing the parameter.
+   *
+   *     - The paramDesc.valueDesc should be a ValueDesc.Xxx object (one of ValueDesc.Same, ValueDesc.Bool, ValueDesc.Int).
+   *       The paramDesc.valueDesc.range should be a ValueRange.Xxx object (one of ValueRange.Same, ValueRange.Bool, ValueRange.Int).
+   *       The paramDesc.valueDesc.range.adjust() is a function for adjusting the parameter value.
+   *
    */
   constructor( aParamDescArray ) {
     this.array = aParamDescArray;
