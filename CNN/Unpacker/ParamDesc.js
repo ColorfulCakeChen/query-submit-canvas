4aeee5aa40be98f7581746411ea9ee69f22169f4
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-30 08:19:45 +0800

    Create ParamDesc.js

---
+++ CNN/Unpacker/ParamDesc.js
@@ -0,0 +1,9 @@
+export { Base, Same, Bool, Int } from "./ParamDesc/ParamDesc_Base.js";
+export { ActivationFunction } from "./ParamDesc/ParamDesc_Base.js";
+export { ConvBlockType } from "./ParamDesc/ParamDesc_Base.js";
+export { AvgMax_Or_ChannelMultiplier } from "./ParamDesc/ParamDesc_Base.js";
+export { SqueezeExcitationChannelCountDivisor } from "./ParamDesc/ParamDesc_Base.js";
+export { StridesPad } from "./ParamDesc/ParamDesc_Base.js";
+export { ConvStageType } from "./ParamDesc/ParamDesc_Base.js";
+export { SequenceArray } from "./ParamDesc/ParamDesc_SequenceArray.js";
+
