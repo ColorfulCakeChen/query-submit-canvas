d5844aa845d7df7438cc1f903376b9b8d4a764da
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-30 18:48:40 +0800

    Update Pool_Asserter.js

--- CNN/util/Pool_Asserter.js
+++ CNN/util/Pool_Asserter.js
@@ -5,17 +5,6 @@ export { assert_Pool_issuedCount_zero };
 import * as Pool from "../../util/Pool.js";
 import * as Recyclable from "../../util/Recyclable.js";
 
-//!!! (2022/06/30 Remarked) Now, no issuedCount for individual pool
-// /**
-//  * @param {Pool.Base} pool
-//  *   The pool object to be asserted.
-//  */
-// function assert_Pool_issuedCount( prefixMsg, pool, issuedCount_shouldBe ) {
-//   if ( pool.issuedCount != issuedCount_shouldBe )
-//     throw Error( `${prefixMsg}: memory leak: `
-//       + `pool ( ${pool.poolName} )'s issuedCount ( ${pool.issuedCount} ) should be ( ${issuedCount_shouldBe} ).` );
-// }
-
 /**
  */
 function assert_Pool_issuedCount( prefixMsg, issuedCount_shouldBe ) {
@@ -27,16 +16,20 @@ function assert_Pool_issuedCount( prefixMsg, issuedCount_shouldBe ) {
 /**
  * Assert all pool's .issuedCount before and after calling pfn() are the same.
  *
- * @param (Function} pfn
+ * @param {Function} pfn
  *   The function to be called.
  *
- * @param (Object} thisArg
+ * @param {Object} thisArg
  *   The "this" value when function pfn is called.
  *
- * @param (Object[]} restArgs
+ * @param {Object[]} restArgs
  *   All arguments when function pfn is called.
+ *
+ * @return {any}
+ *   Return the returned value of function pfn().
  */
 function assert_Pool_issuedCount_same_after_as_before( prefixMsg, pfn, thisArg, ...restArgs ) {
+  let returnedValue;
 
 //!!! (2022/06/30 Remarked) Now, no issuedCount for individual pool
 //  let issuedCount_array_before;
@@ -51,7 +44,7 @@ function assert_Pool_issuedCount_same_after_as_before( prefixMsg, pfn, thisArg, 
 
     let issuedCount_before = Pool.All.issuedCount;
 
-    pfn.apply( thisArg, restArgs );
+    returnedValue = pfn.apply( thisArg, restArgs );
 
 //!!! (2022/06/30 Remarked) Now, no issuedCount for individual pool
 //     for ( let i = 0; i < Pool.All.registeredPoolArray.length; ++i ) {
@@ -71,18 +64,13 @@ function assert_Pool_issuedCount_same_after_as_before( prefixMsg, pfn, thisArg, 
 //       issuedCount_array_before = null;
 //     }
   }
+
+  return returnedValue;
 }
 
 /**
  *
  */
 function assert_Pool_issuedCount_zero( prefixMsg ) {
-
-//!!! (2022/06/30 Remarked) Now, no issuedCount for individual pool
-//   for ( let i = 0; i < Pool.All.registeredPoolArray.length; ++i ) {
-//     let pool = Pool.All.registeredPoolArray[ i ];
-//     assert_Pool_issuedCount( prefixMsg, pool, 0 );
-//   }
-
   assert_Pool_issuedCount( prefixMsg, 0 );
 }
