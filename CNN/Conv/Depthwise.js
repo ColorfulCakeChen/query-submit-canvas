d4c154f6929638e04996e592073c5c477435ba11
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-06-30 14:35:39 +0800

    Update Depthwise.js

--- CNN/Conv/Depthwise.js
+++ CNN/Conv/Depthwise.js
@@ -1,6 +1,8 @@
 export { ChannelPartInfo, FiltersBiasesPartInfo } from "./Depthwise/Depthwise_ChannelPartInfo.js";
 export { PadInfoCalculator } from "./Depthwise/Depthwise_PadInfoCalculator.js";
+export { PadInfoCalculatorRoot } from "./Depthwise/Depthwise_PadInfoCalculator.js";
 export { FiltersArray_BiasesArray } from "./Depthwise/Depthwise_FiltersArray_BiasesArray.js";
 export { PassThrough_FiltersArray_BiasesArray } from "./Depthwise/Depthwise_PassThrough.js";
+export { PassThrough_FiltersArray_BiasesArray_Root } from "./Depthwise/Depthwise_PassThrough.js";
 export { PassThrough_FiltersArray_BiasesArray_Bag } from "./Depthwise/Depthwise_PassThrough.js";
 export { PassThrough } from "./Depthwise/Depthwise_PassThrough.js";
