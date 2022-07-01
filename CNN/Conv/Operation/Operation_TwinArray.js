44fcd0f9835054cf2ac4ea0e73d0d8a514462ed5
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-07-01 15:22:41 +0800

    Update Operation_TwinArray.js

--- CNN/Conv/Operation/Operation_TwinArray.js
+++ CNN/Conv/Operation/Operation_TwinArray.js
@@ -156,26 +156,25 @@ class TwinArray extends Root {
     for ( let i = 0; i < this.operationArray.length; ++i ) {
       let operation = this.operationArray[ i ];
 
-      if ( ( operation.input0 ) && !( this.is_inputs_outputs_byTensorPlaceholder( operation.input0 ) ) ) {
-        ActivationEscaping.ScaleBoundsArrayPool.Singleton.recyle( operation.input0.scaleBoundsArray );
+      if ( ( operation.input0?.scaleBoundsArray ) && !( this.is_inputs_outputs_byTensorPlaceholder( operation.input0 ) ) ) {
+        operation.input0.scaleBoundsArray.disposeResources_and_recycleToPool();
         operation.input0.scaleBoundsArray = null;
       }
 
-      if ( ( operation.input1 ) && !( this.is_inputs_outputs_byTensorPlaceholder( operation.input1 ) ) ) {
-        ActivationEscaping.ScaleBoundsArrayPool.Singleton.recyle( operation.input1.scaleBoundsArray );
+      if ( ( operation.input1?.scaleBoundsArray ) && !( this.is_inputs_outputs_byTensorPlaceholder( operation.input1 ) ) ) {
+        operation.input1.scaleBoundsArray.disposeResources_and_recycleToPool();
         operation.input1.scaleBoundsArray = null;
       }
 
-      if ( ( operation.output0 ) && !( this.is_inputs_outputs_byTensorPlaceholder( operation.output0 ) ) ) {
-        ActivationEscaping.ScaleBoundsArrayPool.Singleton.recyle( operation.output0.scaleBoundsArray );
+      if ( ( operation.output0?.scaleBoundsArray ) && !( this.is_inputs_outputs_byTensorPlaceholder( operation.output0 ) ) ) {
+        operation.output0.scaleBoundsArray.disposeResources_and_recycleToPool();
         operation.output0.scaleBoundsArray = null;
       }
 
-      if ( ( operation.output1 ) && !( this.is_inputs_outputs_byTensorPlaceholder( operation.output1 ) ) ) {
-        ActivationEscaping.ScaleBoundsArrayPool.Singleton.recyle( operation.output1.scaleBoundsArray );
+      if ( ( operation.output1?.scaleBoundsArray ) && !( this.is_inputs_outputs_byTensorPlaceholder( operation.output1 ) ) ) {
+        operation.output1.scaleBoundsArray.disposeResources_and_recycleToPool();
         operation.output1.scaleBoundsArray = null;
       }
-
     }
   }
 
