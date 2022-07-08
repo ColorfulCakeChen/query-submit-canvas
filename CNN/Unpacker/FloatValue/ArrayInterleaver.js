0f28a25975ef707545e69912da786dade0633746
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-07-08 12:55:53 +0800

    Update ArrayInterleaver.js

--- CNN/Unpacker/FloatValue/ArrayInterleaver.js
+++ CNN/Unpacker/FloatValue/ArrayInterleaver.js
@@ -9,15 +9,61 @@ class ArrayInterleaver {
 // Needs another kinds channel shuffling: along input channel (not along output channel)
 
 
-// !!! ...unfinished... (2022/07/08) along output channel (axis X)
-//     { // Shuffle filters.
-//       let filtersWeightsCount = this.inputChannelCount * this.outputChannelCount;
-//
-//       for ( let indexBegin = 0; indexBegin < filtersWeightsCount; indexBegin += this.outputChannelCount ) {
-//         FloatValue.ArrayInterleaver.interleave_asGrouptTwo(
-//           this.filtersArray, indexBegin, this.outputChannelCount, arrayTemp_forInterleave_asGrouptTwo );
-//       }
-//     }
+
+  /**
+   * Rearrange array elements along width by interleaving.
+   *   - Only ( groupCount == 2 ) is supported.
+   *
+   * @param {Array} arrayIn
+   *   The array to be re-arranged. It is an 1D array but represents a 2D array.
+   *
+   * @param {number} height
+   *   The virtual 2D array's height.
+   *
+   * @param {number} width
+   *   The virtual 2D array's width.
+   *
+   * @param {Array} arrayTemp
+   *   A temporary array for placing the original elements (from arrayIn) temporarily. Providing this array could reduce memory
+   * re-allocation and improve performance.
+   *
+   * @return {Array}
+   *   Retrun the (modified) arrayIn itself.
+   */
+  static interleave_asGrouptTwo_alongWidth( arrayIn, height, width, arrayTemp = [] ) {
+
+    let elementCount = height * width;
+
+    if ( elementCount != arrayIn.length )
+      throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongWidth(): `
+        + `( height * width ) = ( ${height} * ${width} ) = ${elementCount} `
+        + ` should be the same as input array length ( ${arrayIn.length} ).`
+      );
+
+    if ( ( width % 2 ) != 0 )
+      throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongWidth(): `
+        + `width ( ${width} ) must be even (i.e. divisible by 2).`
+      );
+
+    arrayTemp.length = width;
+    let widthHalf = Math.floor( width / 2 );
+
+    for ( let indexBegin = 0; indexBegin < elementCount; indexBegin += width ) {
+
+      // Copy the elements to be re-arrange.
+      for ( let i = 0; i < width; ++i ) {
+        arrayTemp[ i ] = arrayIn[ indexBegin + i ];
+      }
+
+      // Interleave the elements order.
+      for ( let i = 0, toIndex = indexBegin; i < widthHalf; ++i, toIndex += 2 ) {
+        arrayIn[ toIndex     ] = arrayTemp[ i ];
+        arrayIn[ toIndex + 1 ] = arrayTemp[ i + widthHalf ];
+      }
+    }
+
+    return arrayIn;
+  }
 
 
   /**
