acda2a2868cf2c896c53b47c0fff900fd3a002f8
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-07-08 13:04:57 +0800

    Update ArrayInterleaver.js

--- CNN/Unpacker/FloatValue/ArrayInterleaver.js
+++ CNN/Unpacker/FloatValue/ArrayInterleaver.js
@@ -5,11 +5,6 @@ export { ArrayInterleaver };
  */
 class ArrayInterleaver {
 
-//!!! ...unfinished... (2022/07/08)
-// Needs another kinds channel shuffling: along input channel (not along output channel)
-
-
-
   /**
    * Rearrange array elements along width by interleaving.
    *   - Only ( groupCount == 2 ) is supported.
@@ -66,18 +61,21 @@ class ArrayInterleaver {
   }
 
 
+//!!! ...unfinished... (2022/07/08)
+// Needs another kinds channel shuffling: along input channel (not along output channel)
+
   /**
-   * Rearrange array elements by interleaving.
+   * Rearrange array elements along height by interleaving.
    *   - Only ( groupCount == 2 ) is supported.
    *
    * @param {Array} arrayIn
-   *   The array to be re-arranged.
+   *   The array to be re-arranged. It is an 1D array but represents a 2D array.
    *
-   * @param {number} indexBegin
-   *   Re-arrange the arrayIn from which element.
+   * @param {number} height
+   *   The virtual 2D array's height.
    *
-   * @param {number} elementCount
-   *   How many elements (begin at indexBegin) will be interleaved. It must be even (i.e. divisible by 2).
+   * @param {number} width
+   *   The virtual 2D array's width.
    *
    * @param {Array} arrayTemp
    *   A temporary array for placing the original elements (from arrayIn) temporarily. Providing this array could reduce memory
@@ -86,27 +84,88 @@ class ArrayInterleaver {
    * @return {Array}
    *   Retrun the (modified) arrayIn itself.
    */
-  static interleave_asGrouptTwo( arrayIn, indexBegin, elementCount, arrayTemp = [] ) {
+  static interleave_asGrouptTwo_alongHeight( arrayIn, height, width, arrayTemp = [] ) {
 
-    if ( ( elementCount % 2 ) != 0 )
-      throw Error( `ArrayInterleaver.interleave_byGrouptTwo(): `
-        + `elementCount ( ${elementCount} ) must be even (i.e. divisible by 2).`
+    let elementCount = height * width;
+
+    if ( elementCount != arrayIn.length )
+      throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongHeight(): `
+        + `( height * width ) = ( ${height} * ${width} ) = ${elementCount} `
+        + ` should be the same as input array length ( ${arrayIn.length} ).`
       );
 
-    // Copy the elements to be re-arrange.
-    arrayTemp.length = elementCount;
-    for ( let i = 0; i < elementCount; ++i ) {
-      arrayTemp[ i ] = arrayIn[ indexBegin + i ];
-    }
+    if ( ( height % 2 ) != 0 )
+      throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongHeight(): `
+        + `height ( ${height} ) must be even (i.e. divisible by 2).`
+      );
 
-    // Interleave the elements order.
-    let elementCountHalf = Math.floor( elementCount / 2 );
-    for ( let i = 0, toIndex = indexBegin; i < elementCountHalf; ++i, toIndex += 2 ) {
-      arrayIn[ toIndex     ] = arrayTemp[ i ];
-      arrayIn[ toIndex + 1 ] = arrayTemp[ i + elementCountHalf ];
+//!!! ...unfinished... (2022/07/08)
+    arrayTemp.length = width???;
+
+    let heightHalf = Math.floor( height / 2 );
+
+//!!! ...unfinished... (2022/07/08)
+
+    for ( let indexBegin = 0; indexBegin < elementCount; indexBegin += width ) {
+
+      // Copy the elements to be re-arrange.
+      for ( let i = 0; i < width; ++i ) {
+        arrayTemp[ i ] = arrayIn[ indexBegin + i ];
+      }
+
+      // Interleave the elements order.
+      for ( let i = 0, toIndex = indexBegin; i < widthHalf; ++i, toIndex += 2 ) {
+        arrayIn[ toIndex     ] = arrayTemp[ i ];
+        arrayIn[ toIndex + 1 ] = arrayTemp[ i + widthHalf ];
+      }
     }
 
     return arrayIn;
   }
+
+
+//!!! (2022/07/08 Remarked) Replaced by interleave_asGrouptTwo_alongWidth()
+//   /**
+//    * Rearrange array elements by interleaving.
+//    *   - Only ( groupCount == 2 ) is supported.
+//    *
+//    * @param {Array} arrayIn
+//    *   The array to be re-arranged.
+//    *
+//    * @param {number} indexBegin
+//    *   Re-arrange the arrayIn from which element.
+//    *
+//    * @param {number} elementCount
+//    *   How many elements (begin at indexBegin) will be interleaved. It must be even (i.e. divisible by 2).
+//    *
+//    * @param {Array} arrayTemp
+//    *   A temporary array for placing the original elements (from arrayIn) temporarily. Providing this array could reduce memory
+//    * re-allocation and improve performance.
+//    *
+//    * @return {Array}
+//    *   Retrun the (modified) arrayIn itself.
+//    */
+//   static interleave_asGrouptTwo( arrayIn, indexBegin, elementCount, arrayTemp = [] ) {
+//
+//     if ( ( elementCount % 2 ) != 0 )
+//       throw Error( `ArrayInterleaver.interleave_byGrouptTwo(): `
+//         + `elementCount ( ${elementCount} ) must be even (i.e. divisible by 2).`
+//       );
+//
+//     // Copy the elements to be re-arrange.
+//     arrayTemp.length = elementCount;
+//     for ( let i = 0; i < elementCount; ++i ) {
+//       arrayTemp[ i ] = arrayIn[ indexBegin + i ];
+//     }
+//
+//     // Interleave the elements order.
+//     let elementCountHalf = Math.floor( elementCount / 2 );
+//     for ( let i = 0, toIndex = indexBegin; i < elementCountHalf; ++i, toIndex += 2 ) {
+//       arrayIn[ toIndex     ] = arrayTemp[ i ];
+//       arrayIn[ toIndex + 1 ] = arrayTemp[ i + elementCountHalf ];
+//     }
+//
+//     return arrayIn;
+//   }
 
 }
