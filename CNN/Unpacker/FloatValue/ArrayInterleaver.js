08c6e86ae82cfb1999d7de9c964f64bb0419a9d4
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-07-09 20:12:26 +0800

    Update ArrayInterleaver.js

--- CNN/Unpacker/FloatValue/ArrayInterleaver.js
+++ CNN/Unpacker/FloatValue/ArrayInterleaver.js
@@ -113,7 +113,7 @@ class ArrayInterleaver {
         + `width ( ${width} ) must be even (i.e. divisible by 2).`
       );
 
-    toArray.length = fromArray.length;
+    toArray.length = elementCount;
     let widthHalf = ( width / 2 );
     for ( let i = 0; i < elementCount; i += width ) {
       ArrayInterleaver.interleave_asGrouptTwo_from_to( fromArray, i, toArray, i, widthHalf );
@@ -142,40 +142,92 @@ class ArrayInterleaver {
    */
   static interleave_asGrouptTwo_alongWidth( arrayIn, height, width, arrayTemp ) {
 
-    let elementCount = height * width;
+    // Copy the elements to be re-arrange.
+    arrayTemp.length = arrayIn.length;
+    for ( let i = 0; i < arrayIn.length; ++i ) {
+      arrayTemp[ i ] = arrayIn[ i ];
+    }
 
-    if ( elementCount != arrayIn.length )
-      throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongWidth(): `
+    ArrayInterleaver.interleave_asGrouptTwo_alongWidth( arrayTemp, arrayIn, height, width );
+
+//!!! (2022/07/09 Remarked) Old Codes.
+//     let elementCount = height * width;
+//
+//     if ( elementCount != arrayIn.length )
+//       throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongWidth(): `
+//         + `( height * width ) = ( ${height} * ${width} ) = ${elementCount} `
+//         + ` should be the same as input array length ( ${arrayIn.length} ).`
+//       );
+//
+//     if ( ( width % 2 ) != 0 )
+//       throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongWidth(): `
+//         + `width ( ${width} ) must be even (i.e. divisible by 2).`
+//       );
+//
+//     arrayTemp.length = width;
+//     let widthHalf = Math.floor( width / 2 );
+//
+//     for ( let indexBegin = 0; indexBegin < elementCount; indexBegin += width ) {
+//
+//       // Copy the elements to be re-arrange.
+//       for ( let i = 0; i < width; ++i ) {
+//         arrayTemp[ i ] = arrayIn[ indexBegin + i ];
+//       }
+//
+//       ArrayInterleaver.interleave_asGrouptTwo_from_to( arrayTemp, 0, arrayIn, indexBegin, widthHalf );
+//
+// //!!! (2022/07/09 Remarked) Old Codes.
+// //       // Interleave the elements order.
+// //       for ( let i = 0, toIndex = indexBegin; i < widthHalf; ++i, toIndex += 2 ) {
+// //         arrayIn[ toIndex     ] = arrayTemp[ i ];
+// //         arrayIn[ toIndex + 1 ] = arrayTemp[ i + widthHalf ];
+// //       }
+//     }
+
+    return arrayIn;
+  }
+
+  /**
+   * Rearrange array elements along height by interleaving.
+   *   - Only ( groupCount == 2 ) is supported.
+   *
+   * @param {Array} fromArray
+   *   The source array. It will not be modified. It is the source of copying.
+   *
+   * @param {Array} toArray
+   *   The destination array. It will be modified (i.e. filled data copied from fromArray).
+   *
+   * @param {number} height
+   *   The virtual 2D array's height.
+   *
+   * @param {number} width
+   *   The virtual 2D array's width.
+   */
+  static interleave_asGrouptTwo_alongHeight_from_to( fromArray, toArray, height, width ) {
+    let elementCount = height * width;
+    if ( elementCount != fromArray.length )
+      throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongHeight(): `
         + `( height * width ) = ( ${height} * ${width} ) = ${elementCount} `
         + ` should be the same as input array length ( ${arrayIn.length} ).`
       );
 
-    if ( ( width % 2 ) != 0 )
-      throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongWidth(): `
-        + `width ( ${width} ) must be even (i.e. divisible by 2).`
+    if ( ( height % 2 ) != 0 )
+      throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongHeight(): `
+        + `height ( ${height} ) must be even (i.e. divisible by 2).`
       );
 
-    arrayTemp.length = width;
-    let widthHalf = Math.floor( width / 2 );
+    toArray.length = elementCount;
+    let elementCountHalf = Math.floor( elementCount / 2 );
 
-    for ( let indexBegin = 0; indexBegin < elementCount; indexBegin += width ) {
-
-      // Copy the elements to be re-arrange.
+    let fromIndex2, toIndex2;
+    for ( let y = 0, fromIndex1 = 0, toIndex1 = 0; y < height; y += 2, fromIndex1 += width, toIndex1 = ( toIndex2 + width ) ) {
+      fromIndex2 = fromIndex1 + elementCountHalf;
+      toIndex2 = toIndex1 + width;
       for ( let i = 0; i < width; ++i ) {
-        arrayTemp[ i ] = arrayIn[ indexBegin + i ];
+        toArray[ toIndex1 + i ] = fromArray[ fromIndex1 + i ];
+        toArray[ toIndex2 + i ] = fromArray[ fromIndex2 + i ];
       }
-
-      ArrayInterleaver.interleave_asGrouptTwo_from_to( arrayTemp, 0, arrayIn, indexBegin, widthHalf );
-
-//!!! (2022/07/09 Remarked) Old Codes.
-//       // Interleave the elements order.
-//       for ( let i = 0, toIndex = indexBegin; i < widthHalf; ++i, toIndex += 2 ) {
-//         arrayIn[ toIndex     ] = arrayTemp[ i ];
-//         arrayIn[ toIndex + 1 ] = arrayTemp[ i + widthHalf ];
-//       }
     }
-
-    return arrayIn;
   }
 
   /**
@@ -200,37 +252,46 @@ class ArrayInterleaver {
    */
   static interleave_asGrouptTwo_alongHeight( arrayIn, height, width, arrayTemp ) {
 
-    let elementCount = height * width;
-
-    if ( elementCount != arrayIn.length )
-      throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongHeight(): `
-        + `( height * width ) = ( ${height} * ${width} ) = ${elementCount} `
-        + ` should be the same as input array length ( ${arrayIn.length} ).`
-      );
-
-    if ( ( height % 2 ) != 0 )
-      throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongHeight(): `
-        + `height ( ${height} ) must be even (i.e. divisible by 2).`
-      );
-
     // Copy the elements to be re-arrange.
-    arrayTemp.length = elementCount;
-    for ( let i = 0; i < elementCount; ++i ) {
+    arrayTemp.length = arrayIn.length;
+    for ( let i = 0; i < arrayIn.length; ++i ) {
       arrayTemp[ i ] = arrayIn[ i ];
     }
 
-    let elementCountHalf = Math.floor( elementCount / 2 );
+    ArrayInterleaver.interleave_asGrouptTwo_alongHeight_from_to( arrayTemp, arrayIn, height, width );
 
-    // Interleave the elements order.
-    let fromIndex2, toIndex2;
-    for ( let y = 0, fromIndex1 = 0, toIndex1 = 0; y < height; y += 2, fromIndex1 += width, toIndex1 = ( toIndex2 + width ) ) {
-      fromIndex2 = fromIndex1 + elementCountHalf;
-      toIndex2 = toIndex1 + width;
-      for ( let i = 0; i < width; ++i ) {
-        arrayIn[ toIndex1 + i ] = arrayTemp[ fromIndex1 + i ];
-        arrayIn[ toIndex2 + i ] = arrayTemp[ fromIndex2 + i ];
-      }
-    }
+//!!! (2022/07/09 Remarked) Old Codes.
+//     let elementCount = height * width;
+//
+//     if ( elementCount != arrayIn.length )
+//       throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongHeight(): `
+//         + `( height * width ) = ( ${height} * ${width} ) = ${elementCount} `
+//         + ` should be the same as input array length ( ${arrayIn.length} ).`
+//       );
+//
+//     if ( ( height % 2 ) != 0 )
+//       throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongHeight(): `
+//         + `height ( ${height} ) must be even (i.e. divisible by 2).`
+//       );
+//
+//     // Copy the elements to be re-arrange.
+//     arrayTemp.length = elementCount;
+//     for ( let i = 0; i < elementCount; ++i ) {
+//       arrayTemp[ i ] = arrayIn[ i ];
+//     }
+//
+//     let elementCountHalf = Math.floor( elementCount / 2 );
+//
+//     // Interleave the elements order.
+//     let fromIndex2, toIndex2;
+//     for ( let y = 0, fromIndex1 = 0, toIndex1 = 0; y < height; y += 2, fromIndex1 += width, toIndex1 = ( toIndex2 + width ) ) {
+//       fromIndex2 = fromIndex1 + elementCountHalf;
+//       toIndex2 = toIndex1 + width;
+//       for ( let i = 0; i < width; ++i ) {
+//         arrayIn[ toIndex1 + i ] = arrayTemp[ fromIndex1 + i ];
+//         arrayIn[ toIndex2 + i ] = arrayTemp[ fromIndex2 + i ];
+//       }
+//     }
 
     return arrayIn;
   }
