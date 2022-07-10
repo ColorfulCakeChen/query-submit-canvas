bba7895c56c52d32c99ffd79e7e134f72c7084be
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-07-10 10:29:19 +0800

    Update ActivationEscaping_ScaleBoundsArrayBase.js

--- CNN/Conv/ActivationEscaping/ActivationEscaping_ScaleBoundsArrayBase.js
+++ CNN/Conv/ActivationEscaping/ActivationEscaping_ScaleBoundsArrayBase.js
@@ -189,13 +189,13 @@ class ScaleBoundsArray extends Recyclable.Root {
   set_all_byInterleave_asGrouptTwo() {
     {
       let boundsArrayShuffled = FloatValue.BoundsArray.Pool.get_or_create_by( this.boundsArray.length );
-      boundsArrayShuffled.set_all_byInterleave_asGrouptTwo_byBoundsArray( this.boundsArray.boundsArray );
+      boundsArrayShuffled.set_all_byInterleave_asGrouptTwo_byBoundsArray( this.boundsArray );
       this.boundsArray.disposeResources_and_recycleToPool();
       this.boundsArray = boundsArrayShuffled;
     }
     {
       let scaleArraySetShuffled = ScaleArraySet.Pool.get_or_create_by( this.scaleArraySet.length );
-      scaleArraySetShuffled.set_all_byInterleave_asGrouptTwo_byScaleArraySet( this.scaleArraySet.scaleArraySet );
+      scaleArraySetShuffled.set_all_byInterleave_asGrouptTwo_byScaleArraySet( this.scaleArraySet );
       this.scaleArraySet.disposeResources_and_recycleToPool();
       this.scaleArraySet = scaleArraySetShuffled;
     }
