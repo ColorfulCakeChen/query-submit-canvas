1793a79f6846276ac25fbe559286f6653a3b93de
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-07-13 09:52:21 +0800

    Update ArrayInterleaver.js

--- CNN/Unpacker/FloatValue/ArrayInterleaver.js
+++ CNN/Unpacker/FloatValue/ArrayInterleaver.js
@@ -92,53 +92,69 @@ class ArrayInterleaver {
   }
 
 
-
-//!!! ...unfinished... (2022/07/12) interleave_asGrouptTwo_along_last_2nd_axis_from_to()
-
   /**
-   * Rearrange array elements along height by interleaving.
+   * Rearrange array elements along the last 2nd axis by interleaving.
    *   - Only ( groupCount == 2 ) is supported.
    *
    * @param {Array} fromArray  The source array. It will not be modified. It is the source of copying.
    * @param {Array} toArray    The destination array. It will be modified (i.e. filled data copied from fromArray).
-   * @param {number} height    The virtual 2D array's height.
-   * @param {number} width     The virtual 2D array's width.
+   * @param {number[]} shape   The virtual N-dimension array's shape (e.g. [ width ] or [ height, width ] or [ height, width, depth ].
    */
-  static interleave_asGrouptTwo_alongHeight_from_to( fromArray, toArray, height, width ) {
-    let elementCount = height * width;
-    if ( elementCount != fromArray.length )
-      throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongHeight_from_to(): `
-        + `( height * width ) = ( ${height} * ${width} ) = ${elementCount} `
-        + ` should be the same as input array length ( ${fromArray.length} ).`
+  static interleave_asGrouptTwo_alongLast2ndAxis_from_to( fromArray, toArray, ...shape ) {
+    if ( shape.length < 2 ) {
+      throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongLast2ndAxis_from_to(): `
+        + `shape.length ( ${shape.length} ) should be at least 2.`
+      );
+    }
+
+    let elementCount = 1;
+    for ( let i = 0; i < shape.length; ++i ) {
+      elementCount *= shape[ i ];
+    }
+
+    if ( elementCount != fromArray.length ) {
+      let shapeString = shape.join( " * " );
+      throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongLast2ndAxis_from_to(): `
+        + `shape ( ${shapeString} ) = ${elementCount} `
+        + `should be the same as input array length ( ${fromArray.length} ).`
+      );
+    }
+
+    let last2ndAxisId = shape.length - 2;
+    let last2ndAxisLength = shape[ last2ndAxisId ];
+    if ( ( last2ndAxisLength % 2 ) != 0 )
+      throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongLast2ndAxis_from_to(): `
+        + `shape's last 2nd axis length ( ${last2ndAxisLength} ) must be even (i.e. divisible by 2).`
       );
 
-    if ( ( height % 2 ) != 0 )
-      throw Error( `ArrayInterleaver.interleave_asGrouptTwo_alongHeight_from_to(): `
-        + `height ( ${height} ) must be even (i.e. divisible by 2).`
-      );
+    let lastAxisId = shape.length - 1;
+    let lastAxisLength = shape[ lastAxisId ];
 
     toArray.length = elementCount;
-    let elementCountHalf = Math.floor( elementCount / 2 );
+    let elementCountHalf = ( elementCount / 2 );
 
+    let fromIndex1 = 0, toIndex1 = 0;
     let fromIndex2, toIndex2;
-    for ( let y = 0, fromIndex1 = 0, toIndex1 = 0; y < height; y += 2, fromIndex1 += width, toIndex1 = ( toIndex2 + width ) ) {
+    for ( let y = 0; y < last2ndAxisLength; y += 2 ) {
       fromIndex2 = fromIndex1 + elementCountHalf;
-      toIndex2 = toIndex1 + width;
-      for ( let i = 0; i < width; ++i ) {
+      toIndex2 = toIndex1 + lastAxisLength;
+
+      for ( let i = 0; i < lastAxisLength; ++i ) {
         toArray[ toIndex1 + i ] = fromArray[ fromIndex1 + i ];
         toArray[ toIndex2 + i ] = fromArray[ fromIndex2 + i ];
       }
+
+      fromIndex1 += lastAxisLength;
+      toIndex1 = ( toIndex2 + lastAxisLength )
     }
   }
 
   /**
-   * Rearrange array elements along height by interleaving.
+   * Rearrange array elements along the last 2nd axis by interleaving.
    *   - Only ( groupCount == 2 ) is supported.
    *
-   * @param {Array} arrayIn  The array to be re-arranged. It is an 1D array but represents a 2D array.
-   *
-   * @param {number} height  The virtual 2D array's height.
-   * @param {number} width   The virtual 2D array's width.
+   * @param {Array} arrayIn   The array to be re-arranged. It is an 1D array but represents a 2D array.
+   * @param {number[]} shape  The virtual N-dimension array's shape (e.g. [ width ] or [ height, width ] or [ height, width, depth ].
    *
    * @param {Array} arrayTemp
    *   A temporary array for placing the original elements (from arrayIn) temporarily. Providing this array could reduce memory
@@ -147,14 +163,14 @@ class ArrayInterleaver {
    * @return {Array}
    *   Retrun the (modified) arrayIn itself.
    */
-  static interleave_asGrouptTwo_alongHeight_inPlace( arrayIn, height, width, arrayTemp ) {
+  static interleave_asGrouptTwo_alongLast2ndAxis_inPlace( arrayIn, height, width, arrayTemp ) {
 
     arrayTemp.length = arrayIn.length;
     for ( let i = 0; i < arrayIn.length; ++i ) { // Copy the elements to be re-arrange.
       arrayTemp[ i ] = arrayIn[ i ];
     }
 
-    ArrayInterleaver.interleave_asGrouptTwo_alongHeight_from_to( arrayTemp, arrayIn, height, width );
+    ArrayInterleaver.interleave_asGrouptTwo_alongLast2ndAxis_from_to( arrayTemp, arrayIn, height, width );
     return arrayIn;
   }
 
