f8b522882af38f4376a09d7d79462e19f6ec11d7
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-07-14 13:50:33 +0800

    Update MobileNetV2_Thin.js

--- CNN/Conv/Stage/Stage_BlockParamsCreator/MobileNetV2_Thin.js
+++ CNN/Conv/Stage/Stage_BlockParamsCreator/MobileNetV2_Thin.js
@@ -1,5 +1,6 @@
 export { MobileNetV2_Thin };
 
+import * as Pool from "../../util/Pool.js";
 import * as ValueDesc from "../../../Unpacker/ValueDesc.js";
 import { Params } from "../Stage_Params.js";
 import { MobileNetV1 } from "./MobileNetV1.js";
@@ -11,23 +12,48 @@ import { MobileNetV1 } from "./MobileNetV1.js";
  */
 class MobileNetV2_Thin extends MobileNetV1 {
 
+  /**
+   * Used as default Stage.BlockParamsCreator.MobileNetV2_Thin provider for conforming to Recyclable interface.
+   */
+  static Pool = new Pool.Root( "Stage.BlockParamsCreator.MobileNetV2_Thin.Pool", MobileNetV2_Thin, MobileNetV2_Thin.setAsConstructor );
+
+  /**
+   */
   constructor( stageParams ) {
     super( stageParams );
+    Base.setAsConstructor_self.call( this );
   }
 
   /** @override */
+  static setAsConstructor( stageParams ) {
+    super.setAsConstructor( stageParams );
+    Base.setAsConstructor_self.call( this );
+    return this;
+  }
+
+  /** @override */
+  static setAsConstructor_self( stageParams ) {
+    // Do nothing.
+  }
+
+  ///** @override */
+  //disposeResources() {
+  //  super.disposeResources();
+  //}
+
+  /** @override */
   configTo_beforeBlock0() {
-    super.configTo_beforeBlock0(); // block0's inputHeight0, inputWidth0, bias, activation.
+    super.configTo_beforeBlock0(); // block0's input0_height, input0_width, input0_channelCount, ConvBlockType, activation.
   }
 
   /** @override */
   configTo_afterBlock0() {
-    super.configTo_afterBlock0(); // block1, 2, 3, ...'s inputHeight0, inputWidth0.
+    super.configTo_afterBlock0(); // block1, 2, 3, ...'s input0_height, input0_width.
 
     // In MobileNetV2:
     //   - All blocks (except block0) do add-input-to-output (without concatenation).
     //   - All blocks (include block0) do not use input1.
-    this.channelCount1_pointwise1Before = ValueDesc.channelCount1_pointwise1Before.Singleton.Ids.ONE_INPUT_ADD_TO_OUTPUT;
+    this.nConvBlockTypeId = ValueDesc.ConvBlockType.Singleton.Ids.MOBILE_NET_V2_BODY_TAIL;
   }
 
   /** @override */
