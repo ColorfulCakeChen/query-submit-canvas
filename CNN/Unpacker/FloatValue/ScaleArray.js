0cc10eb182172543c5baf41dc1d80a7ac1b7e0f1
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-07-14 11:01:04 +0800

    Update ScaleArray.js

--- CNN/Unpacker/FloatValue/ScaleArray.js
+++ CNN/Unpacker/FloatValue/ScaleArray.js
@@ -385,7 +385,7 @@ class ScaleArray extends Recyclable.Root {
     // So the returned scale (if exists) is always powers of two (e.g. 2^(-1), 2^(-2), 2^(-3), ... ) (i.e. 0.5, 0.25, 0.125, ... ).
     // Because they can be represented as a finite floating-point number (v.s. if scale is 0.3, it can not be represented as a finite
     // floating-point number), this reduces floating-point accumulated error when ShuffleNetV2_byMobileNetV2 pass-through do-scale
-    // and undo-scale repeately. 
+    // and undo-scale repeatedly. 
     //
     let scale;
 
