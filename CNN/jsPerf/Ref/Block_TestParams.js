349272d0d8abfcf6ab5f47576bbee2ba7c761346
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-07-15 11:20:14 +0800

    Update Block_TestParams.js

--- CNN/jsPerf/Ref/Block_TestParams.js
+++ CNN/jsPerf/Ref/Block_TestParams.js
@@ -291,9 +291,9 @@ class Base extends TestParams.Base {
 
       nConvBlockTypeId: [
 //!!! (2022/07/06 Temp Remarked) For speed-up debug.
-//         Block.Params.nConvBlockTypeId.valueDesc.range.min,
-//         Block.Params.nConvBlockTypeId.valueDesc.range.max
-//       ],
+        Block.Params.nConvBlockTypeId.valueDesc.range.min,
+        Block.Params.nConvBlockTypeId.valueDesc.range.max
+      ],
 //!!! (2022/07/13 Temp Added and Remarked) For speed-up debug.
 //         Block.Params.nConvBlockTypeId.valueDesc.range.min,
 //         ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD, // (5)
@@ -305,11 +305,11 @@ class Base extends TestParams.Base {
 //         ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_POINTWISE21_TAIL // (11)
 //       ],
 //!!! (2022/07/06 Temp Added and Remarked) For speed-up debug.
-//        Block.Params.nConvBlockTypeId.valueDesc.range.min,
+//       Block.Params.nConvBlockTypeId.valueDesc.range.min,
 //       ValueDesc.ConvBlockType.Singleton.Ids.MOBILE_NET_V2_BODY_TAIL, // (1)
 //       ValueDesc.ConvBlockType.Singleton.Ids.MOBILE_NET_V2_BODY_TAIL // (1)
-       ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD, // (5)
-       ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD // (5)
+//       ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD, // (5)
+//       ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD // (5)
 //       ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_BODY // (6)
 //       ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_TAIL, // (7)
 //       ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_TAIL // (7)
