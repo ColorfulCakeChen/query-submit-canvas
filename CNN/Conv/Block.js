17ba73de1cc7bde665d561c4333348ff1cc9b276
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-07-15 09:17:54 +0800

    Update Block.js

--- CNN/Conv/Block.js
+++ CNN/Conv/Block.js
@@ -1,3 +1,4 @@
 export * as ChannelCountCalculator from "./Block/Block_ChannelCountCalculator.js";
+export { inputTensorPlaceholder_creator } from "./Block/Block_inputTensorPlaceholder_creator.js";
 export { Params } from "./Block/Block_Params.js";
 export { Base } from "./Block/Block_Base.js";
