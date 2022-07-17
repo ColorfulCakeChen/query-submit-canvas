535dc4d8ecd8f6c57763e3d600b9aef92f9059d5
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-07-17 09:08:14 +0800

    Update Block_Base.js

--- CNN/Conv/Block/Block_Base.js
+++ CNN/Conv/Block/Block_Base.js
@@ -862,8 +862,11 @@ class Base extends Recyclable.Root {
   /**
    * Initialize this object by calling initer() and advance the generator by loop until done.
    *
-   * @param {ValueMax.Percentage.Aggregate} progressParent
-   *   If null, a temporary progress object will be created.
+
+//!!! (2022/07/17 Remarked) Who will release it if it is create here automatically? So, do not do that.
+//    * @param {ValueMax.Percentage.Aggregate} progressParent
+//    *   If null, a temporary progress object will be created.
+
    *
    * @return {boolean}
    *   Return true if successfully (and progressParent.valuePercentage will be equal to 100).
@@ -877,7 +880,8 @@ class Base extends Recyclable.Root {
     channelShuffler_ConcatPointwiseConv
   ) {
 
-    progressParent = progressParent ?? ( ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
+//!!! (2022/07/17 Remarked) Who will release it if it is create here automatically? So, do not do that.
+//    progressParent = progressParent ?? ( ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
 
     let initer = this.initer(
       progressParent, inputWeightArray, weightElementOffsetBegin, params,
