ffa15cd3cb75ade7cba8ef59d48794c353ee33f0
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-07-18 15:24:08 +0800

    Update and rename CNN/util/Pool_Asserter.js to CNN/util/Pool/Pool_Asserter.js

--- CNN/util/Pool/Pool_Asserter.js
+++ CNN/util/Pool/Pool_Asserter.js
@@ -31,26 +31,10 @@ function assert_Pool_issuedCount( prefixMsg, issuedCount_shouldBe ) {
 function assert_Pool_issuedCount_same_after_as_before( prefixMsg, pfn, thisArg, ...restArgs ) {
   let returnedValue;
 
-//!!! (2022/06/30 Remarked) Now, no issuedCount for individual pool
-//  let issuedCount_array_before;
   try {
-
-//!!! (2022/06/30 Remarked) Now, no issuedCount for individual pool
-//     issuedCount_array_before = Recyclable.Array.Pool.get_or_create_by( Pool.All.registeredPoolArray.length );
-//     for ( let i = 0; i < Pool.All.registeredPoolArray.length; ++i ) {
-//       let pool = Pool.All.registeredPoolArray[ i ];
-//       issuedCount_array_before[ i ] = pool.issuedCount;
-//     }
-
     let issuedCount_before = Pool.All.issuedCount;
 
     returnedValue = pfn.apply( thisArg, restArgs );
-
-//!!! (2022/06/30 Remarked) Now, no issuedCount for individual pool
-//     for ( let i = 0; i < Pool.All.registeredPoolArray.length; ++i ) {
-//       let pool = Pool.All.registeredPoolArray[ i ];
-//       assert_Pool_issuedCount( prefixMsg, pool, issuedCount_array_before[ i ] );
-//     }
 
     assert_Pool_issuedCount( prefixMsg, issuedCount_before );
 
@@ -58,11 +42,6 @@ function assert_Pool_issuedCount_same_after_as_before( prefixMsg, pfn, thisArg, 
     throw e;
 
   } finally {
-//!!! (2022/06/30 Remarked) Now, no issuedCount for individual pool
-//     if ( issuedCount_array_before ) {
-//       issuedCount_array_before.disposeResources_and_recycleToPool();
-//       issuedCount_array_before = null;
-//     }
   }
 
   return returnedValue;
