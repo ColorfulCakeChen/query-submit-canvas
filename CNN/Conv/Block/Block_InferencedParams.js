fd90cd7b07867cb1337e2543be78ae701fd5b1a4
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-07-18 20:40:21 +0800

    Update Block_InferencedParams.js

--- CNN/Conv/Block/Block_InferencedParams.js
+++ CNN/Conv/Block/Block_InferencedParams.js
@@ -1,12 +1,13 @@
 export { InferencedParams };
 
 import * as Pool from "../../util/Pool.js";
-//import * as Recyclable from "../../util/Recyclable.js";
+import * as Recyclable from "../../util/Recyclable.js";
 import * as ValueDesc from "../../Unpacker/ValueDesc.js";
 import * as ChannelCountCalculator from "./Block_ChannelCountCalculator.js";
 import * as Depthwise from "../Depthwise.js";
 
 /**
+ * All properties inferenced from Block.Params.
  *
  */
 class InferencedParams extends Recyclable.Root {
