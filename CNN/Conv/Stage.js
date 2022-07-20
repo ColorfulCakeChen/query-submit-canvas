460d57118e505fdc40473b194773d3ebb57ed91e
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-07-20 16:02:34 +0800

    InferencedParams

--- CNN/Conv/Stage.js
+++ CNN/Conv/Stage.js
@@ -1,4 +1,4 @@
 export { Params } from "./Stage/Stage_Params.js";
-export { InferencedParamsParams } from "./Stage/Stage_InferencedParamsParams.js";
+export { InferencedParams } from "./Stage/Stage_InferencedParams.js";
 export * as BlockParamsCreator from "./Stage/Stage_BlockParamsCreator.js";
 export { Base } from "./Stage/Stage_Base.js";
