752bd24399a7f2411f59f566b5464b9dc5e50590
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-07-24 10:38:11 +0800

    NameNumberArrayObject.Base

--- CNN/util/NameNumberArrayObject/NameNumberArrayObject_Base.js
+++ CNN/util/NameNumberArrayObject/NameNumberArrayObject_Base.js
@@ -4,8 +4,13 @@ import * as Pool from "./Pool.js";
 import * as Recyclable from "./Recyclable.js";
 
 /**
+ * This object has string named properties and integer numeric properties (just like an Array). No matter
+ * string named or integer numeric properties, their values should be either a number or a number array.
  *
- *
+ * It is mainly used for composing the input parameters and weights of Block, Stage, Embedding,
+ * NeuralNet. The composing order are:
+ *  - All string named properties first according to nameOrderArray. And then,
+ *  - All integer numeric properties according to the incremental order (i.e. 0, 1, 2, ...) 
  *
  *
  */
@@ -32,18 +37,10 @@ class Base extends Recyclable.Array {
 
   /** @override */
   static setAsConstructor_self() {
-    this.weightsArray = Recyclable.Array.Pool.get_or_create_by( 0 );
   }
 
   /** @override */
   disposeResources() {
-    this.weightsElementOffsetBegin = undefined;
-
-    if ( this.weightsArray ) {
-      this.weightsArray.disposeResources_and_recycleToPool();
-      this.weightsArray = null;
-    }
-
     super.disposeResources();
   }
 
