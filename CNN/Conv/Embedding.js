bbcb3f24039c383aee36985ae29b95d17c922b86
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-07-26 20:46:55 +0800

    _FiltersArrray

--- CNN/Conv/Embedding.js
+++ CNN/Conv/Embedding.js
@@ -1,4 +1,6 @@
 export { Params } from "./Embedding/Embedding_Params.js";
 export { InferencedParams } from "./Embedding/Embedding_InferencedParams.js";
+export { AddGatherReshape_FiltersArray } from "./Embedding/Embedding_AddGatherReshape_FiltersArrray.js";
 export { AddGatherReshape } from "./Embedding/Embedding_AddGatherReshape.js";
+export { SplitGatherConcat_FiltersArrray } from "./Embedding/Embedding_SplitGatherConcat_FiltersArrray.js";
 export { SplitGatherConcat } from "./Embedding/Embedding_SplitGatherConcat.js";
