ac215be5ed630d5b03e00031f41dba90fdd53355
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-07-28 09:42:17 +0800

    Embedding_FiltersArray_Multi

--- CNN/Conv/Embedding/Embedding_FiltersArray_Multi.js
+++ CNN/Conv/Embedding/Embedding_FiltersArray_Multi.js
@@ -38,7 +38,7 @@ class Embedding_FiltersArray_Multi extends FiltersArray_Base {
       input_height, input_width, input_channelCount,
       channelMultiplier, vocabularyCountPerInputChannel, bEmbedVocabularyId
     );
-    Embedding_FiltersArray_Base.setAsConstructor_self.call( this );
+    Embedding_FiltersArray_Multi.setAsConstructor_self.call( this );
   }
 
   /** @override */
@@ -50,7 +50,7 @@ class Embedding_FiltersArray_Multi extends FiltersArray_Base {
       input_height, input_width, input_channelCount,
       channelMultiplier, vocabularyCountPerInputChannel, bEmbedVocabularyId
     );
-    Embedding_FiltersArray_Base.setAsConstructor_self.call( this );
+    Embedding_FiltersArray_Multi.setAsConstructor_self.call( this );
     return this;
   }
 
