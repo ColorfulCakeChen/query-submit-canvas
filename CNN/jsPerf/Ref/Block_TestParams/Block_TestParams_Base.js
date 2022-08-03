16e7676b7ce7af3151b2e777ce4f9d4adafb2865
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-03 20:37:40 +0800

    nConvBlockTypeId

--- CNN/jsPerf/Ref/Block_TestParams/Block_TestParams_Base.js
+++ CNN/jsPerf/Ref/Block_TestParams/Block_TestParams_Base.js
@@ -268,9 +268,9 @@ class Block_TestParams_Base extends TestParams.Base {
 
       nConvBlockTypeId: [
 //!!! (2022/07/06 Temp Remarked) For speed-up debug.
-        Block.Params.nConvBlockTypeId.valueDesc.range.min,
-        Block.Params.nConvBlockTypeId.valueDesc.range.max
-      ],
+      //   Block.Params.nConvBlockTypeId.valueDesc.range.min,
+      //   Block.Params.nConvBlockTypeId.valueDesc.range.max
+      // ],
 //!!! (2022/07/13 Temp Added and Remarked) For speed-up debug.
 //         Block.Params.nConvBlockTypeId.valueDesc.range.min,
 //         ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD, // (5)
@@ -283,8 +283,8 @@ class Block_TestParams_Base extends TestParams.Base {
 //       ],
 //!!! (2022/07/06 Temp Added and Remarked) For speed-up debug.
 //       Block.Params.nConvBlockTypeId.valueDesc.range.min,
-//       ValueDesc.ConvBlockType.Singleton.Ids.MOBILE_NET_V2_BODY_TAIL, // (1)
-//       ValueDesc.ConvBlockType.Singleton.Ids.MOBILE_NET_V2_BODY_TAIL // (1)
+      ValueDesc.ConvBlockType.Singleton.Ids.MOBILE_NET_V2_BODY_TAIL, // (1)
+      ValueDesc.ConvBlockType.Singleton.Ids.MOBILE_NET_V2_BODY_TAIL // (1)
 //       ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD, // (5)
       // ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD, // (5)
       // ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_BODY, // (6)
@@ -294,7 +294,7 @@ class Block_TestParams_Base extends TestParams.Base {
       // ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_TAIL, // (7)
       // ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_TAIL // (7)
 //       Block.Params.nConvBlockTypeId.valueDesc.range.max
-  // ],
+  ],
 
 //!!! (2022/07/11 Temp Remarked) For speed-up debug.
       pointwise1ChannelCount: [ 0, 2 ],
