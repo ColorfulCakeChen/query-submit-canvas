3ac2adce0c69e324a5ff8b5506b667b800e0a534
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-04 15:43:05 +0800

    neuralNet_compare_ShuffleNetV2_and_ShuffleNetV2_byMobileNetV1

--- CNN/jsPerf/Ref/NeuralNet_Reference.js
+++ CNN/jsPerf/Ref/NeuralNet_Reference.js
@@ -186,10 +186,10 @@ class NeuralNet_Reference_Base extends Recyclable.Root {
       this.assert_imageOut_Tensors_byNumberArrays( outputTensor3d, this.testCorrectness_imageOutReference, neuralNet );
 
 //!!! (2022/08/02 Temp Remarked) seems hard to be the same.
-//       // Compare result of ShuffleNetV2 and ShuffleNetV2_byMobileNetV1.
-//       NeuralNet_Reference_Base.neuralNet_compare_ShuffleNetV2_and_ShuffleNetV2_byMobileNetV1.call( this,
-//         testParams,
-//         inputTensor3d_fromBag, outputTensor3d );
+      // Compare result of ShuffleNetV2 and ShuffleNetV2_byMobileNetV1.
+      NeuralNet_Reference_Base.neuralNet_compare_ShuffleNetV2_and_ShuffleNetV2_byMobileNetV1.call( this,
+        testParams,
+        inputTensor3d_fromBag, outputTensor3d );
 
       neuralNet.disposeResources_and_recycleToPool();
       neuralNet = null;
