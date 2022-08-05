6724a46252cfa491a99b415a8cb3064f36a474e7
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-05 12:19:39 +0800

    weightsValueStep

--- CNN/jsPerf/Ref/TestParams.js
+++ CNN/jsPerf/Ref/TestParams.js
@@ -511,7 +511,7 @@ class TestParams_Base extends Recyclable.Root {
 }
 
 TestParams_Base.weightsValueBegin = 0;
-TestParams_Base.weightsValueStep = 10; //10;
+TestParams_Base.weightsValueStep = 100; //10;
 
 //!!! (2022/08/03 Temp Remarked) Fixed to non-random to simplify debug.
 //TestParams_Base.weightsRandomOffset = { min: -0, max: +0 };
