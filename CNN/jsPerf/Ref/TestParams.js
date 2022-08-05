faf26d34d81fed0ab1d8aee46a87d3b5641638e3
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-05 20:26:58 +0800

    weightsRandomOffset

--- CNN/jsPerf/Ref/TestParams.js
+++ CNN/jsPerf/Ref/TestParams.js
@@ -515,9 +515,9 @@ TestParams_Base.weightsValueBegin = 0;
 TestParams_Base.weightsValueStep = 10;
 
 //!!! (2022/08/03 Temp Remarked) Fixed to non-random to simplify debug.
-//TestParams_Base.weightsRandomOffset = { min: -0, max: +0 };
+TestParams_Base.weightsRandomOffset = { min: -0, max: +0 };
 //TestParams_Base.weightsRandomOffset = { min: -1, max: +1 };
-TestParams_Base.weightsRandomOffset = { min: -5, max: +5 };
+//TestParams_Base.weightsRandomOffset = { min: -5, max: +5 };
 //TestParams_Base.weightsRandomOffset = { min: -50, max: +50 };
 //TestParams_Base.weightsRandomOffset = { min: -200, max: +200 };
 //TestParams_Base.weightsRandomOffset = { min: -11111, max: 11111 };
