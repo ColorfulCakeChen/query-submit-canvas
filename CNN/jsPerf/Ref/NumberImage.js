d76e0e99a66b648b57a9ac9368eea52b8a38a280
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-05 20:17:31 +0800

    fill_numberArray

--- CNN/jsPerf/Ref/NumberImage.js
+++ CNN/jsPerf/Ref/NumberImage.js
@@ -1439,12 +1439,8 @@ class NumberImage_Base extends Recyclable.Root {
       valueBegin, valueStep,
       randomOffsetMin, randomOffsetMax, divisorForRemainder );
 
-!!! ...unfinished... (2022/08/05) 
-// should let neighbor chnnael have diferent bounds
-// should collect Bounds by channel
-
     // 3. Fill .boundsArraySet
-    imageNew.boundsArraySet.set_outputs_all_byLowerUpper( imageNew.dataArray.lowerBound, imageNew.dataArray.upperBound );
+    imageNew.boundsArraySet.set_outputs_all_byBoundsArray( imageNew.dataArray.boundsArray_byChannel );
 
     return imageNew;
   }
