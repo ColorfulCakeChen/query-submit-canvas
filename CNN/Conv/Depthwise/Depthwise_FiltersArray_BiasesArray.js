81b4e30abcde486865d8b57ba7a8c4109c005963
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-07 10:49:19 +0800

    afterFilter_BoundsArray_ArrayArray

--- CNN/Conv/Depthwise/Depthwise_FiltersArray_BiasesArray.js
+++ CNN/Conv/Depthwise/Depthwise_FiltersArray_BiasesArray.js
@@ -668,7 +668,7 @@ let FiltersArray_BiasesArray = ( ParentClass = Object ) =>
                           for ( let getX = Math.max( 0, virtualInputBeginX + filterX ); getX < virtualInputWidth; ++getX ) {
 
 //!!! ...unfinished... (2022/08/07) use tBounds.
-                            afterFilter_BoundsArray_ArrayArray[ ??? ][ ??? ];
+                            afterFilter_BoundsArray_ArrayArray[ ???outY ][ ???outX ];
 
                           }
                         }
