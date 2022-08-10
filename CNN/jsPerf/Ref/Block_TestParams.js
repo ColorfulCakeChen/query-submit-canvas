2d4a3a087033c49bf132c510a5af496366477d9a
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-10 09:35:35 +0800

    nConvBlockTypeId

--- CNN/jsPerf/Ref/Block_TestParams.js
+++ CNN/jsPerf/Ref/Block_TestParams.js
@@ -247,11 +247,11 @@ class Block_TestParams_Base extends TestParams.Base {
       input0_channelCount: [ 1, 3 ],
       // input0_channelCount: [ 3, 4 ], //[ 1, 3 ],
 
-      nConvBlockTypeId: [
 //!!! (2022/07/06 Temp Remarked) For speed-up debug.
-        Block.Params.nConvBlockTypeId.valueDesc.range.min,
-        Block.Params.nConvBlockTypeId.valueDesc.range.max
-      ],
+      // nConvBlockTypeId: [
+      //   Block.Params.nConvBlockTypeId.valueDesc.range.min,
+      //   Block.Params.nConvBlockTypeId.valueDesc.range.max
+      // ],
 //!!! (2022/07/13 Temp Added and Remarked) For speed-up debug.
 //         Block.Params.nConvBlockTypeId.valueDesc.range.min,
 //         ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD, // (5)
@@ -263,20 +263,21 @@ class Block_TestParams_Base extends TestParams.Base {
 //         ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_POINTWISE21_TAIL // (11)
 //       ],
 //!!! (2022/07/06 Temp Added and Remarked) For speed-up debug.
-      //   Block.Params.nConvBlockTypeId.valueDesc.range.min,
-      //   ValueDesc.ConvBlockType.Singleton.Ids.MOBILE_NET_V2_BODY_TAIL, // (1)
-      //   ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BODY, // (3)
-      //   ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_TAIL, // (4)
-      //   ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD, // (5)
-      //   ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_BODY, // (6)
-      //   ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_TAIL, // (7)
-      //   ValueDesc.ConvBlockType.Singleton.Ids.MOBILE_NET_V2_BODY_TAIL // (1)
-      //   ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_TAIL // (4)
-      //   ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD // (5)
-      //   ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_BODY // (6)
-      //   ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_TAIL // (7)
-      //   Block.Params.nConvBlockTypeId.valueDesc.range.max
-      // ],
+      nConvBlockTypeId: [
+        // Block.Params.nConvBlockTypeId.valueDesc.range.min,
+        // ValueDesc.ConvBlockType.Singleton.Ids.MOBILE_NET_V2_BODY_TAIL, // (1)
+        // ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BODY, // (3)
+        // ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_TAIL, // (4)
+        ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD, // (5)
+        // ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_BODY, // (6)
+        // ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_TAIL, // (7)
+        // ValueDesc.ConvBlockType.Singleton.Ids.MOBILE_NET_V2_BODY_TAIL // (1)
+        // ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_TAIL // (4)
+        // ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD // (5)
+        // ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_BODY // (6)
+        ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_TAIL // (7)
+        // Block.Params.nConvBlockTypeId.valueDesc.range.max
+      ],
 
 //!!! (2022/07/11 Temp Remarked) For speed-up debug.
       // pointwise1ChannelCount: [ 0, 8 ],
