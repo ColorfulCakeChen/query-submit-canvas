0c4e0fa225e6661f5e1f4d8e332e58d92f51f4d9
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-12 10:32:10 +0800

    pointwise20ChannelCount

--- CNN/jsPerf/Ref/Block_TestParams.js
+++ CNN/jsPerf/Ref/Block_TestParams.js
@@ -238,20 +238,20 @@ class Block_TestParams_Base extends TestParams.Base {
 
     // Restrict some parameter's large kinds. Otherwise, too many combination will be generated.
     let valueOutMinMax = this.valueOutMinMax = {
-      input0_height: [ 3, 3 ],
-      // input0_height: [ 1, 5 ],
+      // input0_height: [ 3, 3 ],
+      input0_height: [ 2, 2 ],
 
-      input0_width: [ 4, 5 ],
-      // input0_width: [ 1, 5 ],
+      // input0_width: [ 4, 5 ],
+      input0_width: [ 3, 3 ],
 
-      input0_channelCount: [ 2, 4 ],
-      // input0_channelCount: [ 2, 2 ], //[ 3, 4 ],
+      // input0_channelCount: [ 2, 4 ],
+      input0_channelCount: [ 3, 3 ], //[ 3, 4 ],
 
 //!!! (2022/07/06 Temp Remarked) For speed-up debug.
-      nConvBlockTypeId: [
-        Block.Params.nConvBlockTypeId.valueDesc.range.min,
-        Block.Params.nConvBlockTypeId.valueDesc.range.max
-      ],
+      // nConvBlockTypeId: [
+      //   Block.Params.nConvBlockTypeId.valueDesc.range.min,
+      //   Block.Params.nConvBlockTypeId.valueDesc.range.max
+      // ],
 //!!! (2022/07/06 Temp Added and Remarked) For speed-up debug.
       // nConvBlockTypeId: [
       //   // Block.Params.nConvBlockTypeId.valueDesc.range.min,
@@ -271,31 +271,31 @@ class Block_TestParams_Base extends TestParams.Base {
 
 //!!! (2022/07/11 Temp Remarked) For speed-up debug.
       // pointwise1ChannelCount: [ 0, 8 ],
-      // pointwise1ChannelCount: [ 2, 2 ],
       // pointwise1ChannelCount: [ 0, 0 ],
-      pointwise1ChannelCount: [ 0, 2 ],
+      // pointwise1ChannelCount: [ 0, 2 ],
+      pointwise1ChannelCount: [ 3, 3 ],
 
-      pointwise20ChannelCount: [ 1, 3 ],
-      // pointwise20ChannelCount: [ 1, 8 ],
+      // pointwise20ChannelCount: [ 1, 3 ],
+      pointwise20ChannelCount: [ 1, 8 ],
 
 //!!! (2022/07/07 Temp Remarked) For speed up debug.
-      depthwise_AvgMax_Or_ChannelMultiplier: [
-        ValueDesc.AvgMax_Or_ChannelMultiplier.Singleton.range.min,
-        2
-      ],
       // depthwise_AvgMax_Or_ChannelMultiplier: [
-      //   1,
+      //   ValueDesc.AvgMax_Or_ChannelMultiplier.Singleton.range.min,
       //   2
       // ],
+      depthwise_AvgMax_Or_ChannelMultiplier: [
+        1,
+        2
+      ],
 
       // (2021/10/06) Note: WASM seems not correct when tf.pool() or tf.depthwiseConv2d() with ( depthwiseFilterWidth == 1 ).
 //!!! (2022/08/04 Temp Remarked) For debug neural net (only use 3x3).
-      depthwiseFilterHeight: [ Block.Params.depthwiseFilterHeight.valueDesc.range.min, depthwiseFilterMaxSize ],
-      depthwiseFilterWidth: [ Block.Params.depthwiseFilterWidth.valueDesc.range.min, depthwiseFilterMaxSize ],
+      // depthwiseFilterHeight: [ Block.Params.depthwiseFilterHeight.valueDesc.range.min, depthwiseFilterMaxSize ],
+      // depthwiseFilterWidth: [ Block.Params.depthwiseFilterWidth.valueDesc.range.min, depthwiseFilterMaxSize ],
       // depthwiseFilterHeight: [ 3, 3 ],
       // depthwiseFilterWidth: [ 3, 3 ],
-      // depthwiseFilterHeight: [ 2, 2 ],
-      // depthwiseFilterWidth: [ 2, 2 ],
+      depthwiseFilterHeight: [ 3, 3 ],
+      depthwiseFilterWidth: [ 2, 2 ],
 
       // (2022/05/02) Note: The right-most pixel of depthwise convolution seems wrong when ( strides = 1, pad = "same" ) in backend
       // WebGL of some platforms (e.g. mobile phone Moto e40). But the issue does not exist when ( strides = 2, pad = "same" ) or
@@ -319,8 +319,8 @@ class Block_TestParams_Base extends TestParams.Base {
       // ],
 
       depthwiseActivationId:
-        undefined,
-        // [ ValueDesc.ActivationFunction.Singleton.range.min + 0, ValueDesc.ActivationFunction.Singleton.range.min + 1 ],
+        // undefined,
+        [ ValueDesc.ActivationFunction.Singleton.range.min + 0, ValueDesc.ActivationFunction.Singleton.range.min + 1 ],
         // [ ValueDesc.ActivationFunction.Singleton.range.min + 0, ValueDesc.ActivationFunction.Singleton.range.min + 0 ],
         // [ ValueDesc.ActivationFunction.Singleton.range.min + 1, ValueDesc.ActivationFunction.Singleton.range.min + 1 ],
 
@@ -330,18 +330,18 @@ class Block_TestParams_Base extends TestParams.Base {
       // bSqueezeExcitationPrefix: [ ValueDesc.Bool.Singleton.range.max, ValueDesc.Bool.Singleton.range.max ],
 
       // nSqueezeExcitationChannelCountDivisor: undefined,
-      nSqueezeExcitationChannelCountDivisor: [
-        ValueDesc.SqueezeExcitationChannelCountDivisor.Singleton.range.min,
-        3
-      ],
       // nSqueezeExcitationChannelCountDivisor: [
-      //   3,
+      //   ValueDesc.SqueezeExcitationChannelCountDivisor.Singleton.range.min,
       //   3
       // ],
+      nSqueezeExcitationChannelCountDivisor: [
+        0,
+        0
+      ],
 
       pointwise20ActivationId:
-        undefined,
-        // [ ValueDesc.ActivationFunction.Singleton.range.min + 0, ValueDesc.ActivationFunction.Singleton.range.min + 1 ],
+        // undefined,
+        [ ValueDesc.ActivationFunction.Singleton.range.min + 0, ValueDesc.ActivationFunction.Singleton.range.min + 1 ],
         // [ ValueDesc.ActivationFunction.Singleton.range.min + 0, ValueDesc.ActivationFunction.Singleton.range.min + 0 ],
         // [ ValueDesc.ActivationFunction.Singleton.range.min + 1, ValueDesc.ActivationFunction.Singleton.range.min + 1 ],
 
@@ -349,8 +349,8 @@ class Block_TestParams_Base extends TestParams.Base {
       // (i.e. ( undefined )) for speeding up testing.
  
       nActivationId:
-        undefined,
-        // [ ValueDesc.ActivationFunction.Singleton.range.min + 0, ValueDesc.ActivationFunction.Singleton.range.min + 1 ],
+        // undefined,
+        [ ValueDesc.ActivationFunction.Singleton.range.min + 0, ValueDesc.ActivationFunction.Singleton.range.min + 1 ],
         // [ ValueDesc.ActivationFunction.Singleton.range.min + 0, ValueDesc.ActivationFunction.Singleton.range.min + 0 ],
         // [ ValueDesc.ActivationFunction.Singleton.range.min + 1, ValueDesc.ActivationFunction.Singleton.range.min + 1 ],
 
