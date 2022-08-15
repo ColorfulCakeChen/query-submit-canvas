e04e9c2261872931d894d283ca1396341354c9f1
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-15 10:30:10 +0800

    input_height and input_width

--- CNN/jsPerf/Ref/NeuralNet_Reference.js
+++ CNN/jsPerf/Ref/NeuralNet_Reference.js
@@ -185,7 +185,7 @@ class NeuralNet_Reference_Base extends Recyclable.Root {
       // Test correctness of NeuralNet.apply.
       this.assert_imageOut_Tensors_byNumberArrays( outputTensor3d, this.testCorrectness_imageOutReference, neuralNet );
 
-//!!! (2022/08/02 Temp Remarked) seems hard to be the same.
+//!!! (2022/08/02 Temp Remarked) seems hardly to be the same.
       // Compare result of ShuffleNetV2 and ShuffleNetV2_byMobileNetV1.
       NeuralNet_Reference_Base.neuralNet_compare_ShuffleNetV2_and_ShuffleNetV2_byMobileNetV1.call( this,
         testParams,
