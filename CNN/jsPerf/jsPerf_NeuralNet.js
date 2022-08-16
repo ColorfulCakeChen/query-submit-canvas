03ead041f8f4e5520b12347dec8c79460cd74beb
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-16 13:05:09 +0800

    blockCountRequested

--- CNN/jsPerf/jsPerf_NeuralNet.js
+++ CNN/jsPerf/jsPerf_NeuralNet.js
@@ -141,7 +141,7 @@ class HeightWidthDepth {
     let vocabularyChannelCount = 4;
     let vocabularyCountPerInputChannel = 256;
     let stageCountRequested = 7; //5;
-    let blockCountRequested = 10; //5;
+    let blockCountRequested = 8; //5;
 
     // The neuralNet performance testing should not keep-input-tensor. The reason input
     // image is created from canvas in real time.
