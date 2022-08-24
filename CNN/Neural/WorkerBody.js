b71b9ee55abbb1afbd1063cff8946bebcbfc18ae
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-24 20:41:18 +0800

    Improve comments.

--- CNN/Neural/WorkerBody.js
+++ CNN/Neural/WorkerBody.js
@@ -2,7 +2,7 @@
  * @file This file is the main (i.e. body) javascript file of neural network web worker. It is not an importable module.
  *
  */
-
+ 
 // In module (non-classic) web worker, static import is available. But at the same time, importScripts() will not be avbailable.
 // For solving this problem, using classic (non-module) web worker so that tensorflow.js can be loaded by importScripts().
 // At the same time, using dynamic import() to load ourselves module because import() can be used in classic (non-module) script.
