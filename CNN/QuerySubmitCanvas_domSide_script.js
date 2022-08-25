03bd3d10b78accf2f6e0a8da52c088f3877c89a8
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-25 13:02:12 +0800

    Google Visualization API, Google Tag Manager

--- CNN/QuerySubmitCanvas_domSide_script.js
+++ CNN/QuerySubmitCanvas_domSide_script.js
@@ -2,18 +2,23 @@
  * @file This is a classic javascript file. It should be loaded in a HTML web page
  * by a HTML script tag.
  *
- * 
+ * In module (non-classic) script, static import is available. But at the same
+ * time, importScripts() will not be avbailable. For solving this problem, using
+ * classic (non-module) script so that Google Visualization API, Google Tag Manager,
+ * tensorflow.js can be loaded by importScripts(). At the same time, using dynamic
+ * import() to load ourselves module because import() can be used in classic
+ * (non-module) script.
+ */
+
 
 //!!! ...unfinished... (2022/08/25)
+// Google Visualization API, Google Tag Manager should be load here
+// (before window_onload).
 
- * In module (non-classic) web worker, static import is available. But at the same
- * time, importScripts() will not be avbailable. For solving this problem, using
- * classic (non-module) web worker so that tensorflow.js can be loaded by
- * importScripts(). At the same time, using dynamic import() to load ourselves module
- * because import() can be used in classic (non-module) script.
- */
 
 window.addEventListener( "load", function ( e ) {
   console.log( "QuerySubmitCanvas_domSide_script.js: window_onload..." );
+
+//!!! ...unfinished... (2022/08/25)
 
 } );
--- CNN/tester/QuerySubmitCanvas_tester.js
+++ CNN/tester/QuerySubmitCanvas_tester.js
@@ -1,16 +1,6 @@
 /**
  * @file This is a classic javascript file. It should be loaded in a HTML web page
  * by a HTML script tag.
- *
- * 
-
-//!!! ...unfinished... (2022/08/25)
-
- * In module (non-classic) web worker, static import is available. But at the same
- * time, importScripts() will not be avbailable. For solving this problem, using
- * classic (non-module) web worker so that tensorflow.js can be loaded by
- * importScripts(). At the same time, using dynamic import() to load ourselves module
- * because import() can be used in classic (non-module) script.
  */
 
 window.addEventListener( "load", function ( e ) {
@@ -20,4 +10,6 @@ window.addEventListener( "load", function ( e ) {
   const ctx = canvas.getContext( "2d" );
   ctx.fillStyle = "green";
   ctx.fillRect( 0, 0, canvas.width, canvas.height );
+
+//!!! ...unfinished... (2022/08/25)
 } );
