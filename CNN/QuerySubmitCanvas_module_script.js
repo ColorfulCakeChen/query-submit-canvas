114750c3364ed324f3c81fd316b5acb24fde5e0a
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-25 14:25:13 +0800

    init

--- CNN/QuerySubmitCanvas_module_script.js
+++ CNN/QuerySubmitCanvas_module_script.js
@@ -15,15 +15,32 @@ import * as ScriptLoader from "./util/ScriptLoader.js"
 // Ourselves neural network library should be loaded here by dynamic import() function.
 //
 
+async function init() {
+
+  const googleChartsLoaderUrl = "https://www.gstatic.com/charts/loader.js";
+  let googleChartsLoader = ScriptLoader.createPromise(
+    googleChartsLoaderUrl, false, "googleChartsLoaderJs" );
+
+  await googleChartsLoader;
+  console.log( "QuerySubmitCanvas_module_script.js: googleChartsLoader done..." );
+
+  let googleChartsSafeLoad;
+  {
+    googleChartsSafeLoad = new Promise( ( resolve, reject ) => {
+      google.charts.safeLoad( "current", {
+        //packages: [ "corechart" ],
+        callback: () => resolve()
+      } );
+    } );
+  }
+
+  await googleChartsSafeLoad;
+  console.log( "QuerySubmitCanvas_module_script.js: google.charts.load() done..." );
 
 //!!! ...unfinished... (2022/08/25)
 // should wait for Google Visualization API, Google Tag Manager and our modules.
 // Perhaps, not wait for window loaded.
 
-window.addEventListener( "load", function ( e ) {
-  console.log( "QuerySubmitCanvas_module_script.js: window_onload..." );
+}
 
-//!!! ...unfinished... (2022/08/25)
-
-} );
-
+init();
