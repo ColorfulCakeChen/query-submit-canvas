4ec74b9a4d4db429701fd2e3b401b26f792c6a0e
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-25 10:00:40 +0800

    canvas fill

--- CNN/tester/QuerySubmitCanvas_tester.js
+++ CNN/tester/QuerySubmitCanvas_tester.js
@@ -13,3 +13,9 @@
  * because import() can be used in classic (non-module) script.
  */
 
+{
+  const canvas = document.querySelector('canvas');
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'green';
+  ctx.fillRect(10, 10, 100, 100);
+}
