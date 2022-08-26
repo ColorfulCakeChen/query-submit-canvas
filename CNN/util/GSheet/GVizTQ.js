6d088ad405b881750bbf2045bb08e32043404445
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-26 10:52:35 +0800

    Improve comments

--- CNN/util/GSheet/GVizTQ.js
+++ CNN/util/GSheet/GVizTQ.js
@@ -3,8 +3,9 @@ export { UrlComposer };
 import * as ValueMax from "../../ValueMax.js";
 
 /**
- * Compose a URL for downloading cells data (as JSON or .CSV format) from a Google Sheets by using Google Visualzation Table Query
- * API. The target spreadsheet should be shared by either "Public on the web" or "Anyone with the link".
+ * Compose a URL for downloading cells data (as JSON or .CSV format) from a Google
+ * Sheets by using Google Visualzation Table Query API. The target spreadsheet should
+ * be shared by either "Public on the web" or "Anyone with the link".
  *
  * (GVizTQ = Google Visualzation Table Query)
  *
@@ -38,40 +39,49 @@ import * as ValueMax from "../../ValueMax.js";
 class UrlComposer {
 
   /**
-   * If sheetId is null, sheetName is null, and no sheet name in the range's A1 notation, the first (most left) visible sheet
-   * inside the spreadsheet will be used.
+   * If sheetId is null, sheetName is null, and no sheet name in the range's A1
+   * notation, the first (most left) visible sheet inside the spreadsheet will be used.
    *
    * @param {string} spreadsheetId
-   *   The identifier (the component after the "https://docs.google.com/spreadsheets/d/") of the spreadsheet to be accessed.
+   *   The identifier (the component after the "https://docs.google.com/spreadsheets/d/")
+   * of the spreadsheet to be accessed.
    *
    * @param {string} range
-   *   The cells' A1 notation of the cells. It describes the (name and) range of the sheet inside the spreadsheet.
+   *   The cells' A1 notation. It describes the (name and) range of the sheet inside
+   * the spreadsheet.
    *   - "A1" refers to one cell of the first (most left) visible sheet.
    *   - "B2:C5" refers to cells of a rectangle of the first (most left) visible sheet.
-   *   - "Books!D8:D" refers to the column D of sheet named "Books" from rows 8 to the last rows.
-   *   - "'Name has space'!7:10" refers to the rows 7 to 10 of sheet named "Name has space".
+   *   - "Books!D8:D" refers to the column D of sheet named "Books" from rows 8 to the
+   *       last rows.
+   *   - "'Name has space'!7:10" refers to the rows 7 to 10 of sheet named "Name has
+   *       space".
    *
    * @param {number} headers
-   *   The component after the "headers=". It means how many header rows. It should be zero or a positive integer.
+   *   The component after the "headers=". It means how many header rows. It should be
+   * zero or a positive integer.
    *
    * @param {string} responseHandler
-   *   The function name of JSON content handler. Only meaningful when the content is downloaded as JSONP format. This
-   * responseHandler name will be prepended in front of the downloaded content.
+   *   The function name of JSON content handler. Only meaningful when the content is
+   * downloaded as JSONP format. This responseHandler name will be prepended in front
+   * of the downloaded content.
    *
    * @param {number} sheetId
-   *   The component after the "gid=". If the sheetName is used (or the sheet name in the range's A1 notation is specified),
-   * keep this sheetId null (or undefined).
+   *   The component after the "gid=". If the sheetName is used (or the sheet name in
+   * the range's A1 notation is specified), keep this sheetId null (or undefined).
    *
    * @param {string} sheetName
-   *   The component after the "sheet=". If the sheetId is used (or the sheet name in in the range's A1 notation is specified),
-   * keep this sheetName null (or undefined).
+   *   The component after the "sheet=". If the sheetId is used (or the sheet name in
+   * the range's A1 notation is specified), keep this sheetName null (or undefined).
    *
    * @see {@link https://developers.google.com/sheets/api/guides/concepts}
    * @see {@link https://developers.google.com/chart/interactive/docs/dev/implementing_data_source}
    * @see {@link https://developers.google.com/chart/interactive/docs/spreadsheets}
    * @see {@link https://developers.google.com/chart/interactive/docs/querylanguage}
    */
-  constructor( spreadsheetId, range, headers = 0, responseHandler = null, sheetId = null, sheetName = null ) {
+  constructor(
+    spreadsheetId,
+    range, headers = 0, responseHandler = null, sheetId = null, sheetName = null ) {
+
     this.spreadsheetId = spreadsheetId;
     this.range = range;
     this.headers = headers;
@@ -81,19 +91,21 @@ class UrlComposer {
   }
 
   /**
-   * Generator for composing the URL (according this object's data members), downloading it as JSON format, extracting
-   * data as a two dimension (column-major) array.
+   * Generator for composing the URL (according this object's data members), downloading
+   * it as JSON format, extracting data as a two dimension (column-major) array.
    *
    * @param {ValueMax.Percentage.Aggregate} progressParent
-   *   Some new progressToAdvance will be created and added to progressParent. The created progressToAdvance will be
-   * increased when every time advanced. The progressParent.root_get() will be returned when every time yield.
+   *   Some new progressToAdvance will be created and added to progressParent. The
+   * created progressToAdvance will be increased when every time advanced. The
+   * progressParent.root_get() will be returned when every time yield.
    *
    * @yield {ValueMax.Percentage.Aggregate}
    *   Yield ( value = progressParent.root_get() ) when ( done = false ).
    *
    * @yield {array[]}
-   *   Yield ( value = a two dimension (column-major) array ) when ( done = true ) successfully.
-   *   Yield ( value = null ) when ( done = true ) failed.
+   *   - Yield ( value = a two dimension (column-major) array ) when ( done = true )
+   *       successfully.
+   *   - Yield ( value = null ) when ( done = true ) failed.
    */
   async* fetcher_JSON_ColumnMajorArray( progressParent ) {
     let progressRoot = progressParent.root_get();
