070c82cfeb58ab43872a015f5de972fddf2d19ff
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-26 11:06:13 +0800

    Improve comments

--- CNN/util/GSheet/GSheetsAPIv4.js
+++ CNN/util/GSheet/GSheetsAPIv4.js
@@ -3,8 +3,9 @@ export { UrlComposer };
 import * as ValueMax from "../../ValueMax.js";
 
 /**
- * Compose a URL for downloading cells data (as JSON) from a Google Sheets by using Google Sheets API v4.
- * The target spreadsheet should be shared by either "Public on the web" or "Anyone with the link".
+ * Compose a URL for downloading cells data (as JSON) from a Google Sheets by using
+ * Google Sheets API v4. The target spreadsheet should be shared by either "Public
+ * on the web" or "Anyone with the link".
  *
  *
  * The follwoings are some composed examples:
@@ -18,17 +19,22 @@ import * as ValueMax from "../../ValueMax.js";
 class UrlComposer {
 
   /**
-   * If no sheet name in the range's A1 notation, the first (most left) visible sheet inside the spreadsheet will be used.
+   * If no sheet name in the range's A1 notation, the first (most left) visible sheet
+   * inside the spreadsheet will be used.
    *
    * @param {string} spreadsheetId
-   *   The identifier (the component after the "https://docs.google.com/spreadsheets/d/") of the spreadsheet to be accessed.
+   *   The identifier (the component after the "https://docs.google.com/spreadsheets/d/")
+   * of the spreadsheet to be accessed.
    *
    * @param {string} range
-   *   The cells' A1 notation of the cells. It describes the (name and) range of the sheet inside the spreadsheet.
+   *   The cells' A1 notation of the cells. It describes the (name and) range of the
+   * sheet inside the spreadsheet.
    *   - "A1" refers to one cell of the first (most left) visible sheet.
    *   - "B2:C5" refers to cells of a rectangle of the first (most left) visible sheet.
-   *   - "Books!D8:D" refers to the column D of sheet named "Books" from rows 8 to the last rows.
-   *   - "'Name has space'!7:10" refers to the rows 7 to 10 of sheet named "Name has space".
+   *   - "Books!D8:D" refers to the column D of sheet named "Books" from rows 8 to the
+   *       last rows.
+   *   - "'Name has space'!7:10" refers to the rows 7 to 10 of sheet named "Name has
+   *       space".
    *
    * @param {string} apiKey
    *   The API key string for accessing the spreadsheet.
@@ -44,19 +50,21 @@ class UrlComposer {
   }
 
   /**
-   * Generator for composing the URL (according this object's data members), downloading it as JSON format, extracting
-   * data as a two dimension (column-major) array.
+   * Generator for composing the URL (according this object's data members), downloading
+   * it as JSON format, extracting data as a two dimension (column-major) array.
    *
    * @param {ValueMax.Percentage.Aggregate} progressParent
-   *   Some new progressToAdvance will be created and added to progressParent. The created progressToAdvance will be
-   * increased when every time advanced. The progressParent.root_get() will be returned when every time yield.
+   *   Some new progressToAdvance will be created and added to progressParent. The
+   * created progressToAdvance will be increased when every time advanced. The
+   * progressParent.root_get() will be returned when every time yield.
    *
    * @yield {ValueMax.Percentage.Aggregate}
    *   Yield ( value = progressParent.root_get() ) when ( done = false ).
    *
    * @yield {array[]}
-   *   Yield ( value = a two dimension (column-major) array ) when ( done = true ) successfully.
-   *   Yield ( value = null ) when ( done = true ) failed.
+   *   - Yield ( value = a two dimension (column-major) array ) when ( done = true )
+   *       successfully.
+   *   - Yield ( value = null ) when ( done = true ) failed.
    */
   async* fetcher_JSON_ColumnMajorArray( progressParent ) {
     let progressRoot = progressParent.root_get();
@@ -98,14 +106,18 @@ class UrlComposer {
 
   /**
    * @param {string} majorDimension
-   *   The major dimension of the arrangement for the downloaded json data. It could be "ROWS" or "COLUMNS". If null, the same as "ROWS".
+   *   The major dimension of the arrangement for the downloaded json data. It could
+   * be "ROWS" or "COLUMNS". If null, the same as "ROWS".
    *
    * @return {string} The url for downloading the target as json format.
    */
   getUrl_forJSON( majorDimension = null ) {
-    let url = `${UrlComposer.spreadsheetUrlPrefix}/${encodeURIComponent(this.spreadsheetId)}/values/${
+    let url = `${UrlComposer.spreadsheetUrlPrefix}/${
+      encodeURIComponent(this.spreadsheetId)}/values/${
       encodeURIComponent(this.range)}?${
-      ( majorDimension != null ) ? `majorDimension=${encodeURIComponent(majorDimension)}&` : "" }key=${encodeURIComponent(this.apiKey)}`
+      ( majorDimension != null ) ? `majorDimension=${
+      encodeURIComponent(majorDimension)}&` : "" }key=${
+      encodeURIComponent(this.apiKey)}`
       ;
 
     return url;
