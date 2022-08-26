6f8a8ed3f17909c78814732c13130e254bb5942e
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-26 12:50:26 +0800

    Improve comments.

--- CNN/util/GSheet.js
+++ CNN/util/GSheet.js
@@ -9,26 +9,31 @@ import * as GSheetsAPIv4 from "./GSheet/GSheetsAPIv4.js";
 class UrlComposer {
 
   /**
-   * If no sheet name in the range's A1 notation, the first (most left) visible sheet inside the spreadsheet will be used.
+   * If no sheet name in the range's A1 notation, the first (most left) visible sheet
+   * inside the spreadsheet will be used.
    *
    * @param {string} spreadsheetId
-   *   The identifier (the component after the "https://docs.google.com/spreadsheets/d/") of the spreadsheet to be accessed.
+   *   The identifier (the component after the "https://docs.google.com/spreadsheets/d/")
+   * of the spreadsheet to be accessed.
    *
    * @param {string} range
-   *   The cells' A1 notation of the cells. It describes the (name and) range of the sheet inside the spreadsheet.
+   *   The cells' A1 notation. It describes the (name and) range of the sheet inside
+   * the spreadsheet.
    *   - "A1" refers to one cell of the first (most left) visible sheet.
    *   - "B2:C5" refers to cells of a rectangle of the first (most left) visible sheet.
-   *   - "Books!D8:D" refers to the column D of sheet named "Books" from rows 8 to the last rows.
-   *   - "'Name has space'!7:10" refers to the rows 7 to 10 of sheet named "Name has space".
+   *   - "Books!D8:D" refers to the column D of sheet named "Books" from rows 8 to the
+   *       last rows.
+   *   - "'Name has space'!7:10" refers to the rows 7 to 10 of sheet named "Name has
+   *       space".
    *
    * @param {string} apiKey
-   *   The API key string for accessing the spreadsheet. If not null, Google Sheets API v4 will be used. If null, Google
-   * Visualization Table Query API will be used.
+   *   The API key string for accessing the spreadsheet. If not null, Google Sheets API
+   * v4 will be used. If null, Google Visualization Table Query API will be used.
    *
    * @see {@link https://developers.google.com/sheets/api/guides/concepts}
    */
   constructor( spreadsheetId, range, apiKey = null ) {
-    if ( apiKey ) {
+    if ( apiKey != null ) {
       this.urlComposer = new GSheetsAPIv4.UrlComposer( spreadsheetId, range, apiKey );
     } else {
       this.urlComposer = new GVizTQ.UrlComposer( spreadsheetId, range );
@@ -36,19 +41,21 @@ class UrlComposer {
   }
 
   /**
-   * Generator for composing the URL (according this object's data members), downloading it as JSON format, extracting
-   * data as a two dimension (column-major) array.
+   * Generator for composing the URL (according this object's data members), downloading
+   * it as JSON format, extracting data as a two dimension (column-major) array.
    *
    * @param {ValueMax.Percentage.Aggregate} progressParent
-   *   Some new progressToAdvance will be created and added to progressParent. The created progressToAdvance will be
-   * increased when every time advanced. The progressParent.root_get() will be returned when every time yield.
+   *   Some new progressToAdvance will be created and added to progressParent. The
+   * created progressToAdvance will be increased when every time advanced. The
+   * progressParent.root_get() will be returned when every time yield.
    *
    * @yield {ValueMax.Percentage.Aggregate}
    *   Yield ( value = progressParent.root_get() ) when ( done = false ).
    *
    * @yield {array[]}
-   *   Yield ( value = a two dimension (column-major) array ) when ( done = true ) successfully.
-   *   Yield ( value = null ) when ( done = true ) failed.
+   *   - Yield ( value = a two dimension (column-major) array ) when ( done = true )
+   *       successfully.
+   *   - Yield ( value = null ) when ( done = true ) failed.
    */
   async* fetcher_JSON_ColumnMajorArray( progressParent ) {
     let fetcher = this.urlComposer.fetcher_JSON_ColumnMajorArray( progressParent );
