61398ad3545e9f44435b3e867442116cd74616e6
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-08-26 20:02:29 +0800

    Improve comments

--- CNN/NeuralWorker/DifferentialEvolution/old/Versus_Weights.js
+++ CNN/NeuralWorker/DifferentialEvolution/old/Versus_Weights.js
@@ -35,20 +35,25 @@ class EnitiyChromosomes {
   }
 
   /**
-   * Join the string array, convert to Uint8Array, decode as Base64, result in another Uint8Array.
+   * Join the string array, convert to Uint8Array, decode as Base64, result in
+   * another Uint8Array.
    *
    * @param {string[]} base64EncodedStringArray
-   *   Every element of base64EncodedStringArray is a string whose content is base64 encoded text.
+   *   Every element of base64EncodedStringArray is a string whose content is
+   * base64 encoded text.
    *
    * @param {TextEncoder} textEncoder
-   *   This TextEncoder will convert string to Uint8Array so that the Base64 decoder can work.
+   *   This TextEncoder will convert string to Uint8Array so that the Base64
+   * decoder can work.
    *
    * @param {ValueMax.Percentage.Aggregate} progressRoot
-   *   Return this when every time yield. Usually, this is the root container of the progressParent.
+   *   Return this when every time yield. Usually, this is the root container of
+   * the progressParent.
    *
    * @param {ValueMax.Percentage.Aggregate} progressParent
-   *   This should be progressRoot or some descendant of progressRoot. A new progressToAdvance will be created and added to
-   * progressParent. The progressToAdvance will be increased when every time advanced.
+   *   This should be progressRoot or some descendant of progressRoot. A new
+   * progressToAdvance will be created and added to progressParent. The
+   * progressToAdvance will be increased when every time advanced.
    *
    * @yields {ValueMax.Percentage.Aggregate}
    *   Yield ( value = progressToYield ) when ( done = false ).
@@ -56,7 +61,8 @@ class EnitiyChromosomes {
    * @yields {Uint8Array}
    *   Yield ( value = decoded data as Uint8Array ) when ( done = true ).
    */
-  static *decoder_Base64_StringArray_To_Uint8Array( base64EncodedStringArray, textEncoder, progressRoot, progressParent ) {
+  static *decoder_Base64_StringArray_To_Uint8Array(
+    base64EncodedStringArray, textEncoder, progressRoot, progressParent ) {
 
     // Initialize progress.
     let progressToAdvance = new ValueMax.Percentage.Concrete( ??? );
@@ -70,7 +76,8 @@ class EnitiyChromosomes {
     let suspendByteCount = 1024;
 
     let base64Decoder = Base64ToUint8Array.decoder_FromStringArray(
-      base64EncodedStringArray, skipLineCount, progressRoot, progressParent, suspendByteCount );
+      base64EncodedStringArray, skipLineCount, progressRoot, progressParent,
+      suspendByteCount );
 
     let base64DecodedUint8Array = yield *base64Decoder;
     return base64DecodedUint8Array;
@@ -116,7 +123,8 @@ class EnitiyChromosomes {
 
 
 /**
- * Download differential evolution individual weights (entity vs entity) from network (a publish html of Google Sheets).
+ * Download differential evolution individual weights (entity vs entity) from
+ * network (a publish html of Google Sheets).
  *
  * The format of differential evolution individual weights page is the following:
  *
@@ -153,10 +161,13 @@ class EnitiyChromosomes {
  * </code>
  *
  *   - The gid is the id (which is used in the URL) of the Google Sheet.
- *   - The ExportSheetNo is the array index of the Google Sheet. This is mainly used by server. Client (downloader) can ignore this value.
+ *   - The ExportSheetNo is the array index of the Google Sheet. This is mainly used
+ *       by server. Client (downloader) can ignore this value.
  *   - The 3rd line list all versus published in this web page.
- *     - A versus contains versus id (EntityNo_ParentGenerationNo_OffspringGenerationNo) and its WinCount which are separated by colon (:).
- *     - A versus id contains EntityNo, ParentGenerationNo, OffspringGenerationNo which are separated by underline (_).
+ *     - A versus contains versus id (EntityNo_ParentGenerationNo_OffspringGenerationNo)
+ *         and its WinCount which are separated by colon (:).
+ *     - A versus id contains EntityNo, ParentGenerationNo, OffspringGenerationNo
+ *         which are separated by underline (_).
  *   - List all chromosomes of the parent of every entity.
  *   - List all chromosomes of the offspring of every entity.
  *
@@ -164,7 +175,8 @@ class EnitiyChromosomes {
  *   There are how many chromsomes in the parent (or offspring) of one entity.
  *
  * @member {TextEncoder} textEncoder
- *   This TextEncoder will convert string to Uint8Array so that the Base64 decoder can work.
+ *   This TextEncoder will convert string to Uint8Array so that the Base64 decoder
+ * can work.
  *
  * @member {gid_Versus.Base} gid_Versus
  *   The gid and related versus id (with win count).
@@ -191,29 +203,35 @@ class Base {
    * Fetch and extract.
    *
    * @param {string} versusURL
-   *   The published (should be html, not tsv, not csv) web page URL of the versus sheet of the differential evolution Google Sheets. For example:
+   *   The published (should be html, not tsv, not csv) web page URL of the versus
+   * sheet of the differential evolution Google Sheets. For example:
    * "https://docs.google.com/spreadsheets/d/e/2PACX-1vSzA4SXUR5VyPmJ1cLEkNMiJgfb28zzgp4HtwoBlumIIOTkL_y7mgPldqaGtsunIq5eTu5QndluyFcV/pubhtml?gid=474257400&single=true".
-   * The "gid=474257400" inside the URL should be the same as the 1st line of the table of the web page.
+   * The "gid=474257400" inside the URL should be the same as the 1st line of the
+   * table of the web page.
    *
    * @param {ValueMax.Percentage.Aggregate} progressToYield
-   *   Return this when every time yield. Usually, this is the root container of the progressToAdvance.
+   *   Return this when every time yield. Usually, this is the root container of the
+   * progressToAdvance.
    *
    * @param {ValueMax.Percentage.Concrete} progressToAdvance
-   *   Increase this when every time progress advanced. It will be initialized to zero when decoder starting.
+   *   Increase this when every time progress advanced. It will be initialized to
+   * zero when decoder starting.
    *
    * @return {async generator}
    *   Return am asynchronous 
    */
   async *fetcher( versusURL, progressToYield, progressToAdvance ) {
 
-    // Why using published html web page instead of tsv (or csv)? This is because Google Sheets' published html web page support cross-origin
-    // resource sharing (CORS) while its published web page tsv (or csv) does not.
+    // Why using published html web page instead of tsv (or csv)? This is because
+    // Google Sheets' published html web page support cross-origin resource sharing
+    // (CORS) while its published web page tsv (or csv) does not.
     this.versusURL = versusURL;
 
     let response = await fetch( versusURL );
     let responseText = await response.text();
 
-    let lineMatches = tdTextExtracter.Base.createIterator( responseText ); // Only capture group 1 will be used.
+    // Only capture group 1 will be used.
+    let lineMatches = tdTextExtracter.Base.createIterator( responseText );
     let lineMatch;
 
     if ( ( lineMatch = lineMatches.next() ).done )
@@ -233,41 +251,59 @@ class Base {
 
     // 3. Parse corresponding versus ids (with win counts).
     this.gid_Versus = new gid_Versus.Base();
-    this.gid_versus.set_ByString( lineMatch.value[ 1 ], gidString ); // Split the text of a td tag.
 
-    // 4. Parse every gid and their corresponding weights (of chromosomes of parent and offspring).
-    this.enitiyChromosomesArray = new Array( this.gid_versus.VersusId_WinCount_Array.length );
-    
-    let chromosomeArray = new Array( this.entityChromosomeCount );  // All chromosomes of one entity's parent (or offspring).
+    // Split the text of a td tag.
+    this.gid_versus.set_ByString( lineMatch.value[ 1 ], gidString );
+
+    // 4. Parse every gid and their corresponding weights (of chromosomes of parent
+    // and offspring).
+    this.enitiyChromosomesArray
+      = new Array( this.gid_versus.VersusId_WinCount_Array.length );
+
+    // All chromosomes of one entity's parent (or offspring).
+    let chromosomeArray = new Array( this.entityChromosomeCount );
 
     // All parent's chromosomes of all entities.
-    for ( let i = 0; ( i < this.enitiyChromosomesArray.length ) && ( !lineMatch.done ); ++i, ( lineMatch = lineMatch.next() ) ) {
+    for ( let i = 0;
+          ( i < this.enitiyChromosomesArray.length ) && ( !lineMatch.done );
+          ++i, ( lineMatch = lineMatch.next() ) ) {
       let entityNo = this.gid_versus.VersusId_WinCount_Array[ i ].entityNo;
-      let enitiyChromosomes = this.enitiyChromosomesArray[ i ] = new EnitiyChromosomes( entityNo, this.entityChromosomeCount );
+      let enitiyChromosomes = this.enitiyChromosomesArray[ i ]
+        = new EnitiyChromosomes( entityNo, this.entityChromosomeCount );
 
       // The parent's chromosomes of the entity.
       {
         let k;
-        for ( k = 0; ( k < this.entityChromosomeCount ) && ( !lineMatch.done ); ++k, ( lineMatch = lineMatch.next() ) ) {
+        for ( k = 0;
+              ( k < this.entityChromosomeCount ) && ( !lineMatch.done );
+              ++k, ( lineMatch = lineMatch.next() ) ) {
           chromosomeArray[ k ] = lineMatch.value[ 1 ];
         }
         chromosomeArray.fill( null, k ); // For not enough chromosome, keep them null.
-        enitiyChromosomes.parentChromosome = EnitiyChromosomes.decode_Base64_StringArray_To_Uint8Array( chromosomeArray, this.textEncoder );
+        enitiyChromosomes.parentChromosome
+          = EnitiyChromosomes.decode_Base64_StringArray_To_Uint8Array(
+              chromosomeArray, this.textEncoder );
       }
     }
 
     // All offspring's chromosomes of all entities.
-    for ( let i = 0; ( i < this.enitiyChromosomesArray.length ) && ( !lineMatch.done ); ++i, ( lineMatch = lineMatch.next() ) ) {
+    for ( let i = 0;
+          ( i < this.enitiyChromosomesArray.length ) && ( !lineMatch.done );
+          ++i, ( lineMatch = lineMatch.next() ) ) {
       let enitiyChromosomes = this.enitiyChromosomesArray[ i ];
 
       // The offspring's chromosomes of the entity.
       {
         let k;
-        for ( k = 0; ( k < this.entityChromosomeCount ) && ( !lineMatch.done ); ++k, ( lineMatch = lineMatch.next() ) ) {
+        for ( k = 0;
+              ( k < this.entityChromosomeCount ) && ( !lineMatch.done );
+              ++k, ( lineMatch = lineMatch.next() ) ) {
           chromosomeArray[ k ] = lineMatch.value[ 1 ];
         }
         chromosomeArray.fill( null, k ); // For not enough chromosome, keep them null.
-        enitiyChromosomes.offspringChromosome = EnitiyChromosomes.decode_Base64_StringArray_To_Uint8Array( chromosomeArray, this.textEncoder );
+        enitiyChromosomes.offspringChromosome
+          = EnitiyChromosomes.decode_Base64_StringArray_To_Uint8Array(
+              chromosomeArray, this.textEncoder );
       }
     }
 
