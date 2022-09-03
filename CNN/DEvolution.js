f595aaeb4d92b8277bd09549d0fa52778d7b5c6e
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-09-03 21:27:31 +0800

    Devolution_Versus_Sumitter

--- CNN/DEvolution.js
+++ CNN/DEvolution.js
@@ -1,3 +1,5 @@
 export { VersusSummary } from "./DEvolution/DEvolution_VersusSummary.js";
 export { VersusId } from "./DEvolution/DEvolution_VersusId.js";
 export { Versus } from "./DEvolution/DEvolution_Versus.js";
+
+export { Versus_Sumitter } from "./DEvolution/Devolution_Versus_Sumitter.js";
---
+++ CNN/DEvolution/Devolution_Versus_Sumitter.js
@@ -0,0 +1,15 @@
+export { Devolution_Versus_Sumitter as Versus_Sumitter };
+
+//!!! ...unfinished... (2022/09/03)
+// For measurement protocol.
+// Perhaps,
+//   - trancactionId = `t${versusId}`
+//   - itemId = versusId
+//
+
+/**
+ * 
+ */
+class Devolution_Versus_Sumitter {
+
+}
