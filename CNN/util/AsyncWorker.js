467d4039630f815621ba952d3a44184f1258eb1e
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-09-10 20:10:33 +0800

    AsyncWorker

---
+++ CNN/util/AsyncWorker.js
@@ -0,0 +1,5 @@
+export { PromiseResolveReject } from "./AsyncWorker/AsyncWorker_PromiseResolveReject.js";
+export { PromiseResolveRejectMap } from "./AsyncWorker/AsyncWorker_PromiseResolveReject.js";
+export { Body } from "./AsyncWorker/AsyncWorker_Body.js";
+export { Proxy } from "./AsyncWorker/AsyncWorker_Proxy.js";
+
---
+++ CNN/util/AsyncWorker/AsyncWorker_PromiseResolveReject.js
@@ -0,0 +1,109 @@
+export { PromiseResolveReject };
+export { PromiseResolveRejectMap };
+
+//!!! (2022/09/10 Remarked) replaced by AsyncIterator.
+//export { ProcessRelayPromises };
+
+/**
+ * Hold a processing's id, promise, resolve (fulfilling function object), reject
+ * (rejecting function object).
+ *
+ * @member {number}   processingId The id of the processing.
+ * @member {Promise}  promise      The pending promise for the processing.
+ *
+ * @member {function} resolve
+ *   The fulfilling function object of the pending promise for the processing.
+ *
+ * @member {function} reject
+ *   The rejecting function object of the pending promise for the processing.
+ */
+ class PromiseResolveReject {
+
+  constructor( processingId ) {
+    this.processingId = processingId;
+
+    this.promise = new Promise( ( resolve, reject ) => {
+      this.resolve = resolve;
+      this.reject = reject;
+    });
+  }
+
+}
+
+//!!! ...unfinished... (2022/09/10)
+/**
+ * A collection PromiseResolveReject by processingid as key.
+ */
+class PromiseResolveRejectMap {
+
+  /** */
+  constructor() {
+    this.map = new Map();
+  }
+
+  /**
+   * @param {number} processingId
+   *   The numeric identifier for the processing.
+   *
+   * @return {PromiseResolveReject}
+   *   Return the created PromiseResolveReject object.
+   */
+  set_new_by_processingId( processingId ) {
+    let thePromiseResolveReject = new PromiseResolveReject( processingId );
+    this.map.set( processingId, thePromiseResolveReject );
+    return thePromiseResolveReject;
+  }
+
+  /**
+   * @param {number} processingId
+   *   The numeric identifier for the removing from this Map.
+   *
+   * @return {boolean}
+   *   Return true, if processingId existed and has been removed. Return false,
+   * if processingId does not exist.
+   */
+  remove_by_processingId( processingId ) {
+    return this.map.delete( processingId );
+  }
+
+  /**
+   * @return {AsyncGenerator}
+   *   An async generator:
+   *
+   *   - Always yield the corresponding PromiseResolveReject.promise of the processingId,
+   *      if it exists. (i.e. { done: false, value: PromiseResolveReject.promise } )
+   *
+   *   - Return, if the corresponding PromiseResolveReject does not exist.
+   *       (i.e. { done: true, value: undefined } )
+   *
+   */
+  async *resulter( processingId ) {
+    let thePromiseResolveReject = this.map.get( processingId );
+    if ( thePromiseResolveReject )
+      yield thePromiseResolveReject;
+    else
+      return;
+  }
+
+}
+
+
+//!!! (2022/09/10 Remarked) replaced by AsyncIterator.
+// /**
+//  * Hold two PromiseResolveReject.
+//  *
+//  * @member {PromiseResolveReject} process
+//  *   The promise for reporting worker's function execution done.
+//  *
+//  * @member {PromiseResolveReject} relay
+//  *   The promise for reporting scaledSourceImageData received from this web worker.
+//  */
+// class ProcessRelayPromises {
+//
+//   constructor( processingId, workerId ) {
+//     this.process = new PromiseResolveReject( processingId, workerId );
+//     this.relay = new PromiseResolveReject( processingId, workerId );
+//   }
+//
+// }
+
