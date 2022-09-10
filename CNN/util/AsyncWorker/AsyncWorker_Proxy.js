98c37400833e8f4a3272f64889cbcb6e4a5c54d6
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-09-10 21:11:11 +0800

    onmessage_from_AsyncWorker_Body

--- CNN/util/AsyncWorker/AsyncWorker_Proxy.js
+++ CNN/util/AsyncWorker/AsyncWorker_Proxy.js
@@ -82,46 +82,12 @@ class AsyncWorker_Proxy extends Recyclable.Root {
 //     if ( workerId != this.workerId )
 //       return; // Ignore if wrong worker id.
 
-    // e.data == { processingId, done_value }
+    // ( e.data == { processingId, done_value } )
     let processingId = e.data.processingId;
-    let done_value = e.data.done_value;
+    let { done, value } = e.data.done_value;
 
-
-//!!! ...unfinished... (2022/09/10)
-// Every WorkerProxy method function should be an async generator.
-//
-// Here should receive { done, value } object from WorkerBody.
-// The corresponding PromiseResolveReject.resolve() to the value.
-//
-//   - if ( done == false ), create a new PromiseResolveReject placed at the
-//       smae position (i.e. replace old one) for waiting future result.
-//
-//   - if ( done == true ), delete the entry of processingId from PromiseResolveRejectMap.
-//       because there will be no more result coming in the future.
-// 
-//
-
-    // Discard result with non-existed processing id. (e.g. already handled old
-    // processing result)
-    let thePromiseResolveReject = this.thePromiseResolveRejectMap.get( processingId );
-    if ( !thePromiseResolveReject )
-      return;
-
-    thePromiseResolveReject.process.resolve( result );
-
-//!!! ...unfinished... When will fail?
-    //processRelayPromises.reject();
-
-//!!! ...unfinished... Whether should the older (i.e. smaller) processingId be cleared from map? (Could the processing be out of order?)
-
-//!!! ...unfinished... (2022/09/09)
-// What about processRelayPromises.relay?
-// When to resolve it (before the promise be deleted)?
-
-    // Clear the info entry of handled processing result.
-    this.thePromiseResolveRejectMap.delete( processingId );
-
-
+    this.thePromiseResolveRejectMap.resolve_by_processingId_done_value(
+      processingId, done, value );
   }
 
 }
