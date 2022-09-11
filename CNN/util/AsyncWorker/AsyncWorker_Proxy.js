bf1ff488c96344f9faa43ff400a5681917b3b386
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-09-11 10:37:42 +0800

    Improve comments.

--- CNN/util/AsyncWorker/AsyncWorker_Proxy.js
+++ CNN/util/AsyncWorker/AsyncWorker_Proxy.js
@@ -41,6 +41,8 @@ class AsyncWorker_Proxy extends Recyclable.Root {
 //!!! ...unfinished... (2022/09/11)
 // should also mange processingId generating, worker creating,
 // onmessage registering.
+// provide postCommand() with/without processingId
+//
 
   /** @override */
   static setAsConstructor_self() {
