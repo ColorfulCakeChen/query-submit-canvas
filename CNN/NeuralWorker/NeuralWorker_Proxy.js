eeece6833d2ec9b9b6cbea2f7288c89d8ff2456c
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-09-12 15:32:13 +0800

    comments

--- CNN/NeuralWorker/NeuralWorker_Proxy.js
+++ CNN/NeuralWorker/NeuralWorker_Proxy.js
@@ -142,7 +142,7 @@ class NeuralWorker_Proxy extends AsyncWorker.Proxy {
     return resulter;
   }
 
-
+//!!! ...unfinished... (2022/09/12)
   /**
    * @param {number} processingId
    *   The processing id for distinguishing different processing request and result.
