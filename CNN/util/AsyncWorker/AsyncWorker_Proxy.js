fa005afa7735d025869f4b431cf73305075cfe5c
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-09-12 14:53:43 +0800

    postCommand_with_processingId

--- CNN/util/AsyncWorker/AsyncWorker_Proxy.js
+++ CNN/util/AsyncWorker/AsyncWorker_Proxy.js
@@ -3,7 +3,7 @@ export { AsyncWorker_Proxy as Proxy };
 import * as Pool from "../Pool.js";
 import * as Recyclable from "../Recyclable.js";
 import { PromiseResolveReject } from "./AsyncWorker_PromiseResolveReject.js";
-import { PromiseResolveRejectMap } from "./AsyncWorker_PromiseResolveReject.js";
+import { processingId_Resulter_Map } from "./AsyncWorker_PromiseResolveReject.js";
 
 /**
  * Hold the worker and its related promise map. It is a wrapper of a neural network
@@ -12,13 +12,13 @@ import { PromiseResolveRejectMap } from "./AsyncWorker_PromiseResolveReject.js";
  * @member {number} workerId  The array index of this worker proxy.
  * @member {Worker} worker    The worker.
  *
- * @member {number} processingId_current
- *   The current processing id. Negative means no command has been sent. Every
- * postCommand_by_processingId() call will use a new id.
+ * @member {number} processingId_next
+ *   The next processing id. Zero means no command has been sent. Every
+ * postCommand_with_processingId() call will use a new id.
  *
- * @member {PromiseResolveRejectMap} thePromiseResolveRejectMap
+ * @member {processingId_Resulter_Map} the_processingId_Resulter_Map
  *   Every worker has a result pending promise map. The key of the map is processing
- * id. The value of the map is a PromiseResolveReject.
+ * id. The value of the map is a PromiseResolveReject_Resulter.
  *
  */
 class AsyncWorker_Proxy extends Recyclable.Root {
@@ -55,19 +55,19 @@ class AsyncWorker_Proxy extends Recyclable.Root {
     // A: Because Number.MAX_SAFE_INTEGER is pretty large (at least, 2 ** 52 ),
     //    it is not so easy to become out of bounds.
     //
-    this.processingId_current = -1;
+    this.processingId_next = 0;
 
-    if ( this.thePromiseResolveRejectMap )
-      this.thePromiseResolveRejectMap.clear();
+    if ( this.the_processingId_Resulter_Map )
+      this.the_processingId_Resulter_Map.clear();
     else
-      this.thePromiseResolveRejectMap = new PromiseResolveRejectMap();
+      this.the_processingId_Resulter_Map = new processingId_Resulter_Map();
   }
 
   /** @override */
   disposeResources() {
-    this.thePromiseResolveRejectMap.clear();
+    this.the_processingId_Resulter_Map.clear();
 
-    this.processingId_current = undefined;
+    this.processingId_next = undefined;
 
     super.disposeResources();
   }
@@ -103,6 +103,33 @@ class AsyncWorker_Proxy extends Recyclable.Root {
   }
 
   /**
+   * Pack a message { processingId, command, args }. Send it to WorkerBody.
+   *
+   * @param {string} commandName
+   *   The method name in the WorkerBody.
+   *
+   * @param {object} args
+   *   The argument object which will be sent to WorkerBody.
+   *
+   * @param {Array} tansfer
+   *   The transferable object array when postMessage. It could be undefined (but
+   * can not be null).
+   */
+   postCommand_with_processingId( commandName, args, transfer ) {
+    let processingId = this.processingId_next;
+    ++this.processingId_next;
+
+    let data = {
+      processingId: processingId,
+      command: commandName,
+      args: args
+    };
+    this.worker.postMessage( data, tansfer );  // Inform the worker to initialize.
+
+//!!!
+  }
+
+  /**
    * Dispatch messages come from the owned web worker. Please register this method
    * as this WorkerProxy's Worker.onmessage.
    *
@@ -123,7 +150,7 @@ class AsyncWorker_Proxy extends Recyclable.Root {
     // ( e.data == { processingId, done, value } )
     let { processingId, done, value } = e.data;
 
-    this.thePromiseResolveRejectMap.resolve_by_processingId_done_value(
+    this.the_processingId_Resulter_Map.resolve_by_processingId_done_value(
       processingId, done, value );
   }
 
