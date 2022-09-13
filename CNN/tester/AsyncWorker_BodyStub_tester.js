f10ee4d8f483204da77d489cca6875785a922f7a
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-09-13 15:20:41 +0800

    AsyncWorker_BodyStub_tester

--- CNN/tester/AsyncWorker_BodyStub_tester.js
+++ CNN/tester/AsyncWorker_BodyStub_tester.js
@@ -4,7 +4,7 @@
  *
  * In module (non-classic) web worker, static import is available. But at the same
  * time, importScripts() will not be avbailable. For solving this problem, using
- * classic (non-module) web worker so that some global library (e.g. tensorflow.js
+ * classic (non-module) web worker so that some global library (e.g. tensorflow.js)
  * can be loaded by importScripts(). At the same time, using dynamic import() to
  * load ourselves module because import() can be used in classic (non-module) script.
  */
