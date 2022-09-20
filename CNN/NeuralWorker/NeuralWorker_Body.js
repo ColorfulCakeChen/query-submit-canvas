da6cbcf1891a90200c9b23e7202826aca779b7ca
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-09-20 13:50:04 +0800

    ImageData_scale_once_process_multiple

--- CNN/NeuralWorker/NeuralWorker_Body.js
+++ CNN/NeuralWorker/NeuralWorker_Body.js
@@ -525,7 +525,7 @@ class NeuralWorker_Body extends AsyncWorker.Body {
 
   /**
    * This method is used for:
-   *   - Two web workers.
+   *   - Two web workers. Every worker has one neural network.
    *     - Both workers call this metohd.
    *       - The 1st worker uses ( bFork == true ).
    *       - The 2nd worker uses ( bFork == false ).
@@ -620,6 +620,23 @@ class NeuralWorker_Body extends AsyncWorker.Body {
   /**
    * Process input image data by all (suppose two) neural networks in this web worker.
    *
+   * This method is used for:
+   *   - One web worker. The worker has two neural networks.
+   *
+   *   - If ( bFill == true ), alignment mark filling.
+   *     - The worker will download scaled Int32Array from GPU memory.
+   *     - Fill alignment mark of the 1st neural network, upload and process it.
+   *     - Fill alignment mark of the 2nd neural network, upload and process it.
+   *
+   *   - If ( bFill == false ), no alignment mark filling.
+   *     - The worker needs not wait for downloading scaled Int32Array from GPU memory.
+   *         and needs not upload alignment mark filled Int32Array to GPU.
+   *     - So every neural network always output twice channels. For example,
+   *       - The neural network output 100 channels.
+   *       - channel [ 0, 49 ] are used if the neural network representing alignment A.
+   *       - channel [ 50, 99 ] are used if the neural network representing alignment B.
+   *
+   *    
    * @param {ImageData} sourceImageData
    *   The source image data to be processed.
    *
