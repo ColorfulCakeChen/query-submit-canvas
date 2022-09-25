7802666ef3c5a896d357e82915ed95968044cbdf
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-09-25 22:10:51 +0800

    Improve comments.

--- CNN/NeuralDEvolution/NeuralWorker/NeuralWorker_Proxies.js
+++ CNN/NeuralDEvolution/NeuralWorker/NeuralWorker_Proxies.js
@@ -118,15 +118,12 @@ import { Mode as NeuralWorker_Mode } from "./NeuralWorker_Mode.js";
  *         image is received by the 2nd worker (because the GPU is shared by
  *         all web workers).
  *
-
- 
-!!! ...unfinished... (2022/09/25)
-
  *
- * 
+ * 3. Summary
  *
+ *   - Try mode ONE_WORKER__ONE_SCALE__NO_FILL (0) with backend "webgl".
  *
- *
+ *   - If failed, try mode TWO_WORKER__ONE_SCALE__NO_FILL__APPLIER (5) with backend "cpu".
  *
  *
  *
