b3a7a31a6f978aa9ff6102c7eeb2f7dd590c4f84
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-12-19 12:30:39 +0800

    Rename Float30 to Float12

---
+++ CNN/Unpacker/Float12.js
@@ -0,0 +1,4 @@
+export * as Util from "./Float12/Float12_Util.js";
+export * as Constant from "./Float12/Float12_Constant.js";
+//export * as Decoder from "./Float12/Float12_Decoder.js";
+//export * as Encoder from "./Float12/Float12_Encoder.js";
--- CNN/Unpacker/Float30.js
+++
@@ -1,3 +0,0 @@
-export * as Util from "./Float30/Float30_Util.js";
-export * as Constant from "./Float30/Float30_Constant.js";
-//export * as Decoder from "./Float30/Float30_Decoder.js";
