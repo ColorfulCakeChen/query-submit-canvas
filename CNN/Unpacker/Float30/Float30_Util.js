e16bdb286660aa7cca1c9e1220f925ff2456c2e8
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-12-19 12:27:04 +0800

    ScientificNotation_Exponent_2

--- CNN/Unpacker/Float30/Float30_Util.js
+++ CNN/Unpacker/Float30/Float30_Util.js
@@ -21,16 +21,39 @@ const TenExpTable_n36_p26 = new Array( 63 );
 }
 
 /**
- * For example, ScientificNotation_Exponent( -0.25 ) = -1
+ * For example, ScientificNotation_Exponent_2( -0.125 ) = -3
  *
  * @param {number} aNumber
  *   The number to be found out its exponent.
  *
  * @return {integer}
- *   Return an integer representing the exponent of the specified number when the
- * number is represented in normalized scientific notation (i.e. exponential notation).
+ *   Return an integer representing the base 2 exponent of the specified number when
+ * the number is represented in normalized scientific notation (i.e. exponential notation).
  */
- function ScientificNotation_Exponent( aNumber ) {
+function ScientificNotation_Exponent_2( aNumber ) {
+  if ( aNumber === 0 )
+    return 0; // For avoiding Math.log2( 0 ) which is -Infinity.
+
+  let exponent = Math.floor(
+    Math.log2(
+      Math.abs( aNumber ) // Because Math.log2() can not accept negative value.
+    )
+  );
+
+  return exponent;
+}
+
+/**
+ * For example, ScientificNotation_Exponent_10( -0.25 ) = -1
+ *
+ * @param {number} aNumber
+ *   The number to be found out its exponent.
+ *
+ * @return {integer}
+ *   Return an integer representing the base 10 exponent of the specified number when
+ * the number is represented in normalized scientific notation (i.e. exponential notation).
+ */
+ function ScientificNotation_Exponent_10( aNumber ) {
   if ( aNumber === 0 )
     return 0; // For avoiding Math.log10( 0 ) which is -Infinity.
 
