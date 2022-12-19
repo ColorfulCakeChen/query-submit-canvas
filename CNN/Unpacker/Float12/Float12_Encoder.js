3ec65c5f0491398c34c59dea3af5a9aefc841457
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-12-19 14:53:06 +0800

    Float12.Encoder.Estimate

---
+++ CNN/Unpacker/Float12/Float12_Encoder.js
@@ -0,0 +1,57 @@
+export { Estimate_Exponent_Signed };
+export { Estimate_Significand_Signed };
+
+import * as Floa12_Constant from "./Floa12_Constant.js";
+import * as Floa12_Util from "./Floa12_Util.js";
+
+
+/** Estimate the signed exponent integer of the specified number for 12-bits
+ * floating-point number Base64 encoding.
+ *
+ * It just calls SCIENTIFIC_NOTATION_EXPONENT_2().
+ *
+ * Note: Although it seems more fair to use ROUND() than INT() in
+ * SCIENTIFIC_NOTATION_EXPONENT_2(), using ROUND() may result in the most
+ * (i.e. left most) significant bit of FLOAT12_ESTIMATE_SIGNIFICAND_SIGNED()
+ * not 1 (i.e. illegal significand value). So using INT() instead of ROUND().
+ *
+ * @param {number} aNumber
+ *   The number to be found out its exponent.
+ *
+ * @return {integer}
+ *   Return a signed integer representing the base 2 exponent of the specified number
+ * when the number is represented in normalized scientific notation (i.e. exponential
+ * notation).
+ */
+function Estimate_Exponent_Signed( aNumber ) {
+  return Floa12_Util.ScientificNotation_Exponent_2( aNumber );
+}
+
+/** Estimate the signed significand integer of the specified number for 12-bits
+ * floating-point number Base64 encoding.
+ *
+ * It un-powers the signed exponent integer (in base 2 (i.e. binary) scientific
+ * notation) and TRUNC() the result signed significand (i.e. implicit+fraction)
+ * to integer.
+ *
+ * The result signed significand integer is between either [ -63, -32 ] or [ 32, 63 ].
+ *
+ * Note: Although it seems more fair to use ROUND() than TRUNC(), using ROUND() may
+ * result in -64 or 64 (i.e. illegal significand integer). So using TRUNC().
+ *
+ * @param {number} aNumber
+ *   The number to be found out its significand.
+ *
+ * @param {number} exponent_signed
+ *   The aNumber's exponent (i.e. Estimate_Exponent_Signed( aNumber )).
+ *
+ * @return {integer}
+ *   Return a signed integer representing the base 2 significand of the specified number
+ * when the number is represented in normalized scientific notation (i.e. exponential
+ * notation).
+ */
+function Estimate_Significand_Signed( aNumber, exponent_signed ) {
+  return Math.trunc(
+    aNumber * ( 2 ** ( Float12_Constant.CoderFractionBitCount - exponent_signed ) )
+  );
+}
--- CNN/Unpacker/Float12/Float12_Util.js
+++ CNN/Unpacker/Float12/Float12_Util.js
@@ -1,26 +1,29 @@
-export { ScientificNotation_Exponent_2 };
 export { ScientificNotation_Exponent_10 };
+export { ScientificNotation_Exponent_2 };
 
-//!!! (2022/12/19 Deprecated)
-// export { from_SignificandSigned_ExponentSignedCorrected };
-//
-// /** */
-// const TenExpTable_n36_p26_offset = 36;
-//
-// /**
-//  * Because Math.pow( 10, m ) has accumulated error (e.g. ( 10 ** 26 ) != 1.0E+26 ),
-//  * using a look-up table to generating ( 10 ** m ) for n between [ -36, 26 ].
-//  *
-//  * Please use ( m + TenExpTable_n36_p26_offset ) = ( m + 36 ) as array index of
-//  * TenExpTable_n36_p26[].
-//  */
-// const TenExpTable_n36_p26 = new Array( 63 );
-// {
-//   for ( let i = 0; i < TenExpTable_n36_p26.length; ++i ) {
-//     let exponent = i - TenExpTable_n36_p26_offset;
-//     TenExpTable_n36_p26[ i ] = Number.parseFloat( `1E${exponent}` );
-//   }
-// }
+/**
+ * For example, ScientificNotation_Exponent_10( -0.25 ) = -1
+ *
+ * @param {number} aNumber
+ *   The number to be found out its exponent.
+ *
+ * @return {integer}
+ *   Return a signed integer representing the base 10 exponent of the specified number
+ * when the number is represented in normalized scientific notation (i.e. exponential
+ * notation).
+ */
+function ScientificNotation_Exponent_10( aNumber ) {
+  if ( aNumber === 0 )
+    return 0; // For avoiding Math.log10( 0 ) which is -Infinity.
+
+  let exponent = Math.floor(
+    Math.log10(
+      Math.abs( aNumber ) // Because Math.log10() can not accept negative value.
+    )
+  );
+
+  return exponent;
+}
 
 /**
  * For example, ScientificNotation_Exponent_2( -0.125 ) = -3
@@ -29,8 +32,9 @@ export { ScientificNotation_Exponent_10 };
  *   The number to be found out its exponent.
  *
  * @return {integer}
- *   Return an integer representing the base 2 exponent of the specified number when
- * the number is represented in normalized scientific notation (i.e. exponential notation).
+ *   Return a signed integer representing the base 2 exponent of the specified number
+ * when the number is represented in normalized scientific notation (i.e. exponential
+ * notation).
  */
 function ScientificNotation_Exponent_2( aNumber ) {
   if ( aNumber === 0 )
@@ -44,60 +48,3 @@ function ScientificNotation_Exponent_2( aNumber ) {
 
   return exponent;
 }
-
-/**
- * For example, ScientificNotation_Exponent_10( -0.25 ) = -1
- *
- * @param {number} aNumber
- *   The number to be found out its exponent.
- *
- * @return {integer}
- *   Return an integer representing the base 10 exponent of the specified number when
- * the number is represented in normalized scientific notation (i.e. exponential notation).
- */
- function ScientificNotation_Exponent_10( aNumber ) {
-  if ( aNumber === 0 )
-    return 0; // For avoiding Math.log10( 0 ) which is -Infinity.
-
-  let exponent = Math.floor(
-    Math.log10(
-      Math.abs( aNumber ) // Because Math.log10() can not accept negative value.
-    )
-  );
-
-  return exponent;
-}
-
-//!!! (2022/12/19 Deprecated)
-// /**
-//  * Generate a 30-bits floating-point number by a signed significand and a signed
-//  * and corrected exponent.
-//  *
-//  * @param {integer} significand_signed_n999999_p999999
-//  *   An signed (i.e. already minus Float30.Constant.CoderSignificandOffsetToSign)
-//  * integer representing significand value. It should be between [ -999999, 999999 ]
-//  * = [ -Float30.Constant.UseSignificandPositiveMax,
-//  * Float30.Constant.UseSignificandPositiveMax ].
-//  *
-//  * @param {integer} exponent_signed_n36_p26
-//  *   An signed (i.e. already minus Float30.Constant.CoderExponentOffsetToSign) and
-//  * corrected (i.e. already minus Float30.Constant.UseSignificandFractionDigitCount)
-//  * integer representing exponent value. It should be between [ -36, 26 ]
-//  * = [
-//  * -Float30.Constant.UseExponentPositiveMax
-//  *    - Float30.Constant.UseSignificandFractionDigitCount,
-//  *  Float30.Constant.UseExponentPositiveMax
-//  *    - Float30.Constant.UseSignificandFractionDigitCount
-//  * ]
-//  * = [ -31 - 5, 31 - 5 ].
-//  *
-//  */
-// function from_SignificandSigned_ExponentSignedCorrected(
-//   significand_signed_n999999_p999999, exponent_signed_n36_p26 ) {
-//
-//   //!!! (2022/12/06) Use TenExpTable_n36_p26[] for reducing Math.pow() accumulation error.
-//   //return significand_signed_n999999_p999999 * (10 ** exponent_signed_n36_p26 );
-//
-//   return significand_signed_n999999_p999999
-//            * TenExpTable_n36_p26[ exponent_signed_n36_p26 + TenExpTable_n36_p26_offset ];
-// }
