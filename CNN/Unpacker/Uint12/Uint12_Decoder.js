af012c37ff2424f09acd4469266c09cc84c73011
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-12-19 12:16:55 +0800

    Uint12.Decoder.From_Base64Char_CodePoint_Two

--- CNN/Unpacker/Uint12/Uint12_Decoder.js
+++ CNN/Unpacker/Uint12/Uint12_Decoder.js
@@ -1,4 +1,5 @@
-export { ToStrinFromString };
+export { From_Base64Char_CodePoint_Two };
+export { FromString };
 
 import * as Base64 from "../Base64.js";
 import * as Uint12_Constant from "./Uint12_Constant.js";
@@ -8,30 +9,25 @@ import * as Uint12_Constant from "./Uint12_Constant.js";
  * 
  * Convert two Base64 encoded characters to a 12-bits unsigned integer.
  *
- * @param {integer} base64_uint6_0
- *   A BASE64 encoded value (as a charcater). It represents the most-significant digit.
+ * @param {integer} base64Char_codePoint_0
+ *   A BASE64 encoded value (as a charcater's code point). It represents the
+ * most-significant digit.
  *
- * @param {integer} base64_uint6_1
- *   A BASE64 encoded value (as a charcater). It represents the least-significant digit.
+ * @param {integer} base64Char_codePoint_1
+ *   A BASE64 encoded value (as a charcater's code point). It represents the
+ * least-significant digit.
  *
  * @return {integer}
  *   An uint12 (12-bits unsigned integer) value decoded from the base64_uint6_0 and
  * base64_uint6_1.
  */
-function From_Base64Char_Two( base64_uint6_0, base64_uint6_1 ) {
-
-BITOR(
-  BITLSHIFT(
-    BASE64_CHAR_TO_UINT6(
-      base64_char_0,base64_decode_table,
-      base64_decode_table_value_offset),
-    uint12_coder_most_significant_bitmask_lshift_count
-  ),
-  BASE64_CHAR_TO_UINT6(
-    base64_char_1,base64_decode_table,
-    base64_decode_table_value_offset
-  )
-)
+function From_Base64Char_CodePoint_Two(
+           base64Char_codePoint_0, base64Char_codePoint_1 ) {
+  return (
+      ( Base64.Constant.DecodeTable_CharCodePoint_to_Uint6[ base64Char_codePoint_0 ]
+          << Uint12_Constant.MostSignificantBitmaskLShiftCount )
+    | ( Base64.Constant.DecodeTable_CharCodePoint_to_Uint6[ base64Char_codePoint_1 ] )
+  );
 }
 
 
