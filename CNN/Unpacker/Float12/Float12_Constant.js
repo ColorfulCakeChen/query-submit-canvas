5bc1cf3e23dc47d04ac0c79265d7c9b4b2c43f33
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-12-21 20:21:30 +0800

    Float12.Constant.PositiveMinLess

--- CNN/Unpacker/Float12/Float12_Constant.js
+++ CNN/Unpacker/Float12/Float12_Constant.js
@@ -35,11 +35,10 @@ const NextStringCharCount = StringCharCount;
  *
  * It always is -4.2278584320000000E+009
  */
-const NegativeMin
-  = Float12_Decoder.From_Sign_ExponentSigned_FractionUnsigned(
-      1,
-      Float12_Constant_Coder.ExponentPositiveMax,
-      Float12_Constant_Coder.FractionUnsignedMax );
+const NegativeMin = Float12_Decoder.From_Sign_ExponentSigned_FractionUnsigned(
+  1,
+  Float12_Constant_Coder.ExponentPositiveMax,
+  Float12_Constant_Coder.FractionUnsignedMax );
 
 /**
  * A number which is a little smaller than the minimum negative value of a 12-bits
@@ -57,13 +56,12 @@ const NegativeMinLess = NegativeMin - 1;
 /**
  * The maximum positive value of a Base64 encoded 12-bits floating-point number.
  *
- * It always is  4.2278584320000000E+009
+ * It always is 4.2278584320000000E+009
  */
-const PositiveMax
-  = Float12_Decoder.From_Sign_ExponentSigned_FractionUnsigned(
-    0,
-    Float12_Constant_Coder.ExponentPositiveMax,
-    Float12_Constant_Coder.FractionUnsignedMax );
+const PositiveMax = Float12_Decoder.From_Sign_ExponentSigned_FractionUnsigned(
+  0,
+  Float12_Constant_Coder.ExponentPositiveMax,
+  Float12_Constant_Coder.FractionUnsignedMax );
 
 /**
  * A number which is a little larger than the maximum positive value of a 12-bits
@@ -74,9 +72,43 @@ const PositiveMax
  *
  * The extra value is small but representable by floating-point number.
  *
- * It always is  4.2278584330000000E+009
+ * It always is 4.2278584330000000E+009
  */
 const PositiveMaxMore = PositiveMax + 1;
 
+
+/**
+ * The minimum positive value of a Base64 encoded 12-bits floating-point number.
+ *
+ * In fact, Float12.Constant.PositiveMinLess is the real minimum representable
+ * positive value of a 12-bits floating-point number. However, it is used to
+ * represent zero by
+ * Float12.Decoder.From_Sign_ExponentUnsigned_FractionUnsigned_Zeroable(). The
+ * number which is a little larger than Float12.Constant.PositiveMinLess is used
+ * as this Float12.Constant.PositiveMin (i.e. fraction=1 instead of 0).
+ *
+ * It always is 2.4010660126805300E-010
+ */
+const PositiveMin = Float12_Decoder.From_Sign_ExponentSigned_FractionUnsigned(
+  0,
+  Float12_Constant_Coder.ExponentNegativeMin,
+  1 );
+
+/**
+ * A number which is a little smaller than the minimum positive value of a 12-bits
+ * floating-point number.
+ *
+ * In fact, it is the real minimum representable positive value of a 12-bits
+ * floating-point number. However, it is used to represent zero by
+ * Float12.Decoder.From_Sign_ExponentUnsigned_FractionUnsigned_Zeroable(). So it
+ * should be viewed as a little smaller than the minimum positive value (i.e.
+ * Float12.Constant.PositiveMin) of a 12-bits floating-point number in actual use.
+ *
+ * It always is 2.3283064365387000E-010
+ */
+const PositiveMinLess = Float12_Decoder.From_Sign_ExponentSigned_FractionUnsigned(
+  0,
+  Float12_Constant_Coder.ExponentNegativeMin,
+  0 );
 
 //!!! ...unfinished... (2022/12/21)
