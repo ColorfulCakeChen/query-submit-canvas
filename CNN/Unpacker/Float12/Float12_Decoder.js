0570727ae0f9484ce94b22e1f7789bdd1fea7af1
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-12-21 11:29:19 +0800

    Float12.Decoder.From_Sign_ExponentSigned_SignificandUnsigned()

--- CNN/Unpacker/Float12/Float12_Decoder.js
+++ CNN/Unpacker/Float12/Float12_Decoder.js
@@ -1,6 +1,7 @@
-export { From__ExponentSignedCorrected__SignificandSigned };
+export { From_ExponentSignedCorrected_SignificandSigned };
+export { From_Sign_ExponentSigned_SignificandUnsigned };
 
-//import * as Float12_Constant_Coder from "./Float12_Constant_Coder.js";
+import * as Float12_Constant_Coder from "./Float12_Constant_Coder.js";
 
 /**
  *
@@ -24,7 +25,7 @@ export { From__ExponentSignedCorrected__SignificandSigned };
  *   A 12-bits floating-point number by the signed and corrected exponent and the
  * signed significand.
  */
-function From__ExponentSignedCorrected__SignificandSigned(
+function From_ExponentSignedCorrected_SignificandSigned(
   exponent_signed_corrected_n37_p26, significand_signed__n63_n32__p32_p63 ) {
 
   return (
@@ -32,6 +33,56 @@ function From__ExponentSignedCorrected__SignificandSigned(
   );
 }
 
+/**
+ *
+ * It will call Float12.Decoder.From_ExponentSignedCorrected_SignificandSigned().
+ *
+ * @param {integer} sign_0_1
+ *   The sign bit of the result value. It must be either 0 (for positive) or
+ * 1 (for negative).
+ *
+ * @param {integer} exponent_signed_n32_p31
+ *   An signed (i.e. already minus Float12.Constant.Coder.ExponentOffsetToSigned)
+ * integer representing exponent value. It should be between [ -32, 31 ] = [
+ * Float12.Constant.Coder.ExponentNegativeMin,
+ * Float12.Constant.Coder.ExponentPositiveMax ].
+ *
+ * @param {integer} significand_unsigned_p32_p63
+ *   An unsigned (i.e. already masked out sign bit) integer representing unsigned
+ * significand value. It should be between [ 32, 63 ] = [
+ * Float12.Constant.Coder.SignificandUnsignedMin,
+ * Float12.Constant.Coder.SignificandUnsignedMax ].
+ *
+ * @return {number}
+ *   A 12-bits floating-point number by the sign bit, the signed exponent integer,
+ * and the unsigned significand integer.
+ */
+function From_Sign_ExponentSigned_SignificandUnsigned(
+  sign_0_1, exponent_signed_n32_p31, significand_unsigned_p32_p63 ) {
+
+  let exponent_signed_corrected
+    = exponent_signed_n32_p31 - Float12_Constant_Coder.FractionBitCount;
+
+  if ( sign_0_1 == 0 ) // Positive
+    return From_ExponentSignedCorrected_SignificandSigned(
+      exponent_signed_corrected, significand_unsigned_p32_p63 );
+  else // Negative
+    return From_ExponentSignedCorrected_SignificandSigned(
+      exponent_signed_corrected, -significand_unsigned_p32_p63 );
+}
+
+/**
+ * Generate a 12-bits floating-point number by a sign bit, a signed exponent integer, and a unsigned fraction integer.
+ *
+ * It will call Float12.Decoder.From_Sgin_ExponentSigned_SignificandUnsigned().
+ */
+function From_Sign_ExponentSigned_FractionUnsigned() {
+
+
+//!!! ...unfinished... (2022/12/19)
+
+
+}
 
 //!!! ...unfinished... (2022/12/19)
 
