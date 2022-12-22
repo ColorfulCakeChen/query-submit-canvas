0d5bb34c55f20fa1187f0c8f63241944cabf902a
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-12-22 14:37:48 +0800

    CoderMostSignificantBitmaskLShifted

--- CNN/Unpacker/Uint12/Uint12_Constant.js
+++ CNN/Unpacker/Uint12/Uint12_Constant.js
@@ -50,7 +50,7 @@ const CoderMostSignificantBitmaskLShiftCount = CoderLeastSignificantBitCount;
  *
  * It always is 4032.
  */
-const CoderMostSignificantBitmaskLShifted = Bitmaask.ByBitCount_LShifted(
+const CoderMostSignificantBitmaskLShifted = Bitmask.ByBitCount_LShifted(
   CoderMostSignificantBitmask, CoderMostSignificantBitmaskLShiftCount );
 
 
