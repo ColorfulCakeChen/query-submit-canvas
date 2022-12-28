e1fd45ad8f38395b2c890527b4d600eb897eec16
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-12-28 10:25:23 +0800

    window_onLoad

---
+++ CNN/tester/Versus_tester.js
@@ -0,0 +1,18 @@
+
+
+
+window.addEventListener( "load", window_onLoad );
+
+/** */
+function window_onLoad( event ) {
+  let DownloadButton = document.querySelector( "DownloadButton" );
+  DownloadButton.addEventListener( "click", DownloadButton_onClick );
+
+
+}
+
+/** */
+function DownloadButton_onClick( event ) {
+  alert( "Hi" );
+
+}
