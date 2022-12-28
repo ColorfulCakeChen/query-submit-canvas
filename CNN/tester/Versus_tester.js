30112ced62f18280af813c04d48f65ffb93057c2
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-12-28 12:00:52 +0800

    visitIndex_get()

--- CNN/tester/Versus_tester.js
+++ CNN/tester/Versus_tester.js
@@ -8,7 +8,7 @@ let g_Contorls = {
   SpreadsheetIdText: null,
   DownloadSummaryButton: null,
 
-  NextVersusIdText: null,
+  NextVisitIndexText: null,
   DownloadVersusButton: null,
 };
 
@@ -22,7 +22,7 @@ function window_onLoad( event ) {
   g_Contorls.DownloadSummaryButton.addEventListener(
     "click", DownloadSummaryButton_onClick );
 
-  g_Contorls.NextVersusIdText = document.querySelector( "#NextVersusIdText" );
+  g_Contorls.NextVisitIndexText = document.querySelector( "#NextVisitIndexText" );
 
   g_Contorls.DownloadVersusButton = document.querySelector( "#DownloadVersusButton" );
   g_Contorls.DownloadVersusButton.addEventListener(
@@ -49,15 +49,13 @@ function DownloadVersusButton_onClick( event ) {
 
 //!!! ...unfinished... (2022/12/28)
 //g_VersusSummary.visitCount
- id="NextVersusIdText" type="text" maxlength="150" size="50" readonly>
- id="DownloadVersusButton" type="button">Download Next Versus</button>
-
+  g_Contorls.NextVisitIndexText.value = g_VersusSummary.visitIndex_get();
 }
 
 /** */
 function VersusSummary_onDownload( bDownloadSummaryOk ) {
   if ( !bDownloadSummaryOk ) {
-    g_Contorls.NextVersusIdText.value = "";
+    g_Contorls.NextVisitIndexText.value = "";
 
     let theSpreadsheetId = g_VersusSummary.weightsSpreadsheetId;
     alert( `Failed to download VersusSummary from Google Sheets `
@@ -67,10 +65,8 @@ function VersusSummary_onDownload( bDownloadSummaryOk ) {
     // g_VersusSummary = null;
   }
 
-  g_Contorls.NextVersusIdText.value = g_VersusSummary."";
+  g_Contorls.NextVisitIndexText.value = g_VersusSummary.visitIndex_get();
 
 //!!! ...unfinished... (2022/12/28)
-<input id="NextVersusIdText" type="text" maxlength="150" size="50" readonly>
-<button id="DownloadVersusButton" type="button">Download Next Versus</button>
 
 }
