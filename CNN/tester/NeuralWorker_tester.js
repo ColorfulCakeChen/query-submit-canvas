f2d5a314e8ce70c452aaf2d7acab4fd3703456b1
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-12-29 21:29:14 +0800

    blockCountTotalRequested = 86

--- CNN/tester/NeuralWorker_tester.js
+++ CNN/tester/NeuralWorker_tester.js
@@ -338,7 +338,7 @@ class HeightWidthDepth {
     let nConvStageType
       = ValueDesc.ConvStageType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_PAD_VALID;
 
-    let blockCountTotalRequested = 90; //128; //100; //200; //50; //20; //10;
+    let blockCountTotalRequested = 86; //128; //100; //200; //50; //20; //10;
     let output_channelCount = 6; //12; //64; //8; //4; //400; //300; //64;
     //let output_channelCount_twice = output_channelCount * 2; // For NO_FILL
 
