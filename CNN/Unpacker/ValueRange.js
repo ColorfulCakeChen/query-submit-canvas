d7b956f41fcfd0e1ea4de6eec2da68707436851a
ColorfulCakeChen <colorfulcakechen@gmail.com>
2022-12-30 20:01:34 +0800

    Improve comments.

--- CNN/Unpacker/ValueRange.js
+++ CNN/Unpacker/ValueRange.js
@@ -14,8 +14,9 @@ class Same {
 
   /**
    * @return {function}
-   *   Return a function object. When calling the returned function object with one value parameter, it will return
-   * the adjusted value which is retricted by this object.
+   *   Return a function object. When calling the returned function object with
+   * one value parameter, it will return the adjusted value which is retricted
+   * by this object.
    */
   getAdjuster() {
     return this.adjust.bind( this );
@@ -24,20 +25,27 @@ class Same {
   /**
    * Return a generator which produces a sequence of two-value pair.
    *
-   * For ValueRange.Same, there is no meaningful testable value pair could be generated. The reason is that any value is legal for it.
+   * For ValueRange.Same, there is no meaningful testable value pair could be
+   * generated. The reason is that any value is legal for it.
    *
    * @param {object} io_pair
-   *   An object which will be filled with two number properties: { valueInput, valueOutput }. It is also the object to be yielded
-   * every time. For ValueRange.Same, the valueOutput and valueInput are always the same as offsetMultiplier.
+   *   An object which will be filled with two number properties: { valueInput,
+   * valueOutput }. It is also the object to be yielded every time. For
+   * ValueRange.Same, the valueOutput and valueInput are always the same as
+   * offsetMultiplier.
    *
    * @param {number} offsetMultiplier
-   *   An integer multiplier. The ( offsetMultiplier * this.kinds ) will be used as the first test value. The default value
-   * is RandTools.getRandomIntInclusive( -10, +10 ). The -10 and +10 is just chosen arbitrarily.
+   *   An integer multiplier. The ( offsetMultiplier * this.kinds ) will be used
+   * as the first test value. The default value is
+   * RandTools.getRandomIntInclusive( -10, +10 ). The -10 and +10 is just chosen
+   * arbitrarily.
    *
    * @yield {object}
    *   Yield the io_pair object.
    */
-  * valueInputOutputGenerator( io_pair, offsetMultiplier = Same.defaultOffsetMultiplier ) {
+  * valueInputOutputGenerator(
+    io_pair, offsetMultiplier = Same.defaultOffsetMultiplier ) {
+
     io_pair.valueInput = offsetMultiplier
     io_pair.valueOutput = offsetMultiplier;
     yield io_pair;
@@ -52,7 +60,8 @@ Same.Singleton = new Same;
 
 
 /**
- * Provides methods for converting weight (a number) to integer between min and max (both are integers).
+ * Provides methods for converting weight (a number) to integer between min
+ * and max (both are integers).
  */
 class Int extends Same {
 
@@ -62,13 +71,15 @@ class Int extends Same {
    */
   constructor( min, max ) {
     super();
-    this.min = Math.trunc( Math.min( min, max ) ); // Confirm the minimum. Convert to an integer.
-    this.max = Math.trunc( Math.max( min, max ) ); // Confirm the maximum. Convert to an integer.
+    this.min = Math.trunc( Math.min( min, max ) ); // Confirm the minimum (as integer).
+    this.max = Math.trunc( Math.max( min, max ) ); // Confirm the maximum (as integer).
     this.kinds = ( this.max - this.min ) + 1; // How many possible integer between them.
   }
 
   /**
-   * @return {number} Return the trucated value (i.e. integer). The returned value is forcibly between min and max (as integer too).
+   * @return {number}
+   *   Return the trucated value (i.e. integer). The returned value is forcibly
+   * between min and max (as integer too).
    */
   adjust( value ) {
     let valueInt = Math.trunc( value ); // Convert to an integer.
@@ -76,27 +87,32 @@ class Int extends Same {
     // Rearrange valueInt between min and max fairly (in probability).
     //
     // A1: Why not use remainder operator (%) directly?
-    // Q1: Because remainder always has the same sign as dividend, this can not handle the situation which min and
-    //     max have different sign.
+    // Q1: Because remainder always has the same sign as dividend, this can not
+    //     handle the situation which min and max have different sign.
     //
-    // A2: Why not just restrict all value less than min to valueMin and value greater than max to max?
-    // Q2: Although this could restrict value in range, it will skew the probability of every value in the range.
-    //     Unfair probability could be harmful to evolution algorithm.
+    // A2: Why not just restrict all value less than min to valueMin and value
+    //     greater than max to max?
+    // Q2: Although this could restrict value in range, it will skew the probability
+    //     of every value in the range. Unfair probability could be harmful to
+    //     evolution algorithm.
     //
     let quotient = ( valueInt - this.min ) / this.kinds;
-    let quotientInt = Math.floor( quotient );  // So that negative value could be handled correctly.
+    let quotientInt = Math.floor( quotient );  // For Handling negative value correctly.
     let result = valueInt - ( quotientInt * this.kinds );
     return result;
   }
 
   /**
    * @param {object} io_pair
-   *   An object which will be filled with two number properties: { valueInput, valueOutput }. It is also the object to be returned
-   * by this method. The valueInput is a floating-point value composed from baseIntCongruence, this.min and index. The valueOutput
-   * is an integer value. If calling this.adjust( valueInput ), the valueOutput will be returned.
+   *   An object which will be filled with two number properties: { valueInput,
+   * valueOutput }. It is also the object to be returned by this method. The
+   * valueInput is a floating-point value composed from baseIntCongruence,
+   * this.min and index. The valueOutput is an integer value. If calling
+   * this.adjust( valueInput ), the valueOutput will be returned.
    *
    * @param {number} baseIntCongruence
-   *   An integer which is divisible by this.kinds. This will be used to offset the index value to generate valueInput.
+   *   An integer which is divisible by this.kinds. This will be used to offset
+   * the index value to generate valueInput.
    *
    * @param {number} index
    *   An integer between [ 0, this.kinds ).
@@ -111,18 +127,21 @@ class Int extends Same {
     let valueInputInt = baseIntCongruence + valueOutputInt;
     let valueInputIntSign = Math.sign( valueInputInt );
 
-    // If no sign (i.e. valueInputInt is zero), choose positive sign. Otherwise, there will no fractional part at all.
+    // If no sign (i.e. valueInputInt is zero), choose positive sign. Otherwise,
+    // there will no fractional part at all.
     if ( valueInputIntSign == 0 ) {
       valueInputIntSign = 1;
     }
 
     // A: Why not use Math.random() to generate value between [ 0, 1 ) directly?
-    // Q: In order to avoid rounded into another integer when converted from Float64 (here) to Float32 (weights array),
-    //    the random value should not too close to 1. For example, 1.9999999999999999999 might become 2.0 when converted
-    //    from Float64 to Float32.
+    // Q: In order to avoid rounded into another integer when converted from
+    //    Float64 (here) to Float32 (weights array), the random value should not
+    //    too close to 1. For example, 1.9999999999999999999 might become 2.0
+    //    when converted from Float64 to Float32.
     let randomFractionalPart = RandTools.getRandomIntInclusive( 0, 99 ) / 1000;
 
-    // An floating-point number (the integer with fractional part) which could become valueOutputInt when adjusted by Int.adjust().
+    // An floating-point number (the integer with fractional part) which could
+    // become valueOutputInt when adjusted by Int.adjust().
     let valueInputFloat = valueInputInt + ( valueInputIntSign * randomFractionalPart );
 
     //!!! (2021/07/06 Temp Test) When the random fractional part is converted (from Float64) into Float32, it might shifted to different value.
@@ -142,32 +161,45 @@ class Int extends Same {
    * For ValueRange.Int, this.kinds two-value pairs will be generated in sequence.
    *
    * @param {object} io_pair
-   *   An object which will be filled with two number properties: { valueInput, valueOutput }. It is also the object to be yielded
-   * every time. The valueInput is a floating-point value which could be adjusted to valueOutput by this.adjust(). The valueOutput
-   * is an integer value.
+   *   An object which will be filled with two number properties: { valueInput,
+   * valueOutput }. It is also the object to be yielded every time. The valueInput
+   * is a floating-point value which could be adjusted to valueOutput by
+   * this.adjust(). The valueOutput is an integer value.
    *
    * @param {number} offsetMultiplier
-   *   An integer multiplier. The ( offsetMultiplier * this.kinds ) will be used as the first test value. The default value
-   * is RandTools.getRandomIntInclusive( -10, +10 ). The -10 and +10 is just chosen arbitrarily.
+   *   An integer multiplier. The ( offsetMultiplier * this.kinds ) will be used
+   * as the first test value. The default value is
+   * RandTools.getRandomIntInclusive( -10, +10 ). The -10 and +10 is just chosen
+   * arbitrarily.
    *
    * @param {number[]} valueOutMinMax
-   *   An integer array restricts the generator range to [ valueOutMin, valueOutMax ]. Itself will be restricted to
-   * [ this.min, this.max ] at most. When this.kinds is large, this parameter could lower the kinds to reduce test
-   * cases quantity. If null or undefined, only one value (between [ this.min, this.max ] randomly) will be generated.
+   *   An integer array restricts the generator range to [ valueOutMin, valueOutMax ].
+   * Itself will be restricted to [ this.min, this.max ] at most. When this.kinds
+   * is large, this parameter could lower the kinds to reduce test cases quantity.
+   * If null or undefined, only one value (between [ this.min, this.max ] randomly)
+   * will be generated.
    *
    * @yield {object}
    *   Yield the io_pair object.
    *
    * @override
    */
-  * valueInputOutputGenerator( io_pair, offsetMultiplier = Int.defaultOffsetMultiplier, valueOutMinMax ) {
+  * valueInputOutputGenerator(
+    io_pair, offsetMultiplier = Int.defaultOffsetMultiplier, valueOutMinMax ) {
 
-    // An integer which has the same remainder as offsetMultiplier when divided by this.kinds.
+    // An integer which has the same remainder as offsetMultiplier when divided
+    // by this.kinds.
     let baseIntCongruence = Math.trunc( offsetMultiplier ) * this.kinds;
 
     if ( valueOutMinMax ) {
-      let valueOutMin = Math.max( Math.min( valueOutMinMax[ 0 ], valueOutMinMax[ 1 ] ), this.min ); // The smaller one of valueOutMinMax.
-      let valueOutMax = Math.min( Math.max( valueOutMinMax[ 0 ], valueOutMinMax[ 1 ] ), this.max ); // The larger one of valueOutMinMax.
+
+      // The smaller one of valueOutMinMax.
+      let valueOutMin
+        = Math.max( Math.min( valueOutMinMax[ 0 ], valueOutMinMax[ 1 ] ), this.min );
+
+      // The larger one of valueOutMinMax.
+      let valueOutMax
+        = Math.min( Math.max( valueOutMinMax[ 0 ], valueOutMinMax[ 1 ] ), this.max );
 
       let indexLower = valueOutMin - this.min; // index is 0-base.
       let indexUpper = valueOutMax - this.min;
@@ -204,12 +236,14 @@ class Bool extends Int {
 
   /**
    * @param {object} io_pair
-   *   An object which will be filled with two number properties: { valueInput, valueOutput }. It is also the object to be returned
-   * by this method.  The valueInput is a floating-point value. The valueOutput is a boolean value. If calling this.adjust( valueInput ),
-   * the valueOutput will be returned.
+   *   An object which will be filled with two number properties: { valueInput,
+   * valueOutput }. It is also the object to be returned by this method. The
+   * valueInput is a floating-point value. The valueOutput is a boolean value.
+   * If calling this.adjust( valueInput ), the valueOutput will be returned.
    *
    * @param {number} baseIntCongruence
-   *   An integer which is divisible by this.kinds. This will be used to offset the index value to generate valueInput.
+   *   An integer which is divisible by this.kinds. This will be used to offset
+   * the index value to generate valueInput.
    *
    * @param {number} index
    *   An integer between [ 0, this.kinds ).
@@ -229,25 +263,32 @@ class Bool extends Int {
    * For ValueRange.Bool, two two-value pairs will be generated in sequence.
    *
    * @param {object} io_pair
-   *   An object which will be filled with two number properties: { valueInput, valueOutput }. It is also the object to be yielded
-   * every time. The valueInput is a boolean value which could be adjusted to valueOutput by this.adjust(). The valueOutput
-   * is an integer value.
+   *   An object which will be filled with two number properties: { valueInput,
+   * valueOutput }. It is also the object to be yielded every time. The valueInput
+   * is a boolean value which could be adjusted to valueOutput by this.adjust().
+   * The valueOutput is an integer value.
    *
    * @param {number} offsetMultiplier
-   *   An integer multiplier. The ( offsetMultiplier * this.kinds ) will be used as the first test value. The default value
-   * is RandTools.getRandomIntInclusive( -100, +100 ). The -100 and +100 is just chosen arbitrarily.
+   *   An integer multiplier. The ( offsetMultiplier * this.kinds ) will be used
+   * as the first test value. The default value is
+   * RandTools.getRandomIntInclusive( -100, +100 ). The -100 and +100 is just
+   * chosen arbitrarily.
    *
    * @param {number[]} valueOutMinMax
-   *   An integer restricts the generator range to [ valueOutMin, valueOutMax ]. Itself will be restricted to [ this.min, this.max ]
-   * at most. When this.kinds is large, this parameter could lower the kinds to reduce test cases quantity. If null or undefined,
-   * only one value (between [ this.min, this.max ] randomly) will be generated.
+   *   An integer restricts the generator range to [ valueOutMin, valueOutMax ].
+   * Itself will be restricted to [ this.min, this.max ] at most. When this.kinds
+   * is large, this parameter could lower the kinds to reduce test cases quantity.
+   * If null or undefined, only one value (between [ this.min, this.max ] randomly)
+   * will be generated.
    *
    * @yield {object}
    *   Yield the io_pair object.
    *
    * @override
    */
-  * valueInputOutputGenerator( io_pair, offsetMultiplier = Bool.defaultOffsetMultiplier, valueOutMinMax ) {
+  * valueInputOutputGenerator(
+    io_pair, offsetMultiplier = Bool.defaultOffsetMultiplier, valueOutMinMax ) {
+
     yield* super.valueInputOutputGenerator( io_pair, offsetMultiplier, valueOutMinMax );
   }
 
