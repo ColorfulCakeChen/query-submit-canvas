841e4b1c32c6760bbddf740795fdc95cd2d40a78
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-01-03 15:38:23 +0800

    // parent draw offspring

--- CNN/NeuralDEvolution/DEvolution/DEvolution_VersusSubmitter/DEvolution_VersusSubmitter_MultiMeasurementId_MultiEventName.js
+++ CNN/NeuralDEvolution/DEvolution/DEvolution_VersusSubmitter/DEvolution_VersusSubmitter_MultiMeasurementId_MultiEventName.js
@@ -54,6 +54,98 @@ class DEvolution_VersusSubmitter_MultiMeasurementId_MultiEventName
   }
 
 
+  /**
+   * Every versusId.entityNo and every nNegativeZeroPositive (-1 or 0 or +1) will use
+   * an different event name (and its event content is not important).
+   *
+   * The apiSecret will be looked up from .measurementId_apiSecret_map
+   *
+   * @param {string} measurementId
+   *   The measurement id of stream of property of Google Analytics v4.
+   *
+   * @param {DEvolution.VersusId} versusId
+   *   The differential evolution versus id.
+   *
+   * @param {number} nNegativeZeroPositive
+   *   The lose/draw/win value of the versus. (-1 or 0 or +1)
+   *     - -1 (if parent lose offspring)
+   *     -  0 (if parent draw offspring)
+   *     - +1 (if parent win offspring)
+   */
+  post_by_measurementId_versusId_NegativeZeroPositive(
+    measurementId, versusId, nNegativeZeroPositive ) {
+
 //!!! ...unfinished... (2023/01/03)
 
+    // An integer between [ 0, 4 ].
+    let entityNo = versusId.entityNo;
+
+    // An integer between [ 0, 2 ].
+    let number_0_1_2 = nNegativeZeroPositive + 1;
+
+    let eventObjectTableIndex = entityNo * ( number_0_1_2 + 1 );
+
+    // (e.g. EntityNo_ParentGenerationNo_OffspringGenerationNo).
+    //let versusIdString = versusId.versusIdString;
+
+    let eventObject = DEvolution_VersusSubmitter_MultiMeasurementId_MultiEventName
+      .eventObjectTable[ eventObjectTableIndex ];
+
+    this.post_by_measurementId_event(
+      measurementId, eventObject );
+  }
+
 }
+
+DEvolution_VersusSubmitter_MultiMeasurementId_MultiEventName
+  .eventObjectTable = [
+
+  // Entity 0
+  { name: "add_payment_info", params: { items: [] } },  // parent lose offspring
+  { name: "add_shipping_info", params: { items: [] } }, // parent draw offspring
+  { name: "add_to_cart", params: { items: [] } },
+
+  // Entity 1
+  { name: "add_to_wishlist", params: { items: [] } },   // parent lose offspring
+  { name: "begin_checkout", params: { items: [] } },    // parent draw offspring
+  { name: "earn_virtual_currency", params: {} },
+
+  // Entity 2
+  { name: "generate_lead", params: {} },                // parent lose offspring
+  { name: "join_group", params: {} },                   // parent draw offspring
+  { name: "level_up", params: {} },
+
+  // Entity 3
+  { name: "login", params: {} },                        // parent lose offspring
+  { name: "post_score", params: {} },
+  { name: "purchase", params: { items: [] } },
+
+  // Entity 4
+  { name: "refund", params: { items: [] } },            // parent lose offspring
+  { name: "remove_from_cart", params: { items: [] } },
+  { name: "search", params: {} },
+
+  // Entity 5
+  { name: "select_content", params: {} },               // parent lose offspring
+  { name: "select_item", params: { items: [] } },
+  { name: "select_promotion", params: { items: [] } },
+
+  // Entity 6
+  { name: "share", params: {} },                        // parent lose offspring
+  { name: "sign_up", params: {} },
+  { name: "spend_virtual_currency", params: {} },
+
+  // Entity 7
+  { name: "tutorial_begin" },                           // parent lose offspring
+  { name: "tutorial_complete" },
+  { name: "unlock_achievement", params: {} },
+
+  // Entity 8
+  { name: "view_cart", params: { items: [] } },         // parent lose offspring
+  { name: "view_item", params: { items: [] } },
+  { name: "view_item_list", params: { items: [] } },
+
+  // Entity 9
+  // { name: "view_promotion", params: { items: [] } },
+  // { name: "view_search_results", params: { items: [] } },
+];
