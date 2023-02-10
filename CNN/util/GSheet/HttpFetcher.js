5d309c05fde6606c318bf55ef3ada1aa3f0c6dd4
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-02-10 15:37:36 +0800

    HttpFetcher.ReadyState

--- CNN/util/GSheet/HttpFetcher.js
+++ CNN/util/GSheet/HttpFetcher.js
@@ -11,3 +11,15 @@ export { HttpFetcher };
 class HttpFetcher {
 
 }
+
+
+/**
+ * (Defining named constants for XMLHttpRequest.readyState)
+ */
+HttpFetcher.ReadyState = {
+  UNSENT: 0,
+  OPENED: 1,
+  HEADERS_RECEIVED: 2,
+  LOADING: 3,
+  DONE: 4,
+};
