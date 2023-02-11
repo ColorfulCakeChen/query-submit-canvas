a6634dcc2bc5ed62d0f3caf481a7115319847f19
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-02-11 21:06:33 +0800

    Register event handler.

--- CNN/util/GSheet/HttpFetcher.js
+++ CNN/util/GSheet/HttpFetcher.js
@@ -1,6 +1,7 @@
 export { HttpFetcher };
 
 import * as AsyncWorker from "../AsyncWorker.js";
+import * as ValueMax from "../ValueMax.js";
 
 //!!! ...unfinished... (2023/02/10)
 // A re-used XMLHttpRequest object (reset by abort() or open())?
@@ -26,8 +27,13 @@ class HttpFetcher {
   }
 
   /**
-   * Send command and args (perhaps, with transferable object array) to WorkerBody
-   * and expect result.
+   * An async generator for sending a http request and tracking progress.
+   *
+   *
+   * @param {ValueMax.Percentage.Aggregate} progressParent
+   *   Some new progressToAdvance will be created and added to progressParent. The
+   * created progressToAdvance will be increased when every time advanced. The
+   * progressParent.root_get() will be returned when every time yield.
    *
    * @param {string} method
    *   The HTTP request method to use, e.g. "GET", "POST".
@@ -38,10 +44,19 @@ class HttpFetcher {
    * @param {object} body
    *   A body of data to be sent in the XHR request. It could be null.
    *
+   * @param {object} timeoutMilliseconds
+   *   The time in milliseconds a request can take before automatically being
+   * terminated. The default value is 0, which means there is no timeout.
+   *
    * @return {AsyncWorker.Resulter}
    *   Return an async generator for receving result from XMLHttpRequest.
+   *   - Yield a promise resolves to { done: false, value: progressParent.root_get() }.
+   *   - Yield a promise resolves to { done: true, value: xhr.responseText }.
+   *   - Yield a promise rejects to ProgressEvent. The ProgressEvent.type may be
+   *       "abort", "error", "timeout".
    */
-  createResulter_by_method_url_body( method, url, body ) {
+  createResulter_by_method_url_body( progressParent, method, url, body,
+    timeoutMilliseconds = 0 ) {
 
     // Prepare the processing's result's receiving queue before sending it.
     let resulter = this.the_processingId_Resulter_Map
@@ -52,15 +67,72 @@ class HttpFetcher {
 
     const xhr = this.xhr = new XMLHttpRequest();
     xhr.open( method, url, true );
+    xhr.timeout = timeoutMilliseconds;
 
-    xhr.onreadystatechange = () => { // Call a function when the state changes.
-      if ( xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200 ) {
-        // Request finished. Do processing here.
-      }
-    }
+    xhr.onabort = HttpFetcher.handle_abort.bind( this );
+    xhr.onerror = HttpFetcher.handle_error.bind( this );
+    xhr.onload = HttpFetcher.handle_load.bind( this );
+    xhr.onloadend = HttpFetcher.handle_loadend.bind( this );
+    xhr.onloadstart = HttpFetcher.handle_loadstart.bind( this );
+    xhr.onprogress = HttpFetcher.handle_progress.bind( this );
+    xhr.onreadystatechange = HttpFetcher.handle_readystatechange.bind( this );
+    xhr.ontimeout = HttpFetcher.handle_timeout.bind( this );
+
     xhr.send( body );
 
     return resulter;
+  }
+
+  /**
+   * @param {HttpFetcher} this
+   */
+  static handle_abort( event ) {
+  }
+
+  /**
+   * @param {HttpFetcher} this
+   */
+  static handle_error( event ) {
+  }
+  
+  /**
+   * @param {HttpFetcher} this
+   */
+  static handle_load( event ) {
+  }
+
+  /**
+   * @param {HttpFetcher} this
+   */
+  static handle_loadend( event ) {
+  }
+
+  /**
+   * @param {HttpFetcher} this
+   */
+  static handle_loadstart( event ) {
+  }
+
+  /**
+   * @param {HttpFetcher} this
+   */
+  static handle_progress( event ) {
+  }
+
+  /**
+   * @param {HttpFetcher} this
+   */
+  static handle_readystatechange() {
+    let xhr = this.xhr;
+    if ( xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200 ) {
+      // Request finished. Do processing here.
+    }
+  }
+
+  /**
+   * @param {HttpFetcher} this
+   */
+  static handle_timeout( event ) {
   }
 
 }
