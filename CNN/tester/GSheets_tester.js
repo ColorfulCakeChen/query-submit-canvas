35a2d2ea0490eafabae13d8a143e77622f3498cc
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-02-22 15:21:26 +0800

    loadingMillisecondsMax = 1000

--- CNN/tester/GSheets_tester.js
+++ CNN/tester/GSheets_tester.js
@@ -244,23 +244,29 @@ const gTestCaseArray = [
 
   // Test ProgressEvent timeout.
   // (no retry)
-  new TestCase(  8, "",  2 * 1000, 0, -1, false ),
-  new TestCase(  9, "",  2 * 1000, 0,  0, false ),
-  new TestCase( 10, "",  2 * 1000, 0,  1, false ),
-  new TestCase( 11, "",  2 * 1000, 0,  2, false ),
+  new TestCase(  8, "",  1 * 1000, 0, -1, false ),
+  new TestCase(  9, "",  1 * 1000, 0,  0, false ),
+  new TestCase( 10, "",  1 * 1000, 0,  1, false ),
+  new TestCase( 11, "",  1 * 1000, 0,  2, false ),
+
+  // (no retry)
+  new TestCase( 12, "",  1 * 1000, 1, -1, false ),
+  new TestCase( 13, "",  1 * 1000, 1,  0, false ),
+  new TestCase( 14, "",  1 * 1000, 1,  1, false ),
+  new TestCase( 15, "",  1 * 1000, 1,  2, false ),
 
   // (two retry)
-  new TestCase( 12, "",  2 * 1000, 2, -1, false ),
-  new TestCase( 13, "",  2 * 1000, 2,  0, false ),
-  new TestCase( 14, "",  2 * 1000, 2,  1, false ),
-  new TestCase( 15, "",  2 * 1000, 2,  2, false ),
+  new TestCase( 16, "",  1 * 1000, 2, -1, false ),
+  new TestCase( 17, "",  1 * 1000, 2,  0, false ),
+  new TestCase( 18, "",  1 * 1000, 2,  1, false ),
+  new TestCase( 19, "",  1 * 1000, 2,  2, false ),
 
   // Test abort or succeeded.
   // (one retry)
-  new TestCase( 16, "", 30 * 1000, 1, -1,  true ),
-  new TestCase( 17, "", 30 * 1000, 1,  0, false ),
-  new TestCase( 18, "", 30 * 1000, 1,  1, false ),
-  new TestCase( 19, "", 30 * 1000, 1,  2, false ),
+  new TestCase( 20, "", 30 * 1000, 1, -1,  true ),
+  new TestCase( 21, "", 30 * 1000, 1,  0, false ),
+  new TestCase( 22, "", 30 * 1000, 1,  1, false ),
+  new TestCase( 23, "", 30 * 1000, 1,  2, false ),
 ];
 
 /**
