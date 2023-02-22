718ba2242b21490f760863d3a855b2a5d0a433e5
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-02-22 13:50:26 +0800

    Remark console.error( e )

--- CNN/util/GSheet/HttpFetcher.js
+++ CNN/util/GSheet/HttpFetcher.js
@@ -244,7 +244,7 @@ class HttpFetcher {
         if ( !bRetry ) {
           // Since no retry, the retry waiting timer should be completed to 100%
           HttpFetcher.progressRetryWaiting_set_whenDone.call( this );
-          console.error( e );
+          //console.error( e );
           throw e;
         }
       }
