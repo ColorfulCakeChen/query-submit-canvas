499914b94bd97ecdb60570acf83b002d663364b5
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-02-22 13:41:21 +0800

    bShouldProgress100

--- CNN/tester/GSheets_tester.js
+++ CNN/tester/GSheets_tester.js
@@ -206,10 +206,30 @@ class TestCase {
     urlComposer1 = null;
   }
 
+  /** */
+  toString() {
+    let str =
+        `spreadsheetId_postfix=${this.spreadsheetId_postfix}, `
+      + `loadingMillisecondsMax=${loadingMillisecondsMax}, `
+      + `retryTimesMax=${retryTimesMax}, `
+      + `abortAfterWhichYield=${abortAfterWhichYield}, `
+      + `bShouldProgress100=${bShouldProgress100}`
+    return str;
+  }
 }
 
+//
+// spreadsheetId_postfix,
+// loadingMillisecondsMax, retryTimesMax, abortAfterWhichYield, bShouldProgress100
+//
 const gTestCaseArray = [
-  new TestCase( "_not_exist", 10 * 1000, 1, false, false ),
+  new TestCase( "_not_exist", 10 * 1000, 1, -1, false ),
+  new TestCase( "_not_exist", 10 * 1000, 1,  0, false ),
+  new TestCase( "_not_exist", 10 * 1000, 1,  1, false ),
+
+  new TestCase( "", 30 * 1000, 1, -1,  true ),
+  new TestCase( "", 30 * 1000, 1,  0, false ),
+  new TestCase( "", 30 * 1000, 1,  1, false ),
 ];
 
 /**
@@ -254,49 +274,35 @@ async function* tester( progressParent ) {
 
     yield* testGenerator;
 
-//!!! ...unfinished... (2023/02/22)
-    testCase.bShouldProgress100
+    if ( testCase.bShouldProgress100 ) {
 
-    progressParent.child_disposeAll();
+      if ( progressParent.valuePercentage != 100 )
+        throw Error( `GSheets_tester.tester(): `
+          + `testCase={ ${testCase.toString()} }, `
+          + `progressParent.valuePercentage (${progressParent.valuePercentage} ) `
+          + `should be 100.` );
 
+    } else {
 
+      if ( progressParent.valuePercentage == 100 )
+        throw Error( `GSheets_tester.tester(): `
+          + `testCase={ ${testCase.toString()} }, `
+          + `progressParent.valuePercentage (${progressParent.valuePercentage} ) `
+          + `should not be 100.` );
+
+      // For failed network request (e.g. abort, error, load witho tatus 200,
+      // timeout), drop its (not 100%) progress so that the toal progress could
+      // still 100% after final succeeded network request.
+      //
+      // (So, there must be at least one TestCase (e.g. the last TestCase) is
+      // a succeeded network request.)
+      //
+      progressParent.child_disposeAll();
+    }
   }
 
-//!!! ...unfinished... (2023/02/14) timeout and re-try?
-  //const loadingMillisecondsMax = 2 * 1000; // 2 seconds.
-  //const loadingMillisecondsMax = 10 * 1000; // 10 seconds.
-  //const loadingMillisecondsMax = 2 * 60 * 1000; // 2 minutes.
-  //const loadingMillisecondsMax = 3 * 60 * 1000; // 3 minutes.
-  //const loadingMillisecondsMax = 10 * 60 * 1000; // 10 minutes.
-  const loadingMillisecondsMax = 30 * 60 * 1000; // 30 minutes.
-  //const loadingMillisecondsMax = 0; // no timeout.
-
-  const loadingMillisecondsInterval;
-
-  retryTimesMax,
-  retryWaitingSecondsExponentMax,
-  retryWaitingMillisecondsInterval,
-
-
-
-//!!!
-  let progressNotExist1 = progressParent.child_add(
-    ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
-
-  let progressNotExist2 = progressParent.child_add(
-    ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
-
-  let progress1 = progressParent.child_add(
-    ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
-
-  let progress11 = progressParent.child_add(
-    ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
-
-  let progress2 = progressParent.child_add(
-    ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
-
-  let progress21 = progressParent.child_add(
-    ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
+//!!! ...unfinished... (2023/02/22)
+// How to test error and re-try?
 
 //!!! ...unfinished... (2023/02/21)
 // How to test .abort() in loading and in retry waiting?
@@ -304,80 +310,113 @@ async function* tester( progressParent ) {
 //!!! ...unfinished... (2023/02/22)
 // simulate network offline.
 
-  // Without API key, and error.
-  let testerNotExist1 = GSheets.UrlComposer.Pool.get_or_create_by(
-    bLogFetcherEventToConsole, spreadsheetId + "_not_exist", range );
-
-  let fetcherNotExist1 = testerNotExist1
-    .JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
-      progressNotExist1, timeoutMilliseconds );
-  let resultNotExist1 = yield* fetcherNotExist1;
-
-  // With API key, and error.
-  let testerNotExist2 = GSheets.UrlComposer.Pool.get_or_create_by(
-    bLogFetcherEventToConsole, spreadsheetId + "_not_exist", range, apiKey );
-
-  let fetcherNotExist2 = testerNotExist2
-    .JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
-      progressNotExist2, timeoutMilliseconds );
-  let resultNotExist2 = yield* fetcherNotExist2;
-
-  testerNotExist2?.disposeResources_and_recycleToPool();
-  testerNotExist2 = null;
-
-  testerNotExist1?.disposeResources_and_recycleToPool();
-  testerNotExist1 = null;
 
 
-  // Without API key.
-  let tester1 = GSheets.UrlComposer.Pool.get_or_create_by(
-    bLogFetcherEventToConsole, spreadsheetId, range );
-  let fetcher1 = tester1.JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
-    progress1, timeoutMilliseconds );
-  let result1 = yield* fetcher1;
 
-  // With API key.
-  let tester2 = GSheets.UrlComposer.Pool.get_or_create_by(
-    bLogFetcherEventToConsole, spreadsheetId, range, apiKey );
-  let fetcher2 = tester2.JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
-    progress2, timeoutMilliseconds );
-  let result2 = yield* fetcher2;
+//!!! (2023/02/23 Remarked) Old Codes.
+//
+// //!!! ...unfinished... (2023/02/14) timeout and re-try?
+//   //const loadingMillisecondsMax = 2 * 1000; // 2 seconds.
+//   //const loadingMillisecondsMax = 10 * 1000; // 10 seconds.
+//   //const loadingMillisecondsMax = 2 * 60 * 1000; // 2 minutes.
+//   //const loadingMillisecondsMax = 3 * 60 * 1000; // 3 minutes.
+//   //const loadingMillisecondsMax = 10 * 60 * 1000; // 10 minutes.
+//   const loadingMillisecondsMax = 30 * 60 * 1000; // 30 minutes.
+//   //const loadingMillisecondsMax = 0; // no timeout.
+//
+//   let progressNotExist1 = progressParent.child_add(
+//     ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
+//
+//   let progressNotExist2 = progressParent.child_add(
+//     ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
+//
+//   let progress1 = progressParent.child_add(
+//     ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
+//
+//   let progress11 = progressParent.child_add(
+//     ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
+//
+//   let progress2 = progressParent.child_add(
+//     ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
+//
+//   let progress21 = progressParent.child_add(
+//     ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
+//
+//   // Without API key, and error.
+//   let testerNotExist1 = GSheets.UrlComposer.Pool.get_or_create_by(
+//     bLogFetcherEventToConsole, spreadsheetId + "_not_exist", range );
+//
+//   let fetcherNotExist1 = testerNotExist1
+//     .JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
+//       progressNotExist1, timeoutMilliseconds );
+//   let resultNotExist1 = yield* fetcherNotExist1;
+//
+//   // With API key, and error.
+//   let testerNotExist2 = GSheets.UrlComposer.Pool.get_or_create_by(
+//     bLogFetcherEventToConsole, spreadsheetId + "_not_exist", range, apiKey );
+//
+//   let fetcherNotExist2 = testerNotExist2
+//     .JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
+//       progressNotExist2, timeoutMilliseconds );
+//   let resultNotExist2 = yield* fetcherNotExist2;
+//
+//   testerNotExist2?.disposeResources_and_recycleToPool();
+//   testerNotExist2 = null;
+//
+//   testerNotExist1?.disposeResources_and_recycleToPool();
+//   testerNotExist1 = null;
+//
+//
+//   // Without API key.
+//   let tester1 = GSheets.UrlComposer.Pool.get_or_create_by(
+//     bLogFetcherEventToConsole, spreadsheetId, range );
+//   let fetcher1 = tester1.JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
+//     progress1, timeoutMilliseconds );
+//   let result1 = yield* fetcher1;
+//
+//   // With API key.
+//   let tester2 = GSheets.UrlComposer.Pool.get_or_create_by(
+//     bLogFetcherEventToConsole, spreadsheetId, range, apiKey );
+//   let fetcher2 = tester2.JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
+//     progress2, timeoutMilliseconds );
+//   let result2 = yield* fetcher2;
+//
+//   // Compare results: should the same.
+//   if ( !array2d_compare_EQ( result1, result2 ) )
+//     throw Error( `${result1} != ${result2}` );
+//
+//   // Test change range.
+//   if ( result1 ) {
+//     let newRange = result1[ 0 ][ 0 ];
+//     tester1.range = newRange;
+//     let fetcher11 = tester1.JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
+//       progress11, timeoutMilliseconds );
+//     let result11 = yield* fetcher11;
+//
+//     tester2.range = newRange;
+//     let fetcher21 = tester2.JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
+//       progress21, timeoutMilliseconds );
+//     let result21 = yield* fetcher21;
+//
+//     if ( result11 == null )
+//       throw Error( `result11( ${result11} ) should not be null.` );
+//
+//     // Note: If all cells are empty, GQViz got array with zero length.
+//     //       But APIv4 got undefined.
+//
+//     if ( !array2d_compare_EQ( result11, result21 ) )
+//       throw Error( `${result11} != ${result21}` );
+//
+//   } else {
+//     // (e.g. the nework is offline.)
+//   }
+//
+//   tester2.disposeResources_and_recycleToPool();
+//   tester2 = null;
+//
+//   tester1.disposeResources_and_recycleToPool();
+//   tester1 = null;
 
-  // Compare results: should the same.
-  if ( !array2d_compare_EQ( result1, result2 ) )
-    throw Error( `${result1} != ${result2}` );
-
-  // Test change range.
-  if ( result1 ) {
-    let newRange = result1[ 0 ][ 0 ];
-    tester1.range = newRange;
-    let fetcher11 = tester1.JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
-      progress11, timeoutMilliseconds );
-    let result11 = yield* fetcher11;
-
-    tester2.range = newRange;
-    let fetcher21 = tester2.JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
-      progress21, timeoutMilliseconds );
-    let result21 = yield* fetcher21;
-
-    if ( result11 == null )
-      throw Error( `result11( ${result11} ) should not be null.` );
-
-    // Note: If all cells are empty, GQViz got array with zero length.
-    //       But APIv4 got undefined.
-
-    if ( !array2d_compare_EQ( result11, result21 ) )
-      throw Error( `${result11} != ${result21}` );
-
-  } else {
-    // (e.g. the nework is offline.)
-  }
-
-  tester2.disposeResources_and_recycleToPool();
-  tester2 = null;
-
-  tester1.disposeResources_and_recycleToPool();
-  tester1 = null;
 
   console.log( "GSheet download testing... Done." );
 }
