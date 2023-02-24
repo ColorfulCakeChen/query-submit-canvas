d33a55c7b13b0c0f42f344ccb0caddd02e9e14d4
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-02-24 12:29:52 +0800

    (Invalid URL)

--- CNN/tester/GSheets_tester.js
+++ CNN/tester/GSheets_tester.js
@@ -417,13 +417,13 @@ const gTestCaseArray = new TestCaseArray();
 // should test ProgressEvent error. (how?)
 
   gTestCaseArray
-    .append_by( "https://", "", 10 * 1000, false ) // error. (Invalid URL)
-    .append_by( "https://", "",  0 * 1000, false ) // error. (Invalid URL) (no timeout)
-    .append_by( null, "_none",   10 * 1000, false ) // load (status != 200).
-    .append_by( null, "_none",    0 * 1000, false ) // load (status != 200). (no timeout)
-    .append_by( null, "",      0.01 * 1000, false ) // timeout.
-    .append_by( null, "",        30 * 1000,  true ) // succeeded.
-    .append_by( null, "",         0 * 1000,  true ) // succeeded. (no timeout)
+    .append_by( "http://none", "", 10 * 1000, false ) // error. (Invalid URL)
+    .append_by( "http://none", "",  0 * 1000, false ) // error. (Invalid URL) (no timeout)
+    .append_by( null, "_none",     10 * 1000, false ) // load (status != 200).
+    .append_by( null, "_none",      0 * 1000, false ) // load (status != 200). (no timeout)
+    .append_by( null, "",        0.01 * 1000, false ) // timeout.
+    .append_by( null, "",          30 * 1000,  true ) // succeeded.
+    .append_by( null, "",           0 * 1000,  true ) // succeeded. (no timeout)
     ;
 }
 
