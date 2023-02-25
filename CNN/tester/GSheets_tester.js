17e6e65c3b86cb1ee1ed590b9275aa4e374de37a
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-02-25 14:52:17 +0800

    bShouldProgress100_by

--- CNN/tester/GSheets_tester.js
+++ CNN/tester/GSheets_tester.js
@@ -117,6 +117,16 @@ class AbortTestMode {
       return true;
 
     // Since expected succeeded, the test case should be succeeded even if
+    // we want to abort during loading phase after the 2nd .next().
+    //
+    // The reason is that the request may always have been succeeded before
+    // .abort() is called.
+    //
+    if ( this.wantAbort_DuringLoading ) // in loading phase.
+      if ( this.afterHowManyNext >= 2 ) // too late to .abort()
+        return true;
+
+    // Since expected succeeded, the test case should be succeeded even if
     // we want to abort during retry waiting phase.
     //
     // The reason is that the test case never reaches retry waiting phase
@@ -444,7 +454,25 @@ class TestCaseArray extends Array {
         bShouldProgress100
           = abortTestMode.bShouldProgress100_by( bShouldProgress100Default );
 
-        if ( !bShouldProgress100 ) { // The test case expected to be failed.
+        if ( bShouldProgress100 ) {
+
+//!!! ...unfinished... (2023/02/25)
+
+          // Even if with .abort(), if the test case expected to be failed,
+          // do not infinite retry if .abort() is called in loading phase
+          // after the 2nd .next().
+          //
+          // The reason is that the request may be always failed before
+          // .abort() is called so that the retry will be forever.
+          //
+          if ( abortTestMode.wantAbort_DuringLoading ) // in loading phase.
+            if ( abortTestMode.afterHowManyNext >= 2 ) // too late to .abort()
+              continue; // Otherwise, the test case may never end.
+
+          if ( abortTestMode.wantAbort_DuringRetryWaiting ) // in retry waiting phase.
+            continue; // Otherwise, the test case may never end.
+ 
+        } else { // The test case expected to be failed.
           if ( retryTimesMax < 0 ) { // Infinite retry.
 
             // If the test case expected to be failed, do not infinite retry
@@ -611,118 +639,7 @@ async function* tester( progressParent ) {
   }
 
 //!!! ...unfinished... (2023/02/22)
-// Calling .abort() will not cause retry...
-
-//!!! ...unfinished... (2023/02/22)
 // simulate network offline.
-
-
-
-
-//!!! (2023/02/23 Remarked) Old Codes.
-//
-// //!!! ...unfinished... (2023/02/14) timeout and re-try?
-//   //const loadingMillisecondsMax = 2 * 1000; // 2 seconds.
-//   //const loadingMillisecondsMax = 10 * 1000; // 10 seconds.
-//   //const loadingMillisecondsMax = 2 * 60 * 1000; // 2 minutes.
-//   //const loadingMillisecondsMax = 3 * 60 * 1000; // 3 minutes.
-//   //const loadingMillisecondsMax = 10 * 60 * 1000; // 10 minutes.
-//   const loadingMillisecondsMax = 30 * 60 * 1000; // 30 minutes.
-//   //const loadingMillisecondsMax = 0; // no timeout.
-//
-//   let progressNotExist1 = progressParent.child_add(
-//     ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
-//
-//   let progressNotExist2 = progressParent.child_add(
-//     ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
-//
-//   let progress1 = progressParent.child_add(
-//     ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
-//
-//   let progress11 = progressParent.child_add(
-//     ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
-//
-//   let progress2 = progressParent.child_add(
-//     ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
-//
-//   let progress21 = progressParent.child_add(
-//     ValueMax.Percentage.Aggregate.Pool.get_or_create_by() );
-//
-//   // Without API key, and error.
-//   let testerNotExist1 = GSheets.UrlComposer.Pool.get_or_create_by(
-//     bLogFetcherEventToConsole, spreadsheetId + "_not_exist", range );
-//
-//   let fetcherNotExist1 = testerNotExist1
-//     .JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
-//       progressNotExist1, timeoutMilliseconds );
-//   let resultNotExist1 = yield* fetcherNotExist1;
-//
-//   // With API key, and error.
-//   let testerNotExist2 = GSheets.UrlComposer.Pool.get_or_create_by(
-//     bLogFetcherEventToConsole, spreadsheetId + "_not_exist", range, apiKey );
-//
-//   let fetcherNotExist2 = testerNotExist2
-//     .JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
-//       progressNotExist2, timeoutMilliseconds );
-//   let resultNotExist2 = yield* fetcherNotExist2;
-//
-//   testerNotExist2?.disposeResources_and_recycleToPool();
-//   testerNotExist2 = null;
-//
-//   testerNotExist1?.disposeResources_and_recycleToPool();
-//   testerNotExist1 = null;
-//
-//
-//   // Without API key.
-//   let tester1 = GSheets.UrlComposer.Pool.get_or_create_by(
-//     bLogFetcherEventToConsole, spreadsheetId, range );
-//   let fetcher1 = tester1.JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
-//     progress1, timeoutMilliseconds );
-//   let result1 = yield* fetcher1;
-//
-//   // With API key.
-//   let tester2 = GSheets.UrlComposer.Pool.get_or_create_by(
-//     bLogFetcherEventToConsole, spreadsheetId, range, apiKey );
-//   let fetcher2 = tester2.JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
-//     progress2, timeoutMilliseconds );
-//   let result2 = yield* fetcher2;
-//
-//   // Compare results: should the same.
-//   if ( !array2d_compare_EQ( result1, result2 ) )
-//     throw Error( `${result1} != ${result2}` );
-//
-//   // Test change range.
-//   if ( result1 ) {
-//     let newRange = result1[ 0 ][ 0 ];
-//     tester1.range = newRange;
-//     let fetcher11 = tester1.JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
-//       progress11, timeoutMilliseconds );
-//     let result11 = yield* fetcher11;
-//
-//     tester2.range = newRange;
-//     let fetcher21 = tester2.JSON_ColumnMajorArrayArray_fetch_asyncGenerator(
-//       progress21, timeoutMilliseconds );
-//     let result21 = yield* fetcher21;
-//
-//     if ( result11 == null )
-//       throw Error( `result11( ${result11} ) should not be null.` );
-//
-//     // Note: If all cells are empty, GQViz got array with zero length.
-//     //       But APIv4 got undefined.
-//
-//     if ( !array2d_compare_EQ( result11, result21 ) )
-//       throw Error( `${result11} != ${result21}` );
-//
-//   } else {
-//     // (e.g. the nework is offline.)
-//   }
-//
-//   tester2.disposeResources_and_recycleToPool();
-//   tester2 = null;
-//
-//   tester1.disposeResources_and_recycleToPool();
-//   tester1 = null;
-
 
   console.log( "GSheet download testing... Done." );
 }
