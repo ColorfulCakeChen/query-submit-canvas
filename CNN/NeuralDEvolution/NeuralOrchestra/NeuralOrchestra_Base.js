b9a30f60fbdc7537508f48c3ec848e51491910ac
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-03-13 14:02:51 +0800

    .versusResultSender_send() should be executed only if both .initOk and .versus_loadOk are true.

--- CNN/NeuralDEvolution/NeuralOrchestra/NeuralOrchestra_Base.js
+++ CNN/NeuralDEvolution/NeuralOrchestra/NeuralOrchestra_Base.js
@@ -1270,6 +1270,17 @@ class NeuralOrchestra_Base extends Recyclable.Root {
    */
   versusResultSender_send( nNegativeZeroPositive ) {
 
+    if ( !this.initOk )
+      throw Error( `NeuralOrchestra.Base.versusResultSender_send(): `
+        + `should be executed only if `
+        + `this.initOk ( ${this.initOk} ) is true.` );
+
+    if ( !this.versus_loadOk )
+      throw Error( `NeuralOrchestra.Base.versusResultSender_send(): `
+        + `should be executed only if `
+        + `this.versus_loadOk ( ${this.versus_loadOk} ) is true.` );
+
+
 //!!! ...unfinished... (2023/03/13)
 // should check the following?
 //
