48bb4c828ca7d87fbd2129bfa50b344dfeb76b88
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-03-17 15:21:32 +0800

    delayMilliseconds = 0

--- CNN/tester/NeuralOrchestra_tester.js
+++ CNN/tester/NeuralOrchestra_tester.js
@@ -35,7 +35,9 @@ class TestCase {
     // In the reentrance testing, when try await, the async method may have
     // been completed. To prevent they complete too fast to test, add some
     // delay time.
-    this.delayMilliseconds = 2 * 1000;
+//!!! (2023/03/17 Remarked) seems no longer necessary.
+//    this.delayMilliseconds = 2 * 1000;
+    this.delayMilliseconds = 0;
   }
 
   /**
