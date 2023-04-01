cbd832756aae9484299c394ff5c10f8f78122b81
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-04-01 09:44:23 +0800

    loadingYieldId, retryWaitingYieldId

--- CNN/util/HttpRequest/HttpRequest_Fetcher.js
+++ CNN/util/HttpRequest/HttpRequest_Fetcher.js
@@ -344,20 +344,6 @@ class HttpRequest_Fetcher
     this.loadingTimerPromise = null;
   }
 
-//!!! (2023/03/31 Remarked) Use loop instead of recursive.
-//   /**
-//    * Cancel current loadingTimer (if exists).
-//    *
-//    * @param {HttpRequest_Fetcher} this
-//    */
-//   static loadingTimer_cancel() {
-//     if ( !this.loadingTimerPromise )
-//       return;
-//
-//     this.loadingTimerPromise.cancelTimer(); // Stop timer.
-//     this.allPromiseSet.delete( this.loadingTimerPromise ); // Stop listening.
-//     this.loadingTimerPromise = null;
-//   }
 
   /** @return {boolean} Return true, if not yet reach maximum retry times. */
   get retryTimes_isRunOut() {
@@ -484,6 +470,13 @@ class HttpRequest_Fetcher
     // 0.3 Reset retry waiting progress.
     HttpRequest_Fetcher.retryWaitingMilliseconds_init.call( this );
     HttpRequest_Fetcher.progressRetryWaiting_set_beforeDone.call( this );
+
+//!!! ...unfinished... (2023/04/01) should define:
+// loadingYieldIdCurrent
+// loadingYieldIdFinal
+//
+// retryWaitingYieldIdCurrent
+// retryWaitingYieldIdFinal
 
     // 0.4 Inform outside caller progress when begin loading.
     yield this.progressRoot;
@@ -614,182 +607,6 @@ class HttpRequest_Fetcher
     return xhr.response;
   }
 
-//!!! (2023/03/31 Remarked) Use loop instead of recursive.
-//   /**
-//    * An async generator for sending a http request and tracking its progress
-//    * (without retry).
-//    *
-//    * (This method is called by .fetch_asyncGenerator())
-//    *
-//    *
-//    * @param {HttpRequest_Fetcher} this
-//    *
-//    * @param {ValueMax.Percentage.Concrete} this.progressLoading
-//    *   This .progressLoading will be increased when every time advanced. The
-//    * this.progressRoot will be returned when every time yield.
-//    *
-//    * @return {AsyncGenerator}
-//    *   Return an async generator for receving result from XMLHttpRequest.
-//    *
-//    * @yield {Promise( ValueMax.Percentage.Aggregate )}
-//    *   Yield a promise resolves to { done: false, value: this.progressRoot }.
-//    *
-//    * @yield {Promise( object )}
-//    *   Yield a promise resolves to { done: true, value: xhr.response }.
-//    *
-//    * @throws {ProgressEvent}
-//    *   Yield a promise rejects to ProgressEvent. The ProgressEvent.type may be:
-//    *     - "abort"
-//    *     - "error"
-//    *     - "load": when ( status != 200 ) (e.g. 404 or 500).
-//    *     - "timeout"
-//    * Note: Although they all represent the request is failed, however, the
-//    * ( .progressLoading.valuePercentage == 100 ) will still be reported for
-//    * representing the request already done (with failure, though).
-//    */
-//   static async* load_asyncGenerator() {
-//
-//     // 0.
-//
-//     // 0.1
-//     let progressLoading_max_default;
-//
-//     if ( this.loadingTimer_isUsed ) { // Use timeout time as progress target.
-//       progressLoading_max_default = this.loadingMillisecondsMax;
-//       this.loadingMillisecondsCur = 0;
-//     } else { // Use total content length (perhaps unknown) as progress target.
-//       progressLoading_max_default = HttpRequest_Fetcher.progressTotalFakeLarger;
-//       this.loadingMillisecondsCur = undefined;
-//     }
-//
-//     this.progressLoading.value_max_set( progressLoading_max_default );
-//
-//     // 0.2
-//     this.contentLoaded = undefined;
-//     this.contentTotal = undefined;
-//
-//     // 1.
-//
-//     // 1.1
-//     const xhr = this.xhr = new XMLHttpRequest();
-//     xhr.open( this.method, this.url, true );
-//     xhr.timeout = this.loadingMillisecondsMax;
-//     xhr.responseType = this.responseType;
-//
-//     // 1.2 Prepare promises before sending it.
-//     this.abortPromise = PartTime.Promise_create_by_addEventListener_once(
-//       xhr, "abort", HttpRequest_Fetcher.handle_abort, this );
-//
-//     this.errorPromise = PartTime.Promise_create_by_addEventListener_once(
-//       xhr, "error", HttpRequest_Fetcher.handle_error, this );
-//
-//     this.loadPromise = PartTime.Promise_create_by_addEventListener_once(
-//       xhr, "load", HttpRequest_Fetcher.handle_load, this );
-//
-//     this.loadstartPromise = PartTime.Promise_create_by_addEventListener_once(
-//       xhr, "loadstart", HttpRequest_Fetcher.handle_loadstart, this );
-//
-//     this.progressPromise = PartTime.Promise_create_by_addEventListener_once(
-//       xhr, "progress", HttpRequest_Fetcher.handle_progress, this );
-//
-//     this.timeoutPromise = PartTime.Promise_create_by_addEventListener_once(
-//       xhr, "timeout", HttpRequest_Fetcher.handle_timeout, this );
-//
-//     if ( this.loadingTimer_isUsed ) {
-//       HttpRequest_Fetcher.loadingTimerPromise_create_and_set.call( this );
-//     }
-//
-//     // All promises to be listened.
-//     {
-//       this.allPromiseSet.clear();
-//
-//       this.allPromiseSet.add( this.abortPromise );
-//       this.allPromiseSet.add( this.errorPromise );
-//       this.allPromiseSet.add( this.loadPromise );
-//       this.allPromiseSet.add( this.loadstartPromise );
-//       this.allPromiseSet.add( this.progressPromise );
-//       this.allPromiseSet.add( this.timeoutPromise );
-//
-//       if ( this.loadingTimerPromise )
-//         this.allPromiseSet.add( this.loadingTimerPromise );
-//     }
-//
-//     // 1.3
-//     xhr.send( this.body );
-//
-//     // Abort immediately if caller requests. (For example,
-//     // HttpRequest_Fetcher.abort() may be called during retry waiting.)
-//     //
-//     // Note: Calling xhr.abort() only has effect after xhr.send(). Calling
-//     //       xhr.abort() before xhr.send() has no effect.
-//     //
-//     if ( this.bAbort ) {
-//       xhr.abort();
-//     }
-//
-//     // 1.4 Until done or failed.
-//     let notDone;
-//     do {
-//       let allPromise = Promise.race( this.allPromiseSet );
-//
-// //!!! ...unfinished... (2023/03/31)
-// // Perhaps, let .loadingTimerPromise resolved to
-// // HttpRequest_Fetcher.handle_loadingTimer (instead of progressRoot).
-// // So that it can be ditinguished.
-//
-//       // All succeeded promises resolve to progressRoot.
-//       // All failed promises reject to (i.e. throw exception of) ProgressEvent.
-//       let progressRoot = await allPromise;
-//       yield progressRoot;
-//
-//       // Not done, if:
-//       //   - ( .loadPromise still pending (i.e. still in waiting promises) ).
-//       //
-//       // Note: If .abort() is called, xhr.status will be changed (from 200)
-//       //       to 0 even if loading is succeeded. So,
-//       //         - Do not check ( xhr.status !== 200 ).
-//       //         - Just check .allPromiseSet.has( .loadPromise ) purely.
-//       //
-//       notDone = ( this.allPromiseSet.has( this.loadPromise ) );
-//
-//     // Stop if loading completely and successfully.
-//     //
-//     // Note: The other ways to leave this loop are throwing exceptions (e.g.
-//     //       the pending promises rejected).
-//     } while ( notDone );
-//
-//     // 2. 
-//     // (2023/02/15) For debug.
-//     // (When execution to here, the request should have finished successfully.)
-//     {
-//       // (2023/02/24 Remarked)
-//       // If .abort() is called, xhr.status will be changed (from 200) to 0 even
-//       // if loading is succeeded. So, do not check xhr.status whether 200.
-//       //
-//       // // 2.1
-//       // if ( 200 !== xhr.status ) {
-//       //   //debugger;
-//       //   throw Error( `( ${this.url} ) HttpRequest_Fetcher`
-//       //     + `.load_asyncGenerator(): `
-//       //     + `When done, `
-//       //     + `xhr.status ( ${xhr.status} ) should be 200.` );
-//       // }
-//
-//       // 2.2
-//       if ( 100 != this.progressLoading.valuePercentage ) {
-//         //debugger;
-//         throw Error( `( ${this.url} ) HttpRequest_Fetcher`
-//           + `.load_asyncGenerator(): `
-//           + `When done, `
-//           + `progressLoading.valuePercentage `
-//           + `( ${this.progressLoading.valuePercentage} ) should be 100.` );
-//       }
-//     }
-//
-//     // 3. Return the successfully downloaded result.
-//     return xhr.response;
-//   }
-
   /**
    * An async generator for tracking retry waiting timer progress.
    *
@@ -894,18 +711,6 @@ class HttpRequest_Fetcher
     //      overflow if too many times trigger).
     //
   }
-
-//!!! (2023/03/31 Remarked) Use loop instead of recursive.
-//   /**
-//    * @param {HttpRequest_Fetcher} this
-//    */
-//   static loadingTimerPromise_create_and_set() {
-//     const delayMilliseconds = this.loadingMillisecondsInterval;
-//     const deltaValue = delayMilliseconds;
-//     this.loadingTimerPromise = PartTime.Promise_create_by_setTimeout(
-//       delayMilliseconds, HttpRequest_Fetcher.handle_loadingTimer,
-//       this, deltaValue );
-//   }
 
   /**
    * @param {HttpRequest_Fetcher} this
@@ -1123,56 +928,6 @@ class HttpRequest_Fetcher
       }
     }
   }
-
-//!!! (2023/03/31 Remarked) Use loop instead of recursive.
-//   /**
-//    * @param {HttpRequest_Fetcher} this
-//    */
-//   static handle_loadingTimer( resolve, reject, deltaValue ) {
-//     this.loadingMillisecondsCur += deltaValue;
-//
-//     // 1.
-//     // Advance progress only if loadingTimer used.
-//     if ( this.loadingTimer_isUsed ) {
-//       this.progressLoading.value_set( this.loadingMillisecondsCur );
-//     }
-//
-//     // 2.
-//     let bAbort;
-//     if (   ( !this.bAbort )
-//         && ( this.allPromiseSet.has( this.loadingTimerPromise ) ) ) {
-//       bAbort = false;
-//
-//     // User abort. (i.e. .abort() or .loadingTimer_cancel() is called)
-//     } else {
-//       bAbort = true;
-//     }
-//
-//     // 3.
-//     if ( this.bLogEventToConsole )
-//       console.log( `( ${this.url} ) HttpRequest_Fetcher: loadingTimer: `
-//         + `loadingMillisecondsCur=${this.loadingMillisecondsCur}, `
-//         + `loadingMillisecondsMax=${this.loadingMillisecondsMax}, `
-//         + `progressLoading=${this.progressLoading.valuePercentage}%` );
-//
-//     resolve( this.progressRoot );
-//
-//     // Re-listen on repeatable succeeded event.
-//     {
-//       this.allPromiseSet.delete( this.loadingTimerPromise );
-//
-//       // If user abort, no need to re-generate promise.
-//       if ( bAbort ) {
-//         this.loadingTimerPromise = null;
-//
-//       // Before loadingTimer done, its event could happen many times.
-//       } else {
-//         // Re-generate a new promise for listening on it.
-//         HttpRequest_Fetcher.loadingTimerPromise_create_and_set.call( this );
-//         this.allPromiseSet.add( this.loadingTimerPromise );
-//       }
-//     }
-//   }
 
   /**
    * @param {HttpRequest_Fetcher} this
