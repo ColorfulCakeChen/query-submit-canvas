cb4f02ef613d19d1c64622ad2a5ae242d131aef1
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-04-01 15:02:44 +0800

    determine_byCurrentFinal

--- CNN/Unpacker/ValueDesc/ValueDesc_StartStopState.js
+++ CNN/Unpacker/ValueDesc/ValueDesc_StartStopState.js
@@ -3,10 +3,10 @@ export { StartStopState };
 import { Int } from "./ValueDesc_Base.js";
 
 /**
- * Describe start-stop state composed of current index and final index.
+ * Describe start-stop state.
  *
  *
- * Convert number value into integer between [ 0, 4 ] representing operation:
+ * Convert number value into integer between [ 0, 4 ] representing state:
  *   - 0: NOT_YET_STARTED
  *   - 1: STARTING
  *   - 2: STARTED
@@ -68,16 +68,28 @@ class StartStopState extends Int {
 
       if ( finalIndex == undefined ) {
 
+        if ( currentIndex == 0 )
+          return StartStopState.Singleton.Ids.STARTING; // (1)
+        else
+          return StartStopState.Singleton.Ids.STARTED; // (2)
+
 //!!! ...unfinished... (2023/04/01)
-        return StartStopState.Singleton.Ids.NOT_YET_STARTED; // (0)
 
       } else {
 
+        if ( currentIndex == finalIndex )
+          return StartStopState.Singleton.Ids.STOPPING; // (3)
+
+        if ( currentIndex > finalIndex )
+          return StartStopState.Singleton.Ids.STOPPED; // (4)
+
 //!!! ...unfinished... (2023/04/01)
-        throw Error( `ValueDesc.StartStopState.determine_byCurrentFinal(): `
-          + `finalIndex ( ${finalIndex} ) should be undefined `
-          + `when currentIndex ( ${currentIndex} ) is undefined.`
-        );
+        if ( currentIndex < finalIndex )
+          throw Error( `ValueDesc.StartStopState.determine_byCurrentFinal(): `
+            + `when finalIndex ( ${finalIndex} ) is not undefined, `
+            + `currentIndex ( ${currentIndex} ) should be either `
+            + `equal or greater than finalIndex.`
+          );
       }
 
     }
