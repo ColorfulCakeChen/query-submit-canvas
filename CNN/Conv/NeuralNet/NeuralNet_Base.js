759817bfb4e97b022eddb9805158f29eb730b7d8
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-04-15 12:11:44 +0800

    Tensor_restrict_to_InputValueRange_and_dispose

--- CNN/Conv/NeuralNet/NeuralNet_Base.js
+++ CNN/Conv/NeuralNet/NeuralNet_Base.js
@@ -566,24 +566,15 @@ class NeuralNet_Base extends Recyclable.Root {
       outputTensor = this.blockFinal.output0.realTensor;
 
 //!!! ...unfinished... (2023/04/15)
-// Restricting final output should have the same value range
-// as input (i.e. non-negative integer which can be used in embedding looking up).
-//
-// Implement it by tf.clipByValue( 0, vocabularyCountPerInputChannel )
-// (which is cheaper than tf.mod()) and tf.cast( "int32" ).
-//
-// This is useful if the output should become recurrent feedback of the next
-// times input.
-//
-// Note: This can not be implemented by pointwise20ActivationId (e.g.
-// CLIP_BY_VALUE_N0_P255, CLIP_BY_VALUE_N0_P1023, CLIP_BY_VALUE_N0_P65535,
-// CLIP_BY_VALUE_N0_P2POW20, CLIP_BY_VALUE_N0_P2POW24, ...)
-//
-// The reason are:
-//   - MobileNetV2_Xxx has add-input-to-output behind pointwise2.
-//   - non-MobileNetV2_Xxx has squeeze-and-excitation behind pointwise2.
-// They will destroy the activation function result.
-//
+      // Restricting final output to the same value range as input (i.e.
+      // non-negative integer which can be used in embedding looking up).
+      //
+      // This is useful if the output should become recurrent feedback of the next
+      // times input.
+      if ( ??? ) {
+        outputTensor = this.Tensor_restrict_to_InputValueRange_and_dispose(
+          outputTensor );
+      }
 
       progressToAdvance.value_advance();
       yield progressRoot;  // BlockFinal done. Report progress.
@@ -689,25 +680,6 @@ class NeuralNet_Base extends Recyclable.Root {
 //
 
 
-//!!! ...unfinished... (2023/04/15)
-// Restricting final output should have the same value range
-// as input (i.e. non-negative integer which can be used in embedding looking up).
-//
-// Implement it by tf.clipByValue( 0, vocabularyCountPerInputChannel - 1 )
-// (which is cheaper than tf.mod()) and tf.cast( "int32" ).
-//
-// This is useful if the output should become recurrent feedback of the next
-// times input.
-//
-// Note: This can not be implemented by pointwise20ActivationId (e.g.
-// CLIP_BY_VALUE_N0_P255, CLIP_BY_VALUE_N0_P1023, CLIP_BY_VALUE_N0_P65535,
-// CLIP_BY_VALUE_N0_P2POW20, CLIP_BY_VALUE_N0_P2POW24, ...)
-//
-// The reason are:
-//   - MobileNetV2_Xxx has add-input-to-output behind pointwise2.
-//   - non-MobileNetV2_Xxx has squeeze-and-excitation behind pointwise2.
-// They will destroy the activation function result.
-//
   /**
    * @param {tf.tensor3d} inputTensor
    *   The tensor to be restricted. It will always be disposed, no matter
@@ -726,8 +698,20 @@ class NeuralNet_Base extends Recyclable.Root {
     const valueMin = 0;
     const valueMax = this.vocabularyCountPerInputChannel - 1;
 
+    // Note: This can not be implemented by Block's pointwise20ActivationId
+    //       (e.g. CLIP_BY_VALUE_N0_P255, CLIP_BY_VALUE_N0_P65535,
+    //       CLIP_BY_VALUE_N0_P2POW20, CLIP_BY_VALUE_N0_P2POW24, ...)
+    //
+    // The reason are:
+    //   - MobileNetV2_Xxx has add-input-to-output behind pointwise2.
+    //   - non-MobileNetV2_Xxx has squeeze-and-excitation behind pointwise2.
+    // They will destroy the activation function result.
+    //
+
     try {
       // 1. Let value be in range.
+      //
+      // Note: tf.clipByValue() is cheaper than tf.mod()
       let valueClippedTensor = inputTensor.clipByValue( valueMin, valueMax );
 
       inputTensor.dispose(); // Release immediately to reduce memory footprint.
