ea98ecdd703df56ccd22bcd04b4f94490b7df8fb
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-04-16 09:37:01 +0800

    Improve comments.

--- CNN/Conv/Embedding/Embedding_Base.js
+++ CNN/Conv/Embedding/Embedding_Base.js
@@ -7,28 +7,38 @@ import * as ReturnOrClone from "../ReturnOrClone.js";
 import { Params } from "./Embedding_Params.js";
 
 /**
- * This is the base class of Embedding.AddGatherReshape and Embedding.SplitReshapeGatherConcat.
+ * This is the base class of Embedding.AddGatherReshape and
+ * Embedding.SplitReshapeGatherConcat.
  *
  *
- * Embedding could achieve non-linear mapping (just like any perceptron). But it is achieved by lookup table (instead
- * of weighted sum, bias and activation function). This implies:
- *   - It may use more (CPU or GPU) memory, but may use less (CPU or GPU) computation.
- *   - It can only achieve channel expansion, and can not achieve channel aggregation. (because no weighted sum)
- *   - It can only represent context-independent (not context-dependent) information. (because no weighted sum)
+ * Embedding could achieve non-linear mapping (just like any perceptron). But
+ * it is achieved by lookup table (instead of weighted sum, bias and activation
+ * function). This implies:
+ *   - It may use more (CPU or GPU) memory, but may use less (CPU or GPU)
+ *       computation.
+ *   - It can only achieve channel expansion, and can not achieve channel
+ *       aggregation. (because no weighted sum)
+ *   - It can only represent context-independent (not context-dependent)
+ *       information. (because no weighted sum)
  *   - It can only handle integer input (i.e. int32, not float32).
  *
- * It is useful as the first layer of text or image processing because their inputs are all integer (e.g. character codes,
- * word indices, color codes, etc). And, the first layer only needs carry context-independent information (and all the other
- * layers after it will produce context-dependent information).
+ * It is useful as the first layer of text or image processing because their
+ * inputs are all integer (e.g. character codes, word indices, color codes,
+ * etc). And, the first layer only needs carry context-independent information
+ * (and all the other layers after it will produce context-dependent information).
  *
  * This object always accepts tensor3d (dtype = int32).
- *   - The axis 0 is height. (Image height) (Text lines and usually only 1 line.)
+ *   - The axis 0 is height. (Image height) (Text lines and usually only 1
+ *       line.)
  *   - The axis 1 is width. (Image width) (Text length (e.g. character count).)
- *   - The axis 2 is channel. (Image color channel) (Text character code channel and usually only 1 channel.)
+ *   - The axis 2 is channel. (Image color channel) (Text character code
+ *       channel and usually only 1 channel.)
  *
- * An embedding layer contains one params (this.params) and inChannels embedding vocabulary tables.
+ * An embedding layer contains one params (this.params) and inChannels
+ * embedding vocabulary tables.
  *   - Every input channel has one embedding vocabulary table.
- *   - Every embedding vocabulary table has vocabularyCountPerInputChannel vocabularies.
+ *   - Every embedding vocabulary table has vocabularyCountPerInputChannel
+ *       vocabularies.
  *   - Every vocabulary has channelMultiplier embedding channels.
  *
  *
@@ -37,11 +47,13 @@ import { Params } from "./Embedding_Params.js";
  *  If true, this object initialized (i.e. initer()) successfully.
  *
  * @member {number} weightElementOffsetBegin
- *   The position which is started (inclusive) to extract from inputWeightArray by initer().
+ *   The position which is started (inclusive) to extract from inputWeightArray
+ * by initer().
  *
  * @member {number} weightElementOffsetEnd
- *   The position which is ended to (non-inclusive) extract from inputWeightArray by initer(). Where to extract next weights.
- * Only meaningful when ( this.bInitOk == true ).
+ *   The position which is ended to (non-inclusive) extract from
+ * inputWeightArray by initer(). Where to extract next weights. Only meaningful
+ * when ( this.bInitOk == true ).
  * 
  * @member {ActivationEscaping.ScaleBoundsArray} output_scaleBoundsArray
  *   The element value bounds (per channel) of output (can NOT null).
@@ -50,9 +62,11 @@ import { Params } from "./Embedding_Params.js";
 class Embedding_Base extends Recyclable.Base( ReturnOrClone.Root ) {
 
   /**
-   * Used as default Embedding.Base provider for conforming to Recyclable interface.
+   * Used as default Embedding.Base provider for conforming to Recyclable
+   * interface.
    */
-  static Pool = new Pool.Root( "Embedding.Base.Pool", Embedding_Base, Embedding_Base.setAsConstructor );
+  static Pool = new Pool.Root( "Embedding.Base.Pool",
+    Embedding_Base, Embedding_Base.setAsConstructor );
 
   /**
    */
@@ -110,8 +124,8 @@ class Embedding_Base extends Recyclable.Base( ReturnOrClone.Root ) {
       ;
 
     let progressRoot = progressParent.root_get();
-    let progressToAdvance = progressParent.child_add(
-      ValueMax.Percentage.Concrete.Pool.get_or_create_by( progressMax ) ); // For parameters extracting.
+    let progressToAdvance = progressParent.child_add( // For parameters extracting.
+      ValueMax.Percentage.Concrete.Pool.get_or_create_by( progressMax ) );
 
     // 1. Extract parameters.
     try {
@@ -124,7 +138,8 @@ class Embedding_Base extends Recyclable.Base( ReturnOrClone.Root ) {
 
       // Get parameters' real (adjusted) values.
       //
-      // Do not keep params in this.params so that the inputWeightArray could be released.
+      // Do not keep params in this.params so that the inputWeightArray could
+      // be released.
       this.input_height = params.input_height;
       this.input_width = params.input_width;
       this.input_channelCount = params.input_channelCount;
@@ -160,23 +175,33 @@ class Embedding_Base extends Recyclable.Base( ReturnOrClone.Root ) {
   }
 
   /**
-   * Initialize this object by calling initer() and advance the generator by loop until done.
+   * Initialize this object by calling initer() and advance the generator by
+   * looping until done.
    *
    * @return {boolean}
-   *   Return true, if succeeded (and progressParent.valuePercentage will be equal to 100).
-   *   Return false, if failed (and progressParent.valuePercentage will be less than 100).
+   *   - Return true, if succeeded (and progressParent.valuePercentage will be
+   *       equal to 100).
+   *   - Return false, if failed (and progressParent.valuePercentage will be
+   *       less than 100).
    *
    * @see Block.Base.init()
    */
   init( progressParent, inputWeightArray, weightElementOffsetBegin, params ) {
 
-    let initer = this.initer( progressParent, inputWeightArray, weightElementOffsetBegin, params );
+    let initer = this.initer( progressParent,
+      inputWeightArray, weightElementOffsetBegin, params );
+
     let initerNext;
     do {
       initerNext = initer.next();
-    } while ( ! initerNext.done ); // When ( false == initerNext.done ), the ( initerNext.value ) will be progressParent.root_get().
 
-    let bInitOk = initerNext.value; // When ( true == initerNext.done ), the ( initerNext.value ) will be initialization successfully or failed.
+    // When ( false == initerNext.done ), the ( initerNext.value ) will be
+    // progressParent.root_get().
+    } while ( ! initerNext.done );
+
+    // When ( true == initerNext.done ), the ( initerNext.value ) will be
+    // initialization successfully or failed.
+    let bInitOk = initerNext.value;
     return bInitOk;
   }
 
@@ -213,7 +238,8 @@ class Embedding_Base extends Recyclable.Base( ReturnOrClone.Root ) {
   }
 
   /**
-   * @return {string} The description string of all (adjusted) parameters of initer().
+   * @return {string} The description string of all (adjusted) parameters of
+   * initer().
    *
    * @override
    */
