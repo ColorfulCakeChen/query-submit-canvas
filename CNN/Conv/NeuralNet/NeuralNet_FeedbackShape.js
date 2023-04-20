9e304776a9c5e5788549ceebc9ec8fa66c2608e4
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-04-20 08:59:23 +0800

    Explicit/Implicit Input/Output

--- CNN/Conv/NeuralNet/NeuralNet_FeedbackShape.js
+++ CNN/Conv/NeuralNet/NeuralNet_FeedbackShape.js
@@ -6,7 +6,53 @@ export { NeuralNet_FeedbackShape as FeedbackShape };
  *
  *
  *
- * 1. Explicit/Implicit Input/Output
+ * 1. Where to feedback 
+ *
+ * Where to place recurrent feedback information (i.e. previous time output) in
+ * the next time input?
+ *
+ *
+ * 1.1 From convolution operation's point of view:
+ *
+ *   - Neighbor pixels of the same channel can be moved to different channel
+ *       of the same pixel by pointwise and then depthwise convolution filter.
+ *
+ *   - Different channel of the same pixel can also be moved to neighbor pixels
+ *       of the same channel by depthwise and then pointwise convolution filter.
+ *
+ *   - So, it seems both feasible to place recurrent feedback information
+ *       either on the neighbor pixels of the same channel or on the different
+ *       channel of the same pixel.
+ *
+ *
+ * 1.2 From information dimension's point of view:
+ *
+ *   - Neighbor pixels on the same channel has two dimensions information
+ *       (top-bottom, left-right).
+ *
+ *   - Different channels of the same pixel has only one dimension (front-rear).
+ *
+ *   - So, it seems better a little to place recurrent feedback information as
+ *       on the neighbor pixels of the same channel.
+ *
+ *
+ * 1.3 From image's point of view:
+ *
+ *   - Channels are originally used to represent the relevant information of
+ *       the same pixel.
+ *
+ *   - So, it should view the previous output as channels group. For example,
+ *     - Suppose: Only 6 output values are needed, and per pixel has 4 channel.
+ *     - Then: 24 (= 6 * 4 ) values should be outputted.
+ *       - Element 0, 4, 8, 12, 16, 20 are explicit output values.
+ *       - Element 1, 2, 3 are implicit output values of element 0.
+ *       - Element 5, 6, 7 are implicit output values of element 4.
+ *       - ...
+ *     - Both explicit and implicit output values are used as recurrent
+ *         feedback information.
+ *
+ *
+ * 2. Explicit/Implicit Input/Output
  *
  *   - Input: It is all the data which will be sent to and processed by a
  *       neural network.
@@ -30,55 +76,26 @@ export { NeuralNet_FeedbackShape as FeedbackShape };
  *         time input.
  *
  *
- * 2. Where to feedback 
- *
- * Where to place recurrent feedback information (i.e. previous time output) in
- * the next time input?
- *
- *
- * 2.1 From convolution operation's point of view:
- *
- *   - Neighbor pixels of the same channel can be moved to different channel
- *       of the same pixel by pointwise and then depthwise convolution filter.
- *
- *   - Different channel of the same pixel can also be moved to neighbor pixels
- *       of the same channel by depthwise and then pointwise convolution filter.
- *
- *   - So, it seems both feasible to place recurrent feedback information
- *       either on the neighbor pixels of the same channel or on the different
- *       channel of the same pixel.
- *
- *
- * 2.2 From information dimension's point of view:
- *
- *   - Neighbor pixels on the same channel has two dimensions information
- *       (top-bottom, left-right).
- *
- *   - Different channels of the same pixel has only one dimension (front-rear).
- *
- *   - So, it seems better a little to place recurrent feedback information as
- *       on the neighbor pixels of the same channel.
- *
- *
- * 2.3 From image's point of view:
- *
- *   - Channels are originally used to represent the relevant information of
- *       the same pixel.
- *
- *   - So, it should view the previous output as channels group. For example,
- *     - Suppose: Only 6 output values are needed, and per pixel has 4 channel.
- *     - Then: 24 (= 6 * 4 ) values should be outputted.
- *       - Element 0, 4, 8, 12, 16, 20 are explicit output values.
- *       - Element 1, 2, 3 are implicit output values of element 0.
- *       - Element 5, 6, 7 are implicit output values of element 4.
- *       - ...
- *     - Both explicit and implicit output values are used as recurrent
- *         feedback information.
- *
- *
  * 3. Shape
  *
+ *
+ * 3.1 Implicit input shape
+ *
+
 //!!! ...unfinished... (2023/04/17)
+
+ *
+ *
+ * 3.2 Implicit output shape
+ *
+ * 
+ *
+ *
+ *
+ *
+
+//!!! ...unfinished... (2023/04/17)
+
  * 
  *
  *
