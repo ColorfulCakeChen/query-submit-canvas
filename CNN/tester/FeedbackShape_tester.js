f27b999daed0a3d9a9e20704e2a55b65005698f3
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-04-25 15:45:45 +0800

    FeedbackShape_tester

--- CNN/tester/FeedbackShape_tester.js
+++ CNN/tester/FeedbackShape_tester.js
@@ -20,8 +20,15 @@ async function* tester( progressParent ) {
 //!!! ...unfinished... (2023/04/25)
 //   let progressToAdvance = progressParent.child_add(
 //     ValueMax.Percentage.Concrete.Pool.get_or_create_by( 1 ) );
-//
-//
+
+
+//!!! ...unfinished... (2023/04/25)
+// test:
+//   - init()
+//   - set_implicit_input_by_previous_output()
+//   - valueArray_get_from_output_valueArray_1st_channel()
+
+
 //   progressToAdvance.value_advance();
 //   yield progressRoot;
 
