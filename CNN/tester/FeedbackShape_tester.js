075af02f8ac149cfff59c4f16815befb29775b08
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-04-29 10:59:04 +0800

    from_valueIndex

--- CNN/tester/FeedbackShape_tester.js
+++ CNN/tester/FeedbackShape_tester.js
@@ -761,7 +761,7 @@ class TestCase {
                   const from_valueIndex = from_valueIndex_base_yx + c;
                   const to_valueIndex = to_valueIndex_base_yx + c;
 
-                  if ( from_valueIndex_base_yx > area_from_valueCount_original ) {
+                  if ( from_valueIndex > area_from_valueCount_original ) {
                     if ( to_inputArray[ to_valueIndex ] != 0 )
                       throw Error( `FeedbackShape_tester.TestCase.${funcNameInMessage}(): `
                         + `to_inputArray[ ${to_valueIndex} ]=`
