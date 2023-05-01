590219ba6ef479d655e9ea24ad4fc67ec605267a
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-05-01 10:33:08 +0800

    target_channelCount

--- CNN/Conv/NeuralNet/NeuralNet_ScaleFillTensor.js
+++ CNN/Conv/NeuralNet/NeuralNet_ScaleFillTensor.js
@@ -32,16 +32,18 @@ class NeuralNet_ScaleFillTensor {
   scale_fill_tensor(
     source_TypedArray,
     source_height, source_width, //source_channelCount,
-    target_height, target_width, //target_channelCount,
+    target_height, target_width, target_channelCount,
 
   ) {
     const funcNameInMessage = "scale_fill_tensor";
 
+    // 1.
     const source_size_per_channel = source_height * source_width;
 
-    if ( ( source_TypedArray.length % source_size_per_channel ) != 0 )
+    const source_valueCount = source_TypedArray.length;
+    if ( ( source_valueCount % source_size_per_channel ) != 0 )
       throw Error( `NeuralNet_ScaleFillTensor.${funcNameInMessage}(): `
-        + `source_TypedArray.length ( ${source_TypedArray.length} ) ` 
+        + `source_TypedArray.length ( ${source_valueCount} ) ` 
         + `should be divisible by `
         + `source_size_per_channel ( `
         + `= source_height * source_width `
@@ -49,9 +51,63 @@ class NeuralNet_ScaleFillTensor {
         + `= ${source_size_per_channel} ).`
       );
 
-    let source_channelCount
-      = source_TypedArray.length / source_size_per_channel;
+    let source_channelCount = source_valueCount / source_size_per_channel;
 
+    if ( source_channelCount != target_channelCount )
+      throw Error( `NeuralNet_ScaleFillTensor.${funcNameInMessage}(): `
+        + `source_size_per_channel ( `
+        + `= source_TypedArray.length / ( source_height * source_width ) ` 
+        + `= ${source_valueCount} / ( ${source_height} * ${source_width} ) `
+        + `= ${source_valueCount} / ( ${source_size_per_channel} ) `
+        + `= ${source_channelCount} ) `
+        + `should be the same as `
+        + `target_channelCount ( ${target_channelCount} ).`
+      );
+
+    // 2. Scale
+    if (   ( source_height != target_height )
+        || ( source_width != target_width ) ) {
+
+      let source_shape = [ source_height, source_width, source_channelCount ];
+      let sourceTensor = tf.tensor3d( source_TypedArray, source_shape, "int32" );
+
+//!!! ...unfinished... (2023/05/01)
+// should scale
+
+
+      // Otherwise, resize to the default size (height x width) which is the input
+      // image size used for training the neural network.
+      //
+      let scaledSourceTensorFloat32;
+      try {
+        scaledSourceTensorFloat32 = tf.image.resizeBilinear(
+          sourceTensor, this.input_height_width_array,
+          true // ( alignCorners = true ) for visual image resizing.
+        );
+      } catch ( e ) {
+        throw e; // e.g. out of (GPU) memory.
+      } finally {
+        sourceTensor.dispose();
+      }
+
+      if ( !bForceInt32 )
+        return scaledSourceTensorFloat32;
+
+      // Convert to int32 if necessary. (Note: The dtype of tf.image.resizeXxx()'s
+      // result is float32.)
+      try {
+        let scaledSourceTensorInt32 = scaledSourceTensorFloat32.cast( "int32" );
+        return scaledSourceTensorInt32;
+      } catch ( e ) {
+        throw e; // e.g. out of (GPU) memory.
+      } finally {
+        scaledSourceTensorFloat32.dispose();
+      }
+
+    }
+
+
+//!!! ...unfinished... (2023/05/01)
 
   }
 
