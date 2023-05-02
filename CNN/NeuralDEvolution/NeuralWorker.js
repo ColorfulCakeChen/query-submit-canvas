b6eb56c485bb04b53199390b4d61769e2fa1efc7
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-05-02 09:02:26 +0800

    NeuralWorker_Params

--- CNN/NeuralDEvolution/NeuralWorker.js
+++ CNN/NeuralDEvolution/NeuralWorker.js
@@ -1,4 +1,5 @@
 export * as Common from "./NeuralWorker/NeuralWorker_Common.js";
 export { Mode } from "./NeuralWorker/NeuralWorker_Mode.js";
+export { Params } from "./NeuralWorker/NeuralWorker_Params.js";
 export { Proxy } from "./NeuralWorker/NeuralWorker_Proxy.js";
 export { Proxies } from "./NeuralWorker/NeuralWorker_Proxies.js";
---
+++ CNN/NeuralDEvolution/NeuralWorker/NeuralWorker_Params.js
@@ -0,0 +1,7 @@
+export { NeuralWorker_Params as Params };
+
+
+!!! ...unfinished... (2023.05/02)
+// The interpretation of from_output_valueArray (which contains one or
+// two alignments) should be according to NeuralWorker_Mode.
+// So, these Feedback related operations should belong to NeuralWorker.
