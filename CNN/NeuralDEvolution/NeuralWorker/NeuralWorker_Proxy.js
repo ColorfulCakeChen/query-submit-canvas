79956003df8f683221fd82b5a7d75ca4e692f1a2
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-05-03 20:09:00 +0800

    TypedArray_process_async

--- CNN/NeuralDEvolution/NeuralWorker/NeuralWorker_Proxy.js
+++ CNN/NeuralDEvolution/NeuralWorker/NeuralWorker_Proxy.js
@@ -180,6 +180,32 @@ class NeuralWorker_Proxy extends AsyncWorker.Proxy {
     );
   }
 
+  /**
+   * 
+   *   - When training neural networks, NeuralWorker_Proxies is used.
+   *
+   *   - When real usage after training complete, NeuralWorker_Proxy should be
+   *       used.
+   *
+   *     - This method should be used when real usage. It calls
+   *         .TWO_WORKER__ONE_SCALE__step1_TypedArray_process_async()
+   *         internally because:
+   *
+   *       - It does not yield (i.e. transfer back) any thing. That is, it
+   *         could fill alignment mark and feedback but will not post back
+   *         source TypedArray.
+   *
+   *       - It return a TypedArray (not an array [ TypedArray, TypedArray ] ).
+   *
+   */
+  TypedArray_process_async(
+    source_TypedArray, source_height, source_width, bFill ) {
+    return this.TWO_WORKER__ONE_SCALE__step1_TypedArray_process_async(
+      source_TypedArray, source_height, source_width, bFill
+    );
+  }
+
+
 
   /**
    * Process input image data by all (suppose two) neural networks in this web
@@ -365,23 +391,6 @@ class NeuralWorker_Proxy extends AsyncWorker.Proxy {
     );
   }
 
-
-
-//!!! ...unfinished... (2023/05/02)
-// When training neural networks, NeuralWorker_Proxies is used.
-// When real usage after training complete, NeuralWorker_Proxy should be used.
-//
-// However, what kind of TypedArray_process_Xxx() should be called when real
-// usage?
-// It needs fill alignment mark and feedback but needs not post back source
-// TypedArray.
-//
-// Perhaps,
-// NeuralWorker.TWO_WORKER__ONE_SCALE__step1_TypedArray_process_async()
-// is the most suitable because:
-//   - It does not yield (i.e. transfer back) any thing.
-//   - It return a TypedArray (not an array [ TypedArray, TypedArray ] ).
-//
 
   /**
    * This method is used for:
