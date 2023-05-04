a6339462386dac2ff3f555002974aa4f17bbc352
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-05-04 11:46:06 +0800

    neuralNetCount

--- CNN/NeuralDEvolution/NeuralWorker/NeuralWorker_Mode.js
+++ CNN/NeuralDEvolution/NeuralWorker/NeuralWorker_Mode.js
@@ -22,34 +22,34 @@ class NeuralWorker_Mode extends Int {
     super( 0, 6,
       {
         ONE_WORKER__ONE_SCALE__FILL:             new NeuralWorker_Mode.Info(
-          0, "ONE_WORKER__ONE_SCALE__FILL",             1,
+          0, "ONE_WORKER__ONE_SCALE__FILL",             1, 2,
           true, undefined ),
 
         ONE_WORKER__ONE_SCALE__NO_FILL:          new NeuralWorker_Mode.Info(
-          1, "ONE_WORKER__ONE_SCALE__NO_FILL",          1,
+          1, "ONE_WORKER__ONE_SCALE__NO_FILL",          1, 2,
           false, undefined ),
 
 
         TWO_WORKER__ONE_SCALE__FILL__APPLY:      new NeuralWorker_Mode.Info(
-          2, "TWO_WORKER__ONE_SCALE__FILL__APPLY",      2,
+          2, "TWO_WORKER__ONE_SCALE__FILL__APPLY",      2, 2,
           true,  true ),
 
         TWO_WORKER__ONE_SCALE__FILL__APPLIER:    new NeuralWorker_Mode.Info(
-          3, "TWO_WORKER__ONE_SCALE__FILL__APPLIER",    2,
+          3, "TWO_WORKER__ONE_SCALE__FILL__APPLIER",    2, 2,
           true, false ),
 
 
         TWO_WORKER__ONE_SCALE__NO_FILL__APPLY:   new NeuralWorker_Mode.Info(
-          4, "TWO_WORKER__ONE_SCALE__NO_FILL__APPLY",   2,
+          4, "TWO_WORKER__ONE_SCALE__NO_FILL__APPLY",   2, 2,
           false,  true ),
 
         TWO_WORKER__ONE_SCALE__NO_FILL__APPLIER: new NeuralWorker_Mode.Info(
-          5, "TWO_WORKER__ONE_SCALE__NO_FILL__APPLIER", 2,
+          5, "TWO_WORKER__ONE_SCALE__NO_FILL__APPLIER", 2, 2,
           false, false ),
 
 
         TWO_WORKER__TWO_SCALE__NO_FILL:          new NeuralWorker_Mode.Info(
-          6, "TWO_WORKER__TWO_SCALE__NO_FILL",          2,
+          6, "TWO_WORKER__TWO_SCALE__NO_FILL",          2, 2,
           false, undefined ),
 
 //!!! ...unfinished... (2023/05/03) TWO_WORKER__TWO_SCALE__FILL?
@@ -66,10 +66,12 @@ class NeuralWorker_Mode extends Int {
 
   /**
    * @param {number} nNeuralWorker_ModeId
-   *   The numeric identifier of NeuralWorker_Mode. (NeuralWorker.Mode.Singleton.Ids.Xxx)
+   *   The numeric identifier of NeuralWorker_Mode.
+   * (NeuralWorker.Mode.Singleton.Ids.Xxx)
    *
    * @return {number}
-   *   The web worker count (1 or 2) of the NeuralWorker_Mode.Singleton.Ids.Xxx.
+   *   The web worker count (1 or 2) of the
+   * NeuralWorker_Mode.Singleton.Ids.Xxx.
    */
   static workerCount_get( nNeuralWorker_ModeId ) {
     let info = NeuralWorker_Mode.Singleton.getInfo_byId( nNeuralWorker_ModeId );
@@ -80,10 +82,28 @@ class NeuralWorker_Mode extends Int {
 
   /**
    * @param {number} nNeuralWorker_ModeId
-   *   The numeric identifier of NeuralWorker_Mode. (NeuralWorker.Mode.Singleton.Ids.Xxx)
+   *   The numeric identifier of NeuralWorker_Mode.
+   * (NeuralWorker.Mode.Singleton.Ids.Xxx)
+   *
+   * @return {number}
+   *   The neural network count (1 or 2) of the
+   * NeuralWorker_Mode.Singleton.Ids.Xxx.
+   */
+  static neuralNetCount_get( nNeuralWorker_ModeId ) {
+    let info = NeuralWorker_Mode.Singleton.getInfo_byId( nNeuralWorker_ModeId );
+    if ( info )
+      return info.neuralNetCount;
+    return NaN;
+  }
+ 
+  /**
+   * @param {number} nNeuralWorker_ModeId
+   *   The numeric identifier of NeuralWorker_Mode.
+   * (NeuralWorker.Mode.Singleton.Ids.Xxx)
    *
    * @return {boolean}
-   *   Whether the worker mode will fill alignment mark in image before process it.
+   *   Whether the neural worker mode will fill alignment mark and feedback
+   * (i.e. previous time output) into image before processing it.
    */
   static bFill_get( nNeuralWorker_ModeId ) {
     let info = NeuralWorker_Mode.Singleton.getInfo_byId( nNeuralWorker_ModeId );
@@ -94,7 +114,8 @@ class NeuralWorker_Mode extends Int {
 
   /**
    * @param {number} nNeuralWorker_ModeId
-   *   The numeric identifier of NeuralWorker_Mode. (NeuralWorker.Mode.Singleton.Ids.Xxx)
+   *   The numeric identifier of NeuralWorker_Mode.
+   * (NeuralWorker.Mode.Singleton.Ids.Xxx)
    *
    * @return {boolean}
    *   Only meaningful for mode XXX_APPLY and XXX_APPLIER.
@@ -114,10 +135,16 @@ class NeuralWorker_Mode extends Int {
 /**
  *
  * @member {number} workerCount
- *   The web worker count for The convolution block type. Either 1 or 2.
+ *   The web worker count for the neural worker mode. Either 1 or 2.
+ *
+ * @member {number} neuralNetCount
+ *   The neural network count for the neural worker mode. Either 1 or 2.
+ *   - When training neural network (by diffiential evolution), it is 2.
+ *   - When real using neural network after training, it is 1.
  *
  * @member {boolean} bFill
- *   Whether the worker mode will fill alignment mark in image before process it.
+ *   Whether the neural worker mode will fill alignment mark and feedback
+ * (i.e. previous time output) into image before processing it.
  *
  * @member {boolean} bApply_or_Applier
  *   Only meaningful for mode XXX_APPLY and XXX_APPLIER.
@@ -139,11 +166,12 @@ NeuralWorker_Mode.Info = class NeuralWorker_Mode_Info extends Int.Info {
    *
    */
   constructor( nNeuralWorker_ModeId, nameForMessage,
-    workerCount, bFill, bApply_or_Applier
+    workerCount, neuralNetCount, bFill, bApply_or_Applier
   ) {
     super( nNeuralWorker_ModeId, nameForMessage );
 
     this.workerCount = workerCount;
+    this.neuralNetCount = neuralNetCount;
     this.bFill = bFill;
     this.bApply_or_Applier = bApply_or_Applier;
   }
