732b58bf4d9926afc5a432b0c9dd908b010d1c15
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-05-06 14:52:49 +0800

    nextInputArray_alignmentMark_check

--- CNN/tester/FeedbackShape_tester.js
+++ CNN/tester/FeedbackShape_tester.js
@@ -538,21 +538,12 @@ class TestCase {
   nextInputArray_alignmentMark_check() {
     const funcNameInMessage = "nextInputArray_alignmentMark_check";
 
-//!!! ...unfinished... (2023/05/05)
-
     const to_inputArray = this.nextInputArray;
 
     const feedbackShape = this.feedbackShape;
-    const input_height = feedbackShape.input_height;
+    //const input_height = feedbackShape.input_height;
     const input_width = feedbackShape.input_width;
     const input_channelCount = feedbackShape.input_channelCount;
-
-    const input_width_valueCount = feedbackShape.input_width_valueCount;
-    const implicit_input_width = feedbackShape.implicit_input_width;
-    //const explicit_input_width = feedbackShape.explicit_input_width;
-
-    const area_from_valueCount_original
-      = feedbackShape.area.from_valueCount_original;
 
 
     const area_height_pixelCount_original
@@ -577,14 +568,10 @@ class TestCase {
     const area_width_multiplier = feedbackShape.area.width_multiplier;
 
 
-    const from_output_valueArray = this.from_output_valueArray;
-    const areaIndex = 1; // Area 1 is for feedback.
+    const alignment_mark_value = this.alignment_mark_value_base_positive;
+
+    const areaIndex = 0; // Area 0 is for alignment mark.
     {
-      const from_valueIndex_base = 0;
-
-      const from_valueIndex_upper_bound
-        = from_valueIndex_base + area_from_valueCount_original - 1;
-
       const area_position_left
         = feedbackShape.area_position_leftArray[ areaIndex ];
       const area_position_top
@@ -594,7 +581,6 @@ class TestCase {
         = ( ( area_position_top * input_width ) + area_position_left );
 
       for ( let y = 0; y < area_height_pixelCount_original; ++y ) {
-        const from_valueIndex_base_y = ( y * area_width_pixelCount_original );
 
         for ( let y_multiplier = 0;
           y_multiplier < area_height_multiplier; ++y_multiplier ) {
@@ -604,9 +590,6 @@ class TestCase {
                 * input_width;
 
           for ( let x = 0; x < area_width_pixelCount_original; ++x ) {
-
-            const from_valueIndex_base_yx = from_valueIndex_base
-              + ( from_valueIndex_base_y + x ) * input_channelCount;
 
             for ( let x_multiplier = 0;
               x_multiplier < area_width_multiplier; ++x_multiplier ) {
@@ -620,36 +603,16 @@ class TestCase {
 
               for ( let c = 0; c < input_channelCount; ++c ) {
 
-                const from_valueIndex = from_valueIndex_base_yx + c;
                 const to_valueIndex = to_valueIndex_base_yx + c;
 
-                if ( from_valueIndex > from_valueIndex_upper_bound ) {
-                  if ( to_inputArray[ to_valueIndex ] != 0 )
-                    throw Error( `FeedbackShape_tester.TestCase.${funcNameInMessage}(): `
-                      + `to_inputArray[ ${to_valueIndex} ]=`
-                      + `${to_inputArray[ to_valueIndex ]} `
-                      + `should be ( 0 ). `
-                      + `y=${y}, y_multiplier=${y_multiplier}, `
-                      + `x=${x}, x_multiplier=${x_multiplier}, `
-                      + `c=${c}, `
-                      + `{ ${this} }.`
-                    );
-
-                } else {
-                  if ( to_inputArray[ to_valueIndex ]
-                        != from_output_valueArray[ from_valueIndex ] )
-                    throw Error( `FeedbackShape_tester.TestCase.${funcNameInMessage}(): `
-                      + `to_inputArray[ ${to_valueIndex} ]=`
-                      + `${to_inputArray[ to_valueIndex ]} `
-                      + `should be the same as `
-                      + `from_output_valueArray[ ${from_valueIndex} ]=`
-                      + `${from_output_valueArray[ from_valueIndex ]}. `
-                      + `y=${y}, y_multiplier=${y_multiplier}, `
-                      + `x=${x}, x_multiplier=${x_multiplier}, `
-                      + `c=${c}, `
-                      + `{ ${this} }.`
-                    );
-                }
+                if ( to_inputArray[ to_valueIndex ] != alignment_mark_value )
+                  throw Error( `FeedbackShape_tester.TestCase.${funcNameInMessage}(): `
+                    + `to_inputArray[ ${to_valueIndex} ]=`
+                    + `${to_inputArray[ to_valueIndex ]} `
+                    + `should be the same as `
+                    + `alignment_mark_value ( ${alignment_mark_value} ), `
+                    + `{ ${this} }.`
+                  );
 
               } // c
             } // x_multiplier
@@ -671,9 +634,6 @@ class TestCase {
     const input_width = feedbackShape.input_width;
     const input_channelCount = feedbackShape.input_channelCount;
 
-    const input_width_valueCount = feedbackShape.input_width_valueCount;
-    const implicit_input_width = feedbackShape.implicit_input_width;
-    //const explicit_input_width = feedbackShape.explicit_input_width;
 
     const area_from_valueCount_original
       = feedbackShape.area.from_valueCount_original;
