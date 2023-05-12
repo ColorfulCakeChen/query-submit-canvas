92bc8fcd1fb1a3de362fbe72d4a2ec0369503be5
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-05-12 21:51:34 +0800

    Improve comments.

--- CNN/Unpacker/ParamDesc/ParamDesc_Base.js
+++ CNN/Unpacker/ParamDesc/ParamDesc_Base.js
@@ -14,9 +14,11 @@ import * as ValueDesc from "../ValueDesc.js";
  * Usually, a ParamDesc object is also be used as the unique key of a parameter.
  *
  * Q: Why are there ValueDesc and ParamDesc two layers?
- * A: Let different parameter descriptions (ParamDesc) share the same value description (ValueDesc).
- *    It is especially useful for integer parameter ( new ParamDesc.Base( paramName, new ValueDesc.Int() ) ).
- *    For non-converting and boolean parameter, using ParamDesc.Same and ParamDesc.Bool is enough.
+ * A: Let different parameter descriptions (ParamDesc) share the same value
+ *    description (ValueDesc). It is especially useful for integer parameter
+ *    ( new ParamDesc.Base( paramName, new ValueDesc.Int() ) ). For
+ *    non-converting and boolean parameter, using ParamDesc.Same and
+ *    ParamDesc.Bool is enough.
  *
  * @example
  * let fruitKinds = new ValueDesc.Int( 0, 2, [ APPLE, ORANGE, BANANA ] );
@@ -25,15 +27,18 @@ import * as ValueDesc from "../ValueDesc.js";
  *
  *
  * @member {string} paramName
- *   The name of the parameter. It is a string. It should be a legal identifer too (i.e. A-Z, a-z, 0-9 (not at first character), and "_").
+ *   The name of the parameter. It is a string. It should be a legal identifer
+ * too (i.e. A-Z, a-z, 0-9 (not at first character), and "_").
  *
  * @member {ValueDesc.Xxx} valueDesc
- *   The description of the parameter's all possible values. It should be a ValueDesc.Xxx object (one of ValueDesc.Same,
- * ValueDesc.Bool, ValueDesc.Int). For example, ValueDesc.Same.Singleton, ValueDesc.Bool.Singleton.
+ *   The description of the parameter's all possible values. It should be a
+ * ValueDesc.Xxx object (one of ValueDesc.Same, ValueDesc.Bool, ValueDesc.Int).
+ * For example, ValueDesc.Same.Singleton, ValueDesc.Bool.Singleton.
  *
  * @member {number} seqId
- *   The sequence id of the parameter. It is filled by ParamDesc.SequenceArray' constructor. And it is used as the array index into
- * Weights.Params.
+ *   The sequence id of the parameter. It is filled by
+ * ParamDesc.SequenceArray's constructor. And it is used as the array index
+ * into Weights.Params.
  *
  */
 class ParamDesc_Base {
@@ -61,7 +66,8 @@ class ParamDesc_Base {
  * Describe some properties of a non-converting parameter.
  *
  * @member {ValueDesc.Same} valueDesc
- *   The range of the parameter's all possible values. It is a ValueDesc.Same object.
+ *   The range of the parameter's all possible values. It is a
+ * ValueDesc.Same object.
  */
 class Same extends ParamDesc_Base {
   constructor( paramName ) {
@@ -74,7 +80,8 @@ class Same extends ParamDesc_Base {
  * Describe some properties of a boolean parameter.
  *
  * @member {ValueDesc.Bool} valueDesc
- *   The boolean range of the parameter's all possible values. It is a ValueDesc.Bool object.
+ *   The boolean range of the parameter's all possible values. It is a
+ * ValueDesc.Bool object.
  */
 class Bool extends ParamDesc_Base {
   constructor( paramName ) {
@@ -87,11 +94,16 @@ class Bool extends ParamDesc_Base {
  * Describe some properties of an integer parameter.
  *
  * @member {ValueDesc.Int} valueDesc
- *   The integer range of the parameter's all possible values. It is a ValueDesc.Bool object.
+ *   The integer range of the parameter's all possible values. It is a
+ * ValueDesc.Bool object.
  */
 class Int extends ParamDesc_Base {
-  constructor( paramName, valueIntegerMin, valueIntegerMax, valueNames = [], valueObjects = [] ) {
-    super( paramName, new ValueDesc.Int( valueIntegerMin, valueIntegerMax, valueNames, valueObjects ) );
+  constructor( paramName,
+    valueIntegerMin, valueIntegerMax,
+    valueNames = [], valueObjects = [] ) {
+
+    super( paramName, new ValueDesc.Int(
+      valueIntegerMin, valueIntegerMax, valueNames, valueObjects ) );
   }
 }
 
@@ -100,7 +112,8 @@ class Int extends ParamDesc_Base {
  * Describe some properties of an activation function parameter.
  *
  * @member {ValueDesc.ActivationFunction} valueDesc
- *   The range of the parameter's all possible values. It is a ValueDesc.ActivationFunction object.
+ *   The range of the parameter's all possible values. It is a
+ * ValueDesc.ActivationFunction object.
  */
 class ActivationFunction extends ParamDesc_Base {
   constructor( paramName ) {
@@ -113,7 +126,8 @@ class ActivationFunction extends ParamDesc_Base {
  * Describe some properties of an ConvBlockType parameter.
  *
  * @member {ValueDesc.ConvBlockType} valueDesc
- *   The range of the parameter's all possible values. It is a ValueDesc.ConvBlockType object.
+ *   The range of the parameter's all possible values. It is a
+ * ValueDesc.ConvBlockType object.
  */
 class ConvBlockType extends ParamDesc_Base {
   constructor( paramName ) {
@@ -126,7 +140,8 @@ class ConvBlockType extends ParamDesc_Base {
  * Describe some properties of an depthwise operation parameter.
  *
  * @member {ValueDesc.AvgMax_Or_ChannelMultiplier} valueDesc
- *   The range of the parameter's all possible values. It is a ValueDesc.AvgMax_Or_ChannelMultiplier object.
+ *   The range of the parameter's all possible values. It is a
+ * ValueDesc.AvgMax_Or_ChannelMultiplier object.
  */
 class AvgMax_Or_ChannelMultiplier extends ParamDesc_Base {
   constructor( paramName ) {
@@ -136,10 +151,12 @@ class AvgMax_Or_ChannelMultiplier extends ParamDesc_Base {
 
 
 /**
- * Describe some properties of an squeeze-and-excitation channel count divisor parameter.
+ * Describe some properties of an squeeze-and-excitation channel count divisor
+ * parameter.
  *
  * @member {ValueDesc.AvgMax_Or_ChannelMultiplier} valueDesc
- *   The range of the parameter's all possible values. It is a ValueDesc.AvgMax_Or_ChannelMultiplier object.
+ *   The range of the parameter's all possible values. It is a
+ * ValueDesc.AvgMax_Or_ChannelMultiplier object.
  */
 class SqueezeExcitationChannelCountDivisor extends ParamDesc_Base {
   constructor( paramName ) {
@@ -152,7 +169,8 @@ class SqueezeExcitationChannelCountDivisor extends ParamDesc_Base {
  * Describe some properties of an strides and pad parameter.
  *
  * @member {ValueDesc.StridesPad} valueDesc
- *   The range of the parameter's all possible values. It is a ValueDesc.StridesPad object.
+ *   The range of the parameter's all possible values. It is a
+ * ValueDesc.StridesPad object.
  */
 class StridesPad extends ParamDesc_Base {
   constructor( paramName ) {
@@ -165,7 +183,8 @@ class StridesPad extends ParamDesc_Base {
  * Describe some properties of an ConvStageType parameter.
  *
  * @member {ValueDesc.ConvStageType} valueDesc
- *   The range of the parameter's all possible values. It is a ValueDesc.ConvStageType object.
+ *   The range of the parameter's all possible values. It is a
+ * ValueDesc.ConvStageType object.
  */
 class ConvStageType extends ParamDesc_Base {
   constructor( paramName ) {
