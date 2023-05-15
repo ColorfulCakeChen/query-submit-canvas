f1410eb80d0af01e7c7ab1f5621d5b2a865e60c2
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-05-15 10:23:34 +0800

    nNeuralWorker_ImplicitInputModeId

--- CNN/tester/NeuralWorker_tester.js
+++ CNN/tester/NeuralWorker_tester.js
@@ -1303,7 +1303,7 @@ function TestButton_onClick( event ) {
 //!!! (2023/05/11 Remarked)
 //    largerFactor,
 
-    numeric_controls_valueObject.nNeuralWorker_ImplicitInputModeId,
+    nNeuralWorker_ImplicitInputModeId,
     numeric_controls_valueObject.explicit_input_height,
     numeric_controls_valueObject.explicit_input_width,
     numeric_controls_valueObject.explicit_input_channelCount,
