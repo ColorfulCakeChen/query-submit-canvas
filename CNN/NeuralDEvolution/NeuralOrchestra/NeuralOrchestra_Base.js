342f9c881cd050fd0623a51181a32003446d8744
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-05-20 10:21:56 +0800

    alignmentMarkValueArrayArray

--- CNN/NeuralDEvolution/NeuralOrchestra/NeuralOrchestra_Base.js
+++ CNN/NeuralDEvolution/NeuralOrchestra/NeuralOrchestra_Base.js
@@ -1726,8 +1726,13 @@ class NeuralOrchestra_Base extends
   static throw_if_workerProxies_busy_except_workerProxies_init(
     funcNameInMessage ) {
 
-//!!! ...unfinished... (2023/05/15)
-// throw_if_alignmentMarkValueArrayArray_set_asyncPromise_running()
+    NeuralOrchestra_Base
+      .throw_if_alignmentMarkValueArrayArray_set_asyncPromise_running
+      .call( this, funcNameInMessage );
+
+    NeuralOrchestra_Base
+      .throw_if_alignmentMarkValueArrayArray_swap_asyncPromise_running
+      .call( this, funcNameInMessage );
 
     NeuralOrchestra_Base
       .throw_if_TypedArray_process_asyncPromise_running
