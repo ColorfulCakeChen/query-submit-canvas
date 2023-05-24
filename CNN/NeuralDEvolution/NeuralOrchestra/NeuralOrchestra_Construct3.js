54f4a30c7fe6e67a6930f2ae3778789b96e752eb
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-05-24 12:25:28 +0800

    versus_load_asyncGenerator

--- CNN/NeuralDEvolution/NeuralOrchestra/NeuralOrchestra_Construct3.js
+++ CNN/NeuralDEvolution/NeuralOrchestra/NeuralOrchestra_Construct3.js
@@ -123,17 +123,14 @@ class NeuralOrchestra_Construct3 extends Recyclable.Root {
    */
   static Versus_Step_00_DownloadWeights_Begin( runtime ) {
 
-    const base = this.base;
+    //const base = this.base;
 
 //!!! ...unfinished... (2023/05/24)
 //    base.versus_load_asyncPromise_progress;
 
-//!!! ...unfinished... (2023/04/08)
     let versus_load_asyncGenerator = this.versus_load_asyncGenerator;
     this.versus_load_asyncGeneratorTicker
       = new PartTime.AsyncGeneratorTicker( versus_load_asyncGenerator );
-
-
   }
 
   /**
