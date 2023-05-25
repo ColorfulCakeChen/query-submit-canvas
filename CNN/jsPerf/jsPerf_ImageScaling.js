21e1eddb5f351613ce24cded0da6f35e2e018947
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-05-25 11:03:29 +0800

    testImageScaling_ByTensor3d_from_Canvas

--- CNN/jsPerf/jsPerf_ImageScaling.js
+++ CNN/jsPerf/jsPerf_ImageScaling.js
@@ -108,6 +108,51 @@ class HeightWidthDepth {
 
 //!!! ...unfinished... (2023/05/25)
 
+    const input_Canvas = this.input_Canvas;
+
+    let input_ctx = input_Canvas.getContext( "2d" );
+    let input_ImageData = input_ctx.getImageData(
+      0, 0, input_Canvas.width, input_Canvas.height );
+
+    let input_tensor;
+    try {
+      let input_shape = [ input_Canvas.height, input_Canvas.width,
+        this.output_channelCount ];
+
+      input_tensor = tf.tensor3d( input_ImageData, input_shape, "int32" );
+
+    } finally {
+      if ( input_tensor ) {
+        input_tensor.dispose();
+        input_tensor = null;
+      }
+    }
+
+//!!!
+    let input_offscreenCanvas;
+    {
+      input_offscreenCanvas
+        = new OffscreenCanvas( this.input_width, this.input_height );
+
+      let input_offscreenCanvas_ctx = offscreenCanvas.getContext( contextType );
+      input_offscreenCanvas_ctx.putImageData( input_ImageData, 0, 0 );
+    }
+
+    let output_offscreenCanvas;
+    {
+      output_offscreenCanvas
+        = new OffscreenCanvas( this.output_width, this.output_height );
+
+      let output_offscreenCanvas_ctx = offscreenCanvas.getContext( contextType );
+        output_offscreenCanvas_ctx.drawImage( input_offscreenCanvas,
+        0, 0, input_offscreenCanvas.width, input_offscreenCanvas.height,
+        0, 0, this.output_width, this.output_height
+      );
+
+      let output_ImageData = output_offscreenCanvas_ctx.getImageData(
+        0, 0, this.output_width, this.output_height );
+    }
+
 //!!!
     //tf.dispose( outputTensor3d );
 
