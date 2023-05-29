c6dbbcdecab61b0f77ab1f7969ae90e40d417367
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-05-29 08:24:58 +0800

    Improve comments.

--- CNN/NeuralDEvolution/NeuralOrchestra/NeuralOrchestra_Construct3.js
+++ CNN/NeuralDEvolution/NeuralOrchestra/NeuralOrchestra_Construct3.js
@@ -277,8 +277,8 @@ class NeuralOrchestra_Construct3 extends Recyclable.Root {
     //
     // Note: Do not continue to .pasteInstances() when the
     //       .DrawingCanvas_getImagePixelDataPromise is still pending.
-    //       Otherwise, the promise always will always be pending (i.e.
-    //       never fulfilled).
+    //       Otherwise, the .DrawingCanvas_getImagePixelDataPromise will
+    //       always be pending (i.e. never fulfilled).
     this.DrawingCanvas_pasteInstancesPromise = null;
 
 //!!! ...unfinished... (2023/05/28)
