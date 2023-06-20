b54ad1853c55d680d13dcb205eca0a7d221c53fc
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-06-20 14:57:37 +0800

    Base64_suspendByteCount = 100 * 1024

--- CNN/NeuralDEvolution/DEvolution/DEvolution_Versus.js
+++ CNN/NeuralDEvolution/DEvolution/DEvolution_Versus.js
@@ -236,8 +236,8 @@ class DEvolution_Versus extends
     const Base64_skipLineCount = 0;
 //!!! (2023/06/20 Remarked)
 // Use smaller chunk to reduce stuck feeling when decoding.
-//    const Base64_suspendByteCount = 100 * 1024; //1024 * 1024;
-    const Base64_suspendByteCount = 10 * 1024; //1024 * 1024;
+    const Base64_suspendByteCount = 100 * 1024; //1024 * 1024;
+//    const Base64_suspendByteCount = 10 * 1024; //1024 * 1024;
 
     // 2.1 versusId
     {
