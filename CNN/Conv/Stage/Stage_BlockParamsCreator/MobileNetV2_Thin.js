dbdfd962a5f7da586f27d4cc5773e1a23218bccf
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-06-28 11:44:46 +0800

    Improve comments.

--- CNN/Conv/Stage/Stage_BlockParamsCreator/MobileNetV2_Thin.js
+++ CNN/Conv/Stage/Stage_BlockParamsCreator/MobileNetV2_Thin.js
@@ -6,16 +6,20 @@ import * as ValueDesc from "../../../Unpacker/ValueDesc.js";
 import { MobileNetV1 } from "./MobileNetV1.js";
 
 /**
- * Provide parameters for MobileNetV2_Thin (i.e. add-inut-to-output, pointwise1 is same size of pointwise20).
+ * Provide parameters for MobileNetV2_Thin (i.e. add-inut-to-output, pointwise1
+ * is same size of pointwise20).
  *
  *
  */
 class MobileNetV2_Thin extends MobileNetV1 {
 
   /**
-   * Used as default Stage.BlockParamsCreator.MobileNetV2_Thin provider for conforming to Recyclable interface.
+   * Used as default Stage.BlockParamsCreator.MobileNetV2_Thin provider for
+   * conforming to Recyclable interface.
    */
-  static Pool = new Pool.Root( "Stage.BlockParamsCreator.MobileNetV2_Thin.Pool", MobileNetV2_Thin, MobileNetV2_Thin.setAsConstructor );
+  static Pool = new Pool.Root(
+    "Stage.BlockParamsCreator.MobileNetV2_Thin.Pool",
+    MobileNetV2_Thin, MobileNetV2_Thin.setAsConstructor );
 
   /**
    */
@@ -43,17 +47,23 @@ class MobileNetV2_Thin extends MobileNetV1 {
 
   /** @override */
   configTo_beforeBlock0() {
-    super.configTo_beforeBlock0(); // block0's input0_height, input0_width, input0_channelCount, ConvBlockType, activation.
+    // block0's input0_height, input0_width, input0_channelCount,
+    // ConvBlockType, activation.    
+    super.configTo_beforeBlock0();
   }
 
   /** @override */
   configTo_beforeBlockN_exceptBlock0( blockIndex, input_height, input_width ) {
-    super.configTo_beforeBlockN_exceptBlock0( blockIndex, input_height, input_width ); // block1, 2, 3, ...'s input0_height, input0_width.
+    // block1, 2, 3, ...'s input0_height, input0_width.
+    super.configTo_beforeBlockN_exceptBlock0(
+      blockIndex, input_height, input_width );
 
     // In MobileNetV2:
-    //   - All blocks (except block0) do add-input-to-output (without concatenation).
+    //   - All blocks (except block0) do add-input-to-output (without
+    //       concatenation).
     //   - All blocks (include block0) do not use input1.
-    this.nConvBlockTypeId = ValueDesc.ConvBlockType.Singleton.Ids.MOBILE_NET_V2_BODY_TAIL;
+    this.nConvBlockTypeId = ValueDesc.ConvBlockType.Singleton.Ids
+      .MOBILE_NET_V2_BODY_TAIL;
   }
 
   /** @override */
@@ -62,4 +72,3 @@ class MobileNetV2_Thin extends MobileNetV1 {
   }
 
 }
-
