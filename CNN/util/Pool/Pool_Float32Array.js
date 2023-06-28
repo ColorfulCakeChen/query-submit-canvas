7957fa01fde901fb8c21a43d2f353286dc5f8f3f
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-06-28 14:11:12 +0800

    Improve comments.

--- CNN/util/Pool/Pool_Float32Array.js
+++ CNN/util/Pool/Pool_Float32Array.js
@@ -5,23 +5,27 @@ import { Root } from "./Pool_Base.js";
 /**
  * Providing Float32Array by specifying length.
  *
- * It will try to re-use Float32Array or its underlying ArrayBuffer as possible.
+ * It will try to re-use Float32Array or its underlying ArrayBuffer as
+ * possible.
  *
  *
  * Note:
  *
- * This pool's performance may NOT be good enough. Because Float32Array's property can not be adjusted, it is almost necessary
- * to be re-created. This violates the basic principle (i.e. reducing memory re-allocation) of pool.
+ * This pool's performance may NOT be good enough. Because Float32Array's
+ * property can not be adjusted, it is almost necessary to be re-created. This
+ * violates the basic principle (i.e. reducing memory re-allocation) of pool.
  *
- * Using MultiLayerMap might be better for Float32Array because all Float32Array with the same specification could be re-used
- * directly without re-creating.
+ * Using MultiLayerMap might be better for Float32Array because all
+ * Float32Array with the same specification could be re-used directly without
+ * re-creating.
  *
  *
  */
 class Float32ArrayPool extends Root {
   
   constructor() {
-    super( "Pool.Float32Array", Float32Array, Float32ArrayPool.setAsConstructor_by_length );
+    super( "Pool.Float32Array",
+      Float32Array, Float32ArrayPool.setAsConstructor_by_length );
   }
 
   /**
@@ -34,8 +38,10 @@ class Float32ArrayPool extends Root {
    * @return {Float32Array}
    *   Return a Float32Array object.
    *   - If this.length is the same as required, it will be returned directly.
-   *   - Otherwise, if this.buffer is large enough, it will be used to create (and return) a new Float32Array object.
-   *   - Otherise, a new ArrayBuffer will be created to create (and return) a new Float32Array object.
+   *   - Otherwise, if this.buffer is large enough, it will be used to create
+   *       (and return) a new Float32Array object.
+   *   - Otherise, a new ArrayBuffer will be created to create (and return) a
+   *       new Float32Array object.
    */
   static setAsConstructor_by_length( newLength ) {
 
@@ -50,7 +56,8 @@ class Float32ArrayPool extends Root {
       if ( this.buffer.byteLength >= newByteLength )
         arrayBuffer = this.buffer;
       else
-        arrayBuffer = new ArrayBuffer( newByteLength ); // Create new buffer when old buffer is not large enough.
+        // Create new buffer when old buffer is not large enough.
+        arrayBuffer = new ArrayBuffer( newByteLength );
     }
 
     // 3.
