87976f4c1417f71ee51e6d0386798a86c8784ca9
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-06-28 12:00:44 +0800

    tensorflowJsVer = "4.8.0"

--- CNN/NeuralDEvolution/NeuralWorker/NeuralWorker_Common.js
+++ CNN/NeuralDEvolution/NeuralWorker/NeuralWorker_Common.js
@@ -2,7 +2,7 @@ export { tensorflowJsVer };
 export { tensorflowJsURL };
 
 /** Define tensorflow.js library version. */
-const tensorflowJsVer = "4.6.0";
+const tensorflowJsVer = "4.8.0";
 
 /** Define tensorflow.js library URL. */
 const tensorflowJsURL
