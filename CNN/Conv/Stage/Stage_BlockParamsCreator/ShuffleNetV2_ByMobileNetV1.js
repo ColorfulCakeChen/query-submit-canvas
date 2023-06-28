2d667d6df9acd6b9f7ff273a82cb5df6b2ccd3fd
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-06-28 11:13:12 +0800

    Improve comments.

--- CNN/Conv/Stage/Stage_BlockParamsCreator/ShuffleNetV2_ByMobileNetV1.js
+++ CNN/Conv/Stage/Stage_BlockParamsCreator/ShuffleNetV2_ByMobileNetV1.js
@@ -12,11 +12,11 @@ import { Base } from "./Stage_BlockParamsCreator_Base.js";
  *
  * 1. Motivation
  *
- * Accodring to testing, the original ShuffleNetV2 is faster than MobileNetV2 in
- * backend CPU. This may result from lesser computation. However, in backend WASM
- * and WEBGL, MobileNetV2 is faster than the original ShuffleNetV2. The possible
- * reason may be that the concatenate-shuffle-split (even achieved by pointwise
- * convolution) operation is not friendly for WASM and WebGL.
+ * Accodring to testing, the original ShuffleNetV2 is faster than MobileNetV2
+ * in backend CPU. This may result from lesser computation. However, in backend
+ * WASM and WEBGL, MobileNetV2 is faster than the original ShuffleNetV2. The
+ * possible reason may be that the concatenate-shuffle-split (even achieved by
+ * pointwise convolution) operation is not friendly for WASM and WebGL.
  *
  * This results in an idea that:
  *
@@ -27,16 +27,16 @@ import { Base } from "./Stage_BlockParamsCreator_Base.js";
  *       so that they achieve the same effect of shuffling but without
  *       concatenation and splitting.
  *
- * This may become a faster ShuffleNetV2 in backend WASM and WEBGL (but a slower
- * ShuffleNetV2 in backend CPU due to more computation).
+ * This may become a faster ShuffleNetV2 in backend WASM and WEBGL (but a
+ * slower ShuffleNetV2 in backend CPU due to more computation).
  *
  *
- * Q1: Why not just use MobileNetV1 instead of ShuffleNetV2, since its structure
- *       is MobileNetV1?
- * A1: The filter weights count is different. MobileNetV1 has more (a lot) filter
- *       weights needed to be learned than ShuffleNetV2. The learning (or say,
- *       evolving) performance should be faster by using ShuffleNetV2 (rather
- *       than MobileNetV1).
+ * Q1: Why not just use MobileNetV1 instead of ShuffleNetV2, since its
+ *       structure is MobileNetV1?
+ * A1: The filter weights count is different. MobileNetV1 has more (a lot)
+ *       filter weights needed to be learned than ShuffleNetV2. The learning
+ *       (or say, evolving) performance should be faster by using ShuffleNetV2
+ *       (rather than MobileNetV1).
  *
  *
  * 2. Last Channel Stationary
@@ -97,16 +97,17 @@ class ShuffleNetV2_ByMobileNetV1 extends Base {
 
     // Block0 uses the original input channel count (as input0).
     this.input0_channelCount = stageParams.input_channelCount;
-    this.nConvBlockTypeId
-      = ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD;
+    this.nConvBlockTypeId = ValueDesc.ConvBlockType.Singleton.Ids
+      .SHUFFLE_NET_V2_BY_MOBILE_NET_V1_HEAD;
 
     if ( stageParams.bPointwise1 == false ) {
 
-      // In ShuffleNetV2_ByMobileNetV1's head, if ( stageParams.bPointwise1 == false ),
-      // pointwise1ChannelCount is also 0.
+      // In ShuffleNetV2_ByMobileNetV1's head,
+      // if ( stageParams.bPointwise1 == false ), pointwise1ChannelCount is
+      // also 0.
       //
-      // But, in this case, pointwise1 will still be created by Block intrinsically
-      // and output double of input0.
+      // But, in this case, pointwise1 will still be created by Block
+      // intrinsically and output double of input0.
       //
       // The input0 will just be pass-through as pointwise1's lower half.
       // The input0 will also be copied as pointwise1's higher half.
@@ -117,8 +118,9 @@ class ShuffleNetV2_ByMobileNetV1 extends Base {
 
     } else {
 
-      // In ShuffleNetV2_ByMobileNetV1's head, if ( stageParams.bPointwise1 == true ),
-      // pointwise1ChannelCount is always the same as input0's channel count.
+      // In ShuffleNetV2_ByMobileNetV1's head,
+      // if ( stageParams.bPointwise1 == true ), pointwise1ChannelCount is
+      // always the same as input0's channel count.
       //
       // But, in this case, pointwise1 will still and output double of input0
       // intrinsically.
@@ -138,8 +140,8 @@ class ShuffleNetV2_ByMobileNetV1 extends Base {
     //
     this.depthwise_AvgMax_Or_ChannelMultiplier = 1;
 
-    // In ShuffleNetV2_ByMobileNetV1, pointwise20ChannelCount is always twice of
-    // original input0's channel count.
+    // In ShuffleNetV2_ByMobileNetV1, pointwise20ChannelCount is always twice
+    // of original input0's channel count.
     this.pointwise20ChannelCount = stageParams.input_channelCount * 2;
 
     // In ShuffleNetV2_ByMobileNetV1, all blocks have only output0 (with same
@@ -150,24 +152,27 @@ class ShuffleNetV2_ByMobileNetV1 extends Base {
 
   /** @override */
   configTo_beforeBlockN_exceptBlock0( blockIndex, input_height, input_width ) {
-    super.configTo_beforeBlockN_exceptBlock0( blockIndex, input_height, input_width );
+    super.configTo_beforeBlockN_exceptBlock0(
+      blockIndex, input_height, input_width );
 
     let stageParams = this.stageParams;
 
     this.input0_channelCount = this.output0_channelCount;
-    this.nConvBlockTypeId
-      = ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_BODY;
+    this.nConvBlockTypeId = ValueDesc.ConvBlockType.Singleton.Ids
+      .SHUFFLE_NET_V2_BY_MOBILE_NET_V1_BODY;
 
-    // In ShuffleNetV2_ByMobileNetV1's body/tail, if ( stageParams.bPointwise1 == false ),
-    // pointwise1ChannelCount is also 0. In this case, pointwise1 will not be
-    // created by PointDepthPoint (i.e. different from block0).
+    // In ShuffleNetV2_ByMobileNetV1's body/tail,
+    // if ( stageParams.bPointwise1 == false ), pointwise1ChannelCount is also
+    // 0. In this case, pointwise1 will not be created by PointDepthPoint (i.e.
+    // different from block0).
     //
     if ( stageParams.bPointwise1 == false ) {
       this.pointwise1ChannelCount = 0; // (Intrinsically, zero, too.)
 
-    // In ShuffleNetV2_ByMobileNetV1's body/tail, if ( stageParams.bPointwise1 == true ),
-    // pointwise1ChannelCount is always the same as pointwise20 output channel
-    // count (which is already doubled as twice of block0's input0).
+    // In ShuffleNetV2_ByMobileNetV1's body/tail,
+    // if ( stageParams.bPointwise1 == true ), pointwise1ChannelCount is always
+    // the same as pointwise20 output channel count (which is already doubled
+    // as twice of block0's input0).
     //
     // The input0's lower half will be processed by pointwise1's lower half.
     // The input0's higher half will be pass-through as pointwise1's higher half.
@@ -185,8 +190,7 @@ class ShuffleNetV2_ByMobileNetV1 extends Base {
   /** @override */
   configTo_beforeBlockLast() {
     super.configTo_beforeBlockLast();
-    this.nConvBlockTypeId
-      = ValueDesc.ConvBlockType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_TAIL;
+    this.nConvBlockTypeId = ValueDesc.ConvBlockType.Singleton.Ids
+      .SHUFFLE_NET_V2_BY_MOBILE_NET_V1_TAIL;
   }
 }
-
