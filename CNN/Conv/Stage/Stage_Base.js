54c0fd015c423f842ab95b5eb4163b117286374b
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-06-28 11:54:43 +0800

    Improve comments.

--- CNN/Conv/Stage/Stage_Base.js
+++ CNN/Conv/Stage/Stage_Base.js
@@ -33,24 +33,39 @@ import { InferencedParams } from "./Stage_InferencedParams.js";
  * input image's channel count. They all do it at block0 but by different ways:
  *
  *   - MobileNetV1 (0), MobileNetV1_padValid (1)
- *     - ( bPointwise1 == false ), depthwise1 double ( channelMultiplier == 2 ) of input0.
- *     - ( bPointwise1 ==  true ), pointwise1 double of input0.
+ *     - ( bPointwise1 == false ),
+ *         depthwise1 double ( channelMultiplier == 2 ) of input0.
+ *     - ( bPointwise1 ==  true ),
+ *         pointwise1 double of input0.
  *
  *   - MobileNetV2_Thin (2)
- *     - ( bPointwise1 == false ), depthwise1 double ( channelMultiplier == 2 ) of input0. pointwis21 double of input0.
- *     - ( bPointwise1 ==  true ), pointwise1 double of input0. pointwis21 double of input0.
+ *     - ( bPointwise1 == false ),
+ *         depthwise1 double ( channelMultiplier == 2 ) of input0.
+ *         pointwis21 double of input0.
+ *     - ( bPointwise1 ==  true ),
+ *         pointwise1 double of input0.
+ *         pointwis21 double of input0.
  *
  *   - MobileNetV2 (3)
- *     - ( bPointwise1 == false ), depthwise1 quadruple ( channelMultiplier == 4 ) of input0. pointwis21 double of input0.
- *     - ( bPointwise1 ==  true ), pointwise1 quadruple of input0. pointwis21 double of input0.
+ *     - ( bPointwise1 == false ),
+ *         depthwise1 quadruple ( channelMultiplier == 4 ) of input0.
+ *         pointwis21 double of input0.
+ *     - ( bPointwise1 ==  true ),
+ *         pointwise1 quadruple of input0.
+ *         pointwis21 double of input0.
  *
  *   - ShuffleNetV2 (4), ShuffleNetV2_ByPointwise21 (5)
- *     - ( bPointwise1 == false ), depthwise1 double ( channelMultiplier == 2 ) of input0.
- *     - ( bPointwise1 ==  true ), concatenate depthwise1 and depthwise2.
+ *     - ( bPointwise1 == false ),
+ *         depthwise1 double ( channelMultiplier == 2 ) of input0.
+ *     - ( bPointwise1 ==  true ),
+ *         concatenate depthwise1 and depthwise2.
  *
  *   - ShuffleNetV2_ByMobileNetV1 (6), ShuffleNetV2_ByMobileNetV1_padValid (7)
- *     - ( bPointwise1 == false ), pointwise1 (lower half pass through, higher half copy input0) double of input0.
- *     - ( bPointwise1 ==  true ), pointwise1 (higher half copy input0) double of input0.
+ *     - ( bPointwise1 == false ),
+ *         pointwise1 (lower half pass through, higher half copy input0) double
+ *         of input0.
+ *     - ( bPointwise1 ==  true ),
+ *         pointwise1 (higher half copy input0) double of input0.
  *
  *
  * 3. Bias and Activation
