600396855fccf492727276f99a4adaedc7da91a5
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-06-29 16:19:10 +0800

    BoundsArraySet_tester

---
+++ CNN/tester/BoundsArraySet_tester.js
@@ -0,0 +1,11 @@
+import * as Base64 from "../Unpacker/Base64.js";
+//import * as RandTools from "../util/RandTools.js";
+import * as ValueMax from "../util/ValueMax.js";
+import * as BoundsArraySet from "../Conv/BoundsArraySet.js";
+
+
+
+//!!! ...unfinished... (2023/06/29) should test
+// BoundsArraySet.ConvBiasActivation.set_outputs_all_byInterleave_asGrouptTwo()
+
+
