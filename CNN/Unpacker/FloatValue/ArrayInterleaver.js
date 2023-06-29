83f09045d238f776ee8449fd41d24a93843bc9f1
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-06-29 15:57:40 +0800

    Improve comments.

--- CNN/Unpacker/FloatValue/ArrayInterleaver.js
+++ CNN/Unpacker/FloatValue/ArrayInterleaver.js
@@ -11,21 +11,37 @@ class ArrayInterleaver {
    *
    * The fromArray and toArray can not be an overlapped array.
    *   - They could be different array. Or,
-   *   - Thay could be the same array but fromArray[ fromIndex ] to fromArray[ fromIndex + ( 2 * elementCountHalf ) - 1 ] and
-   *       toArray[ toIndex ] to toArray[ toIndex + ( 2 * elementCountHalf ) - 1 ] can not overlapped.
+   *   - Thay could be the same array but fromArray[ fromIndex ] to
+   *       fromArray[ fromIndex + ( 2 * elementCountHalf ) - 1 ] and
+   *       toArray[ toIndex ] to
+   *       toArray[ toIndex + ( 2 * elementCountHalf ) - 1 ] can not overlapped.
    *
-   * @param {Array} fromArray   The source array. It will not be modified. It is the source of copying.
-   * @param {number} fromIndex  The copying will begin at fromArray[ fromIndex ].
-   * @param {Array} toArray     The destination array. It will be modified (i.e. filled data copied from fromArray).
-   * @param {number} toIndex    The writing will begin at toArray[ toIndex ].
+   * @param {Array} fromArray
+   *   The source array. It will not be modified. It is the source of copying.
+   *
+   * @param {number} fromIndex
+   *   The copying will begin at fromArray[ fromIndex ].
+   *
+   * @param {Array} toArray
+   *   The destination array. It will be modified (i.e. filled data copied from
+   * fromArray).
+   *
+   * @param {number} toIndex
+   *   The writing will begin at toArray[ toIndex ].
    *
    * @param {number} elementCountHalf
-   *   There will be ( 2 * elementCountHalf ) elements copied from fromArray[ fromIndex ] to toArray[ toIndex ] but in a re-arranged
+   *   There will be ( 2 * elementCountHalf ) elements copied from
+   * fromArray[ fromIndex ] to toArray[ toIndex ] but in a re-arranged
    * interleaved order.
    *
    */
-  static interleave_asGrouptTwo_from_to( fromArray, fromIndex, toArray, toIndex, elementCountHalf ) {
-    for ( let i = 0, from = fromIndex, to = toIndex; i < elementCountHalf; ++i, ++from, to += 2 ) {
+  static interleave_asGrouptTwo_from_to(
+    fromArray, fromIndex, toArray, toIndex, elementCountHalf ) {
+
+    for ( let i = 0, from = fromIndex, to = toIndex;
+      i < elementCountHalf;
+      ++i, ++from, to += 2 ) {
+
       toArray[ to     ] = fromArray[ from ];
       toArray[ to + 1 ] = fromArray[ from + elementCountHalf ];
     }
