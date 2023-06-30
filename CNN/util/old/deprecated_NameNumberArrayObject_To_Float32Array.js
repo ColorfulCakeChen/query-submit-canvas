0fa0ddd762544c19485d7d6fba0ba22e3057f4bc
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-06-30 14:49:29 +0800

    Improve comments.

--- CNN/util/old/deprecated_NameNumberArrayObject_To_Float32Array.js
+++ CNN/util/old/deprecated_NameNumberArrayObject_To_Float32Array.js
@@ -12,9 +12,12 @@ import * as Recyclable from "../../util/Recyclable.js";
 class Base extends Recyclable.Root {
 
   /**
-   * Used as default NameNumberArrayObject_To_Float32Array.Base provider for conforming to Recyclable interface.
+   * Used as default NameNumberArrayObject_To_Float32Array.Base provider for
+   * conforming to Recyclable interface.
    */
-  static Pool = new Pool.Root( "NameNumberArrayObject_To_Float32Array.Base.Pool", Base, Base.setAsConstructor );
+  static Pool = new Pool.Root(
+    "NameNumberArrayObject_To_Float32Array.Base.Pool",
+    Base, Base.setAsConstructor );
 
   /**
    */
@@ -37,7 +40,8 @@ class Base extends Recyclable.Root {
   /** @override */
   disposeResources() {
 
-    // Note: It seems that both .underlyingArrayBuffer and .weightsFloat32Array are re-usable. Do not dispose them.
+    // Note: It seems that both .underlyingArrayBuffer and .weightsFloat32Array
+    //       are re-usable. Do not dispose them.
 
     super.disposeResources();
   }
@@ -47,16 +51,19 @@ class Base extends Recyclable.Root {
    *   The underlyingArrayBuffer will be large enough to contain so many bytes.
    */
   ensureArrayBuffer_byByte( byteLength ) {
-    if ( ( !this.underlyingArrayBuffer ) || ( this.underlyingArrayBuffer.byteLength < byteLength ) ) {
+    if (   ( !this.underlyingArrayBuffer )
+        || ( this.underlyingArrayBuffer.byteLength < byteLength ) ) {
       this.underlyingArrayBuffer = new ArrayBuffer( byteLength );
 
-    // Otherwise, this.underlyingArrayBuffer already has the enough space. Use it directly.
+    // Otherwise, this.underlyingArrayBuffer already has the enough space. Use
+    // it directly.
     }
   }
 
   /**
    * @param {number} elementCount
-   *   The underlyingArrayBuffer will be large enough to contain so many (Float32) elements.
+   *   The underlyingArrayBuffer will be large enough to contain so many
+   * (Float32) elements.
    */
   ensureArrayBuffer_byFloat32( elementCount ) {
     this.ensureArrayBuffer_byByte( elementCount * Float32Array.BYTES_PER_ELEMENT );
@@ -64,44 +71,59 @@ class Base extends Recyclable.Root {
 
   /**
    * @param {number} elementCount
-   *   Prepare this.weightsFloat32Array[] (which uses the underlyingArrayBuffer) so that its length equals elementCount.
+   *   Prepare this.weightsFloat32Array[] (which uses the
+   * underlyingArrayBuffer) so that its length equals elementCount.
    */
   prepare_weightsFloat32Array( elementCount ) {
-    if ( ( !this.weightsFloat32Array ) || ( this.weightsFloat32Array.length != elementCount ) ) {
+    if (   ( !this.weightsFloat32Array )
+        || ( this.weightsFloat32Array.length != elementCount ) ) {
       this.ensureArrayBuffer_byFloat32( elementCount );
-      this.weightsFloat32Array = new Float32Array( this.underlyingArrayBuffer, 0, elementCount );
+      this.weightsFloat32Array = new Float32Array(
+        this.underlyingArrayBuffer, 0, elementCount );
 
-    // Otherwise, this.weightsFloat32Array already has the required length. Use it directly.
+    // Otherwise, this.weightsFloat32Array already has the required length. Use
+    // it directly.
     }
   }
 
   /**
-   * Concatenate multiple number arrays into one Float32Array (this.weightsFloat32Array) in a specified order.
+   * Concatenate multiple number arrays into one Float32Array
+   * (this.weightsFloat32Array) in a specified order.
    *
-   * It fills two data members of this object { weightsFloat32Array, weightsByteOffsetBegin }.
-   *   - this.weightsFloat32Array: A Float32Array which is the concatenated result of the numberArrayArray.
-   *   - this.weightsByteOffsetBegin: A random offset byte count inside weightsFloat32Array.
+   * It fills two data members of this object
+   * { weightsFloat32Array, weightsByteOffsetBegin }.
+   *   - this.weightsFloat32Array: A Float32Array which is the concatenated
+   *       result of the numberArrayArray.
+   *   - this.weightsByteOffsetBegin: A random offset byte count inside
+   *       weightsFloat32Array.
    *
-   * Note: A re-usable ArrayBuffer will be used as the underlying ArrayBuffer of the this.weightsFloat32Array.
-   *       The this.weightsFloat32Array itself may also be re-used. So caller should not modify it.
+   * Note: A re-usable ArrayBuffer will be used as the underlying ArrayBuffer
+   *       of the this.weightsFloat32Array. The this.weightsFloat32Array itself
+   *       may also be re-used. So caller should not modify it.
    *
    *
    * @param {string[]} nameOrderArray
-   *   An array of string. Every element is a string name of a parameter. The number array in nameNumberArrayObject will be concatenated
-   * according to the element order of this nameOrderArray[].
+   *   An array of string. Every element is a string name of a parameter. The
+   * number array in nameNumberArrayObject will be concatenated according to
+   * the element order of this nameOrderArray[].
    *
    * @param {object} nameNumberArrayObject
-   *   An object whose all properties are number or number array. It is a map from a string name to a number or number array. The names
-   * should be found in nameOrderArray[].
+   *   An object whose all properties are number or number array. It is a map
+   * from a string name to a number or number array. The names should be found
+   * in nameOrderArray[].
    *
    * @param {number} weightElementOffsetBegin
-   *   Offset how many elements (4 bytes per element) at the beginning of the result weightsFloat32Array.
+   *   Offset how many elements (4 bytes per element) at the beginning of the
+   * result weightsFloat32Array.
    *
    * @returned {number} weightElementOffsetBegin
    */
-  set_byConcat( nameOrderArray, nameNumberArrayObject, weightElementOffsetBegin = 0 ) {
+  set_byConcat(
+    nameOrderArray, nameNumberArrayObject, weightElementOffsetBegin = 0 ) {
 
-    this.weightsByteOffsetBegin = weightElementOffsetBegin * Float32Array.BYTES_PER_ELEMENT; // Skip the un-used byte count.
+    // Skip the un-used byte count.
+    this.weightsByteOffsetBegin
+      = weightElementOffsetBegin * Float32Array.BYTES_PER_ELEMENT;
 
     // Calculate the total length include the extra offset.
     let weightsTotalLength = weightElementOffsetBegin;
@@ -110,7 +132,8 @@ class Base extends Recyclable.Root {
       let number_or_numberArray = nameNumberArrayObject[ name ];
       if ( number_or_numberArray != undefined ) {
         if ( number_or_numberArray instanceof Array ) {
-          weightsTotalLength += number_or_numberArray.length; // Assume number array.
+          // Assume number array.
+          weightsTotalLength += number_or_numberArray.length;
         } else {
           weightsTotalLength += 1; // Assume number.
         }
@@ -120,21 +143,26 @@ class Base extends Recyclable.Root {
     // Concatenate all number array into a (re-used) Float32Array.
     this.prepare_weightsFloat32Array( weightsTotalLength );
     {
-      for ( let i = 0; i < this.weightElementOffsetBegin; ++i ) { // Make-up the un-used weight values.
+      // Make-up the un-used weight values.
+      for ( let i = 0; i < this.weightElementOffsetBegin; ++i ) {
         this.weightsFloat32Array[ i ] = -i;
       }
 
       let offset = weightElementOffsetBegin;
-      for ( let i = 0; i < nameOrderArray.length; ++i ) { // Concatenate all number array into a Float32Array.
+
+      // Concatenate all number array into a Float32Array.
+      for ( let i = 0; i < nameOrderArray.length; ++i ) {
         let name = nameOrderArray[ i ];
         let number_or_numberArray = nameNumberArrayObject[ name ];
 
         if ( number_or_numberArray != undefined ) {
           if ( number_or_numberArray instanceof Array ) {
-            this.weightsFloat32Array.set( number_or_numberArray, offset ); // Assume number array.
+            // Assume number array.
+            this.weightsFloat32Array.set( number_or_numberArray, offset );
             offset += number_or_numberArray.length;
           } else {
-            this.weightsFloat32Array[ offset ] = number_or_numberArray; // Assume number.
+            // Assume number.
+            this.weightsFloat32Array[ offset ] = number_or_numberArray;
             offset += 1;
           }
         }
