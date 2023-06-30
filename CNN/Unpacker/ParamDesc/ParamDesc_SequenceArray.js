7be6188cafa975f496a25a053f4375d8ab298303
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-06-30 13:29:29 +0800

    Improve comments.

--- CNN/Unpacker/ParamDesc/ParamDesc_SequenceArray.js
+++ CNN/Unpacker/ParamDesc/ParamDesc_SequenceArray.js
@@ -1,7 +1,8 @@
 export { SequenceArray };
 
 /**
- * An array of ParamDesc.Xxx objects. It will also fill the value of these ParamDesc.Xxx's .seqId property.
+ * An array of ParamDesc.Xxx objects. It will also fill the value of these
+ * ParamDesc.Xxx's .seqId property.
  *
  *
  */
@@ -10,14 +11,20 @@ class SequenceArray {
   /**
    *
    * @param {ParamDesc.Base[]} aParamDescArray
-   *   An array of ParamDesc.Xxx objects. It will be kept (i.e. owned; not cloned) by this SequenceArray object.
+   *   An array of ParamDesc.Xxx objects. It will be kept (i.e. owned; not
+   * cloned) by this SequenceArray object.
    *
-   *   - every element should be a ParamDesc.Xxx object (one of ParamDesc.Base, ParamDesc.Same, ParamDesc.Bool)
-   *       describing the parameter.
+   *   - every element should be a ParamDesc.Xxx object (one of ParamDesc.Base,
+   *       ParamDesc.Same, ParamDesc.Bool) describing the parameter.
    *
-   *     - The paramDesc.valueDesc should be a ValueDesc.Xxx object (one of ValueDesc.Same, ValueDesc.Bool, ValueDesc.Int).
-   *       The paramDesc.valueDesc.range should be a ValueRange.Xxx object (one of ValueRange.Same, ValueRange.Bool, ValueRange.Int).
-   *       The paramDesc.valueDesc.range.adjust() is a function for adjusting the parameter value.
+   *     - The paramDesc.valueDesc should be a ValueDesc.Xxx object (one of
+   *         ValueDesc.Same, ValueDesc.Bool, ValueDesc.Int).
+   *
+   *     - The paramDesc.valueDesc.range should be a ValueRange.Xxx object (one
+   *         of ValueRange.Same, ValueRange.Bool, ValueRange.Int).
+   *
+   *     - The paramDesc.valueDesc.range.adjust() is a function for adjusting
+   *         the parameter value.
    *
    */
   constructor( aParamDescArray ) {
