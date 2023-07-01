a6545ceb245245ba100bd04e587ea2b82fc423f8
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-07-01 16:07:46 +0800

    Improve comments.

--- CNN/Conv/TensorPlaceholder.js
+++ CNN/Conv/TensorPlaceholder.js
@@ -7,31 +7,37 @@ import * as ActivationEscaping from "./ActivationEscaping.js";
 /**
  * A placeholder for tensor.
  *
- *   - In Operation.TwinArray.operation_append_Xxx(), it is used for tracking a tensor's final operation which should be responsible
- *       for destroying the tensor.
+ *   - In Operation.TwinArray.operation_append_Xxx(), it is used for tracking a
+ *       tensor's final operation which should be responsible for destroying
+ *       the tensor.
  *
- *   - In operation's .apply(), it is used for transferring tensor to the next sub operation.
+ *   - In operation's .apply(), it is used for transferring tensor to the next
+ *       sub operation.
  *
  *
  *
  * @member {Operation.Base} finalOperation
- *   The operation uses this tensor at final. It should be responsible for destroying this tensor. If null, this tensor is
- * not used by any operation.
+ *   The operation uses this tensor at final. It should be responsible for
+ * destroying this tensor. If null, this tensor is not used by any operation.
  *
  * @member {tf.tensor} realTensor
- *   The real tensor represented by this placeholder. It is filled dynamically in an operation's apply() method.
+ *   The real tensor represented by this placeholder. It is filled dynamically
+ * in an operation's apply() method.
  *
  * @member {ActivationEscaping.ScaleBoundsArray} scaleBoundsArray
- *   The element value bounds (per channel) of the tensor. Usually, it is from the output of the previous operation's value bounds
- * set. It will be kept (not cloned) directly. So caller should use them carefully.
+ *   The element value bounds (per channel) of the tensor. Usually, it is from
+ * the output of the previous operation's value bounds set. It will be kept
+ * (not cloned) directly. So caller should use them carefully.
  *
  */
 class TensorPlaceholder_Base extends Recyclable.Root {
 
   /**
-   * Used as default TensorPlaceholder.Base provider for conforming to Recyclable interface.
+   * Used as default TensorPlaceholder.Base provider for conforming to
+   * Recyclable interface.
    */
-  static Pool = new Pool.Root( "TensorPlaceholder.Base.Pool", TensorPlaceholder_Base, TensorPlaceholder_Base.setAsConstructor );
+  static Pool = new Pool.Root( "TensorPlaceholder.Base.Pool",
+    TensorPlaceholder_Base, TensorPlaceholder_Base.setAsConstructor );
 
   /**
    *
@@ -52,14 +58,15 @@ class TensorPlaceholder_Base extends Recyclable.Root {
   static setAsConstructor_self() {
     this.finalOperation = null;
     this.realTensor = null;
-    this.set_height_width_channelCount_scaleBoundsArray_byTensorPlaceholder( null );
+    this.set_height_width_channelCount_scaleBoundsArray_byTensorPlaceholder(
+      null );
   }
 
   /**
    * Release the .scaleBoundsArray
    *
-   * Usually, this method is not responsible for releasing .finalOperation and .realTensor. They should be handled
-   * by the caller of apply().
+   * Usually, this method is not responsible for releasing .finalOperation and
+   * .realTensor. They should be handled by the caller of apply().
    *
    * @override
    */
@@ -68,7 +75,8 @@ class TensorPlaceholder_Base extends Recyclable.Root {
 
     this.finalOperation = null;
     this.realTensor = null;
-    this.set_height_width_channelCount_scaleBoundsArray_byTensorPlaceholder( null );
+    this.set_height_width_channelCount_scaleBoundsArray_byTensorPlaceholder(
+      null );
 
     super.disposeResources();
   }
@@ -97,11 +105,14 @@ class TensorPlaceholder_Base extends Recyclable.Root {
   /**
    *
    * @param {ActivationEscaping.ScaleBoundsArray} scaleBoundsArray
-   *   The tensor placeholder's ScaleBoundsArray. It will be owned by this TensorPlaceholder. (i.e. This TensorPlaceholder will be
-   * responsible for releasing it.)
+   *   The tensor placeholder's ScaleBoundsArray. It will be owned by this
+   * TensorPlaceholder. (i.e. This TensorPlaceholder will be responsible for
+   * releasing it.)
    */
   set_height_width_channelCount_scaleBoundsArray(
-    height, width, channelCount, channelCount_lowerHalf, channelCount_higherHalf, scaleBoundsArray ) {
+    height, width, channelCount,
+    channelCount_lowerHalf, channelCount_higherHalf, scaleBoundsArray ) {
+
     this.height = height;
     this.width = width;
     this.channelCount = channelCount;
@@ -115,15 +126,24 @@ class TensorPlaceholder_Base extends Recyclable.Root {
    * Note: The .finalOperation and .realTensor are not modified by this method.
    *
    * @param {TensorPlaceholder} aTensorPlaceholder
-   *   The tensor placeholder's height, width, channelCount, scaleBoundsArray will be used directly (i.e. not cloned) by this
-   * tensor placeholder. If null, these properties will be set to undefined.
+   *   The tensor placeholder's height, width, channelCount, scaleBoundsArray
+   * will be used directly (i.e. not cloned) by this tensor placeholder. If
+   * null, these properties will be set to undefined.
    */
-  set_height_width_channelCount_scaleBoundsArray_byTensorPlaceholder( aTensorPlaceholder ) {
+  set_height_width_channelCount_scaleBoundsArray_byTensorPlaceholder(
+    aTensorPlaceholder ) {
+
     if ( aTensorPlaceholder ) {
       this.set_height_width_channelCount_scaleBoundsArray(
-        aTensorPlaceholder.height, aTensorPlaceholder.width,
-        aTensorPlaceholder.channelCount, aTensorPlaceholder.channelCount_lowerHalf, aTensorPlaceholder.channelCount_higherHalf,
-        aTensorPlaceholder.scaleBoundsArray?.clone() // Note: Because TensorPlaceholder owns the ScaleBoundsArray, it should be cloned.
+        aTensorPlaceholder.height,
+        aTensorPlaceholder.width,
+        aTensorPlaceholder.channelCount,
+        aTensorPlaceholder.channelCount_lowerHalf,
+        aTensorPlaceholder.channelCount_higherHalf,
+
+        // Note: Because TensorPlaceholder owns the ScaleBoundsArray, it should
+        //       be cloned.
+        aTensorPlaceholder.scaleBoundsArray?.clone()
       );
     } else {
       this.set_height_width_channelCount_scaleBoundsArray(
@@ -135,19 +155,23 @@ class TensorPlaceholder_Base extends Recyclable.Root {
 
   /**
    *
-   * @param {TensorPlaceholder} aTensorPlaceholder  The tensor placeholder to be comapred.
+   * @param {TensorPlaceholder} aTensorPlaceholder
+   *   The tensor placeholder to be comapred.
    *
    * @return {boolean}
-   *   Return true, if this tensor placeholder's height, width, channelCount are the same as aTensorPlaceholder. Note: the
-   * .channelCount_lowerHalf and .channelCount_higherHalf are not compared.
+   *   Return true, if this tensor placeholder's height, width, channelCount
+   * are the same as aTensorPlaceholder. Note: the .channelCount_lowerHalf and
+   * .channelCount_higherHalf are not compared.
    */
   is_height_width_channelCount_same_byTensorPlaceholder( aTensorPlaceholder ) {
     if (   ( this.height != aTensorPlaceholder.height )
         || ( this.width != aTensorPlaceholder.width )
         || ( this.channelCount != aTensorPlaceholder.channelCount )
 
-        //|| ( this.channelCount_lowerHalf != aTensorPlaceholder.channelCount_lowerHalf )
-        //|| ( this.channelCount_higherHalf != aTensorPlaceholder.channelCount_higherHalf )
+        //|| ( this.channelCount_lowerHalf
+        //       != aTensorPlaceholder.channelCount_lowerHalf )
+        //|| ( this.channelCount_higherHalf
+        //       != aTensorPlaceholder.channelCount_higherHalf )
        )
       return false;
 
@@ -155,4 +179,3 @@ class TensorPlaceholder_Base extends Recyclable.Root {
   }
 
 }
-
