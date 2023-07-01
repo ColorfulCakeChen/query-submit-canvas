2ac056b7291f4b88a91d1f364ec280a7facede44
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-07-01 20:46:12 +0800

    Improve comments.

--- CNN/NeuralDEvolution/NeuralWorker.js
+++ CNN/NeuralDEvolution/NeuralWorker.js
@@ -1,5 +1,6 @@
 export * as Common from "./NeuralWorker/NeuralWorker_Common.js";
 export { Mode } from "./NeuralWorker/NeuralWorker_Mode.js";
-export { ImplicitInputMode } from "./NeuralWorker/NeuralWorker_ImplicitInputMode.js";
+export { ImplicitInputMode }
+  from "./NeuralWorker/NeuralWorker_ImplicitInputMode.js";
 export { Proxy } from "./NeuralWorker/NeuralWorker_Proxy.js";
 export { Proxies } from "./NeuralWorker/NeuralWorker_Proxies.js";
