09725a27c901dc878c02de5706c4bf2e2655d18e
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-07-04 15:48:43 +0800

    Improve comments.

--- CNN/jsPerf/jsPerf_Operation.js
+++ CNN/jsPerf/jsPerf_Operation.js
@@ -16,7 +16,11 @@ class Case {
    * @param {number} caseId      The id of this test case.
    * @param {Class} classTested  Operation.Base or Operation.TwinArray
    */
-  constructor( caseId, classTested, bInput0, bInput1, outputTensorCount, bKeepInputTensor0, bKeepInputTensor1 ) {
+  constructor( caseId, classTested,
+    bInput0, bInput1,
+    outputTensorCount,
+    bKeepInputTensor0, bKeepInputTensor1 ) {
+
     this.caseId = caseId;
     this.classTested = classTested;
     this.bInput0 = bInput0;
@@ -25,7 +29,8 @@ class Case {
     this.bKeepInputTensor0 = bKeepInputTensor0;
     this.bKeepInputTensor1 = bKeepInputTensor1;
 
-    this.assertPrefix = `jsPerf_Operation.Case( this.caseId == ${this.caseId} )`;
+    this.assertPrefix
+      = `jsPerf_Operation.Case( this.caseId == ${this.caseId} )`;
 
     try {
 
@@ -33,7 +38,8 @@ class Case {
         Pool.Asserter.assert_Pool_issuedCount_same_after_as_before,
         null, // The "this" when calling Pool.Asserter.assert_Pool_issuedCount_same_after_as_before().
         "jsPerf_Operation.Case()",
-        Case.test_sessionCall_internal, this, // The "this" when calling Case.test_sessionCall_internal().
+        Case.test_sessionCall_internal,
+        this, // The "this" when calling Case.test_sessionCall_internal().
       );
 
     } catch ( e ) {
@@ -55,8 +61,10 @@ class Case {
     if ( this.bInput1 )
       this.input1 = TensorPlaceholder.Base.Pool.get_or_create_by();
 
-    this.operation = this.classTested.Pool.get_or_create_by( this.input0, this.input1, this.outputTensorCount );
-    this.operation.setKeepInputTensor( this.bKeepInputTensor0, this.bKeepInputTensor1 );
+    this.operation = this.classTested.Pool.get_or_create_by(
+      this.input0, this.input1, this.outputTensorCount );
+    this.operation.setKeepInputTensor(
+      this.bKeepInputTensor0, this.bKeepInputTensor1 );
 
     tf.tidy( Case.test_tidy_internal.bind( this ) );
   }
@@ -67,7 +75,8 @@ class Case {
    */
   static test_tidy_internal() {
 
-    let memoryInfo_apply_before = tf.memory(); // Test memory leakage of .apply.
+    // Test memory leakage of .apply.
+    let memoryInfo_apply_before = tf.memory();
 
     let numTensors_delta = 0;
     if ( this.input0 ) {
@@ -88,12 +97,15 @@ class Case {
 
     this.operation.apply();
 
-    if ( !this.bInput0 && !this.bInput1 ) { // If no inputs, outputs should always be null.
+    // If no inputs, outputs should always be null.
+    if ( !this.bInput0 && !this.bInput1 ) {
 
       if ( this.operation.output0 )
-        this.assert_property_equal( "operation", "output0", "realTensor", null );
+        this.assert_property_equal( "operation",
+          "output0", "realTensor", null );
       if ( this.operation.output1 )
-        this.assert_property_equal( "operation", "output1", "realTensor", null );
+        this.assert_property_equal( "operation",
+          "output1", "realTensor", null );
 
     } else {
       numTensors_delta += this.outputTensorCount;
@@ -101,11 +113,15 @@ class Case {
 
     let memoryInfo_apply_after = tf.memory();
 
-    let numTensors_predicted = ( memoryInfo_apply_before.numTensors + numTensors_delta );
-    if ( memoryInfo_apply_after.numTensors != ( memoryInfo_apply_before.numTensors + numTensors_delta ) )
+    let numTensors_predicted
+      = ( memoryInfo_apply_before.numTensors + numTensors_delta );
+
+    if ( memoryInfo_apply_after.numTensors
+           != ( memoryInfo_apply_before.numTensors + numTensors_delta ) )
       throw Error( `${this.assertPrefix}: memory leak. `
         + `result tensor count (${memoryInfo_apply_after.numTensors}) `
-        + `should be ( ${numTensors_predicted} ) = ( ${memoryInfo_apply_before.numTensors} + ${numTensors_delta} ).`
+        + `should be ( ${numTensors_predicted} ) = `
+        + `( ${memoryInfo_apply_before.numTensors} + ${numTensors_delta} ).`
       );
 
     {
@@ -122,7 +138,9 @@ class Case {
     }
   }
 
-  assert_property_equal( strPropertyName0, strPropertyName1, strPropertyName2, rhsValue ) {
+  assert_property_equal(
+    strPropertyName0, strPropertyName1, strPropertyName2, rhsValue ) {
+
     let strWholeName = `Case( caseId = ${this.caseId} ).${strPropertyName0}`;
 
     let thisValue = this[ strPropertyName0 ];
@@ -150,33 +168,48 @@ function testCorrectness() {
 
   let classOperationDummy = Operation.Dummy;
   let classOperationTwinArray = Operation.TwinArray;
-  let classTestedArray = Recyclable.Array.Pool.get_or_create_by( classOperationDummy, classOperationTwinArray );
+  let classTestedArray = Recyclable.Array.Pool.get_or_create_by(
+    classOperationDummy, classOperationTwinArray );
 
   let caseId = -1;
-  let bInput0, bInput1, outputTensorCount, bKeepInputTensor0, bKeepInputTensor1;
+  let bInput0, bInput1,
+      outputTensorCount,
+      bKeepInputTensor0, bKeepInputTensor1;
 
-  for ( let classIndex = 0; classIndex < classTestedArray.length; ++classIndex ) {
+  for ( let classIndex = 0;
+    classIndex < classTestedArray.length; ++classIndex ) {
+
     let classTested = classTestedArray[ classIndex ];
 
     for ( let nInput0 = 0; nInput0 <= 1; ++nInput0 ) {
       for ( let nInput1 = 0; nInput1 <= 1; ++nInput1 ) {
-        for ( let outputTensorCount = 0; outputTensorCount <= 2; ++outputTensorCount ) {
-          for ( let nKeepInputTensor0 = 0; nKeepInputTensor0 <= 1; ++nKeepInputTensor0 ) {
-            for ( let nKeepInputTensor1 = 0; nKeepInputTensor1 <= 1; ++nKeepInputTensor1 ) {
+
+        for ( let outputTensorCount = 0;
+          outputTensorCount <= 2; ++outputTensorCount ) {
+
+          for ( let nKeepInputTensor0 = 0;
+            nKeepInputTensor0 <= 1; ++nKeepInputTensor0 ) {
+
+            for ( let nKeepInputTensor1 = 0;
+              nKeepInputTensor1 <= 1; ++nKeepInputTensor1 ) {
 
               ++caseId;
 
               bInput0 = ( nInput0 != 0 );
               bInput1 = ( nInput1 != 0 );
 
-              //!!! (2022/06/02 Remarked) It could be supported. Just get null as result.
+              //!!! (2022/06/02 Remarked) It could be supported. Just get null
+              // as result.
               //if ( !bInput0 && !bInput1 )
               //  continue; // Operation should have at least one input.
 
               bKeepInputTensor0 = ( nKeepInputTensor0 != 0 );
               bKeepInputTensor1 = ( nKeepInputTensor1 != 0 );
 
-              let testCase = new Case( caseId, classTested, bInput0, bInput1, outputTensorCount, bKeepInputTensor0, bKeepInputTensor1 );
+              let testCase = new Case( caseId, classTested,
+                bInput0, bInput1,
+                outputTensorCount,
+                bKeepInputTensor0, bKeepInputTensor1 );
             }
           }
         }
