76e8d12b85b0815d3e22e7a0a41102fc327caa5e
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-07-04 13:50:25 +0800

    Improve comments.

--- CNN/jsPerf/Ref/SequenceRandom_NumberArray.js
+++ CNN/jsPerf/Ref/SequenceRandom_NumberArray.js
@@ -7,21 +7,25 @@ import * as RandTools from "../../util/RandTools.js";
 import * as FloatValue from "../../Unpacker/FloatValue.js";
 
 /**
- * A pool for number array which is created with sequence and randomized offset. It could reduce re-creating them of same parameters
- * again and again to improve performance.
+ * A pool for number array which is created with sequence and randomized
+ * offset. It could reduce re-creating them of same parameters again and again
+ * to improve performance.
  *
  */
 class Bag extends Recyclable.Base( MultiLayerMap.Base ) {
 
   /**
-   * Used as default SequenceRandom_NumberArray.Bag provider for conforming to Recyclable interface.
+   * Used as default SequenceRandom_NumberArray.Bag provider for conforming to
+   * Recyclable interface.
    */
-  static Pool = new Pool.Root( "SequenceRandom_NumberArray.Bag.Pool", Bag, Bag.setAsConstructor );
+  static Pool = new Pool.Root( "SequenceRandom_NumberArray.Bag.Pool",
+    Bag, Bag.setAsConstructor );
 
   /**
    *
    * @param {number} nCountPerSameSpec
-   *   For the same specification, provide how many different result number array (and randomly select them).
+   *   For the same specification, provide how many different result number
+   * array (and randomly select them).
    */
   constructor( nCountPerSameSpec = 10 ) {
     super();
@@ -40,7 +44,8 @@ class Bag extends Recyclable.Base( MultiLayerMap.Base ) {
     this.nRandSpecIdMin = 0;
     this.nRandSpecIdMax = Math.max( 0, nCountPerSameSpec - 1 );
 
-    this.NumberArrayToBoundsMap = new Map(); // Record value bounds of every generated number array.
+    // Record value bounds of every generated number array.
+    this.NumberArrayToBoundsMap = new Map();
   }
 
   /** @override */
@@ -66,30 +71,54 @@ class Bag extends Recyclable.Base( MultiLayerMap.Base ) {
   }
 
   /**
-   * Similar to Base.ensure_object_property_numberArray_length_filled(). But the property will be a shared number array. Its value
-   * may be shared with other caller.
+   * Similar to Base.ensure_object_property_numberArray_length_filled(). But
+   * the property will be a shared number array. Its value may be shared with
+   * other caller.
    *
-   * This may have better performance because of number array re-using (instead of re-generating).
+   * This may have better performance because of number array re-using (instead
+   * of re-generating).
    *
    *
-   * @param {object} io_object        The object to be checked and modified.
-   * @param {string} propertyName     The property io_object[ propertyName ] will be ensured as a number array.
-   * @param {number} height           The length of axis0 of the io_object[ propertyName ].
-   * @param {number} width            The length of axis1 of the io_object[ propertyName ].
-   * @param {number} channelCount     The length of axis2 of the io_object[ propertyName ].
-   * @param {number} valueBegin       The first value of filled sequence.
-   * @param {number} valueStep        The incremental value of every next filled value in the sequence.
-   * @param {number} randomOffsetMin  The random number offet lower bound.
-   * @param {number} randomOffsetMax  The random number offet upperer bound.
+   * @param {object} io_object
+   *   The object to be checked and modified.
+   *
+   * @param {string} propertyName
+   *   The property io_object[ propertyName ] will be ensured as a number
+   * array.
+   *
+   * @param {number} height
+   *   The length of axis0 of the io_object[ propertyName ].
+   *
+   * @param {number} width
+   *   The length of axis1 of the io_object[ propertyName ].
+   *
+   * @param {number} channelCount
+   *   The length of axis2 of the io_object[ propertyName ].
+   *
+   * @param {number} valueBegin
+   *   The first value of filled sequence.
+   *
+   * @param {number} valueStep
+   *   The incremental value of every next filled value in the sequence.
+   *
+   * @param {number} randomOffsetMin
+   *   The random number offet lower bound.
+   *
+   * @param {number} randomOffsetMax
+   *   The random number offet upperer bound.
    *
    */
   get_by_elementCount_randomOffsetMin_randomOffsetMax(
     height, width, channelCount,
     valueBegin = 0, valueStep = 1,
-    randomOffsetMin = 0, randomOffsetMax = 0, divisorForRemainder = ( 2 ** 26 ) ) {
+    randomOffsetMin = 0, randomOffsetMax = 0,
+    divisorForRemainder = ( 2 ** 26 ) ) {
 
-    let nRandSpecId = RandTools.getRandomIntInclusive( this.nRandSpecIdMin, this.nRandSpecIdMax );
-    let numberArray = this.get_or_create_by_arguments1_etc( Bag.create_by, this,
+    let nRandSpecId = RandTools.getRandomIntInclusive(
+      this.nRandSpecIdMin, this.nRandSpecIdMax );
+
+    let numberArray = this.get_or_create_by_arguments1_etc(
+      Bag.create_by, this,
       height, width, channelCount,
       valueBegin, valueStep,
       randomOffsetMin, randomOffsetMax, divisorForRemainder, nRandSpecId );
@@ -111,10 +140,8 @@ class Bag extends Recyclable.Base( MultiLayerMap.Base ) {
     //  debugger;
     //}
 
-//!!! (2022/08/09 Remarked) RandTools.fill_numberArray() will calculate elementCount.
-//    let elementCount = height * width * channelCount;
-//    let numberArray = Recyclable.NumberArray_withBounds.Pool.get_or_create_by( elementCount );
-    let numberArray = Recyclable.NumberArray_withBounds.Pool.get_or_create_by();
+    let numberArray
+      = Recyclable.NumberArray_withBounds.Pool.get_or_create_by();
 
     // Note: nRandSpecId is not used when generating number array.
     RandTools.fill_numberArray( numberArray,
