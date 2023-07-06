491b5e7a89d8554872ff441c8454ca03cd1e3c4b
ColorfulCakeChen <colorfulcakechen@gmail.com>
2023-07-06 10:36:53 +0800

    shuffleArray_byChannelShuffler

--- CNN/tester/BoundsArraySet_tester.js
+++ CNN/tester/BoundsArraySet_tester.js
@@ -239,6 +239,10 @@ async function* tester( progressParent ) {
     progressToAdvance.value_advance();
     yield progressRoot;
 
+//!!! ...unfinished... (2023/07/06)
+// should also test 1d, 2d, 3d:
+//    FloatValue.ArrayInterleaver.interleave_asGrouptTwo_alongLastAxis_from_to(
+
   } finally {
     if ( asserter_Equal ) {
       asserter_Equal.disposeResources_and_recycleToPool();
