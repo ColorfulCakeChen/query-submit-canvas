d62457250df55bb7493666ccacaa50b54cd58643
ColorfulCakeChen <colorfulcakechen@gmail.com>
2025-05-21 15:59:04 +0800

    tensorflowJsVer = "4.22.0"

--- CNN/NeuralDEvolution/NeuralWorker/NeuralWorker_Common.js
+++ CNN/NeuralDEvolution/NeuralWorker/NeuralWorker_Common.js
@@ -3,8 +3,8 @@ export { tensorflowJsURL };
 
 /** Define tensorflow.js library version. */
 //!!! (2025/05/21 Temp Reamrked) For debug mobile phone.
-//const tensorflowJsVer = "4.22.0";
-const tensorflowJsVer = "4.8.0";
+const tensorflowJsVer = "4.22.0";
+//const tensorflowJsVer = "4.8.0";
 
 /** Define tensorflow.js library URL. */
 const tensorflowJsURL
