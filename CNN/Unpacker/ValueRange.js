71585a9f26d4b0c3acf82bf1379bd5dc419fad20
ColorfulCakeChen <colorfulcakechen@gmail.com>
2025-06-19 14:37:20 +0800

    .kinds = 1 for class Same.

--- CNN/Unpacker/ValueRange.js
+++ CNN/Unpacker/ValueRange.js
@@ -10,7 +10,7 @@ class Same {
   /**
    */
   constructor() {
-    this.kinds = 1; // Since smae value, there is only one kind value.
+    this.kinds = 1; // Since same value, there is only one kind value.
   }
 
   /** @return {any} Return the input value directly. */
