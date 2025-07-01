d69024e25f8d4e2d35f01825e9c92a054f6b5b80
ColorfulCakeChen <colorfulcakechen@gmail.com>
2025-07-01 12:45:31 +0800

    Disable tabe log.

--- CNN/x_tester/Ref/Block_TestParams.js
+++ CNN/x_tester/Ref/Block_TestParams.js
@@ -461,8 +461,8 @@ class Block_TestParams_Base extends TestParams.Base {
 
       bTableLog: [
         // (2025/06/04 Temp Remarked) For debug.
-        // 0, 0
-        1, 1
+        0, 0
+        // 1, 1
       ],
     };
 
