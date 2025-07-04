34efca20f8f0c355822535a87d30db0f377d05c1
ColorfulCakeChen <colorfulcakechen@gmail.com>
2025-07-04 11:22:17 +0800

    Test more Stage TestParams.

--- CNN/x_tester/Ref/Stage_TestParams.js
+++ CNN/x_tester/Ref/Stage_TestParams.js
@@ -329,14 +329,14 @@ class Stage_TestParams_Base extends TestParams.Base {
       ],
 
       nConvStageTypeId: [
-        // Stage.Params.nConvStageTypeId.valueDesc.range.min,
+        Stage.Params.nConvStageTypeId.valueDesc.range.min,
         // ValueDesc.ConvStageType.Singleton.Ids.MOBILE_NET_V1,           // (0)
         // ValueDesc.ConvStageType.Singleton.Ids.MOBILE_NET_V1_PAD_VALID, // (1)
         // ValueDesc.ConvStageType.Singleton.Ids.MOBILE_NET_V2_THIN,      // (2)
         // ValueDesc.ConvStageType.Singleton.Ids.MOBILE_NET_V2,           // (3)
         // ValueDesc.ConvStageType.Singleton.Ids.SHUFFLE_NET_V2,          // (4)
         // ValueDesc.ConvStageType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1,           // (5)
-        ValueDesc.ConvStageType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_PAD_VALID, // (6)
+        // ValueDesc.ConvStageType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_PAD_VALID, // (6)
         // ValueDesc.ConvStageType.Singleton.Ids.SHUFFLE_NET_V2_BY_POINTWISE21,             // (7)
 
         // ValueDesc.ConvStageType.Singleton.Ids.MOBILE_NET_V1           // (0)
@@ -345,9 +345,9 @@ class Stage_TestParams_Base extends TestParams.Base {
         // ValueDesc.ConvStageType.Singleton.Ids.MOBILE_NET_V2           // (3)
         // ValueDesc.ConvStageType.Singleton.Ids.SHUFFLE_NET_V2          // (4)
         // ValueDesc.ConvStageType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1           // (5)
-        ValueDesc.ConvStageType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_PAD_VALID // (6)
+        // ValueDesc.ConvStageType.Singleton.Ids.SHUFFLE_NET_V2_BY_MOBILE_NET_V1_PAD_VALID // (6)
         // ValueDesc.ConvStageType.Singleton.Ids.SHUFFLE_NET_V2_BY_POINTWISE21             // (7)
-        // Stage.Params.nConvStageTypeId.valueDesc.range.max
+        Stage.Params.nConvStageTypeId.valueDesc.range.max
       ],
 
       blockCountRequested: [
@@ -357,16 +357,16 @@ class Stage_TestParams_Base extends TestParams.Base {
       ],
 
       bPointwise1: [
-        // Stage.Params.bPointwise1.valueDesc.range.min,
-        Stage.Params.bPointwise1.valueDesc.range.max,
+        Stage.Params.bPointwise1.valueDesc.range.min,
+        // Stage.Params.bPointwise1.valueDesc.range.max,
         // Stage.Params.bPointwise1.valueDesc.range.min
         Stage.Params.bPointwise1.valueDesc.range.max
       ],
 
       depthwiseFilterHeight: [
-        // Stage.Params.depthwiseFilterHeight.valueDesc.range.min, // 1
+        Stage.Params.depthwiseFilterHeight.valueDesc.range.min, // 1
         // 2,
-        3,
+        // 3,
         depthwiseFilterMaxSize
       ],
 
@@ -375,23 +375,23 @@ class Stage_TestParams_Base extends TestParams.Base {
       //       tf.depthwiseConv2d() with ( depthwiseFilterWidth == 1 ).
 
       depthwiseFilterWidth: [
-        // Stage.Params.depthwiseFilterWidth.valueDesc.range.min, // 2
-        3,
+        Stage.Params.depthwiseFilterWidth.valueDesc.range.min, // 2
+        // 3,
         depthwiseFilterMaxSize
       ],
 
       nSqueezeExcitationChannelCountDivisor: [
-        // ValueDesc.SqueezeExcitationChannelCountDivisor.Singleton.range.min, // -2
+        ValueDesc.SqueezeExcitationChannelCountDivisor.Singleton.range.min, // -2
         // ValueDesc.SqueezeExcitationChannelCountDivisor.Singleton.Ids.NONE, // -2
         // ValueDesc.SqueezeExcitationChannelCountDivisor.Singleton.Ids.EXCITATION, // -1
         // ValueDesc.SqueezeExcitationChannelCountDivisor.Singleton.Ids.SQUEEZE_EXCITATION, // 0
         // 1,
-        2,
+        // 2,
         3
       ],
 
       // Must have ActivationFunction (i.e. can not be NONE). Otherwise, it easily
-      // results in infinity value because of multiple block.
+      // results in infinity value because of multiple blocks.
       //
       nActivationId: [ 
         // ValueDesc.ActivationFunction.Singleton.range.min + 0,
@@ -403,8 +403,8 @@ class Stage_TestParams_Base extends TestParams.Base {
       bKeepInputTensor: [
         Stage.Params.bKeepInputTensor.valueDesc.range.min,
         // Stage.Params.bKeepInputTensor.valueDesc.range.max,
-        Stage.Params.bKeepInputTensor.valueDesc.range.min
-        // Stage.Params.bKeepInputTensor.valueDesc.range.max
+        // Stage.Params.bKeepInputTensor.valueDesc.range.min
+        Stage.Params.bKeepInputTensor.valueDesc.range.max
       ],
 
       bTableLog: [
