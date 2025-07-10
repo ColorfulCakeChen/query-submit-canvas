2daf1147c0593b0cac5d3f0e338ea7b81753fc29
ColorfulCakeChen <colorfulcakechen@gmail.com>
2025-07-10 15:17:48 +0800

    Think: What if ( filterY, filterX ) is outside the source image? For example, filter 2x2 stride_2 to shrink image shape [ 3, 4, 6 ] to [ 2, 2, 6 ]?

--- CNN/Conv/Depthwise/Depthwise_FiltersArray_BiasesArray.js
+++ CNN/Conv/Depthwise/Depthwise_FiltersArray_BiasesArray.js
@@ -846,6 +846,10 @@ let FiltersArray_BiasesArray = ( ParentClass = Object ) =>
                           ;
                       }
 
+!!! ...unfinished... (2025/07/10)
+// What if ( filterY, filterX ) is outside the source image?
+// For example, filter 2x2 stride_2 to shrink image shape [ 3, 4, 6 ] to [ 2, 2, 6 ]?
+
                       // Accumulate value bounds for the filter position
                       // (across the whole virtual input image).
                       //
