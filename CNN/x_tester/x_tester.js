eeae21dd6cfe34f453efe3c50d16d311e889bdf7
ColorfulCakeChen <colorfulcakechen@gmail.com>
2025-07-17 12:17:13 +0800

    CNN_Stage_tester

--- CNN/x_tester/x_tester.js
+++ CNN/x_tester/x_tester.js
@@ -47,11 +47,11 @@ const gTestGeneratorFuncMap = new Map( [
 
   // [ HierarchicalNameable_tester.tester, true ],
 
-  [ CNN_Block_tester.tester, true ],
+  // [ CNN_Block_tester.tester, true ],
   // [ CNN_Embedding_tester.tester, true ],
   // [ CNN_NeuralNet_tester.tester, true ],
   // [ CNN_Operation_tester.tester, true ],
-  // [ CNN_Stage_tester.tester, true ],
+  [ CNN_Stage_tester.tester, true ],
 
   // [ FeedbackShape_tester.tester, true ],
 
