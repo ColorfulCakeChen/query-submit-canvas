8409a58651667680e85d8cfe56f2145a8b7a9666
ColorfulCakeChen <colorfulcakechen@gmail.com>
2025-07-18 20:49:31 +0800

    Stage TestParams more.

--- CNN/x_tester/Ref/Stage_TestParams.js
+++ CNN/x_tester/Ref/Stage_TestParams.js
@@ -423,8 +423,8 @@ class Stage_TestParams_Base extends TestParams.Base {
 
       bTableLog: [
         // (2025/06/04 Temp Remarked) For debug.
-        // 0, 0
-        1, 1
+        0, 0
+        // 1, 1
       ],
     };
 
