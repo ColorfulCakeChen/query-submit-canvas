2286fefba38b3531261b383883d6b9695bef8460
ColorfulCakeChen <colorfulcakechen@gmail.com>
2025-07-18 16:15:37 +0800

    const acceptableDifference = 2 ** (-15); // about 0.00003

--- CNN/x_tester/Ref/Stage_Reference.js
+++ CNN/x_tester/Ref/Stage_Reference.js
@@ -52,11 +52,13 @@ class Stage_Reference_Base
 //!!! (2025/07/05 Temp Remarked) For debug floating-point accumulated error
     {
       // const acceptableDifferenceRate = 0.001;
-      const acceptableDifferenceRate = 2 ** (-16); // about 0.000015
+      const acceptableDifferenceRate = 2 ** (-15); // about 0.00003
+      // const acceptableDifferenceRate = 2 ** (-16); // about 0.000015
       // const acceptableDifferenceRate = 2 ** (-70);
 
       // const acceptableDifference = 0.00001;
-      const acceptableDifference = 2 ** (-16); // about 0.000015
+      const acceptableDifference = 2 ** (-15); // about 0.00003
+      // const acceptableDifference = 2 ** (-16); // about 0.000015
       // const acceptableDifference = 2 ** (-70);
 
       this.asserter_Equal = TensorTools.Asserter_Equal.Pool.get_or_create_by(
