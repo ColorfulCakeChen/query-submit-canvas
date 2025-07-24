a30fefd9b51a72d9bc328d5a917144d657eaa50b
ColorfulCakeChen <colorfulcakechen@gmail.com>
2025-07-24 11:47:05 +0800

    acceptableDifference = 2 ** (-9.9); // about 0.001

--- CNN/x_tester/Ref/Block_Reference.js
+++ CNN/x_tester/Ref/Block_Reference.js
@@ -302,14 +302,14 @@ class Block_Reference_Base extends HierarchicalNameable.SeparatorSlash_Root {
 
 //!!! (2022/08/05 Temp Remarked) For debug floating-point accumulated error
     {
-      // const acceptableDifferenceRate = 0.001;
+      const acceptableDifferenceRate = 2 ** (-9.9); // about 0.001
       // const acceptableDifferenceRate = 2 ** (-16); // about 0.000015
-      const acceptableDifferenceRate = 2 ** (-20); // about 0.000001
+      // const acceptableDifferenceRate = 2 ** (-20); // about 0.000001
       // const acceptableDifferenceRate = 2 ** (-70);
 
-      // const acceptableDifference = 0.00001;
+      const acceptableDifference = 2 ** (-9.9); // about 0.001
       // const acceptableDifference = 2 ** (-16); // about 0.000015
-      const acceptableDifference = 2 ** (-20); // about 0.000001
+      // const acceptableDifference = 2 ** (-20); // about 0.000001
       // const acceptableDifference = 2 ** (-70);
 
       this.asserter_Equal = TensorTools.Asserter_Equal.Pool.get_or_create_by(
