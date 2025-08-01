090bf40094895c87465813dfc3c9d7b6273ad0c7
ColorfulCakeChen <colorfulcakechen@gmail.com>
2025-08-01 14:54:22 +0800

    TableLog_feedbackShape

--- CNN/Conv/NeuralNet/NeuralNet_FeedbackShape.js
+++ CNN/Conv/NeuralNet/NeuralNet_FeedbackShape.js
@@ -812,21 +812,30 @@ class NeuralNet_FeedbackShape extends NeuralNet_FeedbackToInput {
 
     // console.groupCollapsed( "explicit_input" );
     const str = ``
-      + `explicit_input `
-        + `( height, width, channelCount ) ( pixelCount, valueCount ) = ( `
-        + `${this.explicit_input_height}, `
-        + `${this.explicit_input_width}, `
-        + `${this.explicit_input_channelCount} ) ( `
-        + `${this.explicit_input_pixelCount}, `
-        + `${this.explicit_input_valueCount} ),\n`
+      + `input ( height, width, channelCount ) `
+        + `( pixelCount, valueCount, width_valueCount ) = ( `
+        + `${this.input_height}, `
+        + `${this.input_width}, `
+        + `${this.input_channelCount} ) (  `
+        + `${this.implicit_input_pixelCount}, `
+        + `${this.implicit_input_valueCount},`
+        + `${this.input_width_valueCount} ),\n`
 
-      + `implicit_input `
-        + `( height, width, channelCount ) ( pixelCount, valueCount ) = ( `
+      + `implicit_input ( height, width, channelCount ) `
+        + `( pixelCount, valueCount ) = ( `
         + `${this.implicit_input_height}, `
         + `${this.implicit_input_width}, `
         + `${this.implicit_input_channelCount} ) (  `
         + `${this.implicit_input_pixelCount}, `
         + `${this.implicit_input_valueCount} ),\n`
+
+      + `explicit_input ( height, width, channelCount ) `
+        + `( pixelCount, valueCount ) = ( `
+        + `${this.explicit_input_height}, `
+        + `${this.explicit_input_width}, `
+        + `${this.explicit_input_channelCount} ) ( `
+        + `${this.explicit_input_pixelCount}, `
+        + `${this.explicit_input_valueCount} ),\n`
 
       ;
 
